b5c17dceaa6e2235184e76e9b161860295f57acf
David Anderson <davea@ssl.berkeley.edu>
2002-05-30 07:14:21 +0000

    added missing files
    svn path=/trunk/boinc/; revision=75

---
+++ sched/sched_shmem.C
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "db.h"
+
+#include "sched_shmem.h"
+
+void SCHED_SHMEM::init() {
+    memset(this, 0, sizeof(SCHED_SHMEM));
+    ss_size = sizeof(SCHED_SHMEM);
+    platform_size = sizeof(PLATFORM);
+    app_size = sizeof(APP);
+    app_version_size = sizeof(APP_VERSION);
+    wu_result_size = sizeof(WU_RESULT);
+    max_platforms = MAX_PLATFORMS;
+    max_apps = MAX_APPS;
+    max_app_versions = MAX_APP_VERSIONS;
+    max_wu_results = MAX_WU_RESULTS;
+    nwu_results = MAX_WU_RESULTS;
+}
+
+int SCHED_SHMEM::verify() {
+    if (ss_size != sizeof(SCHED_SHMEM)) return -1;
+    if (platform_size != sizeof(PLATFORM)) return -1;
+    if (app_size != sizeof(APP)) return -1;
+    if (app_version_size != sizeof(APP_VERSION)) return -1;
+    if (wu_result_size != sizeof(WU_RESULT)) return -1;
+    if (max_platforms != MAX_PLATFORMS) return -1;
+    if (max_apps != MAX_APPS) return -1;
+    if (max_app_versions != MAX_APP_VERSIONS) return -1;
+    if (max_wu_results != MAX_WU_RESULTS) return -1;
+    return 0;
+}
+
+static void overflow(char* table) {
+    fprintf(stderr,
+        "The SCHED_SHMEM structure is too small for table %s.\n"
+        "Increase the size and restart feeder and fcgi.\n",
+        table
+    );
+}
+
+int SCHED_SHMEM::scan_tables() {
+    PLATFORM platform;
+    APP app;
+    APP_VERSION app_version;
+    int n;
+
+    n = 0;
+    while (!db_platform_enum(platform)) {
+        platforms[n++] = platform;
+        if (n == MAX_PLATFORMS) overflow("platforms");
+    }
+    nplatforms = n;
+
+
+    n = 0;
+    while (!db_app_enum(app)) {
+        apps[n++] = app;
+        if (n == MAX_APPS) overflow("apps");
+    }
+    napps = n;
+
+    n = 0;
+    while (!db_app_version_enum(app_version)) {
+        app_versions[n++] = app_version;
+        if (n == MAX_APP_VERSIONS) overflow("app_versions");
+    }
+    napp_versions = n;
+    return 0;
+}
+
+PLATFORM* SCHED_SHMEM::lookup_platform(char* name) {
+    int i;
+
+    for (i=0; i<nplatforms; i++) {
+        if (!strcmp(platforms[i].name, name)) {
+            return &platforms[i];
+        }
+    }
+    return 0;
+}
+
+APP* SCHED_SHMEM::lookup_app(int id) {
+    int i;
+
+    for (i=0; i<napps; i++) {
+        if (apps[i].id == id) {
+            return &apps[i];
+        }
+    }
+    return 0;
+}
+
+APP_VERSION* SCHED_SHMEM::lookup_app_version(
+    int appid, int platformid, int version
+) {
+    int i;
+    APP_VERSION* avp;
+
+    for (i=0; i<napp_versions; i++) {
+        avp = &app_versions[i];
+        if (avp->appid == appid && avp->platformid == platformid && avp->version_num == version) {
+            return avp;
+        }
+    }
+    return 0;
+}
---
+++ sched/sched_shmem.h
@@ -0,0 +1,46 @@
+#include "db.h"
+
+#define BOINC_KEY   0xdadacafe
+
+#define MAX_PLATFORMS       50
+#define MAX_APPS            10
+#define MAX_APP_VERSIONS    100
+#define MAX_WU_RESULTS      1000
+
+// a workunit/result pair
+struct WU_RESULT {
+    bool present;
+    WORKUNIT workunit;
+    RESULT result;
+};
+
+struct SCHED_SHMEM {
+        // the following fields let the scheduler make sure
+        // that the shared mem has the right format
+    int ss_size;            // sizeof(SCHED_SHMEM)
+    int platform_size;      // sizeof(PLATFORM)
+    int app_size;           // sizeof(APP)
+    int app_version_size;   // sizeof(APP_VERSION)
+    int wu_result_size;     // sizeof(WU_RESULT)
+    int nplatforms;
+    int napps;
+    int napp_versions;
+    int nwu_results;
+    int max_platforms;
+    int max_apps;
+    int max_app_versions;
+    int max_wu_results;
+    PLATFORM platforms[MAX_PLATFORMS];
+    APP apps[MAX_APPS];
+    APP_VERSION app_versions[MAX_APP_VERSIONS];
+    WU_RESULT wu_results[MAX_WU_RESULTS];
+
+    void init();
+    int verify();
+    int scan_tables();
+
+    APP* lookup_app(int);
+    APP_VERSION* lookup_app_version(int appid, int platform, int version);
+    PLATFORM* lookup_platform(char*);
+};
+
