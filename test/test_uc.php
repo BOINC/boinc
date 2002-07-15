c546d812cc561e6f1bac86a34c5c9c250089a7f3
Michael Gary <muon1183@yahoo.com>
2002-07-15 21:09:10 +0000

    fixed test script bug
    svn path=/trunk/boinc/; revision=200

--- test/test_uc.php
+++ test/test_uc.php
@@ -13,14 +13,13 @@
     init_client_dirs("prefs1.xml");
     copy_to_download_dir("input");
     add_platform(null);
-    add_user(null);
+    add_user("prefs.xml");
     add_app("upper_case", null, null);
     create_work("-appname upper_case -rsc_iops 180000000000.0 -rsc_fpops 0.0 -wu_name uc_wu -wu_template uc_wu -result_template uc_result -nresults 2 input input input input input");
     start_feeder();
     //run_client("-exit_after 10");
     run_client("-exit_when_idle");
     stop_feeder();
-
     check_results_done();
     compare_file("uc_wu_0_0", "uc_correct_output");
     compare_file("uc_wu_1_0", "uc_correct_output");
