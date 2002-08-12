6faddfaf496e60923a61cab0aafebe0629a31bc1
Barry Luong <barry@ssl.berkeley.edu>
2002-08-12 21:52:41 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=298

--- html/user/create_account.php
+++ html/user/create_account.php
@@ -5,9 +5,9 @@ include_once("util.inc");
 include_once("login.inc");
 include_once("prefs.inc");
 
-db_init();
-
-page_head("Create Account");
+$project = db_init();
+$head = sprintf("Create User Account for %s", $project);
+page_head("$head");
 print_create_account_form();
 page_tail();
 
