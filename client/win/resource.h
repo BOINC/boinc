ee51e8459739509ac4dfacd5b5599d8591d8f9a9
Seth Cooper <sethcooper@unknown.invalid>
2002-10-05 23:06:23 +0000

    resource fix
    svn path=/trunk/boinc/; revision=468

--- client/win/resource.h
+++ client/win/resource.h
@@ -1,21 +1,30 @@
-
-#define IDD_ABOUTBOX                    100
-#define IDR_MAINFRAME                   128
-#define IDR_CORECLTYPE                  129
-#define IDD_LOGIN                       131
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define IDB_LOGO                        101
+#define IDD_LOGIN                       102
+#define IDD_DIALOG1                     106
+#define IDR_MAINFRAME                   107
+#define IDD_ABOUTBOX                    108
+#define IDB_LOGOSM                      110
+#define IDR_POPUP                       111
 #define IDC_LOGIN_URL                   1000
-#define IDC_LOGIN_AUTH                  1001
-#define IDC_LOGIN_OK                    1002
-#define IDC_LOGIN_CANCEL                1003
+#define IDC_LOGIN_AUTH                  1002
+#define ID_ACCT_LOGIN                   40001
+#define ID_HELP_ABOUT                   40002
+#define ID_POPUP_1                      40003
+#define ID_POPUP_2                      40004
+#define ID_POPUP_3                      40005
+#define ID_POPUP_0                      40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40014
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
