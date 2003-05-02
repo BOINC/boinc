2f9c34c67aa591f21fc72ffedcb2acbad35e684c
Eric Heien <eric@heien.org>
2003-05-02 17:04:47 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1155

--- html/user/get_passwd.php
+++ html/user/get_passwd.php
@@ -9,7 +9,7 @@ page_head("Account key");
     echo "<form method=post action=mail_passwd.php>\n";
     start_table();
     row1("Get your ".PROJECT." account key");
-    row2("Email address","<input size=40 name=email_addr");
+    row2("Email address","<input size=40 name=email_addr>");
     row2("", "<input type=submit value=Submit>");
     echo "</table></form>
         Your account key will be emailed to you.
