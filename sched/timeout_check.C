b42753ab11f7d8805682ee801e71a5d0da3373b2
Karl Chen <quarl@quarl.org>
2003-06-11 23:12:54 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1390

--- sched/timeout_check.C
+++ sched/timeout_check.C
@@ -366,6 +366,8 @@ int main(int argc, char** argv) {
         exit(1);
     }
     write_pid_file(PIDFILE);
+    write_log(MSG_NORMAL, "Starting\n");
+
     install_sigint_handler();
 
     main_loop(one_pass);
