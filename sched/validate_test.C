f0ae670976a6790918544a1650b19c3f9f73dd92
Karl Chen <quarl@quarl.org>
2003-06-14 20:04:45 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1438

--- sched/validate_test.C
+++ sched/validate_test.C
@@ -2,18 +2,18 @@
 // Version 1.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://boinc.berkeley.edu/license_1.0.txt
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 // License for the specific language governing rights and limitations
-// under the License. 
-// 
-// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
-// 
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
 // The Initial Developer of the Original Code is the SETI@home project.
 // Portions created by the SETI@home project are Copyright (C) 2002
-// University of California at Berkeley. All Rights Reserved. 
-// 
+// University of California at Berkeley. All Rights Reserved.
+//
 // Contributor(s):
 //
 
@@ -55,18 +55,20 @@ int check_set(vector<RESULT>& results, int& canonicalid, double& credit) {
     // read the result files into malloc'd memory buffers
     //
     for (i=0; i<n; i++) {
-        retval = get_output_file_path(results[i], path);
+        RESULT& result = results[i];
+        retval = get_output_file_path(result, path);
         if (retval) {
             log_messages.printf(
                 SchedMessages::CRITICAL,
                 "check_set: can't get output filename for %s\n",
-                results[i].name
+                result.name
             );
             return retval;
         }
         retval = read_file_malloc(path, files[i]);
         if (retval) {
-            log_messages.printf(SchedMessages::CRITICAL, "read_file_malloc %s %d\n", path, retval);
+            log_messages.printf(SchedMessages::CRITICAL, "[RESULT#%d %s] Couldn't open %s (read_file_malloc()=%d)\n",
+                                result.id, result.name, path, retval);
             return retval;
         }
     }
