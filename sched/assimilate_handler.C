86be60be0b4e41fec78d0b35c359cfb9150f8c92
Karl Chen <quarl@quarl.org>
2003-06-14 20:25:37 +0000

    improved scheduler logging ; use a library
    svn path=/trunk/boinc/; revision=1456

--- sched/assimilate_handler.C
+++ sched/assimilate_handler.C
@@ -2,23 +2,25 @@
 
 #include "boinc_db.h"
 #include "assimilate_handler.h"
+#include "sched_util.h"
 
 void assimilate_handler(
     WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_result
-) {
-    printf("assimilating WU %s\n", wu.name);
+    )
+{
+    write_log(MSG_NORMAL, "assimilating WU %s\n", wu.name);
     if (wu.canonical_resultid) {
-        printf("canonical result:\n%s", canonical_result.xml_doc_out);
+        write_log(MSG_NORMAL, "canonical result:\n%s", canonical_result.xml_doc_out);
     } else {
-        printf("no canonical result\n");
+        write_log(MSG_NORMAL, "no canonical result\n");
     }
     if (wu.error_mask&WU_ERROR_COULDNT_SEND_RESULT) {
-        printf("Error: couldn't send a result\n");
+        write_log(MSG_NORMAL, "Error: couldn't send a result\n");
     }
     if (wu.error_mask&WU_ERROR_TOO_MANY_ERROR_RESULTS) {
-        printf("Error: too many error results\n");
+        write_log(MSG_NORMAL, "Error: too many error results\n");
     }
     if (wu.error_mask&WU_ERROR_TOO_MANY_RESULTS) {
-        printf("Error: too many total results\n");
+        write_log(MSG_NORMAL, "Error: too many total results\n");
     }
 }
