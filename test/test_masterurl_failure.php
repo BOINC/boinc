9d731958707368f7afeaba7f99c8380d884aac8b
Karl Chen <quarl@quarl.org>
2003-06-17 01:36:47 +0000

    testing upload and connection failures
    svn path=/trunk/boinc/; revision=1510

--- test/test_masterurl_failure.php
+++ test/test_masterurl_failure.php
@@ -1,56 +1,22 @@
-#! /usr/local/bin/php
-<?php
-//This tests the exponential backoff mechanism on the client in case of master IURL failures
-//This test is not automated. It has to be run, and then client.out (in the host directory) must be looked at to examine wether everything is working correctly.    
-    include_once("test.inc");
+#!/usr/local/bin/php -q
+<?php {
+    // $Id$
 
-    $project = new Project;
-    $user = new User();
-    $host = new Host($user);
+    $use_proxy_html = 1;
 
-    $app = new App("upper_case");
-    $app_version = new App_Version($app);
+    include_once("test_uc.inc");
+    test_msg("master url failure");
 
-    // the following is optional (makes client web download possible)
-    $core_app = new App("core client");
-    $core_app_version = new App_Version($core_app);
-    $project->add_app($core_app);
-    $project->add_app_version($core_app_version);
+    $project = new ProjectUC;
 
-    $project->add_user($user);
-    $project->add_app($app);
-    $project->add_app_version($app_version);
+    start_proxy('exit 1 if $nconnections < 4; if_done_kill(); if_done_ping();');
 
-    $project->install();      // must install projects before adding to hosts
+    // TODO: verify it took ??? seconds
 
-    $host->log_flags = "log_flags.xml";
-    $host->add_project($project);
-    $host->install();
+    // TODO: time out after ??? seconds and fail this test
 
-    echo "adding work\n";
+    $project->start_servers_and_host();
+    $project->validate_all_and_stop();
 
-    $work = new Work($app);
-    $work->wu_template = "uc_wu";
-    $work->result_template = "uc_result";
-    $work->redundancy = 2;
-    $work->delay_bound = 10;
-    array_push($work->input_files, "input");
-    $work->install($project);
-
-    $project->start_feeder();
-    $project->delete_masterindex();
-    $pid = $host->run_asynch("-exit_when_idle");
-    echo "sleeping for 100 seconds\n";
-    sleep(100);
-    $project->reestablish_masterindex();
-    $status = 0;
-    pcntl_waitpid($pid,$status,0);
-    $project->stop();
-
-    $result->server_state = RESULT_SERVER_STATE_OVER;
-    $result->stderr_out = "APP: upper_case: starting, argc 1";
-    $result->exit_status = 0;
-    $project->check_results(2, $result);
-    $project->compare_file("uc_wu_0_0", "uc_correct_output");
-    $project->compare_file("uc_wu_1_0", "uc_correct_output");
-?>
+    test_done();
+} ?>
--- test/test_uc.php
+++ test/test_uc.php
@@ -6,64 +6,12 @@
     // Also whether stderr output is reported correctly
     // Also tests if water levels are working correctly
 
-    include_once("test.inc");
-
+    include_once("test_uc.inc");
     test_msg("standard upper_case application");
 
-    $project = new Project;
-    $project->add_core_and_version();
-    $project->add_app_and_version("upper_case");
-
-    $user = new User();
-    $user->project_prefs = "<project_specific>\nfoobar\n</project_specific>\n";
-    $user->global_prefs = "<venue name=\"home\">\n".
-    "<work_buf_min_days>0</work_buf_min_days>\n".
-    "<work_buf_max_days>2</work_buf_max_days>\n".
-    "<disk_interval>1</disk_interval>\n".
-    "<run_on_batteries/>\n".
-    "<max_bytes_sec_down>400000</max_bytes_sec_down>\n".
-    "</venue>\n";
-
-    $project->add_user($user);
-    $project->install();      // must install projects before adding to hosts
-    $project->install_feeder();
-
-    $host = new Host();
-    $host->add_user($user, $project);
-    $host->install();
-
-    $work = new Work();
-    $work->wu_template = "uc_wu";
-    $work->result_template = "uc_result";
-    $work->redundancy = 2;
-    $work->delay_bound = 10;
-    // Say that 1 WU takes 1 day on a ref comp
-    $work->rsc_fpops = 86400*1e9/2;
-    $work->rsc_iops = 86400*1e9/2;
-    $work->rsc_disk = 10e8;
-    array_push($work->input_files, "input");
-    $work->install($project);
-
-    $project->start_servers();
-    $host->run("-exit_when_idle -skip_cpu_benchmarks");
-
-    $project->validate(2);
-    $result->server_state = RESULT_SERVER_STATE_OVER;
-    $result->stderr_out = "APP: upper_case: starting, argc 1
-APP: upper_case: argv[0] is upper_case
-APP: upper_case ending, wrote ";
-    $result->exit_status = 0;
-    $project->check_results(2, $result);
-    $project->compare_file("uc_wu_0_0", "uc_correct_output");
-    $project->compare_file("uc_wu_1_0", "uc_correct_output");
-
-    $project->assimilate();
-    $project->file_delete();
-
-    $project->check_server_deleted("download/input");
-    $project->check_server_deleted("upload/uc_wu_0_0");
-    $project->check_server_deleted("upload/uc_wu_1_0");
-    $project->stop();
+    $project = new ProjectUC;
+    $project->start_servers_and_host();
+    $project->validate_all_and_stop();
 
     test_done();
 } ?>
