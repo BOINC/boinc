066ffc4ddd0883752374a520e81d9cc01e9ef74a
Karl Chen <quarl@quarl.org>
2003-07-10 00:35:51 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1655

--- test/test_masterurl_failure.py
+++ test/test_masterurl_failure.py
@@ -6,16 +6,14 @@ from test_uc import *
 
 if __name__ == '__main__':
     test_msg("scheduler exponential backoff (master url failure)")
-    # Proxy('close_connection if $nconnections < -2; '+
-    #       'kill 6,%d if $nconnections>=100 '%os.getpid(), html=1)
+    Proxy('close_connection if $nconnections < 2; ', html=1)
     # Proxy(('print "hi nc=$nconnections start=$start nchars=$nchars url=$url\n"; ' +
     #        'close_connection if $nconnections < 2; '),
     #       html=1)
-    Proxy(( 'print "hi nc=$nconnections start=$start nchars=$nchars url=$url\n"; ' +
-           ''),
-          html=1)
-    ProjectUC()
+    # Proxy(( 'print "hi nc=$nconnections start=$start nchars=$nchars url=$url\n"; ' +
+    #        ''),
+    #       html=1)
+    ProjectUC(short_name='test_masterurl_failure')
     run_check_all()
 
     ## TODO: verify it took ??? seconds
-    ## TODO: time out after ??? seconds and fail this test
