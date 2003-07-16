4b5a5c1f789245fe25d32aa6aa4b3634d77b498d
Dan Werthimer <danw@ssl.berkeley.edu>
2003-07-16 00:01:59 +0000

    Removed old TODOs.
    svn path=/trunk/boinc/; revision=1695

--- html/user/create_profile.php
+++ html/user/create_profile.php
@@ -5,15 +5,6 @@ require_once("db.inc");
 require_once("project_specific/project.inc");
 require_once("profile.inc");
 
-// TODO: Manually limit max size of stored "big" image.
-// Would just need to make getImages scale the "big"
-// image if it was above a particular size.
-
-// TODO: Allow users to delete their profiles.
-// Perhaps an empty profile submission (including selection
-// of the "delete image" checkbox) should be considered
-// a delete action for the profile itself?
-
 db_init();
 $user = get_logged_in_user(true);
 show_profile_creation_page($user);
