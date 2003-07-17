7e007fb594013ff1d0d0e5a2fb8ab5eae64da293
Dan Werthimer <danw@ssl.berkeley.edu>
2003-07-17 18:30:10 +0000

    Undid path changes of the previous commiter.
    svn path=/trunk/boinc/; revision=1708

--- html/user/index.php
+++ html/user/index.php
@@ -12,28 +12,28 @@
 <h3>Join ".PROJECT." </h3>
 <p>
 <ul>
-<li><a href=".URL_BASE."/info.php>Rules and policies <b>[read this first]</b></a>
-<li><a href=".URL_BASE."/create_account_form.php>Create an account</a>
+<li><a href=".URL_BASE."info.php>Rules and policies <b>[read this first]</b></a>
+<li><a href=".URL_BASE."create_account_form.php>Create an account</a>
 </ul>
 
 <h3>Returning participants</h3>
 <ul>
-<li><a href=".URL_BASE."/login_form.php>Log in</a>
-<li><a href=".URL_BASE."/home.php>User page</a> - view stats, modify preferences
-<li><a href=".URL_BASE."/team.php>Teams</a> - create or join a team
-<li><a href=".URL_BASE."/download.php>Download BOINC</a>
-<li><a href=".URL_BASE."/bug_report_form.php>Report problems</a>
+<li><a href=".URL_BASE."login_form.php>Log in</a>
+<li><a href=".URL_BASE."home.php>User page</a> - view stats, modify preferences
+<li><a href=".URL_BASE."team.php>Teams</a> - create or join a team
+<li><a href=".URL_BASE."download.php>Download BOINC</a>
+<li><a href=".URL_BASE."bug_report_form.php>Report problems</a>
 </ul>
 <h3>Community</h3>
 <ul>
-<li><a href=".URL_BASE."/profile_menu.php>User profiles</a>
+<li><a href=".URL_BASE."profile_menu.php>User profiles</a>
 </ul>
 <h3>Project totals and leader boards</h3>
 <ul>
-<li><a href=".URL_BASE."/totals.php>Project totals</a>
-<li><a href=".URL_BASE."/top_users.php>Top users</a>
-<li><a href=".URL_BASE."/top_hosts.php>Top computers</a>
-<li><a href=".URL_BASE."/top_teams.php>Top teams</a>
+<li><a href=".URL_BASE."totals.php>Project totals</a>
+<li><a href=".URL_BASE."top_users.php>Top users</a>
+<li><a href=".URL_BASE."top_hosts.php>Top computers</a>
+<li><a href=".URL_BASE."top_teams.php>Top teams</a>
 </ul>
 <!--
 ";
