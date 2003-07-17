175f22aa6bd0e8b7c9cdfdde423d552f59b84c92
David Anderson <davea@ssl.berkeley.edu>
2003-07-17 23:54:53 +0000

    When run, rebuilds the picture gallery and country pages.
    svn path=/trunk/boinc/; revision=1724

---
+++ html/ops/update_profile_pages.php
@@ -0,0 +1,14 @@
+#!/usr/local/bin/php
+
+<?php
+
+require_once("../html_user/db.inc");
+require_once("gallery.inc");
+require_once("../html_user/profile.inc");
+
+db_init();
+
+build_country_pages();
+build_picture_pages(GALLERY_WIDTH, GALLERY_HEIGHT);
+
+?>
