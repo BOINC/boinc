1b2113e71a6c03abc55d14e6a840f924d9b34e45
David Anderson <davea@ssl.berkeley.edu>
2003-07-18 22:36:22 +0000

    Added functionality to allow users to edit their own posts.  Currently disabled until edit.php is complete.
    svn path=/trunk/boinc/; revision=1736

--- html/forum/thread.php
+++ html/forum/thread.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../include.php');
+require_once('../util.inc');
 require_once('forum.inc');
 doHeader('Forum');
 
@@ -10,11 +11,14 @@ $thread = getThread($_GET['id']);
 $thread->incView();
 
 $forum = getForum($thread->forum);
+
+$logged_in_user = get_logged_in_user(false);
+    
 ?>
 
 <p>
 	<span class="title"><?php echo $thread->title ?></span>
-	<br><a href="index.php"><?php echo $cfg['sitename'] ?> Forum</a> -> <a href="forum.php?id=<?php echo $forum->id ?>"><?php echo $forum->title ?></a>
+<br><a href="index.php"><?php echo $cfg['sitename'] ?> Forum</a> -> <a href="forum.php?id=<?php echo $forum->id ?>"><?php echo $forum->title ?></a>
 </p>
 <p>
 	<a href="reply.php?id=<?php echo $_GET['id'] ?>">Reply to Thread / Question</a>
@@ -30,14 +34,15 @@ $forum = getForum($thread->forum);
 		$n = 0;
 		$posts = $thread->getPosts();
 		while ($post = getNextPost($posts)):
-			$user = getUser($post->user);
+                    $user = getUser($post->user);
+                    $can_edit = $logged_in_user && $user->id == $logged_in_user->id;
 			?>
 			<tr class="row<?php echo (($n++)%2+1) ?>" style="vertical-align:top">
                                 <td>
                                         <p style="font-weight:bold">
                                         <?php if ($user->has_profile) { ?>
-                                                                             <a href="../view_profile.php?userid=<?php echo $post->user ?>"><?php echo $user->name ?></a>
-                                                                             <?php } else { echo $user->name; }?>
+                                                <a href="../view_profile.php?userid=<?php echo $post->user ?>"><?php echo $user->name ?></a>
+                                        <?php } else { echo $user->name; } ?>
 					</p>
 					<p style="font-size:8pt">
 						Joined: <?php echo date('M j, Y', $user->create_time) ?>
@@ -45,7 +50,8 @@ $forum = getForum($thread->forum);
 					</p>
 				</td>
 				<td>
-					<p style="font-size:8pt">Posted: <?php echo date('D M j, Y g:i a', $post->timestamp) ?></p>
+					<p style="font-size:8pt">Posted: <?php echo date('D M j, Y g:i a', $post->timestamp) ?><?php /*if ($can_edit) echo "&nbsp;<a href=\"edit.php?id=$post->id\">[<i>Edit this post</i>]</a>"*/ ?></p>
+                                        
 					<p><?php echo nl2br(stripslashes($post->content)) ?></p>
 				</td>
 			</tr>
