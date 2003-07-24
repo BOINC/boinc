aab722e37bef24a8746913b7ba0f18562306b6fc
Karl Chen <quarl@quarl.org>
2003-07-24 21:08:48 +0000

    added from http://www.boinc.dk/index.php?page=download_languages
    svn path=/trunk/boinc/; revision=1788

---
+++ client/translation/language.ini.cs
@@ -0,0 +1,166 @@
+#Czech language.ini file
+#Czech version v 0.1 by Spok
+
+#PROJECT_ID
+[HEADER-Projects]
+Title=Projekty
+Project=Projekt
+Account=Úèet
+Total Credit=Celkový kredit
+Avg. Credit=Prùmìrný kredit
+Resource Share=Sdílení zdrojù
+
+#RESULT_ID
+[HEADER-Work]
+Title=Úlohy
+Project=Projekt
+Application=Aplikace
+Name=Název
+CPU time=Procesorový èas
+Progress=Prùbìh
+To Completion=Dokonèí za
+Status=Stav
+
+#XFER_ID
+[HEADER-Transfers]
+Title=Pøenos
+Project=Projekt
+File=Soubor
+Progress=Prùbìh
+Size=Velikost
+Time=Èas
+Speed=Rychlost
+Status=Stav
+Direction=Smìr
+
+#MESSAGE_ID
+[HEADER-Messages]
+Title=Zprávy
+Project=Projekt
+Time=Èas
+Message=Zpráva
+Started download of=Zapoèato stahování222
+Finished download of=Ukonèeno stahování222
+
+#USAGE_ID
+[HEADER-Disk]
+Title=Disk
+Free space: not available for use=Volné místo: Nevyužitelné
+Free space: available for use=Volné místo: Využitelné
+Used space: other than BOINC=Obsazené místo: Mimo BOINC
+Used space: BOINC=Obsazené místo: BOINC
+Used space:=Obsazené místo:
+
+#miscellaneous text
+[HEADER-MISC]
+New=Nový
+Running=Probíhající
+Ready to run=Pøipravený
+Computation done=Výpoèet ukonèen
+Results uploaded=Odesláno
+Acknowledged=Potvrzeno
+Error: invalid state=Chyba: Neplatný stav
+Completed=Dokonèeno
+Uploading=Odesílá se
+Downloading=Pøijímá se
+Retry in=Zopakuj po
+Upload failed=Chyba pøi odesílání
+Download failed=Chyba pøi stahování
+Starting computation for result=Zaèínám výpoèet pro
+Started upload of=Zapoèato odesílání
+Finished upload of=Ukonèeno odesílání
+Started download of=Zapoèato stahování
+Finished download of=Ukonèeno stahování
+
+#menu items
+# NOTE: add an & (ampersand) to the letter to be used as mnemonic
+#       i.e. Show Graphics=Show &Graphics
+#                               ^^ the "G" will trigger the menu item
+#       you can compare it with a saved language.ini.XX file
+[MENU-File]
+Title=&Soubor
+Show Graphics=Zobraz &Grafiku
+Clear Messages=&Smaž zprávy
+Clear Inactive=Smaž &Neaktivní
+Suspend=&Pozastavit
+Resume=&Obnovit
+Exit=&Konec
+
+[MENU-Settings]
+Title=&Nastavení
+Login to Project...=&Pøihlásit se k projektu...
+Quit Project...=&Ukonèit projekt...
+Proxy Server...=Proxy &Server...
+
+[MENU-Connection]
+Title=&Pøipojení
+Connect Now=&Pøipojit 
+Hangup Connection if Dialed=&Ukonèuj vytáèené (Dial-Up) pøipojení
+Confirm Before Connecting=Potvzení pøed &Vytáèením
+
+[MENU-Help]
+Title=&Pomoc
+About...=&O programu...
+
+[MENU-StatusIcon]
+Suspend=Pozastavit
+Resume=Obnovit
+Exit=Konec
+
+[MENU-Project]
+Relogin...=Znovu pøihlásit...
+Quit Project...=Ukonèit projekt...
+Web site=Webová stránka
+Get Preferences=Získej nastavení
+Detach...=Zrušit projekt...
+Reset project...=Restartuj projekt...
+
+[MENU-Work]
+Show Graphics=Zobraz &grafiku
+
+[DIALOG-LOGIN]
+Title=&Pøihlásit se k projektu
+URL:=URL:
+Account Key:=Klíè (Account Key):
+OK=OK
+Cancel=Zrušit
+The URL for the website of the project.=URL projektu.
+The authorization code recieved in your confirmation email.=Autorizaèní klíè obdržený v potvrzovacím e-mailu.
+
+[DIALOG-QUIT]
+Title=Ukonèit
+URL:=URL:
+Account Key:=Klíè (Account Key):
+OK=OK
+Cancel=Zrušit
+Select the project you wish to quit.=Vyberte projekt, který chcete ukonèit.
+
+[DIALOG-CONNECT]
+Title=Pøipojit
+BOINC needs to connect to the network.  May it do so now?=BOINC vyžaduje pøipojení na internet. Pøipojit?
+Don't ask this again (connect automatically)=Pøíštì se nedotazovat (pøipojovat automaticky).
+OK=OK
+Cancel=Zrušit
+
+[DIALOG-ABOUT]
+Title=BOINC - Beta verze
+Berkeley Open Infrastructure for Network Computing=Berkeley Open Infrastructure for Network Computing
+Open Beta=Open Beta
+OK=OK
+
+[DIALOG-PROXY]
+Title=Nastavení proxy serveru
+Some organizations use an "HTTP proxy" or a "SOCKS proxy" (or both) for increased security.  If you need to use a proxy, fill in the information below.  If you need help, ask your System Administrator or Internet Service Provider.=Nìkteré organizace používají HTTP nebo SOCKS proxy server (pøípadnì oba), kvùli zvýšení bezpeènosti. Pokud se pøipojujete pøes proxy server, vyplòte tento formuláø. Potøebujete-li pomoc, kontaktujte vašeho správce sítì nebo vašeho poskytovatele pøipojení.
+HTTP Proxy=HTTP proxy server
+Connect via HTTP Proxy Server=Pøipojit pøes HTTP proxy server
+http://=http://
+Port Number:=Èíslo portu:
+SOCKS Proxy=SOCKS proxy server
+Connect via SOCKS Proxy Server=Pøipojit pøes SOCKS proxy server
+SOCKS Host:=Jméno/IP-adresa SOCKS serveru:
+Port Number:=Èíslo portu:
+Leave these blank if not needed=Nevyplòujte, pokud není vyžadováno
+SOCKS User Name:=SOCKS - Uživatelské jméno:
+SOCKS Password:=SOCKS - Heslo:
+OK=OK
+Cancel=Zrušit
---
+++ client/translation/language.ini.nl
@@ -0,0 +1,156 @@
+# Nederlandse taalversie van language.ini
+# Versie 1.0.1, voor BOINC vanaf versie 0.17
+# Dit bestand zorgt ervoor dat menu's en meldingen in BOINC
+# in de Nederlandse taal worden getoond
+# Vertaald door Derk Buisman, www.seti-nl.org/www.seti.nl
+
+#PROJECT_ID
+[HEADER-Projects]
+Title=Projecten
+Project=Project
+Account=Account
+Total Credit=Totale Score
+Avg. Credit=Gemiddelde Score
+Resource Share=Verdeling van projecten
+
+#RESULT_ID
+[HEADER-Work]
+Title=Werk
+Project=Project
+Application=Applicatie
+Name=Naam
+CPU time=Processortijd
+Progress=Voortgang
+To Completion=Resterende Tijd
+Status=Status
+
+#XFER_ID
+[HEADER-Transfers]
+Title=Overdracht
+Project=Project
+File=Bestand
+Progress=Voortgang
+Size=Grootte
+Time=Tijd
+Speed=Snelheid
+Status=Status
+
+#MESSAGE_ID
+[HEADER-Messages]
+Title=Meldingen
+Project=Project
+Time=Tijd
+Message=Melding
+
+#USAGE_ID
+[HEADER-Disk]
+Title=Harde Schijf
+Free space: not available for use=Vrije ruimte: niet beschikbaar voor gebruik
+Free space: available for use=Vrije ruimte: beschikbaar voor gebruik
+Used space: other than BOINC=Gebruikte ruimte: anders dan BOINC
+Used space: BOINC=Gebruikte ruimte: BOINC
+Used space:=Gebruikte ruimte
+
+#miscellaneous text
+[HEADER-MISC]
+New=Nieuw
+Running=Aktief
+Ready to run=In wachtrij
+Computation done=Berekening klaar
+Results uploaded=Resultaat verzonden
+Acknowledged=Bevestigd
+Error: invalid state=Fout: ongeldige toestand
+Completed=Klaar
+Uploading=Bezig met verzenden
+Downloading=Bezig met ontvangen
+Retry in=Nieuwe poging in
+Upload failed=Verzenden mislukt
+Download failed=Ontvangen mislukt
+
+#menu items
+# NOTE: add an & (ampersand) to the letter to be used as mnemonic
+#       i.e. Show Graphics=Show &Graphics
+#                               ^^ the "G" will trigger the menu item
+#       you can compare it with a saved language.ini.XX file
+
+[MENU-File]
+Title=&Data
+Clear Messages=Wis Meldingen
+Clear Inactive=Wis inactieve items
+Suspend=Pauze
+Resume=Doorgaan
+Exit=Afsluiten
+
+[MENU-Settings]
+Title=&Instellingen
+Login to Project...=Aanmelden bij project...
+Quit Project...=Project Stoppen...
+Proxy Server...=Proxy Server...
+
+[MENU-Connection]
+Title=&Verbinding
+Connect To Scheduling Server=Verbind met Server
+
+[MENU-Help]
+Title=&Help
+About...=Over...
+
+[MENU-StatusIcon]
+Suspend=Pauze
+Resume=Doorgaan
+Exit=Afsluiten
+
+[MENU-Project]
+Relogin...=Opnieuw aanmelden...
+Quit Project...=Project Stoppen...
+
+[MENU-Work]
+Show Graphics=Toon grafieken
+
+[DIALOG-LOGIN]
+Title=Aanmelden bij Project
+URL:=URL:
+Account Key:=Aanmeld Code
+OK=OK
+Cancel=Afbreken
+The URL for the website of the project.=De URL van de website van het project.
+The authorization code recieved in your confirmation email.=De aanmeldcode die u in de bevestigings e-mail is toegezonden.
+
+[DIALOG-QUIT]
+Title=Project Stoppen
+URL:=URL:
+Account Key:=Aanmeld Code
+OK=OK
+Cancel=Afbreken
+Select the project you wish to quit.=Selecteer het project waarmee u wilt stoppen.
+
+[DIALOG-CONNECT]
+Title=Netwerk verbinding
+BOINC needs to connect to the network.  May it do so now?=BOINC heeft een internetverbinding nodig. Nu verbinden?
+Don't ask this again (connect automatically)=Niet opnieuw vragen (automatisch verbinden)
+OK=OK
+Cancel=Afbreken
+
+[DIALOG-ABOUT]
+Title=Boinc Beta Versie
+Berkeley Open Infrastructure for Network Computing=Berkeley Open Infrastructure for Network Computing
+Open Beta=Open Beta
+OK=OK
+
+[DIALOG-PROXY]
+Title=Proxy Server Instellingen
+Some organizations use an "HTTP proxy" or a "SOCKS proxy" (or both) for increased security.  If you need to use a proxy, fill in the information below.  If you need help, ask your System Administrator or Internet Service Provider.=Sommige organisaties gebruiken voor verbeterde veiligheid een "HTTP Proxy" of een "Socks Proxy" (of beiden).  Als u een proxy dient te gebruiken, vul dan onderstaande velden in.  Indien u hulp nodig heeft, vraag het uw systeembeheerder of internet provider.
+HTTP Proxy=HTTP Proxy
+Connect via HTTP Proxy Server=Verbind via HTTP Proxy
+http://=http://
+Port Number:=Poortnummer
+SOCKS Proxy=SOCKS Proxy
+Connect via SOCKS Proxy Server=Verbind via SOCKS Proxy
+SOCKS Host:=
+Port Number:=Poortnummer
+Leave these blank if not needed=Vul deze velden niet in als dat onnodig is
+SOCKS User Name:=SOCKS Gebruikersnaam
+SOCKS Password:=SOCKS Wachtwoord
+OK=OK
+Cancel=Afbreken
+
