be564f11dc34513cf9b5d1f85e8ecc1980f414e0
David Anderson <davea@ssl.berkeley.edu>
2003-07-26 00:20:44 +0000

    Bug fixes.
    svn path=/trunk/boinc/; revision=1827

--- html/forum/subscribe.php
+++ html/forum/subscribe.php
@@ -2,14 +2,25 @@
 require_once('subscribe.inc');
 require_once('../util.inc');
 
+$action = $_GET['action'];
 $thread = $_GET['thread'];
 
-if ($thread) {
+if ($thread && $action) {
     $result = sql_query("SELECT * FROM thread WHERE id = $thread");
     $thread = mysql_fetch_object($result);
-    $user = get_logged_in_user($true);
-    subscribe($thread, $user);
+    $user = get_logged_in_user($true, '../');
+    
+    if ($action == "subscribe") {
+        subscribe($thread, $user);
+        exit();
+    } else if ($action == "unsubscribe") {
+        unsubscribe($thread, $user);
+        exit();
+    } else {
+        show_result_page(null, false, $thread);
+    }
 } else {
-    show_result_page(false, NULL);
+    show_result_page(null, false, NULL);
 }
+
 ?>
