23f491368baea79ade18d7897b0178d7d5b016e3
David Anderson <davea@ssl.berkeley.edu>
2003-07-30 00:54:22 +0000

    Updates the activity field of every thread in the forum, based on an exponentially decreasing score calculated from how recent the posts in the thread are.
    svn path=/trunk/boinc/; revision=1873

---
+++ html/ops/update_forum_activities.php
@@ -0,0 +1,13 @@
+#!/usr/local/bin/php
+<?php
+
+require_once("../html_user/db.inc");
+require_once("../html_user/forum/forum.inc");
+
+db_init();
+
+// TODO: Update activity fields for all threads here.
+
+
+
+?>
