4eb9398e73f8a03284e5cef17b9642b9fa4f0c4d
David Anderson <davea@ssl.berkeley.edu>
2003-07-30 20:06:18 +0000

    Finished function to compute forum activity values and store them in the database.
    svn path=/trunk/boinc/; revision=1874

--- html/ops/update_forum_activities.php
+++ html/ops/update_forum_activities.php
@@ -2,11 +2,30 @@
 <?php
 
 require_once("../html_user/db.inc");
-require_once("../html_user/forum/forum.inc");
+require_once("../html_user/util.inc");
+
+define('MAX_REWARD', 4096);
 
 db_init();
 
-// TODO: Update activity fields for all threads here.
+$currentTime = time();
+$result = mysql_query("SELECT * FROM thread");
+
+while ($thread = mysql_fetch_assoc($result)) {
+    $sql = "SELECT * FROM post WHERE thread = " . $thread['id'];
+    $result2 = mysql_query($sql);
+    $points = 0;
+    
+    while ($post = mysql_fetch_assoc($result2)) {
+        $timeDiff = $currentTime - $post['timestamp'];
+        $dayDiff = floor($timeDiff / 86400);  // 86400 = # of seconds in a day;
+        
+        $points += (MAX_REWARD / pow(2, $dayDiff));
+    }
+    
+    $sql = "UPDATE thread SET activity = " . ceil($points) . " WHERE id = " . $thread['id'];
+    mysql_query($sql);
+}
 
 
 
