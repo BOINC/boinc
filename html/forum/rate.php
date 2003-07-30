659744798d99119d5c710a89fc0711339e0a8a20
David Anderson <davea@ssl.berkeley.edu>
2003-07-30 00:52:46 +0000

    Updated rating system and added a link back to the thread/post the user came from.
    svn path=/trunk/boinc/; revision=1872

--- html/forum/rate.php
+++ html/forum/rate.php
@@ -3,17 +3,24 @@ require_once("../util.inc");
 require_once("../include.php");
 require_once("forum.inc");
 
-if (!empty($_GET['post']) && !empty($_POST['rating'])) {
+if (!empty($_GET['post'])) {
     $postId = $_GET['post'];
     $rating = $_POST['rating'];
+    
+    // TODO: Define constants for these.
+    if ($rating == null || $rating > 2 || $rating < -2) {
+        show_result_page(false, NULL);
+        exit();
+    }
     
     $user = get_logged_in_user($true, '../');
     $result = mysql_query("SELECT * FROM post WHERE id = $postId");
     if ($result) {
         if (mysql_num_rows($result) > 0) {
             $post = mysql_fetch_object($result);
+            $points = $post->votes * $post->score;
             $votes = $post->votes + 1;
-            $score = $post->score + ($rating / $votes);
+            $score = ($points + $rating) / $votes;
             
             $result2 = mysql_query("UPDATE post SET votes = $votes, score = $score WHERE id = $postId");
             if ($result2) {
@@ -40,9 +47,10 @@ function show_result_page($success, $post) {
         if ($post) {
             echo "<p>There was a problem recording your vote in our database.  Please try again later.</p>";
         } else {
-            echo "<p>There post you specified does not exist.</p>";
+            echo "<p>There post you specified does not exist, or your rating was invalid.</p>";
         }
     }
+    echo "<a href=\"thread.php?id=", $post->thread, "#", $post->id, "\">Return to thread</a>";
     doFooter();
 }
 ?>
