7aba7153f7f059c3851adcc46b6294e7e516b99f
David Anderson <davea@ssl.berkeley.edu>
2003-07-30 00:51:44 +0000

    Added support for ordering threads and filtering out threads with particular rating levels.
    svn path=/trunk/boinc/; revision=1870

--- html/forum/thread.php
+++ html/forum/thread.php
@@ -7,6 +7,17 @@ doHeader('Forum');
 /* sanitize variable */
 $_GET['id'] = stripslashes(strip_tags($_GET['id']));
 
+$sort_style = $_GET['sort'];
+$filter_min = $_GET['filter'];
+
+// TODO: Constant for default sort style and filter values.
+if ($sort_style == NULL) {
+    $sort_style = "date-old";
+}
+if ($filter_min == NULL || $filter_min < -2 || $filter_min > 2) {
+    $filter_min = -2;
+}
+
 $thread = getThread($_GET['id']);
 $thread->incView();
 
@@ -23,13 +34,17 @@ if ($logged_in_user) {
     }
 }
 ?>
+<form action="thread.php" method="get">
+<input type="hidden" name="id" value=<?php echo $thread->id ?>>
+<table width=100% cellspacing=0 cellpadding=0>
+  <tr valign="bottom">
+    <td align="left" style="border:0px">
+      <p>
+        <span class="title"><?php echo $thread->title ?></span>
+        <br><a href="index.php"><?php echo $cfg['sitename'] ?> Forum</a> -> <a href="forum.php?id=<?php echo $forum->id ?>"><?php echo $forum->title ?></a>
+      </p>
 
-<p>
-<span class="title"><?php echo $thread->title ?></span>
-<br><a href="index.php"><?php echo $cfg['sitename'] ?> Forum</a> -> <a href="forum.php?id=<?php echo $forum->id ?>"><?php echo $forum->title ?></a>
-</p>
-<p>
-<a href=reply.php?thread=<?php echo $thread->id ?>#input>Reply to this thread</a><br>
+      <a href=reply.php?thread=<?php echo $thread->id ?>#input>Reply to this thread</a><br>
 <?php
 if ($is_subscribed) {
     echo "You are currently subscribed to this thread.  <a href=subscribe.php?action=unsubscribe&thread=$thread->id>Click here to unsubscribe</a>.";
@@ -37,14 +52,31 @@ if ($is_subscribed) {
     echo "<a href=subscribe.php?action=subscribe&thread=$thread->id>Subscribe to this thread</a>";
 }
 ?>
-</p>
+</td>
+<td align="right" style="border:0px">Sort / Filter
+<select name="sort">
+  <option <?php if ($sort_style == 'date-old') echo 'selected' ?> value="date-old">Oldest first</option>
+  <option <?php if ($sort_style == 'date-new') echo 'selected' ?> value="date-new">Newest first</option>
+  <option <?php if ($sort_style == 'rating-high') echo 'selected' ?> value="rating-high">Highest rated first</option>
+</select> 
+<select name="filter">
+  <option <?php if ($filter_min == 2) echo 'selected' ?> value="2">"Very helpful" only</option>
+  <option <?php if ($filter_min == 1) echo 'selected' ?> value="1">At least "helpful"</option>
+  <option <?php if ($filter_min == 0) echo 'selected' ?> value="0">At least "neutral"</option>
+  <option <?php if ($filter_min == -1) echo 'selected' ?> value="-1">At least "unhelpful"</option>
+  <option <?php if ($filter_min == -2) echo 'selected' ?> value="-2">All Posts</option>
+</select>
+  <input type="submit" value="Sort">
+</td>
+</tr></table>
+</form>
 <p style="text-align:center">
 	<table class="content" border="0" cellpadding="5" cellspacing="0" width="100%">
 		<tr>
 			<th style="width: 150px">Author</th>
 			<th>Message</th>
 		</tr>
-		<?php show_posts($thread, true, true) ?>
+		<?php show_posts($thread, $sort_style, $filter_min, true, true) ?>
 	</table>
 </p>
 <p>
