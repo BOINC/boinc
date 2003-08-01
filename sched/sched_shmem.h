741a06d6cd57fb4b67703950a7905047d411c563
Karl Chen <quarl@quarl.org>
2003-08-01 23:40:16 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1954

--- sched/sched_shmem.h
+++ sched/sched_shmem.h
@@ -2,18 +2,18 @@
 // Version 1.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://boinc.berkeley.edu/license_1.0.txt
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 // License for the specific language governing rights and limitations
-// under the License. 
-// 
-// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
-// 
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
 // The Initial Developer of the Original Code is the SETI@home project.
 // Portions created by the SETI@home project are Copyright (C) 2002
-// University of California at Berkeley. All Rights Reserved. 
-// 
+// University of California at Berkeley. All Rights Reserved.
+//
 // Contributor(s):
 //
 
@@ -24,7 +24,7 @@
 //
 #define MAX_PLATFORMS       50
 #define MAX_APPS            10
-#define MAX_APP_VERSIONS    100
+#define MAX_APP_VERSIONS    1000
 #define MAX_WU_RESULTS      1000
 
 // a workunit/result pair
