bb8b62569998e6839adf33cfb7c24f703b5ec25f
Karl Chen <quarl@quarl.org>
2003-08-01 23:23:20 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=1953

--- sched/sched_shmem.C
+++ sched/sched_shmem.C
@@ -2,18 +2,18 @@
 // Version 1.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://boinc.berkeley.edu/license_1.0.txt
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 // License for the specific language governing rights and limitations
-// under the License. 
-// 
-// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
-// 
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
 // The Initial Developer of the Original Code is the SETI@home project.
 // Portions created by the SETI@home project are Copyright (C) 2002
-// University of California at Berkeley. All Rights Reserved. 
-// 
+// University of California at Berkeley. All Rights Reserved.
+//
 // Contributor(s):
 //
 
@@ -28,6 +28,7 @@
 #include "boinc_db.h"
 
 #include "sched_shmem.h"
+#include "sched_util.h"
 
 void SCHED_SHMEM::init() {
     memset(this, 0, sizeof(SCHED_SHMEM));
@@ -133,5 +134,12 @@ APP_VERSION* SCHED_SHMEM::lookup_app_version(
             }
         }
     }
+
+    log_messages.printf(
+        SchedMessages::DEBUG,
+        "SCHED_SHMEM::lookup_app_version(appid=%d, platformid=%d, min_version=%d): best_avp=%d\n",
+        appid, platformid, min_version,
+        best_version);
+
     return best_avp;
 }
