0914e760618ec4ea2df5a55dcb58f4bed951bf7c
David Anderson <davea@ssl.berkeley.edu>
2003-08-01 20:27:03 +0000

    Added logic to compute a separate activity rating for helpdesk forums.
    svn path=/trunk/boinc/; revision=1937

--- html/ops/update_forum_activities.php
+++ html/ops/update_forum_activities.php
@@ -5,6 +5,7 @@ require_once("../html_user/db.inc");
 require_once("../html_user/util.inc");
 
 define('MAX_REWARD', 4096);
+define('SCALAR', 0.9);
 
 db_init();
 
@@ -12,19 +13,39 @@ $currentTime = time();
 $result = mysql_query("SELECT * FROM thread");
 
 while ($thread = mysql_fetch_assoc($result)) {
-    $sql = "SELECT * FROM post WHERE thread = " . $thread['id'];
-    $result2 = mysql_query($sql);
-    $points = 0;
+    $sql = "SELECT * FROM forum WHERE id = " . $thread['forum'];
+    $result3 = mysql_query($sql);
+    $forum = NULL;
     
-    while ($post = mysql_fetch_assoc($result2)) {
-        $timeDiff = $currentTime - $post['timestamp'];
+    if ($result3) {
+        $forum = mysql_fetch_assoc($result3);
+    }
+                       
+    // Helpdesk activiy is scaled linearly by the number of days since the last post.
+    if ($forum != NULL && $forum['is_helpdesk']) {
+        $timeDiff = $currentTime - $thread['timestamp'];
         $dayDiff = floor($timeDiff / 86400);  // 86400 = # of seconds in a day;
+        $points = $thread['sufferers'];
+        if ($dayDiff > 0) {
+            $points = $points * ($dayDiff * SCALAR);
+        }
+    // Other forum activity is scaled exponentially based on the recency of the posts.
+    } else {
+        $sql = "SELECT * FROM post WHERE thread = " . $thread['id'];
+        $result2 = mysql_query($sql);
+        $points = 0;
+    
+        while ($post = mysql_fetch_assoc($result2)) {
+            $timeDiff = $currentTime - $post['timestamp'];
+            $dayDiff = floor($timeDiff / 86400);
         
-        $points += (MAX_REWARD / pow(2, $dayDiff));
+            $points += (MAX_REWARD / pow(2, $dayDiff));
+        }
     }
     
     $sql = "UPDATE thread SET activity = " . ceil($points) . " WHERE id = " . $thread['id'];
     mysql_query($sql);
+    
 }
 
 
