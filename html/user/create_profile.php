c1474f8e1fe84cf1db7df6cd414b9547d60043bc
David Anderson <davea@ssl.berkeley.edu>
2003-08-07 00:43:39 +0000

    Cleaned up some unnecessary includes.
    svn path=/trunk/boinc/; revision=2005

--- html/user/create_profile.php
+++ html/user/create_profile.php
@@ -1,11 +1,7 @@
 <?php
 
-require_once("util.inc");
-require_once("db.inc");
-require_once("project_specific/project.inc");
 require_once("profile.inc");
 
-db_init();
 $user = get_logged_in_user(true);
 show_profile_creation_page($user);
 
