c3832541743aa6dcb89de2b8cd234e11c587fda8
Karl Chen <quarl@quarl.org>
2003-10-22 21:51:55 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=2545

--- client/win/win_config.h
+++ client/win/win_config.h
@@ -1,4 +1,4 @@
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 6
-#define PACKAGE_VERSION "2.06"
-#define VERSION "2.06"
+#define MINOR_VERSION 7
+#define PACKAGE_VERSION "2.07"
+#define VERSION "2.07"
