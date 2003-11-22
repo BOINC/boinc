8ec0df8e67d6db299417d9c6ec7ff151e34d59ce
Karl Chen <quarl@quarl.org>
2003-11-22 23:45:13 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=2669

--- lib/filesys.C
+++ lib/filesys.C
@@ -2,18 +2,18 @@
 // Version 1.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://boinc.berkeley.edu/license_1.0.txt
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 // License for the specific language governing rights and limitations
-// under the License. 
-// 
-// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
-// 
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
 // The Initial Developer of the Original Code is the SETI@home project.
 // Portions created by the SETI@home project are Copyright (C) 2002
-// University of California at Berkeley. All Rights Reserved. 
-// 
+// University of California at Berkeley. All Rights Reserved.
+//
 // Contributor(s):
 //
 
@@ -375,11 +375,13 @@ void full_path(char* relname, char* path) {
 int get_filesystem_info(double &total_space, double &free_space) {
 #ifdef _WIN32
 	FreeFn pGetDiskFreeSpaceEx;
-	pGetDiskFreeSpaceEx = (FreeFn)GetProcAddress(GetModuleHandle("kernel32.dll"), "GetDiskFreeSpaceExA");
+	pGetDiskFreeSpaceEx = (FreeFn)GetProcAddress(GetModuleHandle("kernel32.dll"),
+                                                 "GetDiskFreeSpaceExA");
 	if(pGetDiskFreeSpaceEx) {
 		ULARGE_INTEGER TotalNumberOfFreeBytes;
 		ULARGE_INTEGER TotalNumberOfBytes;
-		pGetDiskFreeSpaceEx(NULL, NULL, &TotalNumberOfBytes, &TotalNumberOfFreeBytes);
+		ULARGE_INTEGER TotalNumberOfBytesFreeToCaller;
+		pGetDiskFreeSpaceEx(NULL, &TotalNumberOfBytesFreeToCaller, &TotalNumberOfBytes, &TotalNumberOfFreeBytes);
 		signed __int64 uMB;
 		uMB = TotalNumberOfFreeBytes.QuadPart / (1024 * 1024);
 		free_space = uMB * 1024.0 * 1024.0;
