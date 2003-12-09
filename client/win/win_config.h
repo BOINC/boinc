b52bb717c6515b50f0390e888e38873d74f81f51
Gary Gibson <gmg@unknown.invalid>
2003-12-09 20:23:29 +0000

    boinc release 2.14
    svn path=/trunk/boinc/; revision=2763

--- client/win/win_config.h
+++ client/win/win_config.h
@@ -1,4 +1,4 @@
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 13
-#define PACKAGE_VERSION "2.13"
-#define VERSION "2.13"
+#define MINOR_VERSION 14
+#define PACKAGE_VERSION "2.14"
+#define VERSION "2.14"
