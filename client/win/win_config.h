c89faac837f338daa7a07b80f17776e6975e9a60
Rom Walton <rwalton@ssl.berkeley.edu>
2004-03-15 22:40:48 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3080

--- client/win/win_config.h
+++ client/win/win_config.h
@@ -1,4 +1,4 @@
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 27
-#define PACKAGE_VERSION "2.27"
-#define VERSION "2.27"
+#define MINOR_VERSION 28
+#define PACKAGE_VERSION "2.28"
+#define VERSION "2.28"
