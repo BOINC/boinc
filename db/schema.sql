aca82e920a9276856bc78bfd95f1722ec21991a6
David Anderson <davea@ssl.berkeley.edu>
2004-03-18 18:08:35 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3095

--- db/schema.sql
+++ db/schema.sql
@@ -225,14 +225,22 @@ create table result (
     primary key (id)
 );
 
-create table trickle (
+create table trickle_up (
     id                  integer     not null auto_increment,
     create_time         integer     not null,
-    client_create_time  integer     not null,
+    send_time           integer     not null,
+    resultid            integer     not null,
+    appid               integer     not null,
+    handled             smallint    not null,
+    xml                 text,
+    primary key (id)
+);
+
+create table trickle_down (
+    id                  integer     not null auto_increment,
+    create_time         integer     not null,
     resultid            integer     not null,
     hostid              integer     not null,
-    userid              integer     not null,
-    appid               integer     not null,
     handled             smallint    not null,
     xml                 text,
     primary key (id)
