82ac59c9693498752aad9abc1f41372c5a57921d
Rom Walton <rwalton@ssl.berkeley.edu>
2004-05-04 21:40:14 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3336

--- html/ops/index.php
+++ html/ops/index.php
@@ -42,7 +42,8 @@ while ($app = mysql_fetch_object($result)) {
         <ul>
         <li><a href=result_summary.php?appid=$app->id&nsecs=86400>last 24 hours</a>
         <li><a href=failure_result_summary_by_platform.php?appid=$app->id&nsecs=86400>last 24 hours - failure by platform</a>
-        <li><a href=result_summary.php?appid=$app->id&nsecs=$w>last week</a>
+        <li><a href=result_summary.php?appid=$app->id&nsecs=604800>last week</a>
+        <li><a href=failure_result_summary_by_platform.php?appid=$app->id&nsecs=604800>last week - failure by platform</a>
         </ul>
     ";
 }
