7088a913bc0f87b2a51a27cd16aa3ebdcf306ee7
Bob Bankay <bobb@ssl.berkeley.edu>
2004-06-04 02:39:24 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3500

--- db/schema.sql
+++ db/schema.sql
@@ -73,7 +73,7 @@ create table user (
     send_email          smallint    not null,
     show_hosts          smallint    not null,
     posts               smallint    not null,
-    seti_id             integer     not null,
+    seti_id             integer     ,
     seti_nresults       integer     not null,
     seti_last_result_time   integer not null,
     seti_total_cpu      double      not null,
