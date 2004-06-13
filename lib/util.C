3878be3df3e36603af5e3671edf7359630bff317
Rom Walton <rwalton@ssl.berkeley.edu>
2004-06-13 22:29:12 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3557

--- lib/util.C
+++ lib/util.C
@@ -409,7 +409,7 @@ void canonicalize_master_url(string& url) {
     string::size_type p = url.find("://");
     // lowercase http://
     if (p != string::npos) {
-        transform(url.begin(), url.begin()+p, url.begin(), tolower);
+        transform(url.begin(), url.begin()+p, url.begin(), std::tolower);
         p += 3;
     } else {
         p = 0;
