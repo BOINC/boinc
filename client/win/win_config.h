48cc4778f019bf458c7c2af5138146db99adf610
David Anderson <davea@ssl.berkeley.edu>
2004-06-18 19:53:46 +0000

    version 3.15
    svn path=/trunk/boinc/; revision=3665

--- client/win/win_config.h
+++ client/win/win_config.h
@@ -1,4 +1,4 @@
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 14
-#define PACKAGE_VERSION "3.14"
-#define VERSION "3.14"
+#define MINOR_VERSION 15
+#define PACKAGE_VERSION "3.15"
+#define VERSION "3.15"
