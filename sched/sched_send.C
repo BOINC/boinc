b4fab5bf765a0da45669bc8d35fb6c79feac7523
Brian Boshes <boshesbr@unknown.invalid>
2004-07-09 21:17:42 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=3835

--- sched/sched_send.C
+++ sched/sched_send.C
@@ -2,18 +2,18 @@
 // Version 1.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://boinc.berkeley.edu/license_1.0.txt
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 // License for the specific language governing rights and limitations
-// under the License. 
-// 
-// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
-// 
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
 // The Initial Developer of the Original Code is the SETI@home project.
 // Portions created by the SETI@home project are Copyright (C) 2002
-// University of California at Berkeley. All Rights Reserved. 
-// 
+// University of California at Berkeley. All Rights Reserved.
+//
 // Contributor(s):
 //
 
@@ -98,7 +98,7 @@ double max_allowable_disk(SCHEDULER_REQUEST& req) {
     // if they're zero, project will get no work.
     //
 
-    x1 = prefs.disk_max_used_gb*1e9 - req.total_disk_usage;
+    x1 = prefs.disk_max_used_gb*(1024.*1024.*1024.) - req.total_disk_usage;
     x2 = host.d_total*prefs.disk_max_used_pct/100.;
     x3 = host.d_free - prefs.disk_min_free_gb*1e9;      // may be negative
 
@@ -779,7 +779,7 @@ int send_work(
                 reply.host.id
             );
         }
-            
+
         strcpy(reply.message_priority, "high");
         reply.request_delay = 3600;
 
