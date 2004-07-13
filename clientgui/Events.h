904e0000ba26987b28bd1bfb28a456bc9451cf81
Rom Walton <rwalton@ssl.berkeley.edu>
2004-07-13 06:04:28 +0000

    Hooked up the Project and Work tab for the new GUI.
    svn path=/trunk/boinc/; revision=3854

---
+++ clientgui/Events.h
@@ -0,0 +1,46 @@
+// $Id$
+//
+// The contents of this file are subject to the BOINC Public License
+// Version 1.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://boinc.berkeley.edu/license_1.0.txt
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License. 
+// 
+// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
+// 
+// The Initial Developer of the Original Code is the SETI@home project.
+// Portions created by the SETI@home project are Copyright (C) 2002
+// University of California at Berkeley. All Rights Reserved. 
+// 
+// Contributor(s):
+//
+// Revision History:
+//
+// $Log$
+// Revision 1.7  2004/07/13 06:04:28  rwalton
+// Hooked up the Project and Work tab for the new GUI.
+//
+//
+
+
+#ifndef _EVENTS_H_
+#define _EVENTS_H_
+
+
+#define ID_STATUSBAR                        wxID_HIGHEST + 1
+#define ID_COMMANDSATTACHPROJECT            wxID_HIGHEST + 2
+#define ID_TOOLSOPTIONS                     wxID_HIGHEST + 3
+#define ID_FRAMERENDERTIMER                 wxID_HIGHEST + 4
+#define ID_LIST_PROJECTSVIEW                wxID_HIGHEST + 5
+#define ID_LIST_WORKVIEW                    wxID_HIGHEST + 6
+#define ID_LIST_TRANSFERSVIEW               wxID_HIGHEST + 7
+#define ID_LIST_MESSAGESVIEW                wxID_HIGHEST + 8
+#define ID_LIST_RESOURCEUTILIZATIONVIEW     wxID_HIGHEST + 9
+
+
+#endif
+
