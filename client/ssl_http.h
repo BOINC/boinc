febe38240aff17233f8b45673e421b9594cd2b98
David Anderson <davea@ssl.berkeley.edu>
2004-10-06 21:26:29 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4299

---
+++ client/ssl_http.h
@@ -0,0 +1,128 @@
+// The contents of this file are subject to the BOINC Public License
+// Version 1.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://boinc.berkeley.edu/license_1.0.txt
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code is the Berkeley Open Infrastructure for Network Computing.
+//
+// The Initial Developer of the Original Code is the SETI@home project.
+// Portions created by the SETI@home project are Copyright (C) 2002
+// University of California at Berkeley. All Rights Reserved.
+//
+// Contributor(s):
+//
+
+// HTTP_OP represents an HTTP operation.
+// There are variants for GET and POST,
+// and for the data source/sink (see below).
+
+#ifndef _HTTP_
+#define _HTTP_
+
+#include "proxy.h"
+
+// official HTTP status codes
+#define HTTP_STATUS_OK              200
+#define HTTP_STATUS_RANGE_REQUEST_ERROR    416
+#define HTTP_STATUS_MOVED_PERM      301
+#define HTTP_STATUS_MOVED_TEMP      302
+#define HTTP_STATUS_NOT_FOUND       404
+#define HTTP_STATUS_PROXY_AUTH_REQ  407
+
+struct HTTP_REPLY_HEADER {
+    int http_status;
+    int content_length;
+    string redirect_location;
+    string recv_buf;
+
+    void init();
+    int read_reply(int socket);
+    void parse();
+};
+
+#define HTTP_OP_NONE    0
+// For the first 4, data source/sink are files
+#define HTTP_OP_GET     1
+#define HTTP_OP_POST    2
+#define HTTP_OP_HEAD    4
+#define HTTP_OP_POST2   5
+    // a POST operation where the request comes from a combination
+    // of a string and a file w/offset,
+    // and the reply goes into a memory buffer
+
+class HTTP_OP : public PROXY {
+public:
+    HTTP_OP();
+    ~HTTP_OP();
+
+    int port;
+    char filename[256];
+    char url_hostname[256];
+        // the hostname part of the URL.
+        // May not be the host we connect to (if using proxy)
+    char* req1;
+    char infile[256];
+    char outfile[256];
+    int content_length;
+    double file_offset;
+    char request_header[256];
+    HTTP_REPLY_HEADER hrh;
+    int http_op_state;     // values below
+    int http_op_type;
+    int http_op_retval;
+        // zero if success, or a BOINC error code, or an HTTP status code
+////    bool proxy_auth_done;
+
+    int init_head(const char* url);
+    int init_get(const char* url, char* outfile, bool del_old_file, double offset=0);
+#ifdef _SSL_USE_
+    int init_ssl_get(char* outfile, bool del_old_file, double offset=0);
+    int init_ssl_post(char* infile, char* outfile);
+    int init_ssl_post2(char* req1, char* infile, double offset);
+#endif
+    int init_post(const char* url, char* infile, char* outfile);
+    int init_post2(
+        const char* url, char* req1, char* infile, double offset
+    );
+    void get_base64_str(bool, const char*, const char*, char*);
+    bool http_op_done();
+};
+
+// represents a set of HTTP requests in progress
+//
+class HTTP_OP_SET {
+    vector<HTTP_OP*> http_ops;
+    NET_XFER_SET* net_xfers;
+public:
+    HTTP_OP_SET(NET_XFER_SET*);
+    bool poll();
+    int insert(HTTP_OP*);
+    int remove(HTTP_OP*);
+};
+
+#define HTTP_STATE_IDLE             0
+#define HTTP_STATE_CONNECTING       1
+#define HTTP_STATE_SOCKS_CONNECT    2
+#define HTTP_STATE_REQUEST_HEADER   3
+#define HTTP_STATE_REQUEST_BODY1    4
+    // sending the string part of a POST2 operation
+#define HTTP_STATE_REQUEST_BODY     5
+#define HTTP_STATE_REPLY_HEADER     6
+#define HTTP_STATE_REPLY_BODY       7
+#define HTTP_STATE_DONE             8
+#ifdef _SSL_USE_
+#define URL_IS_SSL                  1
+#endif
+
+#ifdef _SSL_USE_
+extern int parse_url(const char* url, char* host, int &port, char* file);
+#else
+extern void parse_url(const char* url, char* host, int &port, char* file);
+#endif
+
+#endif
---
+++ client/ssl_net_xfer.h
@@ -0,0 +1,136 @@
+// The contents of this file are subject to the BOINC Public License
+// Version 1.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://boinc.berkeley.edu/license_1.0.txt
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License. 
+// 
+// The Original Code is the Berkeley Open Infrastructure for Network Computing. 
+// 
+// The Initial Developer of the Original Code is the SETI@home project.
+// Portions created by the SETI@home project are Copyright (C) 2002
+// University of California at Berkeley. All Rights Reserved. 
+// 
+// Contributor(s):
+//
+
+#ifndef _NET_XFER_
+#define _NET_XFER_
+
+#ifndef _WIN32
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#endif
+
+
+#ifdef _SSL_USE_
+namespace INCSSL{
+    #include <openssl/ssl.h>
+}
+using namespace INCSSL;
+#endif
+
+// The following classes implement polling (non-blocking) I/O
+// between a disk file (or memory block) and a socket
+
+#define MAX_BLOCKSIZE   16384
+
+#ifdef _SSL_USE_
+#define SSL_READ_MODE    1
+#define SSL_WRITE_MODE   2
+#endif
+
+// represents a network connection, either being accessed directly
+// or being transferred to/from a file
+//
+class NET_XFER {
+    char hostname[256];
+        // The host we're connecting to (possibly a proxy)
+public:
+#ifdef _SSL_USE_
+    NET_XFER();
+    ~NET_XFER();
+#endif
+    int socket;
+    bool is_connected;
+    bool want_download;     // at most one should be true
+    bool want_upload;
+    bool do_file_io;
+        // whether poll() should transfer data to/from file
+        // (in which case "file" and blocksize are relevant)
+        // or just set io_ready
+    bool io_done;           // got error or EOF
+    FILE* file;
+    bool io_ready;
+        // can read or write socket now (used if !do_file_io)
+    int error;
+    int port;
+    int blocksize;
+    double start_time;
+    double xfer_speed;
+    double bytes_xferred;   // bytes transferred in this session
+    char file_read_buf[MAX_BLOCKSIZE];
+    int file_read_buf_offset, file_read_buf_len;
+#ifdef _SSL_USE_
+    INCSSL::SSL *ssl; 
+    INCSSL::SSL_CTX *ctx;
+    int url_mode;
+    bool proxy_connect_finished;
+#endif
+    int seconds_until_timeout;
+
+    void init(char* host, int port, int blocksize);
+#ifdef _SSL_USE_
+    void init_2();
+    void init_ssl();
+    int close_ssl();
+    int ssl_connect(int, int*);
+    int ssl_read_write(int, char*, int, int, int*);
+    void ssl_error_print(char*, int, int);
+#endif
+    int get_ip_addr(int &ip_addr);
+    int get_ip_addr(char *hostname, int &ip_addr);
+    int open_server();
+    void close_socket();
+    int do_xfer(int&);
+    void update_speed(int);
+    void got_error();
+    char* get_hostname();
+    bool check_timeout(bool);
+    void reset_timeout();
+};
+
+// bandwidth limitation is implemented at this level, as follows:
+// There are limits max_bytes_sec_up and max_bytes_sec_down.
+// We keep track of the last time and bytes_left_up and bytes_left_down;
+// Each second we reset these to zero.
+
+class NET_XFER_SET {
+    vector<NET_XFER*> net_xfers;
+public:
+    NET_XFER_SET();
+    double max_bytes_sec_up, max_bytes_sec_down;
+        // user-specified limits on throughput
+    double bytes_left_up, bytes_left_down;
+        // bytes left to transfer in the current second
+    double bytes_up, bytes_down;
+        // total bytes transferred
+    bool up_active, down_active;
+        // has there been transfer activity since last call to check_active()?
+    time_t last_time;
+    int insert(NET_XFER*);
+    int remove(NET_XFER*);
+    bool poll();
+    int net_sleep(double);
+    int do_select(double& bytes_transferred, double timeout);
+    NET_XFER* lookup_fd(int);   // lookup by fd
+    void check_active(bool&, bool&);
+};
+
+extern int get_socket_error(int fd);
+
+#endif
