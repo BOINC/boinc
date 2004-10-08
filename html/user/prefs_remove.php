79d3c7d3c4b7df138039b6a8f0d9926aa223a370
David Anderson <davea@ssl.berkeley.edu>
2004-10-08 19:57:20 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4305

--- html/user/prefs_remove.php
+++ html/user/prefs_remove.php
@@ -10,16 +10,30 @@ $user = get_logged_in_user();
 
 $subset = $_GET["subset"];
 $venue = $_GET["venue"];
+$confirmed = $_GET["confirmed"];
 
-if ($subset == "global") {
-    $main_prefs = prefs_parse_global($user->global_prefs);
-    $main_prefs->$venue = null;
-    global_prefs_update($user, $main_prefs);
+if ($confirmed) {
+    if ($subset == "global") {
+        $main_prefs = prefs_parse_global($user->global_prefs);
+        $main_prefs->$venue = null;
+        global_prefs_update($user, $main_prefs);
+    } else {
+        $main_prefs = prefs_parse_project($user->project_prefs);
+        $main_prefs->$venue = null;
+        project_prefs_update($user, $main_prefs);
+    }
+    Header("Location: prefs.php?subset=$subset");
 } else {
-    $main_prefs = prefs_parse_project($user->project_prefs);
-    $main_prefs->$venue = null;
-    project_prefs_update($user, $main_prefs);
+    page_head("Confirm delete preferences");
+    echo "
+        <p>
+        Are you sure you want to delete your separate ", subset_name($subset),
+        " preferences for $venue?
+        <br><br>
+        <a href=prefs_remove.php?subset=$subset&venue=$venue&confirmed=yes>Yes</a>
+        | <a href=prefs.php?subset=$subset>Cancel</a>
+    ";
+    page_tail();
 }
-Header("Location: prefs.php?subset=$subset");
 
 ?>
