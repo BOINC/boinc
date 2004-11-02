564f076fbb6f6ddd4dc5fabbab081a54914a44fb
David Anderson <davea@ssl.berkeley.edu>
2004-11-02 20:30:19 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4477

--- client/dhrystone.C
+++ client/dhrystone.C
@@ -46,67 +46,19 @@
 
 #include "boinc_api.h"
 #include "cpu_benchmark.h"
+#include "dhrystone.h"
 
 #define structassign(d, s)      d = s
-
-#define REG register
-
-typedef enum    {Ident_1, Ident_2, Ident_3, Ident_4, Ident_5} Enumeration;
-
-typedef int     One_Thirty;
-typedef int     One_Fifty;
-typedef char    Capital_Letter;
-typedef char    Str_30[31];
-typedef int     Arr_1_Dim[50];
-typedef int     Arr_2_Dim[50][50];
-
-typedef struct  Record {
-        struct Record           *Ptr_Comp;
-        Enumeration             Discr;
-    union {
-          struct {
-                  Enumeration Enum_Comp;
-                  int         Int_Comp;
-                  char        Str_Comp [31];
-                  } var_1;
-          struct {
-                  Enumeration E_Comp_2;
-                  char        Str_2_Comp [31];
-                  } var_2;
-          struct {
-                  char        Ch_1_Comp;
-                  char        Ch_2_Comp;
-                  } var_3;
-          } variant;
-} Rec_Type, *Rec_Pointer;
 
 
 #define TRUE            1
 #define FALSE           0
 
 
-
 /*
  * Package 1
  */
-struct DS_DATA {
-    Rec_Pointer       _Ptr_Glob;
-    Rec_Pointer       _Next_Ptr_Glob;
-    int             _Int_Glob;
-    bool            _Bool_Glob;
-    char            _Ch_1_Glob;
-    char            _Ch_2_Glob;
-    Arr_1_Dim       _Arr_1_Glob;
-    Arr_2_Dim       _Arr_2_Glob;
-};
-#define Int_Glob dd._Int_Glob
-#define Bool_Glob dd._Bool_Glob
-#define Ch_1_Glob dd._Ch_1_Glob
-#define Ch_2_Glob dd._Ch_2_Glob
-#define Arr_1_Glob dd._Arr_1_Glob
-#define Arr_2_Glob dd._Arr_2_Glob
-#define Ptr_Glob dd._Ptr_Glob
-#define Next_Ptr_Glob dd._Next_Ptr_Glob
+
 
 void Proc_0();
 void Proc_1(DS_DATA&, Rec_Pointer PtrParIn);
@@ -288,127 +240,4 @@ void Proc_5(DS_DATA& dd)
         Ch_1_Glob = 'A';
         Bool_Glob = FALSE;
 }
-
-extern bool Func_3();
-
-void Proc_6(DS_DATA& dd, Enumeration Enum_Val_Par, Enumeration *Enum_Ref_Par)
-{
-  *Enum_Ref_Par = Enum_Val_Par;
-  if (! Func_3 (Enum_Val_Par))
-    /* then, not executed */
-    *Enum_Ref_Par = Ident_4;
-  switch (Enum_Val_Par)
-  {
-    case Ident_1:
-      *Enum_Ref_Par = Ident_1;
-      break;
-    case Ident_2:
-      if (Int_Glob > 100)
-        /* then */
-      *Enum_Ref_Par = Ident_1;
-      else *Enum_Ref_Par = Ident_4;
-      break;
-    case Ident_3: /* executed */
-      *Enum_Ref_Par = Ident_2;
-      break;
-    case Ident_4: break;
-    case Ident_5:
-      *Enum_Ref_Par = Ident_3;
-      break;
-  } /* switch */
-}
-
-void Proc_7(One_Fifty Int_1_Par_Val, One_Fifty Int_2_Par_Val, One_Fifty *Int_Par_Ref)
-{
-  One_Fifty Int_Loc;
-
-  Int_Loc = Int_1_Par_Val + 2;
-  *Int_Par_Ref = Int_2_Par_Val + Int_Loc;
-}
-
-void Proc_8(DS_DATA& dd,
-        Arr_1_Dim       Arr_1_Par_Ref,
-        Arr_2_Dim       Arr_2_Par_Ref,
-        int             Int_1_Par_Val,
-        int             Int_2_Par_Val
-) {
-  REG One_Fifty Int_Index;
-  REG One_Fifty Int_Loc;
-
-  Int_Loc = Int_1_Par_Val + 5;
-  Arr_1_Par_Ref [Int_Loc] = Int_2_Par_Val;
-  Arr_1_Par_Ref [Int_Loc+1] = Arr_1_Par_Ref [Int_Loc];
-  Arr_1_Par_Ref [Int_Loc+30] = Int_Loc;
-  for (Int_Index = Int_Loc; Int_Index <= Int_Loc+1; ++Int_Index)
-    Arr_2_Par_Ref [Int_Loc] [Int_Index] = Int_Loc;
-  Arr_2_Par_Ref [Int_Loc] [Int_Loc-1] += 1;
-  Arr_2_Par_Ref [Int_Loc+20] [Int_Loc] = Arr_1_Par_Ref [Int_Loc];
-  Int_Glob = 5;
-}
-
-Enumeration Func_1(
-        DS_DATA& dd,
-        Capital_Letter   Ch_1_Par_Val,
-        Capital_Letter   Ch_2_Par_Val
-) {
-  Capital_Letter        Ch_1_Loc;
-  Capital_Letter        Ch_2_Loc;
-
-  Ch_1_Loc = Ch_1_Par_Val;
-  Ch_2_Loc = Ch_1_Loc;
-  if (Ch_2_Loc != Ch_2_Par_Val)
-    /* then, executed */
-    return (Ident_1);
-  else  /* not executed */
-  {
-    Ch_1_Glob = Ch_1_Loc;
-    return (Ident_2);
-   }
-}
-
-bool Func_2(DS_DATA& dd, Str_30  Str_1_Par_Ref, Str_30  Str_2_Par_Ref) {
-  REG One_Thirty        Int_Loc;
-      Capital_Letter    Ch_Loc=0;
-
-  Int_Loc = 2;
-  while (Int_Loc <= 2) /* loop body executed once */
-    if (Func_1 (dd, Str_1_Par_Ref[Int_Loc],
-                Str_2_Par_Ref[Int_Loc+1]) == Ident_1)
-      /* then, executed */
-    {
-      Ch_Loc = 'A';
-      Int_Loc += 1;
-    } /* if, while */
-  if (Ch_Loc >= 'W' && Ch_Loc < 'Z')
-    /* then, not executed */
-    Int_Loc = 7;
-  if (Ch_Loc == 'R')
-    /* then, not executed */
-    return (true);
-  else /* executed */
-  {
-    if (strcmp (Str_1_Par_Ref, Str_2_Par_Ref) > 0)
-      /* then, not executed */
-    {
-      Int_Loc += 7;
-      Int_Glob = Int_Loc;
-      return (true);
-    }
-    else /* executed */
-      return (false);
-  } /* if Ch_Loc */
-}
-
-bool Func_3(Enumeration Enum_Par_Val)
-{
-  Enumeration Enum_Loc;
-
-  Enum_Loc = Enum_Par_Val;
-  if (Enum_Loc == Ident_3)
-    /* then, executed */
-    return (true);
-  else /* not executed */
-    return (false);
-}
-
 
