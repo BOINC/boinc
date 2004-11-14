3482b3713d5708a814ecfd426a5c85f4daff83f5
David Anderson <davea@ssl.berkeley.edu>
2004-11-14 17:49:16 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4555

--- html/ops/cancel_wu_action.php
+++ html/ops/cancel_wu_action.php
@@ -7,22 +7,23 @@ echo "<!--\$Id$ -->";
 //
 
 function test_mysql_query($msg) {
-	echo "mysql_query($msg)<br/>";
+        echo "mysql_query($msg)<br/>";
         return 1;
 }
 
-// for purposes of testing and seeing queries, replace the two instances of mysql_query() below with test_mysql_query().
+// for purposes of testing and seeing queries,
+// replace the two instances of mysql_query() below with test_mysql_query().
+//
 function cancel_wu($wuid1, $wuid2) {
-
     $command1="update result set server_state=5, outcome=5 where server_state=2 and $wuid1<=workunitid and workunitid<=$wuid2";
     $command2="update workunit set error_mask=error_mask|16 where $wuid1<=id and id<=$wuid2";
 
     if (!mysql_query($command1)) {
-	echo "MySQL command $command1 failed:<br/>unable to cancel unsent results.<br/>";
-	return 1;
+        echo "MySQL command $command1 failed:<br/>unable to cancel unsent results.<br/>";
+        return 1;
     } else if (!mysql_query($command2)) {
-	echo "MySQL command $command2 failed:<br/>unable to cancel workunits.<br/>";
-	return 2;
+        echo "MySQL command $command2 failed:<br/>unable to cancel workunits.<br/>";
+        return 2;
     }
 
     return 0;
@@ -47,19 +48,21 @@ $wuid1 = $_GET['wuid1'];
 $wuid2 = $_GET['wuid2'];
 
 if ($wuid1<1 || $wuid2<$wuid1) {
-	echo "<h2>Workunit IDs fail to satisfy the conditions:<br/>
-	      1 <= WU1 ($wuid1) <= WU2 ($wuid2)<br/>
-	      Unable to process request to cancel workunits.
-             </h2>";
-	exit();
+    echo "<h2>Workunit IDs fail to satisfy the conditions:<br/>
+        1 <= WU1 ($wuid1) <= WU2 ($wuid2)<br/>
+        Unable to process request to cancel workunits.
+        </h2>
+    ";
+    exit();
 }
 
 echo "CANCELLING workunits $wuid1 to $wuid2 inclusive....<br/>";
 
-if (cancel_wu($wuid1, $wuid2))
-	echo "<h2>Failed in";
-   else
-        echo "<h2>Success in";
-    echo " cancelling workunits $wuid1 <= WUID <= $wuid2</h2>";
+if (cancel_wu($wuid1, $wuid2)) {
+    echo "<h2>Failed in";
+} else {
+    echo "<h2>Success in";
+}
+echo " cancelling workunits $wuid1 <= WUID <= $wuid2</h2>";
 
 ?>
