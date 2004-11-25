d75f601efedab3bb1cc7c05a65dbd1dd335814d1
David Anderson <davea@ssl.berkeley.edu>
2004-11-25 07:24:35 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4652

--- api/boinc_api.C
+++ api/boinc_api.C
@@ -626,8 +626,7 @@ static int mem_usage(unsigned long& vm_kb, unsigned long& rs_kb) {
     OSVERSIONINFO osvi; 
     osvi.dwOSVersionInfoSize = sizeof(osvi);
     GetVersionEx( &osvi );
-    if (osvi.dwPlatformId == VER_PLATFORM_WIN32_NT)
-    {
+    if (osvi.dwPlatformId == VER_PLATFORM_WIN32_NT) {
         SIZE_T lpMinimumWorkingSetSize;
         SIZE_T lpMaximumWorkingSetSize;
 
@@ -640,12 +639,9 @@ static int mem_usage(unsigned long& vm_kb, unsigned long& rs_kb) {
         // return value is in bytes, we need to return kb
         vm_kb = lpMinimumWorkingSetSize / 1024;
         rs_kb = lpMaximumWorkingSetSize / 1024;
-    }
-    else
-    {
+    } else {
         return ERR_NOT_IMPLEMENTED;
     }
-
     return 0;
 #else
 
