c3a74d384cc037565ec08c9ad2d23b34f2633534
Eric J. Korpela <korpela@ssl.berkeley.edu>
2004-12-03 21:28:17 +0000

    config.h[.in] now includes win_config.h on WIN32 systems.
    Split platform independent version info into version.h[.in]
    (which in included from config.h)
    
    Added all the config.h macros into win_config.h
    
    svn path=/trunk/boinc/; revision=4731

---
+++ version.h
@@ -0,0 +1,17 @@
+/* $Id$ */
+/* Platform independent version definitions... */
+
+#ifndef BOINC_VERSION_H
+#define BOINC_VERSION_H
+
+/* Major part of BOINC version number */
+#define BOINC_MAJOR_VERSION 4
+
+/* Minor part of BOINC version number */
+#define BOINC_MINOR_VERSION 57
+
+/* String representation of BOINC version number */
+#define BOINC_VERSION_STRING "4.57"
+
+#endif /* #ifndef BOINC_VERSION_H */
+
