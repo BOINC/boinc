7ac4351f569aa9caecf78bda01ac73f88761e5c8
Bruce Allen <bruce.allen@aei.mpg.de>
2004-12-12 15:07:27 +0000

    graphics_lib_handle has to be extern C to be usable in a C program.
    svn path=/trunk/boinc/; revision=4830

--- api/graphics_lib.h
+++ api/graphics_lib.h
@@ -19,16 +19,17 @@
 
 #include "boinc_api.h"
 
-extern void* graphics_lib_handle;
-
 #ifdef __cplusplus
 extern "C" {
-  extern int boinc_init_graphics_lib(void (*worker)(), char* argv0);
+    extern int boinc_init_graphics_lib(void (*worker)(), char* argv0);
+    extern void* graphics_lib_handle;
 }
 
 extern int boinc_init_options_graphics_lib(
     BOINC_OPTIONS&, void (*worker)(), char* argv0
 );
+
 #else
-  extern int boinc_init_graphics_lib(void (*worker)(), char* argv0);
+    extern int boinc_init_graphics_lib(void (*worker)(), char* argv0);
+    extern void* graphics_lib_handle;
 #endif
