751b0ebd9b8f2623a6391524c736c1c66d0125aa
David Anderson <davea@ssl.berkeley.edu>
2004-12-15 19:35:42 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4854

--- api/graphics_impl.C
+++ api/graphics_impl.C
@@ -240,10 +240,6 @@ bool throttled_app_render(int x, int y, double t) {
     return false;
 }
 
-int boinc_get_init_data(APP_INIT_DATA& app_init_data) {
-    return bmsp->boinc_get_init_data_hook(app_init_data);
-}
-
 #ifdef __GNUC__
 static volatile const char  __attribute__((unused)) *BOINCrcsid="$Id$";
 #else
