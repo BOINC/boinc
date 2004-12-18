36b5be4bac67c045973aca0e1b5d8961183d4c6a
Rom Walton <rwalton@ssl.berkeley.edu>
2004-12-18 18:29:57 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4896

--- doc/contact.php
+++ doc/contact.php
@@ -72,7 +72,7 @@ show_name("Comatose");
 show_name("Jay Curtis");
 show_name("Fons Maartens");
 show_name("Rob Hague");
-show_name("Fred Harmon");
+show_name("Fred Harmon aka fharmon");
 show_name("Darrell Holz");
 show_name("John McLeod VII");
 show_name("Dennis Peters");
