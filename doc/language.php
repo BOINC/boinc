8f80a3f73da3943aba4289b4cb7a9f8008a62c0f
Rom Walton <rwalton@ssl.berkeley.edu>
2004-12-20 19:42:28 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=4907

--- doc/language.php
+++ doc/language.php
@@ -3,13 +3,15 @@ require_once("docutil.php");
 page_head("Language customization of the work manager");
 echo "
     Menu names and other text in the work manager are stored in
-    a file called <i>language.ini</i>.
+    a file called <i>BOINC Manager.po</i>.
     The release uses American English.
     Many other languages are available.
     The BOINC distribution includes all current language files.
-    To use a particular language file,
-    just rename it to 'language.ini'.
 
+	A tool you can use to modify the 'po' file is called poEdit
+	and can be found here:<br>
+	<a src=http://sourceforge.net/projects/poedit/>http://sourceforge.net/projects/poedit/</a>
+	
     <p>
     To submit a new language file,
     please email it to Rom Walton (rwalton at ssl.berkeley.edu).
