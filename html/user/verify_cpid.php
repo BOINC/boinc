0f13e6d73c30aaa1480d11a138a913851c8cd301
David Anderson <davea@ssl.berkeley.edu>
2005-02-07 20:47:45 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=5345

--- html/user/verify_cpid.php
+++ html/user/verify_cpid.php
@@ -3,29 +3,36 @@
 // "web service" for verifying that a user CPID matches an account key
 
 require_once("../inc/db.inc");
+require_once("../inc/xml.inc");
 
 db_init();
+
+xml_header();
+
+function reply($x) {
+    echo "<reply>\n$x</reply>\n";
+}
 
 $cpid = process_user_text($_GET["cpid"]);;
 $authenticator = process_user_text($_GET["authenticator"]);;
 
 if (!$cpid || !$authenticator) {
-    echo "<error>missing argument</error>\n";
+    reply("<error>missing argument</error>\n");
     exit();
 }
 
 $user = lookup_user_auth($authenticator);
 
 if (!$user) {
-    echo "<error>bad authenticator</error>\n";
+    reply("<error>bad authenticator</error>\n");
     exit();
 }
 
 $x = $user->cross_project_id.$user->email_addr;
 if (md5($x) == $cpid) {
-    echo "<success></success>\n";
+    reply( "<success></success>\n");
 } else {
-    echo "<error>bad CPID</error>\n";
+    reply( "<error>bad CPID</error>\n");
 }
 
 ?>
