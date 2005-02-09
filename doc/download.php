8a5871051cdd0c077f2365dc43bc912d2246dacd
Rom Walton <rwalton@ssl.berkeley.edu>
2005-02-09 04:14:14 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=5369

--- doc/download.php
+++ doc/download.php
@@ -32,30 +32,6 @@ function version_end($xml) {
     list_end();
 }
 
-function show_466($xml=false) {
-    version_start("4.66", "5 Feb 2005", $xml);
-    version("Windows", "boinc_4.66_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_465($xml=false) {
-    version_start("4.65", "3 Feb 2005", $xml);
-    version("Windows", "boinc_4.65_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_464($xml=false) {
-    version_start("4.64", "1 Feb 2005", $xml);
-    version("Windows", "boinc_4.64_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_463($xml=false) {
-    version_start("4.63", "1 Feb 2005", $xml);
-    version("Windows", "boinc_4.63_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
 function show_462($xml=false) {
     version_start("4.62", "24 Jan 2005", $xml);
     version("Windows", "boinc_4.62_windows_intelx86.exe", $xml);
@@ -64,33 +40,9 @@ function show_462($xml=false) {
     version_end($xml);
 }
 
-function show_460($xml=false) {
-    version_start("4.60", "20 Jan 2005", $xml);
-    version("Windows", "boinc_4.60_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_459($xml=false) {
-    version_start("4.59", "13 Jan 2005", $xml);
-    version("Windows", "boinc_4.59_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_458($xml=false) {
-    version_start("4.58", "9 Jan 2005", $xml);
-    version("Windows", "boinc_4.58_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_457($xml=false) {
-    version_start("4.57", "23 Dec 2004", $xml);
-    version("Windows", "boinc_4.57_windows_intelx86.exe", $xml);
-    version_end($xml);
-}
-
-function show_456($xml=false) {
-    version_start("4.56", "23 Nov 2004", $xml);
-    version("Windows", "boinc_4.56_windows_intelx86.exe", $xml);
+function show_420($xml=false) {
+    version_start("4.20", "8 Feb 2005", $xml);
+    version("Windows", "boinc_4.20_windows_intelx86.exe", $xml);
     version_end($xml);
 }
 
@@ -112,57 +64,12 @@ function show_418($xml=false) {
     version_end($xml);
 }
 
-function show_417($xml=false) {
-    version_start("4.17", "23 Jan 2005", $xml);
-    version("Windows", "boinc_4.17_windows_intelx86.exe", $xml);
-    version("Linux/x86", "boinc_4.17_i686-pc-linux-gnu.gz", $xml);
-    version("Mac OS X", "boinc_4.17_powerpc-apple-darwin.gz", $xml);
-    version("Solaris/SPARC", "boinc_4.17_sparc-sun-solaris2.7.gz", $xml);
-    version_end($xml);
-}
-
-function show_416($xml=false) {
-    version_start("4.16", "19 Jan 2005", $xml);
-    version("Windows", "boinc_4.16_windows_intelx86.exe", $xml);
-    version("Linux/x86", "boinc_4.16_i686-pc-linux-gnu.gz", $xml);
-    version("Mac OS X", "boinc_4.16_powerpc-apple-darwin.gz", $xml);
-    version("Solaris/SPARC", "boinc_4.16_sparc-sun-solaris2.7.gz", $xml);
-    version_end($xml);
-}
-
-function show_415($xml=false) {
-    version_start("4.15", "18 Jan 2005", $xml);
-    version("Windows", "boinc_4.15_windows_intelx86.exe", $xml);
-    version("Linux/x86", "boinc_4.15_i686-pc-linux-gnu.gz", $xml);
-    version("Mac OS X", "boinc_4.15_powerpc-apple-darwin.gz", $xml);
-    version("Solaris/SPARC", "boinc_4.15_sparc-sun-solaris2.7.gz", $xml);
-    version_end($xml);
-}
-
-function show_414($xml=false) {
-    version_start("4.14", "13 Jan 2005", $xml);
-    version("Windows", "boinc_4.14_windows_intelx86.exe", $xml);
-    version("Linux/x86", "boinc_4.14_i686-pc-linux-gnu.gz", $xml);
-    version("Mac OS X", "boinc_4.14_powerpc-apple-darwin.gz", $xml);
-    version("Solaris/SPARC", "boinc_4.14_sparc-sun-solaris2.7.gz", $xml);
-    version_end($xml);
-}
-
-function show_413($xml=false) {
-    version_start("4.13", "13 Oct 2004", $xml);
-    version("Windows", "boinc_4.13_windows_intelx86.exe", $xml);
-    version("Linux/x86", "boinc_4.13_i686-pc-linux-gnu.gz", $xml);
-    version("Mac OS X", "boinc_4.13_powerpc-apple-darwin.gz", $xml);
-    version("Solaris/SPARC", "boinc_4.13_sparc-sun-solaris2.7.gz", $xml);
-    version_end($xml);
-}
-
 function show_stable($xml) {
     show_419($xml);
 }
 
 function show_dev($xml) {
-    show_466($xml);
+    show_420($xml);
     show_462($xml);
 }
 
