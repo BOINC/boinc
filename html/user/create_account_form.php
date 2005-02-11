8ecc7868eab116267fec38034d0f6570ab09637d
Janus B. Kristensen <jbk@visualgroup.dk>
2005-02-11 22:08:56 +0000

    Translation features enabled on create account form
    svn path=/trunk/boinc/; revision=5403

--- html/user/create_account_form.php
+++ html/user/create_account_form.php
@@ -2,9 +2,10 @@
 require_once('../inc/db.inc');
 require_once('../inc/util.inc');
 require_once('../inc/countries.inc');
+require_once('../inc/translation.inc');
 
 db_init();
-page_head('Create an account');
+page_head(tr(CREATE_AC_TITLE));
 
 $config = get_config();
 if (parse_bool($config, "disable_account_creation")) {
@@ -20,11 +21,10 @@ if (parse_bool($config, "disable_account_creation")) {
 }
 echo "
 
-    <p><b>Read the <a href=info.php>Rules and Policies</a>
-    before creating an account.</b></p>
+    <p><b>"; printf(tr(CREATE_AC_READ_RULES), "<a href=\"info.php\">".tr(RULES_TITLE)."</a>");echo "</b></p>
 
-    <p>If you already received an account ID, do not submit this form.
-    <a href=account_created.php>Activate your account</a> instead.</p>
+    <p>"; printf(tr(CREATE_AC_ALREADY_GOT), "<a href=\"account_created.php\">".tr(AC_CREATED_TITLE)."</a>"); echo "
+    </p>
 
     <form action=create_account_action.php method=post>
 ";
@@ -47,25 +47,25 @@ if ($teamid) {
 }
 start_table();
 row2(
-    "Name<br><span class=description>Identifies you on our web site. Use your real name or a nickname.</span>",
+    tr(CREATE_AC_NAME)."<br><span class=description>".tr(CREATE_AC_NAME_DESC)."</span>",
     "<input name=new_name size=30>"
 );
 row2(
-    "Email Address<br><span class=description>Must be a valid address of the form 'name@domain'.</span>",
+    tr(CREATE_AC_EMAIL)."<br><span class=description>".tr(CREATE_AC_EMAIL_DESC)."</span>",
     "<input name=new_email_addr size=50>"
 );
 row2_init(
-    "Country <br><span class=description>Select the country you want to represent, if any.</span>",
+    tr(CREATE_AC_COUNTRY)."<br><span class=description>".tr(CREATE_AC_COUNTRY_DESC)."</span>",
     "<select name=country>"
 );
 print_country_select();
 echo "</select></td></tr>\n";
 row2(
-    "Postal or ZIP Code <br><span class=description>Optional.</span>",
+    tr(CREATE_AC_ZIP)."<br><span class=description>".tr(OPTIONAL).".</span>",
     "<input name=postal_code size=20>"
 );
 row2("",
-    "<input type=submit value='Create Account'>"
+    "<input type=submit value='".tr(CREATE_AC_CREATE)."'>"
 );
 end_table();
 echo "
