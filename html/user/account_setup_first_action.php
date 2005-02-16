45942fbe5f79cb969c6fd0e81ac8d5147cc08c22
David Anderson <davea@ssl.berkeley.edu>
2005-02-16 19:47:00 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=5442

--- html/user/account_setup_first_action.php
+++ html/user/account_setup_first_action.php
@@ -21,6 +21,6 @@ project_prefs_update($user, $prefs);
 venue_parse_form($user);
 venue_update($user);
 
-Header("Location: http://boinc.berkeley.edu/download.php");
+Header("Location: account_setup_first_done.php");
 
 ?>
