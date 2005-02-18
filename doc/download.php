4cb4bb23466621909752b0d8b5dd673636692ff3
Rom Walton <rwalton@ssl.berkeley.edu>
2005-02-18 02:45:22 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=5471

--- doc/download.php
+++ doc/download.php
@@ -40,6 +40,12 @@ function show_462($xml=false) {
     version_end($xml);
 }
 
+function show_422($xml=false) {
+    version_start("4.22", "18 Feb 2005", $xml);
+    version("Windows", "boinc_4.22_windows_intelx86.exe", $xml);
+    version_end($xml);
+}
+
 function show_421($xml=false) {
     version_start("4.21", "11 Feb 2005", $xml);
     version("Windows", "boinc_4.21_windows_intelx86.exe", $xml);
@@ -75,7 +81,7 @@ function show_stable($xml) {
 }
 
 function show_dev($xml) {
-    show_421($xml);
+    show_422($xml);
     show_462($xml);
 }
 
