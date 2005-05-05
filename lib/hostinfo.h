f42b511669364b39e40b04aa50bfb201e881e3fa
Bruce Allen <bruce.allen@aei.mpg.de>
2005-05-05 05:18:40 +0000

    Fixing the documentation
    svn path=/trunk/boinc/; revision=6030

--- lib/hostinfo.h
+++ lib/hostinfo.h
@@ -31,7 +31,7 @@
 // NET_STATS (average network bandwidths)
 
 struct HOST_INFO {
-    int timezone;    // seconds added to local time to get UTC
+    int timezone;    // local STANDARD time - UTC time
     char domain_name[256];
     char serialnum[256];
     char ip_addr[256];
