159acff3a29654d7d3ccfd93f63a6f6a5cf43b44
Bruce Allen <bruce.allen@aei.mpg.de>
2005-05-11 11:09:57 +0000

    Get rid of two more undefined indices
    svn path=/trunk/boinc/; revision=6123

--- html/user/view_profile.php
+++ html/user/view_profile.php
@@ -8,10 +8,10 @@ $userid = get_int('userid');
 
 // Check for recommendation or rejection votes
 
-if ($_POST['recommend']) {
+if (isset($_POST['recommend'])) {
     process_view_results("recommend", $userid);
     exit();
-} else if ($_POST['reject']) {
+} else if (isset($_POST['reject'])) {
     process_view_results("reject", $userid);
     exit();
 }
