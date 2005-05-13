2aa5497d4e0bc4b6b9b5ee8c1be793ee07ffddef
Janus B. Kristensen <jbk@visualgroup.dk>
2005-05-13 19:14:48 +0000

    Postal code and url are both optional
    svn path=/trunk/boinc/; revision=6148

--- html/user/edit_user_info_action.php
+++ html/user/edit_user_info_action.php
@@ -15,13 +15,13 @@ if ($name != strip_tags($name)) {
 if (strlen($name) == 0) {
    error_page("You must supply a name for your account.");
 }
-$url = process_user_text(post_str("url"));
+$url = process_user_text(post_str("url", true));
 $url = strip_tags($url);
 $country = post_str("country");
 if (!is_valid_country($country)) {
     error_page("bad country");
 }
-$postal_code = process_user_text(post_str("postal_code"));
+$postal_code = process_user_text(post_str("postal_code",true));
 $postal_code = strip_tags($postal_code);
 
 $result = mysql_query("update user set name='$name', url='$url', country='$country', postal_code='$postal_code' where id=$user->id");
