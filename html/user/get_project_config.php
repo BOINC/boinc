f1206c6222cc663f7e424ad1f5dd18219addef95
Rom Walton <rwalton@ssl.berkeley.edu>
2005-08-10 23:53:33 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=7232

--- html/user/get_project_config.php
+++ html/user/get_project_config.php
@@ -1,13 +1,16 @@
 <?php
 
+require_once("../inc/util.inc");
 require_once("../inc/xml.inc");
 
 xml_header();
-echo "
-<project_config>
-    <name>Example name</name>
-    <min_passwd_length>7</min_passwd_length>
-</project_config>
-";
+
+$config = get_config();
+$name = parse_config($config, "long_name");
+
+echo "<project_config>\n";
+echo "    <name>$name</name>\n";
+echo "    <min_passwd_length>7</min_passwd_length>\n";
+echo "</project_config>\n";
 
 ?>
