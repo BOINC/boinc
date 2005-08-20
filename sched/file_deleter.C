4d0bcfbe2779767d8802fadb7c37dc6171f45258
Bruce Allen <bruce.allen@aei.mpg.de>
2005-08-20 02:45:49 +0000

    Restored 'delete antiques' code overwritten by Jeff Cobb Now delete files in increasing mtime order Additional paranoia to avoid deleting 'the wrong' files if there   is a configuration or other user/admin error Use C++ string type for storage efficiency in list Only delete antique files when there is no OTHER file deletion to do
    svn path=/trunk/boinc/; revision=7461

--- sched/file_deleter.C
+++ sched/file_deleter.C
@@ -20,11 +20,15 @@
 
 // file_deleter: deletes files that are no longer needed
 
+#include <list>
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
 #include <unistd.h>
 #include <errno.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "boinc_db.h"
 #include "parse.h"
@@ -35,6 +39,8 @@
 #include "sched_config.h"
 #include "sched_util.h"
 #include "sched_msgs.h"
+
+using namespace std;
 
 #define LOCKFILE "file_deleter.out"
 #define PIDFILE  "file_deleter.pid"
@@ -144,9 +150,20 @@ int result_delete_files(RESULT& result) {
                     pathname
                 );
                 if (retval) {
-                    log_messages.printf(SCHED_MSG_LOG::CRITICAL,
-                        "[RESULT#%d] get_file_path: %s: %d\n",
-                        result.id, filename, retval
+                    // the fact that no result files were found is a critical
+                    // error if this was a successful result, but is to be
+                    // expected if the result outcome was failure, since in
+                    // that case there may well be no output file produced.
+                    //
+                    int debug_or_crit;
+                    if (RESULT_OUTCOME_SUCCESS == result.outcome) {
+                        debug_or_crit=SCHED_MSG_LOG::CRITICAL;
+                    } else {
+                        debug_or_crit=SCHED_MSG_LOG::DEBUG;
+                    }
+                    log_messages.printf(debug_or_crit,
+                        "[RESULT#%d] outcome=%d client_state=%d No file %s to delete\n",
+                        result.id, result.outcome, result.client_state, filename
                     );
                 } else {
                     retval = unlink(pathname);
@@ -207,7 +224,7 @@ bool do_pass(bool retry_error) {
     }
     while (!wu.enumerate(buf)) {
         did_something = true;
-        
+
         retval = 0;
         if (!preserve_wu_files) {
             retval = wu_delete_files(wu);
@@ -251,9 +268,231 @@ bool do_pass(bool retry_error) {
     return did_something;
 }
 
+
+struct FILE_RECORD {
+     string name;
+     int date_modified;
+};
+
+bool operator == (const FILE_RECORD& fr1, const FILE_RECORD& fr2) {
+    return (fr1.date_modified == fr2.date_modified && fr1.name == fr2.name);
+}
+
+bool operator < (const FILE_RECORD& fr1, const FILE_RECORD& fr2) {
+    if (fr1.date_modified < fr2.date_modified)
+        return true;
+    else if (fr1.date_modified > fr2.date_modified)
+        return false;
+    else if (fr1.name < fr2.name)
+        return true;
+    else
+        return false;
+}
+
+// list of antique files to delete.  If order is
+// NOT sorted by mod time (primary key) and name
+// (secondary key) then unsorted==true.
+//
+std::list<FILE_RECORD> files_to_delete;
+bool unsorted = true;
+
+// delete files from list of old files.  Returns number
+// of files deleted, or negative for error.
+int delete_antique_files(int max_to_delete) {
+    int nfiles=0;
+
+    if (files_to_delete.empty()) return 0;
+
+    // sort list and eliminate duplicates
+    //
+    if (unsorted) {
+        files_to_delete.sort();
+        files_to_delete.unique();
+        unsorted = false;
+    }
+
+    while (nfiles<max_to_delete && !files_to_delete.empty()) {
+        char timestamp[128];
+        char pathname[1024];
+        int retval;
+
+      FILE_RECORD fr=files_to_delete.front();
+      check_stop_daemons();
+
+        retval=get_file_path((char *)fr.name.c_str(), config.upload_dir, config.uldl_dir_fanout, pathname);
+        if (retval) {
+            log_messages.printf(SCHED_MSG_LOG::CRITICAL,
+                "get_file_path(%s) failed: %d\n",
+                fr.name.c_str(), retval
+            );
+            return -1;
+        }
+
+      strcpy(timestamp, time_to_string(fr.date_modified));
+        log_messages.printf(SCHED_MSG_LOG::DEBUG,
+            "deleting [antique %s] %s\n",
+          timestamp, pathname 
+        );
+        if (unlink(pathname)) {
+          int save_error=errno;
+          log_messages.printf(SCHED_MSG_LOG::CRITICAL,
+              "unlink(%s) failed: %s\n",
+              pathname, strerror(save_error)
+            );
+          return -1;
+      }
+      else {
+            nfiles++;
+          files_to_delete.pop_front();
+      }
+    } // while
+    return nfiles;
+}
+
+
+// construct a list of old files.  return number of files
+// added to list, or negative number for error.
+//
+int add_antiques_to_list(int days) {
+    char command[256];
+    char single_line[1024];
+    FILE *fp;
+    int dirlen=strlen(config.upload_dir);
+    struct passwd *apache_info=getpwnam("apache");
+    int del_time=time(0)-86400*days;
+    int nfiles=0;
+
+    if (!apache_info) {
+        log_messages.printf(SCHED_MSG_LOG::CRITICAL, "no user named 'apache' found!\n");
+      return -1;
+    }
+
+    sprintf(command,  "find %s -type f -mtime +%d", config.upload_dir, days);
+    
+    // Now execute the command, read output on a stream.  We could use
+    // find to also exec a 'delete' command.  But we want to log all
+    // file names into the log, and do lots of sanity checking, so
+    // this way is better.
+    //
+    if (!(fp=popen(command, "r"))) {
+        log_messages.printf(SCHED_MSG_LOG::CRITICAL, "command %s failed\n", command);
+        return -2;
+    }
+
+    while (fgets(single_line, 1024, fp)) {
+        char pathname[1024];
+      char *fname_at_end=NULL;
+      int nchars=strlen(single_line);
+      struct stat statbuf;
+      char *err=NULL;
+        FILE_RECORD fr;        
+
+      // We can interrupt this at any point.
+      // pclose() is called when process exits.
+        check_stop_daemons();
+
+      // Do serious sanity checking on the path before
+      // adding the file!!
+      //
+      if (!err && nchars > 1022) err="line too long";
+      if (!err && nchars < dirlen + 1) err="path shorter than upload directory name";
+      if (!err && single_line[nchars-1] != '\n') err="no newline terminator in line";
+      if (!err && strncmp(config.upload_dir, single_line, dirlen)) err="upload directory not in path";
+      if (!err && single_line[dirlen] != '/') err="no slash separator in path";
+      if (!err) single_line[nchars-1]='\0';
+      if (!err && stat(single_line, &statbuf)) err="stat failed";
+      if (!err && statbuf.st_mtime > del_time) err="file too recent";
+      if (!err && apache_info->pw_uid != statbuf.st_uid) err="file not owned by httpd user";
+      if (!err && !(fname_at_end=rindex(single_line+dirlen, '/'))) err="no trailing filename";
+      if (!err) fname_at_end++;
+      if (!err && !strlen(fname_at_end)) err="trailing filename too short";
+      if (!err && get_file_path(fname_at_end, config.upload_dir, config.uldl_dir_fanout, pathname)) err="get_file_path() failed";
+      if (!err && strcmp(pathname, single_line)) err="file in wrong hierarchical upload subdirectory";
+
+      if (err) {
+            log_messages.printf(SCHED_MSG_LOG::CRITICAL,
+              "Can't list %s for deletion: %s\n",
+              single_line, err
+            );
+          // This file deleting business is SERIOUS.  Give up at the
+          // first sign of ANYTHING amiss.
+          //
+          pclose(fp);
+          return -3;
+      }
+
+        // insert this file onto the list
+      fr.date_modified = statbuf.st_mtime;
+      fr.name = fname_at_end;
+      files_to_delete.push_back(fr);
+        unsorted = true;
+        nfiles++;
+       
+    } // while (fgets(single_line, 1024, fp)) {  
+    pclose(fp);
+    log_messages.printf(SCHED_MSG_LOG::DEBUG,
+        "Found %d antique files to delete\n",
+        nfiles 
+    );
+    return nfiles;
+}
+
+// returns number of files found & added, or negative for error.
+//
+int find_antique_files() {
+    char buf[256];
+    DB_WORKUNIT wu;
+    static int nextrun=0;
+
+    check_stop_daemons();
+
+    // list files once per day
+    //
+    if (time(0) < nextrun) return 0;
+    nextrun = time(0)+86400;
+
+    // Find the oldest workunit.  We could add "where
+    // file_delete_state!=FILE_DELETE_DONE" to the query, but this
+    // might create some race condition with the 'regular' file delete
+    // mechanism, so better to do it like this.
+    //
+    sprintf(buf, "order by create_time limit 1");
+    if (!wu.enumerate(buf)) {
+        // Don't ever delete files younger than a month.
+        //
+        int days = 1 + (time(0) - wu.create_time)/86400;
+        if (days<31) days=31;
+
+      return add_antiques_to_list(days);
+    }
+    return 0;
+}
+
+bool do_antique_pass(bool& delete_antiques) {
+    bool did_something=false;
+
+    if (!delete_antiques) return did_something;
+
+    // If any problems appear in deleting antique files
+    // immediately DISABLE this feature.
+    //
+    int howmany = find_antique_files();
+    if (howmany < 0) {
+        delete_antiques=false;
+        return did_something;
+    }
+
+    howmany = delete_antique_files(10);
+    if (howmany < 0) delete_antiques=false;
+    if (howmany > 0) did_something=true;   
+
+    return did_something;
+}
+
+
 int main(int argc, char** argv) {
     int retval;
-    bool asynch = false, one_pass = false, retry_error = false;
+    bool asynch = false, one_pass = false, retry_error = false, delete_antiques = false;
     int i;
 
     check_stop_daemons();
@@ -287,6 +526,14 @@ int main(int argc, char** argv) {
         } else if (!strcmp(argv[i], "-mod")) {
             id_modulus   = atoi(argv[++i]);
             id_remainder = atoi(argv[++i]);
+        } else if (!strcmp(argv[i], "-delete_antiques")) {
+          // If turned on, look for and then delete files in the
+          // upload/ directory which are OLDER than any workunit in
+          // the database.  Such files are ones that were uploaded
+          // by hosts which turned in results far after the
+          // deadline. These files are not deleted from the upload
+          // directory unless you have this option enabled.
+          delete_antiques = true;
         } else {
             log_messages.printf(SCHED_MSG_LOG::CRITICAL, "Unrecognized arg: %s\n", argv[i]);
         }
@@ -326,7 +573,9 @@ int main(int argc, char** argv) {
     while (1) {
         if (!do_pass(retry_error)) {
             if (one_pass) break;
-            sleep(SLEEP_INTERVAL);
+            if (!do_antique_pass(delete_antiques)) {
+                sleep(SLEEP_INTERVAL);
+            }
         }
     }
 }
