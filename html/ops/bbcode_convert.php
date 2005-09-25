5d9e3a61fd636c9c89aac71c020f2b0dcb2b1ca0
David Anderson <davea@ssl.berkeley.edu>
2005-09-25 05:47:48 +0000

    profile picture verification
    svn path=/trunk/boinc/; revision=8169

--- html/ops/bbcode_convert.php
+++ html/ops/bbcode_convert.php
@@ -4,16 +4,17 @@ require_once("../inc/util.inc");
 require_once('../inc/sanitize_html.inc');
 db_init();
 
+set_time_limit(0);
 
 function image_as_bb($text){
-        /* This function depends on sanitized HTML */
-    // Build some regex (should be a *lot* faster)
-    $pattern = '@<img src=\"([^>]+)\">@si'; // Gives us the URL in ${1}...
-    $replacement = '[img]${1}[/img]'; // Turns that URL into a hyperlink
+    // This function depends on sanitized HTML
+
+    $pattern = '@<img(.*) src=\"([^>^"]+)\"([^>]*)>@si';
+    $replacement = '[img]$2[/img]';
     $text = preg_replace($pattern, $replacement, $text);
 
-    $pattern = "@<img src='([^>]+)'>@si"; // Gives us the URL in ${1}...
-    $replacement = '[img]${1}[/img]'; // Turns that URL into a hyperlink
+    $pattern = "@<img(.*) src='([^>^']+)'([^>]*)>@si";
+    $replacement = '[img]$2[/img]';
     $text = preg_replace($pattern, $replacement, $text);
 
     return $text;
@@ -22,15 +23,15 @@ function image_as_bb($text){
 function link_as_bb($text){
         /* This function depends on sanitized HTML */
     // Build some regex (should be a *lot* faster)
-    $pattern = '@<a href=\"([^>]+)\">@si'; // Gives us the URL in ${1}...
-    $replacement = '[url="${1}"]'; // Turns that URL into a hyperlink
+    $pattern = '@<a href=\"([^>]+)\">@si'; // Gives us the URL in $1...
+    $replacement = '[url=$1]'; // Turns that URL into a hyperlink
     $text = preg_replace($pattern, $replacement, $text);
-    $pattern = "@<a href='([^>]+)'>@si"; // Gives us the URL in ${1}...
-    $replacement = '[url="${1}"]'; // Turns that URL into a hyperlink
+    $pattern = "@<a href='([^>]+)'>@si"; // Gives us the URL in $1...
+    $replacement = '[url=$1]'; // Turns that URL into a hyperlink
     $text = preg_replace($pattern, $replacement, $text);
 
-    $pattern = "@</a>@si"; // Gives us the URL in ${1}...
-    $replacement = '[/url]'; // Turns that URL into a hyperlink
+    $pattern = "@</a>@si";
+    $replacement = '[/url]';
     $text = preg_replace($pattern, $replacement, $text);
     return $text;
 }
@@ -59,6 +60,9 @@ function formatting_as_bb($text){
     $in[]="</pre>";$out[]="[/pre]";
 
     $in[]="<br>";$out[]="\n";
+    $in[]="&gt;";$out[]=">";
+    $in[]="&lt;";$out[]="<";
+    $in[]="&amp;";$out[]="&";
 
     return str_replace($in, $out, $text);
 }
@@ -71,31 +75,46 @@ function fix_text($text) {
     return $text;
 }
 
+function fix_post($post) {
+    $text = fix_text($post->content);
+    if ($text != $post->content) {
+        $query = "update post set content = '".mysql_escape_string($text)."' where id=".$post->id;
+        //echo "$post->content\n\n";
+        //echo "$post->thread $query\n\n";
+        $retval = mysql_query($query);
+        if (!$retval) {
+            echo mysql_error();
+            exit();
+        }
+    }
+}
+
 function fix_posts() {
     $start_id = 0; //Set this to something else if you like
     $posts = mysql_query("select * from post where id>$start_id order by id");
     echo mysql_error();
     $i=0;
-    while ($thispost = mysql_fetch_object($posts)){
+    while ($post = mysql_fetch_object($posts)){
         $i++; 
         if ($i%100 == 0) {                      //For every 100 posts
-            echo $thispost->id.". "; flush();   // print out where we are
-            usleep(200000); // Wait a short amount of time (1/5 sec) to give other DB queries a chance
+            echo $post->id.". "; flush();   // print out where we are
+            //usleep(200000);
         }
         
-        if ($thispost->id > $start_id){
-            $text = fix_text($thispost->content);
-            if ($text != $thispost->content) {
-                $query = "update low_priority post set content = '".mysql_escape_string($text)."' where id=".$thispost->id;
-                //echo $query;
-                //exit();
-                mysql_query($query);
-                echo mysql_error();
-            }
+        if ($post->id > $start_id){
+            fix_post($post);
         }
     }
 }
 
+// use this to patch problem cases; hand-edit
+function fix_fix() {
+    $posts = mysql_query("select * from post where id=99");
+    $post = mysql_fetch_object($posts);
+    fix_post($post);
+}
+
 fix_posts();
+//fix_fix();
 
 ?>
