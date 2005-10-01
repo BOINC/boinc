0cd8eab5bfb571ac150bb0d7eda20192e5a4aec2
Bruce Allen <bruce.allen@aei.mpg.de>
2005-10-01 18:30:15 +0000

    Eliminate a few more undefined variables
    svn path=/trunk/boinc/; revision=8401

--- html/user/delete_profile.php
+++ html/user/delete_profile.php
@@ -6,7 +6,7 @@ require_once("../inc/profile.inc");
 db_init();
 $user = get_logged_in_user();
 
-if ($_POST['delete']) {
+if (isset($_POST['delete']) && $_POST['delete']) {
     delete_profile($user);
     exit();
 }
