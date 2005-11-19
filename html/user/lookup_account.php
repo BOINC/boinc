b803f9b43246e6c0a26bdb0a7f9aabd417181508
David Anderson <davea@ssl.berkeley.edu>
2005-11-19 20:33:55 +0000

    fill in blank passwd_hash
    svn path=/trunk/boinc/; revision=8882

--- html/user/lookup_account.php
+++ html/user/lookup_account.php
@@ -15,6 +15,14 @@ $email_addr = get_str("email_addr");
 $passwd_hash = process_user_text(get_str("passwd_hash"));
 
 $user = lookup_user_email_addr($email_addr);
+
+// if no password set, make it the account key
+//
+if (!strlen($user->passwd_hash)) {
+    $user->passwd_hash = md5($user->authenticator.$user->email_addr);
+    mysql_query("update user set passwd_hash='$user->passwd_hash' where id=$user->id");
+}
+
 if (!$user || $user->passwd_hash != $passwd_hash) {
     echo "<account_out>\n";
     echo "<error_num>-161</error_num>\n";
