783aa55c424713fd76318aeaf35bc4df165daac3
David Anderson <davea@ssl.berkeley.edu>
2006-01-09 13:49:54 +0000

    version 5.3.9
    svn path=/trunk/boinc/; revision=9199

--- version.h
+++ version.h
@@ -10,10 +10,10 @@
 #define BOINC_MINOR_VERSION 3
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 8
+#define BOINC_RELEASE 9
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "5.3.8"
+#define BOINC_VERSION_STRING "5.3.9"
 
 #if (defined(_WIN32) || defined(__APPLE__))
 /* Name of package */
@@ -26,13 +26,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 5.3.8"
+#define PACKAGE_STRING "BOINC 5.3.9"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.3.8"
+#define PACKAGE_VERSION "5.3.9"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
