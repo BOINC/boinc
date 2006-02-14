3a92ed9d02d0ad89789139446dcc26fea949ba88
David Anderson <davea@ssl.berkeley.edu>
2006-02-14 22:36:29 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=9469

--- html/user/stats.php
+++ html/user/stats.php
@@ -1,12 +1,20 @@
 <?php
 require_once('../inc/util.inc');
 require_once('../inc/stats_sites.inc');
-page_head('Project statistics');
+page_head('Statistics and leaderboards');
 
 echo "
 <p>
-Statistics from this project
-(and other BOINC-based projects) are available
+Statistics for ".PROJECT.":
+<ul>
+<li><a href=top_users.php>Top participants</a>
+<li><a href=top_hosts.php>Top computers</a>
+<li><a href=team.php>Top teams</a>
+</ul>
+
+<p>
+More detailed statistics for ".PROJECT."
+and other BOINC-based projects are available
 at several web sites:
 ";
 shuffle($stats_sites);
