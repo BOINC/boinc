c73b36279810f8a645017c4a2c45694884c01c38
Charlie Fenton <charlief@example.com>
2006-02-22 11:18:36 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=9524

--- mac_build/setupForBOINC.sh
+++ mac_build/setupForBOINC.sh
@@ -23,7 +23,7 @@
 # Master script to build Universal Binary libraries needed by BOINC:
 # curl-7.15.1, jpeg-6b and wxMac-2.6.2
 #
-# by Charlie Fenton 2/17/06
+# by Charlie Fenton 2/22/06
 #
 # Download these three packages and place them in a common parent 
 # directory with the BOINC source tree.
@@ -42,8 +42,18 @@
 # This script will work even if you have renamed the boinc/ directory
 #
 
+echo ""
+echo "----------------------------------"
+echo "------- BUILD CURL-7.15.1 --------"
+echo "----------------------------------"
+echo ""
 
 pushd ./
+
+cd ../../curl-7.15.1/
+if [  $? -ne 0 ]; then exit 1; fi
+source "${DIRSTACK[1]}/buildcurl.sh"
+if [  $? -ne 0 ]; then exit 1; fi
 
 echo ""
 echo "----------------------------------"
@@ -51,23 +61,12 @@ echo "--------- BUILD JPEG-6B ----------"
 echo "----------------------------------"
 echo ""
 
-cd ../../jpeg-6b/
-if [  $? -ne 0 ]; then exit 1; fi
-source "${DIRSTACK[1]}/buildjpeg.sh"
-if [  $? -ne 0 ]; then exit 1; fi
-
-echo ""
-echo "----------------------------------"
-echo "------- BUILD CURL-7.15.1 --------"
-echo "----------------------------------"
-echo ""
-
 popd
 pushd ./
 
-cd ../../curl-7.15.1/
+cd ../../jpeg-6b/
 if [  $? -ne 0 ]; then exit 1; fi
-source "${DIRSTACK[1]}/buildcurl.sh"
+source "${DIRSTACK[1]}/buildjpeg.sh"
 if [  $? -ne 0 ]; then exit 1; fi
 
 echo ""
