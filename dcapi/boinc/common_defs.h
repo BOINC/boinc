4703bbd0a74800ad5029c610ee9b4fcd5855804e
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-03-23 16:02:26 +0000

    Add basic messaging support
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@395 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/boinc/common_defs.h
+++ dcapi/boinc/common_defs.h
@@ -6,8 +6,18 @@
 extern "C" {
 #endif
 
+/* Prefix of subresult file labels */
 #define SUBRESULT_PFX		"_dc_subresult_"
-#define CKPT_LABEL		"_dc_checkpoint"
+
+/* Logical names of the checkpoint file */
+#define CKPT_LABEL_IN		"_dc_checkpoint_in"
+#define CKPT_LABEL_OUT		"_dc_checkpoint_out"
+
+/* Maximum allowed message length */
+#define MAX_MESSAGE_SIZE	16384
+
+/* Prefix for internal messages between the client-side and master-side DC-API */
+#define DCAPI_MSG_PFX		"__dcapi__"
 
 #ifdef __cplusplus
 }
