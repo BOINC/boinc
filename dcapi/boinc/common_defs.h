87dd679c18dc36b9cd93c624648851ad48823074
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-03-23 12:11:25 +0000

    Add definitions common to the client and server side
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@386 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ dcapi/boinc/common_defs.h
@@ -0,0 +1,16 @@
+/* Definitions common for both the server and client side */
+#ifndef COMMON_DEFS_H
+#define COMMON_DEFS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SUBRESULT_PFX		"_dc_subresult_"
+#define CKPT_LABEL		"_dc_checkpoint"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COMMON_DEFS_H */
