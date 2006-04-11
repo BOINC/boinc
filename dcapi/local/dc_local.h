7a6f9f97116a6db767d942a4cb11a8918222a048
vida <vida@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-04-11 14:02:30 +0000

    Deleted the function prototypes used only in the boinc section.
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@448 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/local/dc_local.h
+++ dcapi/local/dc_local.h
@@ -21,10 +21,7 @@ extern "C" {
 #define CFG_SLEEPINTERVAL	"SleepingInterval"
 #define CFG_INSTANCEUUID        "InstanceUUID"
 
-
 #define DEFAULT_SLEEP_INTERVAL		5
-#define MAX_SUBRESULTS			20
-
 
 /* File types in the working directory */
 typedef enum
@@ -58,16 +55,13 @@ struct _DC_Workunit
 	char			*workdir;
 	int			pid;
 
-	/* Input file definitions. Elements are of type char * */
+	/* Input file definitions. Elements are of type DC_LogicalFile */
 	GList			*input_files;
 	int			num_inputs;
 
 	/* Output file definitions. Elements are of type char * */
 	GList			*output_files;
 	int			num_outputs;
-
-	/* Name of the checkpoint file, if exists (relative to workdir) */
-	char			*ckpt_name;
 };
 
 struct _DC_Result
@@ -105,41 +99,8 @@ DC_PhysicalFile *_DC_createPhysicalFile(const char *label,
 /* De-allocates a physical file descriptor */
 void _DC_destroyPhysicalFile(DC_PhysicalFile *file);
 
+/* Calls a g_hash_table_foreach function for the wu_table */
 int _DC_searchForEvents();
-
-/* Parses the project's config.xml */
-int _DC_parseConfigXML(const char *file);
-
-/* Returns the Boinc upload directory */
-char *_DC_getUploadDir(void);
-
-/* Returns the Boinc download directory */
-char *_DC_getDownloadDir(void);
-
-/* Returns the Boinc upload/download hashing */
-int _DC_getUldlDirFanout(void);
-
-/* Returns the database name */
-char *_DC_getDBName(void);
-
-/* Returns the database host */
-char *_DC_getDBHost(void);
-
-/* Returns the database user ID */
-char *_DC_getDBUser(void);
-
-/* Returns the database password */
-char *_DC_getDBPasswd(void);
-
-/* Initializes the database connection */
-int _DC_initDB(void);
-
-/* Allocates a physical file descriptor */
-DC_PhysicalFile *_DC_createPhysicalFile(const char *label,
-	const char *path);
-
-/* De-allocates a physical file descriptor */
-void _DC_destroyPhysicalFile(DC_PhysicalFile *file);
 
 /* Creates a new DC_Result */
 DC_Result *_DC_createResult(const char *wu_name);
