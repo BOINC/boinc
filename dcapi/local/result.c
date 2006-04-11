dee05dbc8af66a376f2366b29272052cf8f1a5ff
vida <vida@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-04-11 14:21:20 +0000

    Completed the DC_createResult function.
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@452 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/local/result.c
+++ dcapi/local/result.c
@@ -14,7 +14,11 @@
 
 DC_Result *_DC_createResult(const char *wu_name)
 {
+	char *logicalName;
+	GList *l;
 	DC_Result *result;
+	DC_PhysicalFile *file;
+	FILE *f;
 
 	result = g_new0(DC_Result, 1);
 	result->wu = _DC_getWUByName(wu_name);
@@ -25,7 +29,19 @@ DC_Result *_DC_createResult(const char *wu_name)
 		return NULL;
 	}
 
-	// XXX Finish the result correctly
+	for (l = result->wu->output_files; l; l = l->next)
+	{
+		logicalName = g_strdup_printf("%s/%s", result->wu->workdir, (char *)l->data);
+		f = fopen(logicalName, "r");
+		if (f != NULL) /* File exists */
+		{
+			file = _DC_createPhysicalFile((char *)l->data, result->wu->workdir);
+			result->output_files = g_list_append(result->output_files, file);
+			result->num_outputs++;
+			fclose(f);
+		}
+		g_free(logicalName);
+	}
 
 	return result;
 }
@@ -34,7 +50,7 @@ void _DC_destroyResult(DC_Result *result)
 {
 	while (result->output_files)
 	{
-//		_DC_destroyPhysicalFile(result->output_files->data);
+		_DC_destroyPhysicalFile((_DC_PhysicalFile *)result->output_files->data);
 		result->output_files = g_list_delete_link(result->output_files,
 			result->output_files);
 	}
