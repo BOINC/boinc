bee24634171579057ca3f9ab47758928f8bc2c2d
vida <vida@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-04-11 14:09:55 +0000

    Added API functions to client.c and corrected earliers.
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@450 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/local/client.c
+++ dcapi/local/client.c
@@ -1,72 +1,86 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <dc_client.h>
 #include "common_defs.h"
 
-static time_t ckpt_start;
-static time_t ckpt_freq = 10; /* Checkpoint frequency in seconds */
+/********************************************************************
+ * Common API functions
+ */
+
+int DC_getMaxMessageSize(void)
+{
+        return MAX_MESSAGE_SIZE;
+}
+
+int DC_getMaxSubresults(void)
+{
+        return MAX_SUBRESULTS;
+}
+
+DC_GridCapabilities DC_getGridCapabilities(void)
+{
+        return (DC_GridCapabilities)(DC_GC_STDERR | DC_GC_STDOUT);
+}
+
+/********************************************************************
+ * Client API functions
+ */
 
 int DC_init(void)
 {
-  ckpt_start = time(NULL);
-  return DC_OK;
+	return 0;
 }
 
 char *DC_resolveFileName(DC_FileType type, const char *logicalFileName)
 {
-  return (char *)logicalFileName;
+        if (!strcmp(logicalFileName, DC_CHECKPOINT_FILE))
+        {
+		return CKPT_LABEL;
+        }
+	return (char *)logicalFileName;
 }
 
 int DC_sendResult(const char *logicalFileName, const char *path, DC_FileMode fileMode)
 {
-  // not implemented yet!!
+	// not implemented yet!
 
-  return DC_OK;
+	return DC_ERR_NOTIMPL;
 }
 
 int DC_sendMessage(const char *message)
 {
-  // not implemented yet!!
+	// not implemented yet!
 
-  return DC_OK;
+	return DC_ERR_NOTIMPL;
 }
 
 DC_Event *DC_checkEvent(void)
 {
-  // not impl.
+	// not implemented yet!
 
-  return NULL;
+	return NULL;
 }
 
 void DC_destroyEvent(DC_Event *event)
 {
-  // not impl.
+	// not implemented yet!
 }
-
-#if 0
-int DC_timeToCheckpoint(void)
-{
-  time_t current;
-  current = time(NULL);
-  if (current - ckpt_start > ckpt_freq)
-    return 1;
-  return 0;
-}
-#endif
 
 void DC_checkpointMade(const char *fileName)
 {
-  ckpt_start = time(NULL);
+	// Nothing to do with it.
 }
 
 void DC_fractionDone(double fraction)
 {
-
+	// Nothing to do with it.
 }
 
 void DC_finish(int exitcode)
 {
-  exit(exitcode);
+	// XXX Need to send a message to the master side of the DC_API!
+
+	exit(exitcode);
 }
