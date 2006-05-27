f893914fe1b44df7f6430a5509fefd2f77310722
David Anderson <davea@ssl.berkeley.edu>
2006-05-27 05:00:03 +0000

    *** empty log message ***
    svn path=/trunk/boinc_samples/; revision=10211

--- sleeper/sleeper.C
+++ sleeper/sleeper.C
@@ -1,9 +1,22 @@
 #include "util.h"
 #include "boinc_api.h"
 
-int main() {
+int main(int, char**) {
     boinc_init();
     while (1) {
         boinc_sleep(1);
     }
 }
+
+#ifdef _WIN32
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR Args, int WinMode) {
+    LPSTR command_line;
+    char* argv[100];
+    int argc;
+
+    command_line = GetCommandLine();
+    argc = parse_command_line( command_line, argv );
+    return main(argc, argv);
+}
+#endif
