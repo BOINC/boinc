f59ecbbf392f5ff379b3446bd66bfc6ea31c82a7
drdani <drdani@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-06-08 12:20:16 +0000

    functions of result stucture handling
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@658 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ dcapi/condor/condor_result.h
@@ -0,0 +1,24 @@
+/* Local variables: */
+/* c-file-style: "linux" */
+/* End: */
+
+#ifndef _DC_API_CONDOR_RESULT_H_
+#define _DC_API_CONDOR_RESULT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "condor_defs.h"
+
+
+extern DC_Result *_DC_create_result(DC_Workunit *wu);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* End of condor/condor_result.h */
