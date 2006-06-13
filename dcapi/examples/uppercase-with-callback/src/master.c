b92ee9ef1b33819979df3fbbf26f958e65e62a35
gombasg <gombasg@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-06-13 15:34:41 +0000

    Rework master: - Reindent - Unify function name style by converting to lower case - Simplify WU creation by creating fixed-size WUs instead of fixed number of WUs - Add more diagnostic messages - Add command line argument parsing
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@693 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/examples/uppercase-with-callback/src/master.c
+++ dcapi/examples/uppercase-with-callback/src/master.c
@@ -1,238 +1,278 @@
 /**************************************************
  * Uppercase Master program
- * 
+ *
  * Created by: Gabor Vida
  * Fine tuned by: Adam Kornafeld
  * Upgraded to DC-API 2.0 by: Gabor Vida
  *
- * The aim of the program is to process text files 
- * (make all of their characters uppercase) in a Public Resource Computing
- * environment. The program follows the client server model, therefore
- * having a server-side and a client-side part.
+ * The aim of the program is to process text files (make all of their
+ * characters uppercase) in a Public Resource Computing environment. The
+ * program follows the client server model, therefore having a server-side and
+ * a client-side part.
  *
- * This is the server-side part of the program. It creates WorkUnits (WUs) 
- * for a Boinc project using the DC Application Programming Interface.
- * The program takes a text file (master_input) divides it up to x (MAX_N_OF_WUS)
- * parts, creates x WUs of them and submits them into the PRC-infrastructure,
- * where they get processed by the clients. After the clients have returned
- * the results of the WUs, the program assimilates them and constructs a 
- * final output file, by concatenating the output of the results.
+ * This is the server-side part of the program. It creates WorkUnits (WUs) for
+ * a Boinc project using the DC Application Programming Interface.  The program
+ * takes a text file (master_input) divides it to smaller parts of LINES_PER_WU
+ * lines each and submits them into the PRC-infrastructure, where they get
+ * processed by the clients. After the clients have returned the results of the
+ * WUs, the program assimilates them and constructs a final output file, by
+ * concatenating the output of the results.
  *
  *****************************************************/
 
 #include <dc.h>
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+#include <errno.h>
 
-#define	MAX_N_OF_WUS	20
+#define INPUT_LABEL		"in.txt"
+#define OUTPUT_LABEL		"out.txt"
+#define LINES_PER_WU		20
 
-static DC_Workunit *wus[MAX_N_OF_WUS];
-static int max_wus;
-static int assimilatedWUs = 0;
+/* Number of WUs we have created */
+static int created_wus;
+/* Number of results we have received so far */
+static int processed_wus;
 
-/* Create the master_output.txt file, from the minor output files */
-static void CalculateFinalResult(void){
+/* Command line options */
+static const struct option longopts[] =
+{
+	{ "config",	required_argument,	NULL,	'c' },
+	{ "help",	no_argument,		NULL,	'h' },	
+	{ NULL }
+};
 
-    /* We have to open the minor_output files (results) for reading
-     * and write their content in the major_output file.
-     */
+/* Concatenate all results in their original order to form the final output */
+static void calculate_final_result(void)
+{
+	FILE *output;
+	int i;
 
-    int i;
-    FILE *major_output;
-    FILE *minor_output;
-    char minor_filename[256];
-    char buf[512];
-
-    major_output = fopen("master_output.txt", "w");
-    if (major_output == NULL){
-	fprintf(stderr, "Error: Cannot open master_output.txt for writing.\n");
-	fflush(stderr);
-	exit(1);
-    }
-
-    for(i = 0; i < max_wus; i++){
-	snprintf(minor_filename, sizeof(minor_filename), "master_output_%d.txt", i);
-	minor_output = fopen(minor_filename, "r");
-	if (minor_output == NULL){
-	    fprintf(stderr, "Error: Cannot open %s for reading.\n", minor_filename);
-	    fflush(stderr);
-	    exit(1);
+	output = fopen("final_output.txt", "w");
+	if (!output)
+	{
+		DC_log(LOG_ERR, "Cannot open final_output.txt for writing: %s",
+			strerror(errno));
+		exit(1);
 	}
-	while(fgets(buf, sizeof(buf), minor_output)){
-	    fprintf(major_output, buf);
-	}
-	fclose(minor_output);
-    }    
 
-    fclose(major_output);    
+	for(i = 0; i < created_wus; i++)
+	{
+		char result_filename[PATH_MAX], buf[256];
+		FILE *result;
+
+		snprintf(result_filename, sizeof(result_filename),
+			"result_%d.txt", i);
+		result = fopen(result_filename, "r");
+		if (!result)
+			DC_log(LOG_WARNING, "The result for WU %d is missing "
+				"from the final output", i);
+		else
+		{
+			while (fgets(buf, sizeof(buf), result))
+				fprintf(output, buf);
+			fclose(result);
+		}
+	}
+
+	fclose(output);
 }
 
 /* Copies the output files into the master working directory */
-static void cb_assimilate_result(DC_Workunit *wu, DC_Result *result)
+static void process_result(DC_Workunit *wu, DC_Result *result)
 {
-    char syscmd[256];
-    char *output_filename;
-    int i;
+	char *output_filename, *tag, cmd[256];
 
-    output_filename = DC_getResultOutput(result, "out.txt");
+	/* Internal housekeeping of the number of results we have seen */
+	processed_wus++;
 
-    if (!output_filename)
-    {
-	fprintf(stderr, "Master: Error: DC_getResultOutput returned with NULL for 'out.txt'\n");
-	fflush(stderr);
-        exit(1);
-    }
-    fprintf(stdout, "outputfile: %s\n", output_filename);
+	/* Extract our own private identifier */
+	tag = DC_getWUTag(wu);
 
-    for (i = 0; i < max_wus; i++){
-	if (wu == wus[i]){
-	    snprintf(syscmd, 256, "cp %s master_output_%d.txt", output_filename, i);
-	    system(syscmd);
-	    assimilatedWUs++;
-	}
-    }
-
-    DC_destroyWU(wu);
-}
-
-static void cb_assimilate_subresult(DC_Workunit *wu, const char *logicalFileName, const char *path)
-{
-    /* This example application doesn't create subresult */
-    return;
-}
-
-static void cb_assimilate_message(DC_Workunit *wu, const char *message)
-{
-    /* This example application doesn't send message */
-    return;
-}
-
-
-/* Waiting for the results */
-static void WaitForResults(void)
-{
-    while(assimilatedWUs < max_wus){
-	DC_processMasterEvents(0);
-    }
-}
-
-/* split the major input file into MAX_N_OF_WUS minor input files */
-static void CreateInputFiles(void)
-{
-    int i, j;
-    int major_lines_num = 0;
-    div_t minor_lines_num;
-    char buf[512];
-    char minor_filename[256];
-    FILE *major_input;	
-    FILE *minor_input;
-
-    /* Count how many lines the major input file has, and how many the minor files will get */
-    major_input = fopen("master_input.txt", "r");
-    if (major_input == NULL){
-	fprintf(stderr, "Cannot open master_input.txt file.\n");
-	fflush(stderr);
-	exit(1);
-    }
-    while(!feof(major_input)){
-        fgets(buf, sizeof(buf), major_input);
-        major_lines_num++;
-    }
-    fseek(major_input, 0, SEEK_SET);
-
-    if (MAX_N_OF_WUS > major_lines_num) max_wus = major_lines_num;
-    else max_wus = MAX_N_OF_WUS;
-    
-    minor_lines_num = div(major_lines_num, max_wus);
-
-
-    /* Create the minor input files */
-    for (i = 0; i < max_wus; i++){
-	snprintf(minor_filename, sizeof(minor_filename), "master_input_%d.txt", i);
-	minor_input = fopen(minor_filename, "w");
-	if (minor_input == NULL){
-	    fprintf(stderr, "Cannot open %s file.\n", minor_filename);
-	    exit(1);
-	}
-	for(j = 0; j < minor_lines_num.quot; j++){
-	    snprintf(buf, sizeof(buf), "");
-	    fgets(buf, sizeof(buf), major_input);
-	    fprintf(minor_input, buf);
-	}
-	if (i < minor_lines_num.rem){
-	    fgets(buf, sizeof(buf), major_input);
-	    fprintf(minor_input, buf);
-	}
-	fclose(minor_input);
-    }
-
-    fclose(major_input);
-}
-
-static void CreateWork(void)
-{
-    int retval, i;
-    char minor_filename[256];
-
-    CreateInputFiles();
-    for (i = 0; i < max_wus; i++){
-	wus[i] = DC_createWU("uppercase-example", NULL, 0, NULL);
-	if (wus[i] == NULL)
+	if (!result)
 	{
-	    fprintf(stderr, "Cannot create more workunit!\n");
-	    fflush(stderr);
-	    exit(1);
+		DC_log(LOG_WARNING, "Work unit %s has failed", tag);
+		free(tag);
+
+		/* In a real application we would create a new work unit
+		 * instead of the failed one and submit it again. Here we
+		 * just let it fail. */
+		return;
 	}
 
-	snprintf(minor_filename, sizeof(minor_filename), "master_input_%d.txt", i);
-	retval = DC_addWUInput(wus[i], "in.txt", minor_filename, DC_FILE_VOLATILE);
-	if (retval)
+	output_filename = DC_getResultOutput(result, OUTPUT_LABEL);
+	if (!output_filename)
 	{
-	    fprintf(stderr, "Master: Error: Cannot add WU input %s -> in.txt\n", minor_filename);
-	    exit(1);
+		DC_log(LOG_WARNING, "Work unit %s contains no output file",
+			tag);
+		free(tag);
+
+		/* The same comment applies here as above for the no result
+		 * case */
+		return;
 	}
 
-	retval = DC_addWUOutput(wus[i], "out.txt");
-	if (retval)
+	DC_log(LOG_NOTICE, "Work unit %s has completed", tag);
+
+	/* The result file is available only as long as the work unit exists,
+	 * so we have to save it if we want to use it later */
+	snprintf(cmd, sizeof(cmd), "/bin/cp '%s' 'result_%s.txt'",
+		output_filename, tag);
+	system(cmd);
+
+	/* We no longer need the work unit */
+	DC_destroyWU(wu);
+
+	free(tag);
+}
+
+static void create_work(void)
+{
+	FILE *input;
+
+	input = fopen("input.txt", "r");
+	if (!input)
 	{
-	    fprintf(stderr, "Master: Error: Cannot add WU output out.txt\n");
-	    exit(1);
+		DC_log(LOG_ERR, "Failed to open the master input file: %s",
+			strerror(errno));
+		exit(1);
 	}
 
-	retval = DC_submitWU(wus[i]);
-	if (retval)
+	while (!feof(input))
 	{
-	    fprintf(stderr, "Master: Error: DC_submitWU returned with error\n");
-	    exit(1);
-	}
-    }
+		char wu_tag[16], buf[1024];
+		DC_Workunit *wu;
+		FILE *f;
+		int i;
 
+		/* Tag every WU with its index, so we can reconstruct the
+		 * final output file in the correct order */
+		snprintf(wu_tag, sizeof(wu_tag), "%d", created_wus + 1);
+
+		wu = DC_createWU("uppercase-example", NULL, 0, wu_tag);
+		if (!wu)
+		{
+			DC_log(LOG_ERR, "Work unit creation has failed");
+			exit(1);
+		}
+
+		f = fopen("wu-input.txt", "w");
+		if (!f)
+		{
+			DC_log(LOG_ERR, "Failed to create wu-input.txt: %s",
+				strerror(errno));
+			exit(1);
+		}
+
+		for (i = 0; i < LINES_PER_WU; i++)
+		{
+			if (!fgets(buf, sizeof(buf), input))
+				break;
+			fprintf(f, "%s", buf);
+		}
+		fclose(f);
+
+		if (DC_addWUInput(wu, INPUT_LABEL, "wu-input.txt",
+				DC_FILE_VOLATILE))
+		{
+			DC_log(LOG_ERR, "Failed to register WU input file");
+			exit(1);
+		}
+		if (DC_addWUOutput(wu, OUTPUT_LABEL))
+		{
+			DC_log(LOG_ERR, "Failed to register WU output file");
+			exit(1);
+		}
+
+		if (DC_submitWU(wu))
+		{
+			DC_log(LOG_ERR, "Failed to submit WU");
+			exit(1);
+		}
+
+		/* If everything went well, increment the number of WUs we have
+		 * created */
+		++created_wus;
+	}
+	fclose(input);
+}
+
+static void print_help(const char *prog) __attribute__((noreturn));
+static void print_help(const char *prog)
+{
+	const char *p;
+
+	/* Strip the path component if present */
+	p = strrchr(prog, '/');
+	if (p)
+		prog = p;
+
+	printf("Usage: %s {-c|--config} <config file>\n", prog);
+	printf("Available options:\n");
+	printf("\t-c <file>, --config <file>\tUse the specified config. file\n");
+	printf("\t-h, --help\t\t\tThis help text\n");
+	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    /* Initializing DC-API */
-    if (DC_initMaster("master.conf") != DC_OK) {
-        fprintf(stderr, "Master: DC_initMaster failed, exiting.\n");
-	exit(1);
-    }
+	char *config_file = NULL;
+	int c;
 
-    /* Set callback functions */
-    DC_setMasterCb(cb_assimilate_result, cb_assimilate_subresult, cb_assimilate_message);
+	while ((c = getopt_long(argc, argv, "c:h", longopts, NULL)) != -1)
+	{
+		switch (c)
+		{
+			case 'c':
+				config_file = optarg;
+				break;
+			case 'h':
+				print_help(argv[0]);
+				break;
+			default:
+				exit(1);
+		}
+	}
 
-    /* Creating Workunits : splitting up the input file */
-    fprintf(stdout, "Master: Creating %d WorkUnit.\n", MAX_N_OF_WUS);
-    fflush(stdout);
-    CreateWork();
-    fprintf(stdout, "Master: %d WorkUnit created successfully. Waiting for Results...\n", MAX_N_OF_WUS);
-    fflush(stdout);
-    
-    /* Waiting for the returning results */
-    WaitForResults();
+	if (optind != argc)
+	{
+		fprintf(stderr, "Extra arguments on the command line\n");
+		exit(1);
+	}
 
-    /* Got all the results -> building up the final output file */
-    CalculateFinalResult();
+	/* Specifying the config file is mandatory, otherwise the master can't
+	 * run as a BOINC daemon */
+	if (!config_file)
+	{
+		fprintf(stderr, "You must specify the config file\n");
+		exit(1);
+	}
 
-    return 0;
+	/* Initialize the DC-API */
+	if (DC_initMaster(config_file))
+	{
+		fprintf(stderr, "Master: DC_initMaster failed, exiting.\n");
+		exit(1);
+	}
+
+	/* We need the result callback function only */
+	DC_setMasterCb(process_result, NULL, NULL);
+
+	DC_log(LOG_NOTICE, "Master: Creating work units");
+	create_work();
+	DC_log(LOG_NOTICE, "Master: %d work units have been created. Waiting "
+		"for results...\n", created_wus);
+
+	/* Wait for the work units to finish */
+	while (processed_wus < created_wus)
+		DC_processMasterEvents(600);
+
+	/* Got all the results -> build the final output file */
+	calculate_final_result();
+
+	return 0;
 }
