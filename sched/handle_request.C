d81e37f29fa564476f60e50ea3834c70b4e24f9c
Bruce Allen <bruce.allen@aei.mpg.de>
2006-06-17 17:08:02 +0000

    Scheduler: populate sreq.host.credit_per_cpu_sec in case project wants to send different work to slow and fast hosts.  I'm experimenting with the use of this in the locality scheduler.
    (DAVID: please check that the one-line change in handle_request.C is
    globally harmless.)
    
    svn path=/trunk/boinc/; revision=10388

--- sched/handle_request.C
+++ sched/handle_request.C
@@ -450,6 +450,7 @@ static int modify_host_struct(SCHEDULER_REQUEST& sreq, HOST& host) {
     host.fix_nans();
 
     compute_credit_rating(host);
+    sreq.host.credit_per_cpu_sec=host.credit_per_cpu_sec;
     return 0;
 }
 
