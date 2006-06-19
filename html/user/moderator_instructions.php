a209a533119696fb0fe25386bd59256c1e853133
David Anderson <davea@ssl.berkeley.edu>
2006-06-19 17:17:34 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=10399

--- html/user/moderator_instructions.php
+++ html/user/moderator_instructions.php
@@ -12,6 +12,7 @@ one or more of these rules</b>.
 If in doubt, discuss it with other moderators on the email list.
 Please respect the following guidelines:
 <ul>
+
 <li> Don't let your personal opinions
 or moods affect your moderation decisions.
 You may not delete a post simply because
@@ -21,16 +22,14 @@ If you find yourself getting angry, take a break.
 <li> Don't discuss moderation decisions on the forums
 of this or other BOINC projects.
 Use the email list that has been set up for this purpose.
-
-<li> Consider using an anonymous account for moderation,
+Consider using an anonymous account for moderation,
 so that your own forum activities are kept separate from
 your moderation duties.
 
-<li> Don't preemptively delete posts.
-Except for obscene language or pictures,
-avoid deleting a post until someone complains about it
-(and, of course, don't delete it simply because someone
-has complained).
+<li>
+Except for posts containing obscene language or pictures,
+avoid deleting posts until someone complains about them
+(and, of course, don't delete them simply because someone has complained).
 
 </ul>
 ";
