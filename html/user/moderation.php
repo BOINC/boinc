7cd1316854fd6325b05b2caefc1d5bef84656604
David Anderson <davea@ssl.berkeley.edu>
2006-06-19 17:02:50 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=10398

--- html/user/moderation.php
+++ html/user/moderation.php
@@ -16,6 +16,14 @@ The authors of deleted posts will be notified via email.
 If you think a post violates any of the posting rules,
 click the red X on the post and fill out the form;
 moderators will be notified of your complaint.
+<p>
+We try to be as fair as we can when moderating,
+but in a large community of users, with many different viewpoints,
+there will always be some people that will not be happy
+with our moderation decisions.
+While we regret that this happens,
+please realize that we cannot suit all of the people all of the time
+and have to make decisions based on what is best for the forum overall.
 
 <p>
 This moderation policy is set by the ".PROJECT." project.
--- html/user/moderator_instructions.php
+++ html/user/moderator_instructions.php
@@ -7,23 +7,31 @@ page_head("Moderator instructions");
 echo "
 Your job as a moderator is to enforce the following rules:
 ".post_rules()."
-Delete posts or threads only if they clearly violate
-one or more of these rules.
+Delete posts or threads <b>only if they clearly violate
+one or more of these rules</b>.
 If in doubt, discuss it with other moderators on the email list.
 Please respect the following guidelines:
 <ul>
-<li> Don't let your personal feelings and opinions
-affect your moderation decisions.
+<li> Don't let your personal opinions
+or moods affect your moderation decisions.
 You may not delete a post simply because
 you disagree with it or dislike its author.
+If you find yourself getting angry, take a break.
+
 <li> Don't discuss moderation decisions on the forums
 of this or other BOINC projects.
 Use the email list that has been set up for this purpose.
+
+<li> Consider using an anonymous account for moderation,
+so that your own forum activities are kept separate from
+your moderation duties.
+
 <li> Don't preemptively delete posts.
 Except for obscene language or pictures,
 avoid deleting a post until someone complains about it
 (and, of course, don't delete it simply because someone
 has complained).
+
 </ul>
 ";
 page_tail();
