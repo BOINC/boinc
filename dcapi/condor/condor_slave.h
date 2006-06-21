06edac9ba5e63e8a966fccddb7718a990e2c31fd
drdani <drdani@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-06-21 09:57:25 +0000

    definition of CKPT_LABEL
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@732 a7169a2c-3604-0410-bc95-c702d8d87f7a

---
+++ dcapi/condor/condor_slave.h
@@ -0,0 +1,23 @@
+/* Local variables: */
+/* c-file-style: "linux" */
+/* End: */
+
+#ifndef __DC_API_CONDOR_SLAVE_H_
+#define __DC_API_CONDOR_SLAVE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Name of the checkpoint file */
+#define CKPT_LABEL		"dc_checkpoint"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* End of condor/condor_slave.h */
