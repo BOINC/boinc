43ec6e4ef8263417713434fff0277896513ed727
drdani <drdani@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2006-06-21 09:59:56 +0000

    utility fn to remove a directory recursively
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@737 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/condor/condor_utils.c
+++ dcapi/condor/condor_utils.c
@@ -2,8 +2,14 @@
 /* c-file-style: "linux" */
 /* End: */
 
+#include <stdio.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
+#include "dc_common.h"
 #include "condor_utils.h"
 
 
@@ -34,5 +40,54 @@ _DC_mkdir_with_parents(char *dn, mode_t mode)
 	return(res);
 }
 
+
+static int
+_DC_rmsubdir(char *name)
+{
+	DIR *d;
+	struct dirent *de;
+	int i= 0;
+
+	if ((d= opendir(name)) == NULL)
+		return(0);
+	while ((de= readdir(d)))
+	{
+		if (strcmp(de->d_name, ".") != 0 &&
+		    strcmp(de->d_name, "..") != 0)
+		{
+			i+= _DC_rm(de->d_name);
+		}
+	}
+	closedir(d);
+	if (rmdir(name))
+		DC_log(LOG_ERR, "Failed to rmdir %s: %s",
+		       name, strerror(errno));
+	return(i);
+}
+
+int
+_DC_rm(char *name)
+{
+	struct stat s;
+	int i;
+
+	if (!name ||
+	    !*name)
+		return(0);
+	i= lstat(name, &s);
+	if (i != 0)
+		return(0);
+	if (S_ISDIR(s.st_mode) &&
+	    strcmp(name, ".") != 0 &&
+	    strcmp(name, "..") != 0)
+		return(_DC_rmsubdir(name));
+	else
+		{
+			if (remove(name))
+				DC_log(LOG_ERR, "Failed to remove %s: %s",
+				       name, strerror(errno));
+		}
+	return(1);
+}
 
 /* End of condor_utils.c */
