8a26572913230dc9cc405f5c4a607d7ada9231e7
Rom Walton <rwalton@ssl.berkeley.edu>
2006-07-07 19:14:32 +0000

    version 5.7.0
    svn path=/trunk/boinc/; revision=10599

--- version.h
+++ version.h
@@ -7,13 +7,13 @@
 #define BOINC_MAJOR_VERSION 5
 
 /* Minor part of BOINC version number */
-#define BOINC_MINOR_VERSION 5
+#define BOINC_MINOR_VERSION 7
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 4
+#define BOINC_RELEASE 0
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "5.5.4"
+#define BOINC_VERSION_STRING "5.7.0"
 
 #if (defined(_WIN32) || defined(__APPLE__))
 /* Name of package */
@@ -26,13 +26,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 5.5.4"
+#define PACKAGE_STRING "BOINC 5.7.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.5.4"
+#define PACKAGE_VERSION "5.7.0"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
