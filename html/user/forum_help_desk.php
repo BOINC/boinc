05909d9424dd066b8996a291bc11ea18283574e9
Janus B. Kristensen <jbk@visualgroup.dk>
2006-08-22 08:48:40 +0000

    - Link to our new and improved search mechanism instead of google (unless ours doesn't find anything). - A few HTML4.01 trans corrections - Added CVS auto-header
    svn path=/trunk/boinc/; revision=10961

--- html/user/forum_help_desk.php
+++ html/user/forum_help_desk.php
@@ -1,4 +1,5 @@
 <?php
+$cvs_version_tracker[]="\$Id$";  //Generated automatically - do not edit
 
 require_once('../inc/forum.inc');
 require_once('../inc/util.inc');
@@ -8,39 +9,27 @@ db_init();
 
 page_head("Questions and answers");
 
-if (true) {
-    echo "
-        <form action=http://www.google.com/search>
-        <input type=hidden name=domains value=".URL_BASE.">
-        <input type=hidden name=sitesearch value=".URL_BASE."/forum_thread.php>
-        <input class=small name=q size=20>
-        <input type=submit value=Search>
-        </form>
-    ";
-} else {
-    echo "
+echo "
         <p>
-        Do a <a href=forum_text_search_form.php>keyword search</a> of messages.
-        <p>
-    ";
-}
+        Select a topic or do a <br /><a href=forum_search.php>keyword search</a> to find what you are looking for.
+        <p>";
 
 start_forum_table(array("Topic", "# Questions", "Last post"));
 
 $categories = getHelpDeskCategories();
 while ($category = mysql_fetch_object($categories)) {
     echo "
-    <tr class=subtitle>
-        <td class=category colspan=4>", $category->name, "</td>
+    <tr class=\"subtitle\">
+        <td class=\"category\" colspan=\"4\">", $category->name, "</td>
     </tr>
     ";
 
     $forums = getForums($category->id);
     while ($forum = mysql_fetch_object($forums)) {
         echo "
-        <tr class=row1>
+        <tr class=\"row1\">
         <td>
-            <b><a href=forum_forum.php?id=$forum->id>$forum->title</a></b>
+            <b><a href=\"forum_forum.php?id=$forum->id\">$forum->title</a></b>
             <br>", $forum->description, "
         </td>
         <td>", $forum->threads, "</td>
