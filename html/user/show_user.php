847b399a8fb96d9b45284481d1e8fc78dcd2ba29
Janus B. Kristensen <jbk@visualgroup.dk>
2006-08-27 21:03:29 +0000

    - This page is now translation capable and uses the object cache (when accessed throught the web frontend). This should fix the issues where the page appears in another language than the selected one. - Simplification: Now only allow people to do userid lookups using the id of a user instead of the authenticator (when in XML mode people can still use the authenticator)
    svn path=/trunk/boinc/; revision=11017

--- html/user/show_user.php
+++ html/user/show_user.php
@@ -1,54 +1,66 @@
 <?php
-    require_once("../inc/cache.inc");
-    require_once("../inc/util.inc");
-    require_once("../inc/xml.inc");
+/**
+ * This page shows basic account details about a given user
+ * The page can be output as either XML or HTML. If output as xml the user may optionally
+ * also get a list of hosts in case the user provides his/her authenticator.
+ * Object-caching and full-file caching is used to speed up queries for data from this page.
+ */
 
-    $id = get_int("userid", true);
-    $format = get_str("format", true);
-    $auth = get_str("auth", true);
+$cvs_version_tracker[]="\$Id$";  //Generated automatically - do not edit
 
-    require_once("../inc/db.inc");
-    require_once("../inc/user.inc");
-    require_once("../inc/forum.inc");
+require_once("../inc/cache.inc");
+require_once("../inc/util.inc");
+require_once("../inc/xml.inc");
+require_once("../inc/db.inc");
+require_once("../inc/user.inc");
+require_once("../inc/forum.inc");
+
+
+$id = get_int("userid", true);
+$format = get_str("format", true);
+$auth = get_str("auth", true);
+
+if ($format=="xml"){
+    // XML doesn't need translating, so let's use the full-file cache for this
+    $cache_args="userid=".$id."&auth=".$auth;
+    start_cache(USER_PAGE_TTL, $cache_args);
     db_init();
-
-    $show_hosts = false;
-    if ($id) {
-        $user = lookup_user_id($id);
-    } else if (strlen($auth)) {
-        $user = lookup_user_auth($auth);
-        $show_hosts = true;
-    }
-    if (!$user) {
-        if ($format == "xml") {
-            xml_error("no such user ID");
-        } else {
-            error_page("no such user");
-        }
-    }
-
-    if ($id) {
-        $cache_args = "userid=$id";
-        if ($format=="xml") {
-            $cache_args .= "&format=xml";
-        }
-        start_cache(USER_PAGE_TTL, $cache_args);
-    }
-
-    $user = getForumPreferences($user);
-
-    if ($format == "xml") {
-        show_user_xml($user, $show_hosts);
+    if ($auth){
+        $user = lookup_user_auth($auth);	
+	$show_hosts = true;
     } else {
-        page_head("Account data for $user->name");
-        start_table();
-        show_user_summary_public($user);
-        show_profile_link($user);
-        end_table();
-        project_user_summary($user);
-        page_tail(true);
+        $user = lookup_user_id($id);
+	$show_hosts = false;
     }
-    if ($id) {
-        end_cache(USER_PAGE_TTL,$cache_args);
+    if (!$user) xml_error("no such user ID");
+
+    // Output:
+    show_user_xml($user, $show_hosts);
+    end_cache(USER_PAGE_TTL,$cache_args);
+} else {
+    // The webfrontend may be presented in many different languages, so here we cache the data instead
+    $cache_args="userid=".$id;
+    $cached_data = get_cached_data(TOP_PAGES_TTL,$cache_args);
+    if ($cached_data){
+	// We found some old but non-stale data, let's use it
+	$user = unserialize($cached_data);
+    } else {
+	// No data was found, generate new data for the cache and store it
+	db_init();
+        $user = lookup_user_id($id);
+	$user = getForumPreferences($user);
+	set_cache_data(serialize($user),$cache_args);
     }
+    if (!$user->id) error_page("No such user found - please check the ID and try again. If the user is very new it may take a while before it can be displayed.");
+
+    // Output:
+    page_head("Account data for $user->name");
+    start_table();
+    show_user_summary_public($user);
+    show_profile_link($user);
+    end_table();
+    project_user_summary($user);
+    page_tail(true);
+}
+
 ?>
