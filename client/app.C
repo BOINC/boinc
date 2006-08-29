0bdeba142458f985217549eb66073fde4d4c3b5a
Charlie Fenton <charlief@example.com>
2006-08-29 06:30:53 +0000

    *** empty log message ***
    svn path=/branches/boinc_core_release_5_6/boinc/; revision=11033

--- client/app.C
+++ client/app.C
@@ -225,19 +225,20 @@ bool ACTIVE_TASK_SET::poll() {
         gstate.set_client_state_dirty("ACTIVE_TASK_SET::poll");
     }
 
+#ifdef _WIN32
 	if (log_flags.mem_usage_debug) {
 		static double last_mem_time=0;
 		if (gstate.now - last_mem_time > 10) {
 			last_mem_time = gstate.now;
 			vector<PROCINFO> piv;
 			PROCINFO pi;
-//			procinfo_setup(piv);
+			procinfo_setup(piv);
 			for (i=0; i<active_tasks.size(); i++) {
 				ACTIVE_TASK* atp = active_tasks[i];
 				if (atp->task_state == PROCESS_EXECUTING) {
 					memset(&pi, 0, sizeof(pi));
 					pi.id = atp->pid;
-//					procinfo_app(pi, piv);
+					procinfo_app(pi, piv);
 					msg_printf(NULL, MSG_INFO, "%s: RAM %dKB, page %dKB, user %f, kernel %f",
 						atp->result->name,
 						(int)(pi.working_set_size/1024), (int)(pi.swap_size/1024),
@@ -245,13 +246,14 @@ bool ACTIVE_TASK_SET::poll() {
 					);
 				}
 			}
-//			procinfo_other(pi, piv);
+			procinfo_other(pi, piv);
 			msg_printf(NULL, MSG_INFO, "All others: RAM %dKB, page %dKB, user %f, kernel %f",
 				(int)(pi.working_set_size/1024), (int)(pi.swap_size/1024),
 				pi.user_time, pi.kernel_time
 			);
 		}
 	}
+#endif
 
     return action;
 }
