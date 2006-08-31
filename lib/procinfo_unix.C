221f40274e8d9af23da3ae6b4c79f740dc54fd24
Walt Gribben <waltg@speakeasy.net>
2006-08-31 18:52:26 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=11050

--- lib/procinfo_unix.C
+++ lib/procinfo_unix.C
@@ -1,11 +1,31 @@
+// Berkeley Open Infrastructure for Network Computing
+// http://boinc.berkeley.edu
+// Copyright (C) 2006 University of California
+//
+// This is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation;
+// either version 2.1 of the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// To view the GNU Lesser General Public License visit
+// http://www.gnu.org/copyleft/lesser.html
+// or write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 #include "config.h"
 #include <stdio.h>
 
-#if HAVE_SYS_TYPES_H
+#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
 
-#if HAVE_DIRENT_H
+#ifdef HAVE_DIRENT_H
 #include <dirent.h>
 #endif
 
@@ -118,7 +138,7 @@ int PROC_STAT::parse(char* buf) {
 //
 int procinfo_setup(vector<PROCINFO>& pi) {
 
-#if HAVE_DIRENT_H
+#ifdef HAVE_DIRENT_H
     DIR *dir;
     dirent *piddir;
     FILE* fd;
