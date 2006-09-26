2074fda9898c94be8e6ef6d000201b8244c74864
Charlie Fenton <charlief@example.com>
2006-09-26 00:11:44 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=11191

--- client/main.C
+++ client/main.C
@@ -770,8 +770,13 @@ int main(int argc, char** argv) {
             "File ownership or permissions are set in a way that\n"
             "does not allow sandboxed execution of BOINC applications.\n"
             "To use BOINC anyway, use the -insecure command line option.\n"
-            "To change ownership/permission, reinstall BOINC or run\n"
-            " the shell script Mac_SA_Secure.sh.\n"
+            "To change ownership/permission, reinstall BOINC"
+#ifdef __APPLE__
+            " or run\n the shell script Mac_SA_Secure.sh"
+#elif defined linux
+            " or run\n the shell script secure.sh"
+#endif
+            ".\n"
         );
         return ERR_USER_PERMISSION;
     }
