3263a5d3031e6bb7a2c021cabb3c64b3f5e25a3b
Rytis Slatkevičius <rytis.s@gmail.com>
2006-11-02 19:34:02 +0000

    Update team_display.php to use object cache instead of full page cache to fix cache bug with team founder transfer
    svn path=/trunk/boinc/; revision=11449

--- html/user/team_display.php
+++ html/user/team_display.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../inc/cache.inc");
 require_once("../inc/util.inc");
+require_once("../inc/db.inc");
+require_once("../inc/team.inc");
 
 if (isset($_GET["sort_by"])) {
     $sort_by = $_GET["sort_by"];
@@ -13,19 +15,23 @@ if (!$offset) $offset=0;
 $teamid = get_int("teamid");
 
 if ($offset > 1000) {
-	error_page("Limit exceeded:  Only displaying the first 1000 members.");
+    error_page("Limit exceeded: Only displaying the first 1000 members.");
 }
 
-$cache_args = "teamid=$teamid&sort_by=$sort_by&offset=$offset";
-start_cache(TEAM_PAGE_TTL, $cache_args);
-
-require_once("../inc/db.inc");
-require_once("../inc/team.inc");
-
 db_init();
+
+// We can only cache team object, as the page is customised to the current user
 $user = get_logged_in_user(false);
 
-$team = lookup_team($teamid);
+$cache_args = "teamid=$teamid&sort_by=$sort_by&offset=$offset";
+$cached_data = get_cached_data(TEAM_PAGE_TTL, $cache_args);
+if ($cached_data) {
+    $team = unserialize($cached_data);
+} else {
+    $team = lookup_team($teamid);
+    set_cache_data(serialize($team), $cache_args);
+}
+
 if (!$team) {
     error_page("No such team");
 }
@@ -33,6 +39,5 @@ if (!$team) {
 display_team_page($team, $offset, $sort_by);
 
 page_tail(true);
-end_cache(TEAM_PAGE_TTL,$cache_args);
 
 ?>
