b9a36f4e875ad37c7d12fffaf5ce271602546e50
Walt Gribben <waltg@speakeasy.net>
2007-01-09 20:09:20 +0000

    *** empty log message ***
    svn path=/trunk/boinc/; revision=11814

--- lib/procinfo_unix.C
+++ lib/procinfo_unix.C
@@ -201,7 +201,9 @@ void add_child_totals(PROCINFO& pi, vector<PROCINFO>& piv, int pid, int rlvl) {
             pi.kernel_time += p.kernel_time;
             pi.user_time += p.user_time;
             pi.swap_size += p.swap_size;
-            pi.working_set_size += p.working_set_size;
+            if (p.working_set_size > pi.working_set_size) {
+                pi.working_set_size = p.working_set_size;
+            }
             p.is_boinc_app = true;
             // look for child process of this one
             add_child_totals(pi, piv, p.id, rlvl+1); // recursion - woo hoo!
