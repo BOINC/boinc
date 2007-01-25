3d839043524d97c588418ad0feabcc1f68c35531
drotos <drotos@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2007-01-25 12:37:36 +0000

    handling of configuration parameters
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@1022 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/local/local_common.h
+++ dcapi/local/local_common.h
@@ -60,6 +60,39 @@ static const char *const workdir_prefixes[] =
         "in_", "out_", "checkpoint", "dc_"
 };
 
+
+/****************************************************************************/
+
+#define CLIENT_CONFIG_NAME	"_dcapi_configfile.txt"
+
+
+enum _DC_e_param {
+	cfg_client_message_box= 0,
+	cfg_master_message_box,
+	cfg_subresults_box,
+	cfg_management_box,
+	cfg_architectures,
+	cfg_submit_file,
+	cfg_executable,
+	cfg_leave_files,
+	cfg_condor_log,
+	cfg_checkpoint_file,
+	cfg_output_cache,
+	cfg_condor_submit_template,
+	cfg_nuof
+};
+
+struct _DC_s_param {
+	char *name;
+	char *def;
+	char *lvalue;
+	char *gvalue;
+};
+
+
+extern struct _DC_s_param _DC_params[cfg_nuof];
+
+extern void _DC_init_common(void);
 #define _DCAPI_MSG_MESSAGE      "message"
 #define _DCAPI_MSG_LOGICAL      "logical_name"
 #define _DCAPI_MSG_COMMAND      "command"
@@ -71,6 +104,7 @@ static const char *const workdir_prefixes[] =
 #define _DCAPI_CMD_RESUME       "resume"
 
 #define _DCAPI_SIG_SERIALIZED   "serialized"
+
 
 #ifdef __cplusplus
 }
