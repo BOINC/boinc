6b8fac28a69486f9ce43d3443f7b664410a558ec
drotos <drotos@a7169a2c-3604-0410-bc95-c702d8d87f7a>
2007-02-05 08:58:33 +0000

    do not use dc_internal.h in slave side move _DC_state_name() to master side
    git-svn-id: svn+ssh://cvs.lpds.sztaki.hu/var/lib/svn/szdg/dcapi/trunk@1032 a7169a2c-3604-0410-bc95-c702d8d87f7a

--- dcapi/condor/condor_utils.h
+++ dcapi/condor/condor_utils.h
@@ -37,8 +37,6 @@ extern int _DC_nuof_messages(char *box, char *name);
 extern char *_DC_message_name(char *box, char *name);
 extern char *_DC_read_message(char *box, char *name, int del_msg);
 
-/* remove it from here, it is a master only utility! */
-extern char *_DC_state_name(DC_WUState state);
 extern char *_DC_quote_string(char *str);
 extern char *_DC_unquote_string(char *str);
 
