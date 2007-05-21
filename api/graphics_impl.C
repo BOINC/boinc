6f7f1bfd5c826f7302cd94076ebbd535cc46c879
David Anderson <davea@ssl.berkeley.edu>
2007-05-21 18:19:28 +0000

    - API: don't include config.h in a few places
    	api/
    		graphics_impl.C
    		texture.C
    		tgalib.C
    
    svn path=/trunk/boinc/; revision=12705

--- api/graphics_impl.C
+++ api/graphics_impl.C
@@ -23,8 +23,6 @@
 // and thus can be put in a shared library,
 // separate from the application.
 
-#include "config.h"
-
 #if defined(_WIN32) && !defined(__STDWX_H__) && !defined(_BOINC_WIN_) && !defined(_AFX_STDAFX_H_)
 #include "boinc_win.h"
 #endif
--- api/tgalib.C
+++ api/tgalib.C
@@ -1,5 +1,3 @@
-#include "config.h"
-
 #include "tgalib.h"
 
 tImageTGA *LoadTGA(const char *filename)
