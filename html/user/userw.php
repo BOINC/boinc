1d89096c3fdc0a78058ea1ab3085d7e7febdec5e
Rytis Slatkevičius <rytis.s@gmail.com>
2007-07-06 17:26:26 +0000

    Fix #268: use fullpage cache for WAP stats, cache interval USER_PAGE_TTL (default 60 minutes)
    svn path=/trunk/boinc/; revision=13111

--- html/user/userw.php
+++ html/user/userw.php
@@ -3,14 +3,20 @@
     require_once("../inc/userw.inc");
     require_once("../inc/db.inc");
     require_once("../inc/wap.inc");
- 
+    require_once("../inc/cache.inc");
+    
     $userid = get_int('id');
+    
+    $cache_args = "userid=".$userid;
+    start_cache(USER_PAGE_TTL, $cache_args);
+    
     db_init();
-
+    
     $user = lookup_user_id($userid);
     if (!$user) {
-	sleep(5);
         error_page("No such user");
     }
     show_user_wap($user);
+    
+    end_cache(USER_PAGE_TTL, $cache_args);
 ?>
