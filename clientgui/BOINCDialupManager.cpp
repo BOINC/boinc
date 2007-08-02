d2ca33b9e7aaf0d983e328f18101b5cbe6063517
Charlie Fenton <charlief@example.com>
2007-08-02 08:11:29 +0000

    MGR: Dialup Manager updates current preferences from client before using them
    svn path=/trunk/boinc/; revision=13261

--- clientgui/BOINCDialupManager.cpp
+++ clientgui/BOINCDialupManager.cpp
@@ -293,6 +293,7 @@ int CBOINCDialUpManager::Connect() {
     wxTimeSpan          tsLastDialupRequest;
     int                 iAnswer;
     wxString            strDialogMessage = wxEmptyString;
+    GLOBAL_PREFS_MASK   mask;
 
 
     wxASSERT(pDoc);
@@ -310,6 +311,8 @@ int CBOINCDialUpManager::Connect() {
 
         if(pFrame->GetDialupConnectionName().size()) {
             // We have a valid connection name that we can dial.
+            // Update current working preferences (including any overrides) from client
+            pDoc->rpc.get_global_prefs_working_struct(pDoc->state.global_prefs, mask);
             if(pDoc->state.global_prefs.confirm_before_connecting) {
                 // %s is the project name
                 //    i.e. 'BOINC', 'GridRepublic'
@@ -485,6 +488,7 @@ int CBOINCDialUpManager::Disconnect() {
     CBOINCBaseFrame*    pFrame = wxGetApp().GetFrame();
     CSkinAdvanced*      pSkinAdvanced = wxGetApp().GetSkinManager()->GetAdvanced();
     wxString            strDialogMessage = wxEmptyString;
+    GLOBAL_PREFS_MASK   mask;
 
 
     wxASSERT(pDoc);
@@ -497,6 +501,8 @@ int CBOINCDialUpManager::Disconnect() {
 
     wxLogTrace(wxT("Function Status"), wxT("CBOINCDialUpManager::Disconnect - Connection Detected, disconnect requested via the CC."));
 
+    // Update current working preferences (including any overrides) from client
+    pDoc->rpc.get_global_prefs_working_struct(pDoc->state.global_prefs, mask);
     if (pDoc->state.global_prefs.hangup_if_dialed) {
         wxLogTrace(wxT("Function Status"), wxT("CBOINCDialUpManager::Disconnect - Connection Detected, Don't need the network, Hanging up."));
         if (m_pDialupManager->HangUp()) {
