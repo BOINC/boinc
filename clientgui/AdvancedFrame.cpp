269a23247c0fe1304747e8db7ad50d765d335bd1
Charlie Fenton <charlief@example.com>
2007-08-18 00:22:47 +0000

    MGR: Disable Simple GUI if the screen height < 600 (instead of== 480)
    svn path=/branches/boinc_core_release_5_10/; revision=13410

--- clientgui/AdvancedFrame.cpp
+++ clientgui/AdvancedFrame.cpp
@@ -371,6 +371,11 @@ bool CAdvancedFrame::CreateMenu() {
         _("Display the simple BOINC graphical interface.")
     );
     
+    // Screen too small?
+    if (wxGetDisplaySize().GetHeight() < 600) {
+        menuView->Enable(ID_FILESWITCHGUI, false);
+    }
+    
     menuView->Check(ID_VIEWACCESSIBLE, VIEW_LIST == m_iDisplayViewType);
     menuView->Check(ID_VIEWGRID, VIEW_GRID == m_iDisplayViewType);
 
@@ -1307,7 +1312,10 @@ void CAdvancedFrame::OnSwitchView(wxCommandEvent& event) {
 void CAdvancedFrame::OnSwitchGUI(wxCommandEvent& WXUNUSED(event)) {
     wxLogTrace(wxT("Function Start/End"), wxT("CAdvancedFrame::OnSwitchGUI - Function Begin"));
 
-    wxGetApp().SetActiveGUI(BOINC_SIMPLEGUI, true);
+    // Screen too small?
+    if (wxGetDisplaySize().GetHeight() >= 600) {
+        wxGetApp().SetActiveGUI(BOINC_SIMPLEGUI, true);
+    }
 
     wxLogTrace(wxT("Function Start/End"), wxT("CAdvancedFrame::OnSwitchGUI - Function End"));
 }
--- clientgui/BOINCGUIApp.cpp
+++ clientgui/BOINCGUIApp.cpp
@@ -280,12 +280,12 @@ bool CBOINCGUIApp::OnInit() {
     // Is there a condition in which the Simple GUI should not be used?
     if (BOINC_SIMPLEGUI == m_iGUISelected) {
 
-        // Screen to small?
-        if ((wxGetDisplaySize().GetWidth() == 640) && (wxGetDisplaySize().GetHeight() == 480)) {
+        // Screen too small?
+        if (wxGetDisplaySize().GetHeight() < 600) {
             m_iGUISelected = BOINC_ADVANCEDGUI;
         }
 
-        // Screen reader in use?
+    // Screen reader in use?
 #ifdef __WXMSW__
         BOOL bScreenReaderEnabled = false;
         SystemParametersInfo(SPI_GETSCREENREADER, NULL, &bScreenReaderEnabled, NULL);
