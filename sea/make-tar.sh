92cf1b334dd248120ea283d2ab8f4fb8e2665157
Rom Walton <rwalton@ssl.berkeley.edu>
2007-08-23 17:03:32 +0000

    Reduce the number of warnings
    svn path=/trunk/boinc/; revision=13440

--- sea/make-tar.sh
+++ sea/make-tar.sh
@@ -3,7 +3,7 @@
 rm -fr BOINC/.CVS
 mkdir BOINC
 mkdir BOINC/locale
-find ../locale/client -name '*.mo' | cut -d '/' -f 4 | awk '{print "BOINC/locale/"$0}' | xargs mkdir
+find ../locale/client -name '*.mo' | cut -d '/' -f 4 | awk '{print "BOINC/locale/"$0}' | xargs mkdir -p 
 find ../locale/client -name '*.mo' | cut -d '/' -f 4,5 | awk '{print "cp \"../locale/client/"$0"\" \"BOINC/locale/"$0"\""}' | sh
 tar cvf sea.tar BOINC
 
