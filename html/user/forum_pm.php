31901159fc6fe439d5d7be34527e840276ef441d
Rytis Slatkevičius <rytis.s@gmail.com>
2007-08-25 15:04:29 +0000

    Make private message functions translatable, using newly added string-based translation method. Update English and Lithuanian languages.
    svn path=/trunk/boinc/; revision=13460

--- html/user/forum_pm.php
+++ html/user/forum_pm.php
@@ -1,5 +1,4 @@
 <?php
-
 $cvs_version_tracker[]="\$Id$";
 
 require_once("../inc/forum.inc");
@@ -16,19 +15,19 @@ if ($action == null) { $action = "inbox"; }
 $logged_in_user = get_logged_in_user();
 
 if ($action == "inbox") {
-    page_head("Private messages : Inbox");
+    page_head(tra("Private messages")." : ".tra("Inbox"));
     pm_header();
     
     if (get_int("sent", true) == 1) {
-        echo "<div class=\"notice\">Your message has been sent.</div>\n";
+        echo "<div class=\"notice\">".tra("Your message has been sent.")."</div>\n";
     }
     
     $query = mysql_query("SELECT * FROM private_messages WHERE userid=".$logged_in_user->id." ORDER BY date DESC");
     if (mysql_num_rows($query) == 0) {
-        echo "You have no private messages.";
+        echo tra("You have no private messages.");
     } else {
         start_table();
-        print "<tr><th>Subject</th><th>Sender</th><th>Date</th></tr>\n";
+        print "<tr><th>".tra("Subject")."</th><th>".tra("Sender")."</th><th>".tra("Date")."</th></tr>\n";
         while ($row = mysql_fetch_object($query)) {
             print "<tr>\n";
             $subject = "<a href=\"forum_pm.php?action=read&id=".$row->id."\">".$row->subject."</a>";
@@ -47,24 +46,24 @@ if ($action == "inbox") {
     $id = get_int("id");
     $message = mysql_query("SELECT * FROM private_messages WHERE id=".$id." AND userid=".$logged_in_user->id);
     if (mysql_num_rows($message) == 0) {
-        error_page("No such message.");
+        error_page(tra("No such message"));
     } else {
         $message = mysql_fetch_object($message);
-        page_head("Private messages : ".$message->subject);
+        page_head(tra("Private messages")." : ".$message->subject);
         pm_header();
         
         $options = new output_options;
         
         start_table();
-        echo "<tr><th>Subject</th><td>".$message->subject."</td></tr>";
-        echo "<tr><th>Sender</th><td>".user_links(get_user_from_id($message->senderid))." 
-    <a href=\"forum_pm.php?action=block&amp;id=".$message->senderid."\"><img src=\"img/report_post.png\" width=\"9\" height=\"9\" alt=\"Block user\"></a></td></tr>";
-        echo "<tr><th>Date</th><td>".time_str($message->date)."</td></tr>";
-        echo "<tr><th>Message</th><td>".output_transform($message->content, $options)."</td></tr>";
+        echo "<tr><th>".tra("Subject")."</th><td>".$message->subject."</td></tr>";
+        echo "<tr><th>".tra("Sender")."</th><td>".user_links(get_user_from_id($message->senderid))." 
+    <a href=\"forum_pm.php?action=block&amp;id=".$message->senderid."\"><img src=\"img/report_post.png\" width=\"9\" height=\"9\" alt=\"".tra("Block user")."\"></a></td></tr>";
+        echo "<tr><th>".tra("Date")."</th><td>".time_str($message->date)."</td></tr>";
+        echo "<tr><th>".tra("Message")."</th><td>".output_transform($message->content, $options)."</td></tr>";
         echo "<tr><td class=\"pm_footer\"></td><td>\n";
-        echo "<a href=\"forum_pm.php?action=delete&amp;id=$id\">Delete</a>\n";
-        echo " | <a href=\"forum_pm.php?action=new&amp;replyto=$id\">Reply</a>\n";
-        echo " | <a href=\"forum_pm.php?action=inbox\">Inbox</a>\n";
+        echo "<a href=\"forum_pm.php?action=delete&amp;id=$id\">".tra("Delete")."</a>\n";
+        echo " | <a href=\"forum_pm.php?action=new&amp;replyto=$id\">".tra("Reply")."</a>\n";
+        echo " | <a href=\"forum_pm.php?action=inbox\">".tra("Inbox")."</a>\n";
         end_table();
         
         if ($message->opened == 0) {
@@ -87,22 +86,22 @@ if ($action == "inbox") {
         if (mysql_num_rows($message) == 1) {
             $message = mysql_fetch_object($message);
             $sender = lookup_user_id($message->senderid);
-            page_head("Private messages : Really delete?");
+            page_head(tra("Private messages")." : ".tra("Really delete?"));
             pm_header();
-            echo "<div>Are you sure you want to delete the message with subject \"".$message->subject."\" (sent by ".$sender->name." on ".time_str($message->date).")?</div>\n";
+            echo "<div>".tra("Are you sure you want to delete the message with subject &quot;%1&quot; (sent by %2 on %3)?", $message->subject, $sender->name, time_str($message->date))."</div>\n";
             echo "<form action=\"forum_pm.php\" method=\"post\">\n";
             echo form_tokens($logged_in_user->authenticator);
             echo "<input type=\"hidden\" name=\"action\" value=\"delete\">\n";
             echo "<input type=\"hidden\" name=\"confirm\" value=\"1\">\n";
             echo "<input type=\"hidden\" name=\"id\" value=\"$id\">\n";
-            echo "<input type=\"submit\" value=\"Yes, delete\">\n";
+            echo "<input type=\"submit\" value=\"".tra("Yes, delete")."\">\n";
             echo "</form>\n";
             echo "<form action=\"forum_pm.php\" method=\"post\">\n";
             echo "<input type=\"hidden\" name=\"action\" value=\"inbox\">\n";
-            echo "<input type=\"submit\" value=\"No, cancel\">\n";
+            echo "<input type=\"submit\" value=\"".tra("No, cancel")."\">\n";
             echo "</form>\n";
         } else {
-            error_page("No such message.");
+            error_page(tra("No such message."));
         }
     }
 } elseif ($action == "send") {
@@ -113,11 +112,11 @@ if ($action == "inbox") {
     $subject = stripslashes(post_str("subject", true));
     $content = stripslashes(post_str("content", true));
     
-    if (post_str("preview", true) == "Preview") {
+    if (post_str("preview", true) == tra("Preview")) {
         pm_create_new();
     }
     if (($to == null) || ($subject == null) || ($content == null)) {
-        pm_create_new("You need to fill all fields to send a private message");
+        pm_create_new(tra("You need to fill all fields to send a private message"));
     } else {
         akismet_check(new User($logged_in_user->id), $content);
         $to = str_replace(", ", ",", $to); // Filter out spaces after separator
@@ -132,14 +131,14 @@ if ($action == "inbox") {
                 $userid = $user[0];
                 $user = lookup_user_id($userid);
                 if ($user == null) {
-                    pm_create_new("Could not find user with id $userid");
+                    pm_create_new(tra("Could not find user with id %1", $userid));
                 }
             } else {
                 $user = lookup_user_name($username);
                 if ($user == null) {
-                    pm_create_new("Could not find user $username");
+                    pm_create_new(tra("Could not find user with username %1", $username));
                 } elseif ($user == -1) { // Non-unique username
-                    pm_create_new("User $username is not unique; you will have to use user ID");
+                    pm_create_new(tra("%1 is not a unique username; you will have to use user ID", $username));
                 }
             }
             $ignorelist = mysql_query("SELECT ignorelist FROM forum_preferences WHERE userid=".$user->id);
@@ -147,7 +146,7 @@ if ($action == "inbox") {
             $ignorelist = $ignorelist->ignorelist;
             $ignorelist = explode("|", $ignorelist);
             if (in_array($logged_in_user->id, $ignorelist)) {
-                pm_create_new("User ".$user->name." (ID: ".$user->id.") is not accepting private messages from you.");
+                pm_create_new(tra("User %1 (ID: %2) is not accepting private messages from you.", $user->name, $user->id));
             }
             if ($userids[$user->id] == null) {
                 $userlist[] = $user;
@@ -167,20 +166,20 @@ if ($action == "inbox") {
     $user = mysql_query("SELECT name FROM user WHERE id=$id");
     if ($user) {
         $user = mysql_fetch_object($user);
-        page_head("Really block ".$user->name."?");
-        echo "<div>Are you really sure you want to block user ".$user->name." from sending you private messages?<br>\n";
-        echo "Please have in mind that you can only block a limited amount of users.</div>\n";
-        echo "<div>Once the user has been blocked you can unblock it using forum preferences page.</div>\n";
+        page_head(tra("Really block %1?", $user->name));
+        echo "<div>".tra("Are you really sure you want to block user %1 from sending you private messages?", $user->name)."<br>\n";
+        echo tra("Please note that you can only block a limited amount of users.")."</div>\n";
+        echo "<div>".tra("Once the user has been blocked you can unblock it using forum preferences page.")."</div>\n";
         
         echo "<form action=\"forum_pm.php\" method=\"POST\">\n";
         echo form_tokens($logged_in_user->authenticator);
         echo "<input type=\"hidden\" name=\"action\" value=\"confirmedblock\">\n";
         echo "<input type=\"hidden\" name=\"id\" value=\"$id\">\n";
-        echo "<input type=\"submit\" value=\"Add user to filter\">\n";
-        echo "<a href=\"forum_pm.php?action=inbox\">No, cancel</a>\n";
+        echo "<input type=\"submit\" value=\"".tra("Add user to filter")."\">\n";
+        echo "<a href=\"forum_pm.php?action=inbox\">".tra("No, cancel")."</a>\n";
         echo "</form>\n";
     } else {
-        error_page("No such user");
+        error_page(tra("No such user"));
     }
 } elseif ($action == "confirmedblock") {
     check_tokens($logged_in_user->authenticator);
@@ -189,10 +188,10 @@ if ($action == "inbox") {
     $blocked = new User($id);
     $user->addIgnoredUser($blocked);
     
-    page_head("User ".$blocked->getName()." blocked");
+    page_head(tra("User %1 blocked", $blocked->getName()));
     
-    echo "<div>User ".$blocked->getName()." has been blocked from sending you private messages. To unblock, visit
-        <a href=\"edit_forum_preferences_form.php\">message board preferences</a>.</div>\n";
+    echo "<div>".tra("User %1 has been blocked from sending you private messages.", $blocked->getName())."\n";
+    echo tra("To unblock, visit %1message board preferences%2", "<a href=\"edit_forum_preferences_form.php\">", "</a>")."</div>\n";
 }
 
 page_tail();
