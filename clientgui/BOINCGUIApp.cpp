e0424f33650df9628f5d93ea828856a0ca2c7f01
Charlie Fenton <charlief@example.com>
2007-09-03 01:16:11 +0000

    Mac MGR: if  BOINC is running as a service / daemon, connect to BOINC Client before creating menubar icon menu to work around unresponsive icon bug
    svn path=/trunk/boinc/; revision=13523

--- clientgui/BOINCGUIApp.cpp
+++ clientgui/BOINCGUIApp.cpp
@@ -295,6 +295,15 @@ bool CBOINCGUIApp::OnInit() {
 #endif
     }
 
+#ifdef __WXMAC__
+    // When running BOINC Client as a daemon / service, the menubar icon is sometimes 
+    // unresponsive to mouse clicks if we create it before connecting to the Client.
+    bool running_as_daemon = boinc_file_exists("/Library/StartupItems/boinc/boinc") && 
+                        (TickCount() < (120*60));     // If system has been up for less than 2 minutes
+
+    if (running_as_daemon) StartupBOINCCore();
+#endif
+
     // Initialize the task bar icon
 #if defined(__WXMSW__) || defined(__WXMAC__)
 	m_pTaskBarIcon = new CTaskBarIcon(
@@ -312,8 +321,10 @@ bool CBOINCGUIApp::OnInit() {
     // Startup the System Idle Detection code
     ClientLibraryStartup();
 
-    // Detect if we need to start the BOINC Core Client due to configuration
-    StartupBOINCCore();
+#ifdef __WXMAC__
+    if (! running_as_daemon)
+#endif
+        StartupBOINCCore();
 
 #ifdef __WXMAC__
     s_bSkipExitConfirmation = false;
