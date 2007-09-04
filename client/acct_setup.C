340b55ece870a9b054fded5cad5b1b4b1ad68542
Charlie Fenton <charlief@example.com>
2007-09-04 11:40:46 +0000

    Display new version available message in red
    svn path=/trunk/boinc/; revision=13539

--- client/acct_setup.C
+++ client/acct_setup.C
@@ -250,11 +250,11 @@ void GET_CURRENT_VERSION_OP::handle_reply(int http_op_retval) {
         while (fgets(buf, 256, f)) {
             if (match_tag(buf, "<version>")) {
                 if (parse_version(f, new_version)) {
-                    msg_printf(0, MSG_INFO,
+                    msg_printf(0, MSG_USER_ERROR,
                         "A new version of BOINC (%s) is available for your computer",
                         new_version
                     );
-                    msg_printf(0, MSG_INFO,
+                    msg_printf(0, MSG_USER_ERROR,
                         "Visit http://boinc.berkeley.edu/download.php to get it."
                     );
                     gstate.newer_version = string(new_version);
