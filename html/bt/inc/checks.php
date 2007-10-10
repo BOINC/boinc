d6d1b2bf7392e183662d63874e0210a83a2918bf
Janus B. Kristensen <jbk@visualgroup.dk>
2007-10-10 21:04:20 +0000

    Added check for magic quotes
    svn path=/trunk/boinc/; revision=13824

--- html/bt/inc/checks.php
+++ html/bt/inc/checks.php
@@ -15,4 +15,17 @@ function isIPBanned($ip){
     }
 } 
  
+if (get_magic_quotes_gpc()) {
+    function stripslashes_deep($value) {
+        $value = is_array($value) ?
+        array_map('stripslashes_deep', $value) :
+        stripslashes($value);
+        return $value;
+    }
+							    
+    $_POST = array_map('stripslashes_deep', $_POST);
+    $_GET = array_map('stripslashes_deep', $_GET);
+    $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+    $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+}
 ?>
