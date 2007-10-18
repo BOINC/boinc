23802ae381d2e6bc61135829d3a00a45c5f5a568
Charlie Fenton <charlief@example.com>
2007-10-18 08:18:22 +0000

    Fix compiler warning
    svn path=/trunk/boinc/; revision=13896

--- client/cs_scheduler.C
+++ client/cs_scheduler.C
@@ -393,7 +393,7 @@ int CLIENT_STATE::handle_scheduler_reply(
     if (retval) return retval;
 
     if (log_flags.sched_ops) {
-        msg_printf(project, MSG_INFO, "Scheduler request succeeded: got %d new tasks", sr.results.size());
+        msg_printf(project, MSG_INFO, "Scheduler request succeeded: got %d new tasks", (int)sr.results.size());
     }
     if (log_flags.sched_op_debug) {
         if (sr.scheduler_version) {
