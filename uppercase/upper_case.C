1e1c8ee5bfc4693c35aa1616b1244fb7d5c79937
David Anderson <davea@ssl.berkeley.edu>
2008-02-25 22:46:11 +0000

    - uppercase is deprecated
    svn path=/trunk/boinc_samples/; revision=14790

--- uppercase/upper_case.C
+++ uppercase/upper_case.C
@@ -17,6 +17,8 @@
 // or write to the Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+// DEPRECATED!!!  Use example_app/ instead
+//
 // This is the primary sample BOINC application;
 // it shows most of the features of the BOINC API.
 //
