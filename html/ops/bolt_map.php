370778e4840edd74b45e113f6d904fd8db9c3c8c
David Anderson <davea@ssl.berkeley.edu>
2008-06-27 23:15:57 +0000

    - Bolt
    svn path=/trunk/boinc/; revision=15511

--- html/ops/bolt_map.php
+++ html/ops/bolt_map.php
@@ -9,20 +9,24 @@
 // map
 //      show a map;
 //      show form to set or change filter or breakdown
-
-// things to show:
-// all:
-//      # views (as number)
-// items:
-//      fraction of dead-ends
-//          as bar graph, red=dead-end, green=next, yellow=back
-//      seconds spent
-//          (for exercises, show answer sheet separately)
-//          bar graph of log(t)
-//      exercises:
-//          average score (bar graph)
-// exercise sets:
-//      average score (bar graph)
+//
+// columns:
+// name
+// type
+// breakdown category (if using breakdown)
+// nviews (as number)
+// outcome (as color-coded bar graph: green=next, yellow=back, red=none)
+// time (bar graph of log(t))
+// score (bar graph)
+//
+// what's shown:
+// lessons: nviews, outcome, time
+// exercise: nviews, outcome, time, score
+// exercise answer: nviews, outcome, time
+// exercise set: score
+//
+// When breakdown is used, each of above has N lines
+// Total, followed by each breakdown category
 
 require_once("../inc/util.inc");
 require_once("../inc/bolt_db.inc");
@@ -30,8 +34,19 @@ require_once("../inc/bolt_cat.inc");
 require_once("../inc/bolt_util.inc");
 require_once("../inc/bolt.inc");
 
+// the following are to minimize argument passing
+
+$snap = null;
+$course_id = 0;
+$top_unit = null;
+$filter = null;
+$filter_cat = null;
+$breakdown = null;
+$breakdown_cat = null;
+
 function show_snap_form() {
     global $course_id;
+
     page_head("Data snapshot");
     $s = read_map_snapshot($course_id);
 
@@ -70,86 +85,190 @@ function snap_action() {
 }
 
 function spaces($level) {
-    for ($i=0; $i<level; $i++) {
-        echo "&nbsp;&nbsp;";
+    $x = "";
+    for ($i=0; $i<$level; $i++) {
+        $x .= "&nbsp;&nbsp;&nbsp;";
     }
+    return $x;
 }
 
-function filter_array($array, $snap, $filter, $filter_cat) {
+// used to filter arrays of views or xset_results
+//
+function filter_array($array) {
+    global $snap, $filter, $filter_cat, $breakdown, $breakdown_cat;
+
     if (!$filter) return $array;
     $x = array();
     foreach ($array as $y) {
         $u = $snap->users[$y->user_id];
-        if ($filter->category($u) == $filter_cat) {
-            $x[] = $y;
+        if ($filter->category($u) != $filter_cat) {
+            continue;
         }
+        if ($breakdown && $breakdown_cat) {
+            if ($breakdown->category($u) != $breakdown_cat) {
+                continue;
+            }
+        }
+        $x[] = $y;
     }
     return $x;
 }
 
 function avg_score($array) {
     $sum = 0;
+    $n = count($array);
+    if ($n ==0) return 0;
     foreach ($array as $a) {
         $sum += $a->score;
     }
     return $sum/count($array);
 }
 
-function show_unit($snap, $unit, $level, $filter, $filter_cat, $breakdown) {
-    $class = get_class($unit);
-    echo "<tr>
-        <td>".spaces($level)."$unit->name</td>
-        <td>$class</td>
-    ";
-    if ($unit->is_item) {
-        if (array_key_exists($unit->name, $snap->views)) {
-            $a = filter_array(
-                $snap->views[$unit->name], $snap, $filter, $filter_cat
-            );
-            $n = count($a);
-        } else {
-            $n = 0;
+function avg_time($views) {
+    $sum = 0;
+    $n = 0;
+    foreach ($views as $v) {
+        if ($v->start_time && $v->end_time) {
+            $sum += $v->end_time - $v->start_time;
+            $n++;
         }
-        echo "<td>$n</td>";
-    } else {
-        echo "<td><br></td>";
     }
-    if ($class == "BoltExercise") {
-        if (array_key_exists($unit->name, $snap->results)) {
-            $a = filter_array(
-                $snap->results[$unit->name], $snap, $filter, $filter_cat
-            );
-            $avg = avg_score($a);
-            $n = count($a);
-            bolt_bar($avg);
-        }
-    } else if ($class == "BoltExerciseSet") {
-        if (array_key_exists($unit->name, $snap->xset_results)) {
-            $a = filter_array(
-                $snap->xset_results[$unit->name], $snap, $filter, $filter_cat
-            );
-            $avg = avg_score($a);
-            bolt_bar($avg);
-        }
-    } else {
-        echo "<td><br></td>";
-    }
-    echo "\n";
+    if ($n ==0) return 0;
+    return $sum/$n;
 }
 
-function show_unit_recurse(
-    $snap, $unit, $level, $filter, $filter_cat, $breakdown
-) {
-    show_unit($snap, $unit, $level, $filter, $filter_cat, $breakdown);
+function outcomes($views) {
+    $x = array();
+    $x[0] = 0;
+    $x[1] = 0;
+    $x[2] = 0;
+
+    foreach ($views as $v) {
+        switch ($v->action) {
+        case BOLT_ACTION_NONE: $x[0]++; break;
+        case BOLT_ACTION_NEXT: $x[1]++; break;
+        default: $x[2]++; break;
+        }
+    }
+    return $x;
+}
+
+function get_views($unit, $mode) {
+    global $snap;
+
+    $y = array();
+    if (array_key_exists($unit->name, $snap->views)) {
+        $a = filter_array($snap->views[$unit->name]);
+        foreach ($a as $x) {
+            if ($x->mode == $mode) $y[] = $x;
+        }
+    }
+    return $y;
+}
+
+function get_results($unit) {
+    global $snap;
+
+    if (array_key_exists($unit->name, $snap->results)) {
+        return filter_array($snap->results[$unit->name]);
+    }
+    return array();
+}
+
+function get_xset_results($unit) {
+    global $snap;
+
+    if (array_key_exists($unit->name, $snap->xset_results)) {
+        return filter_array($snap->xset_results[$unit->name]);
+    }
+    return array();
+}
+
+function show_unit_row($unit, $class, $level, $is_answer) {
+    global $breakdown, $breakdown_cat;
+
+    $a = $is_answer?" (answer)":"";
+    echo "<tr>
+        <td>".spaces($level)."$unit->name</td>
+        <td>$class $a</td>
+    ";
+    if ($breakdown) {
+        if ($breakdown_cat) {
+            echo "<td>$breakdown_cat</td>\n";
+        } else {
+            echo "<td>Total</td>\n";
+        }
+    }
+    switch ($class) {
+    case "BoltLesson":
+        $views = get_views($unit, BOLT_MODE_LESSON);
+        $n = count($views);
+        $out = outcomes($views);
+        $t = avg_time($views);
+        echo "<td>$n</td>";
+        echo outcome_graph($out);
+        echo time_graph($t);
+        echo empty_cell();
+        break;
+    case "BoltExercise":
+        $views = get_views($unit, $is_answer?BOLT_MODE_ANSWER:BOLT_MODE_SHOW);
+        $results = get_results($unit);
+        $n = count($views);
+        $out = outcomes($views);
+        $t = avg_time($views);
+        $score = avg_score($results);
+        echo "<td>$n</td>";
+        echo outcome_graph($out);
+        echo time_graph($t);
+        echo score_graph($score);
+        break;
+    case "BoltExerciseSet":
+        $xr = get_xset_results($unit);
+        $score = avg_score($xr);
+        echo score_graph($score);
+        break;
+    default:
+    }
+    echo "</tr>\n";
+}
+
+function show_unit($unit, $level) {
+    global $snap, $filter, $filter_cat, $breakdown, $breakdown_cat;
+
+    $class = get_class($unit);
+    $breakdown_cat = null;
+    show_unit_row($unit, $class, $level, false);
+    if ($breakdown) {
+        foreach ($breakdown->categories() as $c) {
+            $breakdown_cat = $c;
+            show_unit_row($unit, $class, $level, false);
+        }
+    }
+
+    // if exercise, show answer page views
+    //
+    if ($class == "BoltExercise") {
+        $breakdown_cat = null;
+        show_unit_row($unit, $class, $level, true);
+        if ($breakdown) {
+            foreach ($breakdown->categories() as $c) {
+                $breakdown_cat = $c;
+                show_unit_row($unit, $class, $level, $true);
+            }
+        }
+    }
+}
+
+function show_unit_recurse($unit, $level) {
+    show_unit($unit, $level);
     if ($unit->is_item) return;
     foreach ($unit->units as $u) {
-        show_unit_recurse($snap, $u, $level+1, $filter, $filter_cat, $breakdown);
+        show_unit_recurse($u, $level+1);
     }
 }
 
 function show_map() {
-    global $course_id;
-    global $top_unit;
+    global $snap, $course_id, $top_unit, $filter, $filter_cat, $breakdown;
 
     $breakdown_name = get_str('breakdown', true);
     if ($breakdown_name) {
@@ -184,7 +303,7 @@ function show_map() {
             <th>Score</th>
         </tr>
     ";
-    show_unit_recurse($snap, $top_unit, $filter, $filter_cat, $breakdown);
+    show_unit_recurse($top_unit, 0);
     echo "
         </table>
         <form action=bolt_map.php>
