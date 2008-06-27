70eefb482875e9ec966ce619d4b4b0a94d3aa0e1
David Anderson <davea@ssl.berkeley.edu>
2008-06-27 18:59:33 +0000

    - make_project: change the --test_app option so that it gets the     app executable from boinc_samples/example_app/uc2,     rather than boinc/apps/upper_case.     (boinc_samples must be installed in the same dir as boinc).
        The reason: the BOINC autoconf-based build process
        produces executables that have zillions of shared library dependencies,
        and generally won't run on machines other than
        the one where they were built.
        Executables built in boinc_samples/ don't have this problem.
    
    svn path=/trunk/boinc/; revision=15509

--- html/ops/bolt_map.php
+++ html/ops/bolt_map.php
@@ -10,6 +10,20 @@
 //      show a map;
 //      show form to set or change filter or breakdown
 
+// things to show:
+// all:
+//      # views (as number)
+// items:
+//      fraction of dead-ends
+//          as bar graph, red=dead-end, green=next, yellow=back
+//      seconds spent
+//          (for exercises, show answer sheet separately)
+//          bar graph of log(t)
+//      exercises:
+//          average score (bar graph)
+// exercise sets:
+//      average score (bar graph)
+
 require_once("../inc/util.inc");
 require_once("../inc/bolt_db.inc");
 require_once("../inc/bolt_cat.inc");
@@ -55,51 +69,81 @@ function snap_action() {
     show_map();
 }
 
-function show_unit($snap, $unit) {
+function spaces($level) {
+    for ($i=0; $i<level; $i++) {
+        echo "&nbsp;&nbsp;";
+    }
+}
+
+function filter_array($array, $snap, $filter, $filter_cat) {
+    if (!$filter) return $array;
+    $x = array();
+    foreach ($array as $y) {
+        $u = $snap->users[$y->user_id];
+        if ($filter->category($u) == $filter_cat) {
+            $x[] = $y;
+        }
+    }
+    return $x;
+}
+
+function avg_score($array) {
+    $sum = 0;
+    foreach ($array as $a) {
+        $sum += $a->score;
+    }
+    return $sum/count($array);
+}
+
+function show_unit($snap, $unit, $level, $filter, $filter_cat, $breakdown) {
     $class = get_class($unit);
-    echo "<li> $unit->name ($class); ";
+    echo "<tr>
+        <td>".spaces($level)."$unit->name</td>
+        <td>$class</td>
+    ";
     if ($unit->is_item) {
         if (array_key_exists($unit->name, $snap->views)) {
-            $n = count($snap->views[$unit->name]);
+            $a = filter_array(
+                $snap->views[$unit->name], $snap, $filter, $filter_cat
+            );
+            $n = count($a);
         } else {
             $n = 0;
         }
-        echo "$n views";
+        echo "<td>$n</td>";
+    } else {
+        echo "<td><br></td>";
     }
     if ($class == "BoltExercise") {
         if (array_key_exists($unit->name, $snap->results)) {
-            $rs = $snap->results[$unit->name];
-            $sum = 0;
-            $n = count($rs);
-            foreach ($rs as $r) {
-                $sum += $r->score;
-            }
-            $avg = $sum/$n;
-            echo " avg score: $avg ($n)";
+            $a = filter_array(
+                $snap->results[$unit->name], $snap, $filter, $filter_cat
+            );
+            $avg = avg_score($a);
+            $n = count($a);
+            bolt_bar($avg);
         }
-    }
-    if ($class == "BoltExerciseSet") {
+    } else if ($class == "BoltExerciseSet") {
         if (array_key_exists($unit->name, $snap->xset_results)) {
-            $xrs = $snap->xset_results[$unit->name];
-            $sum = 0;
-            $n = count($xrs);
-            foreach ($xrs as $xr) {
-                $sum += $xr->score;
-            }
-            $avg = $sum/$n;
-            echo " avg score: $avg ($n)";
+            $a = filter_array(
+                $snap->xset_results[$unit->name], $snap, $filter, $filter_cat
+            );
+            $avg = avg_score($a);
+            bolt_bar($avg);
         }
+    } else {
+        echo "<td><br></td>";
     }
     echo "\n";
 }
 
-function show_unit_recurse($snap, $unit) {
-    show_unit($snap, $unit);
+function show_unit_recurse(
+    $snap, $unit, $level, $filter, $filter_cat, $breakdown
+) {
+    show_unit($snap, $unit, $level, $filter, $filter_cat, $breakdown);
     if ($unit->is_item) return;
     foreach ($unit->units as $u) {
-        echo "<ul>\n";
-        show_unit_recurse($snap, $u);
-        echo "</ul>\n";
+        show_unit_recurse($snap, $u, $level+1, $filter, $filter_cat, $breakdown);
     }
 }
 
@@ -107,9 +151,56 @@ function show_map() {
     global $course_id;
     global $top_unit;
 
+    $breakdown_name = get_str('breakdown', true);
+    if ($breakdown_name) {
+        $breakdown = lookup_categorization($breakdown_name);
+        if (!$breakdown) error_page("unknown breakdown $breakdown_name");
+    } else {
+        $breakdown = null;
+    }
+    $filter_info = get_str('filter', true);
+    if ($filter_info && $filter_info != "none") {
+        $arr = explode(":", $filter_info);
+        $filter_name = $arr[0];
+        $filter_cat = $arr[1];
+        $filter = lookup_categorization($filter_name);
+        if (!$filter) error_page("unknown filter $filter_name");
+    } else {
+        $filter_name = "";
+        $filter_cat = "";
+        $filter = null;
+    }
+
     page_head("Course map");
     $snap = read_map_snapshot($course_id);
-    show_unit_recurse($snap, $top_unit);
+    echo "
+        <table class=bolt_box>
+        <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Views</th>
+            <th>Outcome</th>
+            <th>Time</th>
+            <th>Score</th>
+        </tr>
+    ";
+    show_unit_recurse($snap, $top_unit, $filter, $filter_cat, $breakdown);
+    echo "
+        </table>
+        <form action=bolt_map.php>
+        <input type=hidden name=action value=map>
+        <input type=hidden name=course_id value=$course_id>
+        <table width=600><tr><td valign=top>
+    ";
+    filter_form($filter_name, $filter_cat);
+    echo "</td><td valign=top>";
+    breakdown_form($breakdown_name);
+    echo "
+        </td></tr></table>
+        <p>
+        <input type=submit value=OK>
+        </form>
+    ";
     page_tail();
 }
 
