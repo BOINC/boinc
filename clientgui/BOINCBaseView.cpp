6a40f89107416f26de391a8d84b196472a8a7449
Charlie Fenton <charlief@example.com>
2008-07-11 08:16:38 +0000

    MGR: Small bug fix in new sortable Accessible Views
    svn path=/trunk/boinc/; revision=15592

--- clientgui/BOINCBaseView.cpp
+++ clientgui/BOINCBaseView.cpp
@@ -253,29 +253,29 @@ void CBOINCBaseView::OnListRender(wxTimerEvent& event) {
             }
         }
 
-        if (iDocCount) {
+        if (iDocCount > 0) {
             SynchronizeCache();
-        }
 
-        if (_EnsureLastItemVisible() && (iDocCount != iCacheCount)) {
-            m_pListPane->EnsureVisible(iDocCount - 1);
-        }
-
-        if (m_pListPane->m_bIsSingleSelection) {
-            // If no item has been selected yet, select the first item.
-#ifdef __WXMSW__
-             if ((m_pListPane->GetSelectedItemCount() == 0) &&
-                (m_pListPane->GetItemCount() >= 1)) {
-
-                long desiredstate = wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED;
-                m_pListPane->SetItemState(0, desiredstate, desiredstate);
+            if (_EnsureLastItemVisible() && (iDocCount != iCacheCount)) {
+                m_pListPane->EnsureVisible(iDocCount - 1);
             }
+
+            if (m_pListPane->m_bIsSingleSelection) {
+                // If no item has been selected yet, select the first item.
+#ifdef __WXMSW__
+                if ((m_pListPane->GetSelectedItemCount() == 0) &&
+                    (m_pListPane->GetItemCount() >= 1)) {
+
+                    long desiredstate = wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED;
+                    m_pListPane->SetItemState(0, desiredstate, desiredstate);
+                }
 #else
-             if ((m_pListPane->GetFirstSelected() < 0) &&
-                (m_pListPane->GetItemCount() >= 1))
-                m_pListPane->SetItemState(0, wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED, 
-                                                wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED);
+                if ((m_pListPane->GetFirstSelected() < 0) &&
+                    (m_pListPane->GetItemCount() >= 1))
+                    m_pListPane->SetItemState(0, wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED, 
+                                                    wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED);
 #endif
+            }
         }
         
         UpdateSelection();
