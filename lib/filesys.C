c2c5d04c3197a28ae3900512768e3da8fb844c1c
Charlie Fenton <charlief@example.com>
2008-08-19 22:36:14 +0000

    lib: changed boinc_copy() to enclose path strings in quotes instead of escaping spaces
    svn path=/trunk/boinc/; revision=15894

--- lib/filesys.C
+++ lib/filesys.C
@@ -492,14 +492,12 @@ int boinc_copy(const char* orig, const char* newf) {
     }
     return 0;
 #elif defined(__EMX__)
-    char cmd[1024], cmd_esc[1024];
-    sprintf(cmd, "copy %s %s", orig, newf);
-    string_substitute(cmd, cmd_esc, sizeof(cmd_esc), " ", "\\ ");
+    char cmd[1024];
+    sprintf(cmd, "copy \"%s\" \"%s\"", orig, newf);
     return system(cmd_esc);
 #else
     char cmd[1024], cmd_esc[1024];
-    sprintf(cmd, "cp %s %s", orig, newf);
-    string_substitute(cmd, cmd_esc, sizeof(cmd_esc), " ", "\\ ");
+    sprintf(cmd, "cp \"%s\" \"%s\"", orig, newf);
     return system(cmd_esc);
 #endif
 }
