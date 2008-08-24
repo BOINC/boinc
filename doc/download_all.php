d5f19d951510005d2afe4c0a98d8f03e8e3b3bcf
Rom Walton <rwalton@ssl.berkeley.edu>
2008-08-24 16:19:28 +0000

    Quick Updates
    svn path=/trunk/boinc/; revision=15933

--- doc/download_all.php
+++ doc/download_all.php
@@ -136,7 +136,10 @@ function show_platform($short_name, $p, $dev) {
 function show_platform_xml($short_name, $p, $dev) {
     foreach ($p["versions"] as $i=>$v) {
         if (!$dev && is_dev($v)) continue;
-        //if (!$dev && ((time() - strtodate($v["date"])) >= 86400*30) continue;
+        // show only those builds that have been around for over
+        // two weeks.  Gives us time to address any showstoppers
+        // found by the early adopters
+        if (!$dev && ((time() - strtodate($v["date"])) >= 86400*14)) continue;
         show_version_xml($v, $p);
     }
 }
@@ -210,4 +213,4 @@ if ($xml) {
     page_tail();
 }
 
-?>
+//?>
