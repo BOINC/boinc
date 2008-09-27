5e9056ae0e6b2b83759b446294c42f12a8bd20fe
Rom Walton <rwalton@ssl.berkeley.edu>
2008-09-27 03:21:09 +0000

    Doxygen Test
    svn path=/trunk/boinc/; revision=16074

--- clientgui/BOINCGUIApp.h
+++ clientgui/BOINCGUIApp.h
@@ -14,6 +14,12 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+///
+/// @defgroup BOINCMgr BOINC Manager
+/// The BOINC Manager
+/// @{
 
 #ifndef _BOINCGUIAPP_H_
 #define _BOINCGUIAPP_H_
@@ -161,3 +167,4 @@ DECLARE_APP(CBOINCGUIApp)
 
 #endif
 
+/// @}
