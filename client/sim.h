89e6131dec0c20c2ffbd3da52d2b8834fb8f2e62
David Anderson <davea@ssl.berkeley.edu>
2008-10-09 22:58:28 +0000

    compile fix
    svn path=/trunk/boinc/; revision=16179

--- client/sim.h
+++ client/sim.h
@@ -195,7 +195,7 @@ private:
     bool enforce_schedule();
     bool no_work_for_a_cpu();
     void rr_simulation();
-    void make_running_task_heap(vector<ACTIVE_TASK*>&, double&);
+    void make_preemptable_task_list(vector<ACTIVE_TASK*>&, double&);
     void print_deadline_misses();
 public:
     double retry_shmem_time;
