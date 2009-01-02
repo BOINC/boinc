50d0bb8a316ddbcc58bb13bf12eb85e94293991a
David Anderson <davea@ssl.berkeley.edu>
2009-01-02 20:02:04 +0000

    - added a script to make profile.has_picture consistent     with the images actually on disk
    svn path=/trunk/boinc/; revision=16764

---
+++ html/ops/repair_profile_pictures.php
@@ -0,0 +1,30 @@
+<?php
+
+ini_set("memory_limit", "1023M");
+
+require_once("../inc/boinc_db.inc");
+
+BoincDb::get();
+
+$profiles = BoincProfile::enum("");
+
+foreach ($profiles as $p) {
+    $id = $p->userid;
+    $path = "../user_profile/images/$id.jpg";
+    $smpath = "../user_profile/images/".$id."_sm.jpg";
+    $has_pic = file_exists($path);
+    $has_pic_sm = file_exists($smpath);
+    if ($p->has_picture) {
+        if (!$has_pic || !$has_pic_sm) {
+            echo "$id $p->has_picture $has_pic $has_pic_sm\n";
+            BoincProfile::update_aux("has_picture=0 where userid=$id");
+        }
+    } else {
+        if ($has_pic && $has_pic_sm) {
+            echo "$id $p->has_picture $has_pic $has_pic_sm\n";
+            BoincProfile::update_aux("has_picture=1 where userid=$id");
+        }
+    }
+}
+
+?>
