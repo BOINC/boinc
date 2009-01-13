c10890f0fcf566714e342b7e4186913699a94827
David Anderson <davea@ssl.berkeley.edu>
2009-01-13 23:35:49 +0000

    - compile fixes
    svn path=/trunk/boinc/; revision=16906

--- sched/edf_sim.cpp
+++ sched/edf_sim.cpp
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "config.h"
-
 #ifndef SIM
 #include "sched_config.h"
 #include "sched_msgs.h"
