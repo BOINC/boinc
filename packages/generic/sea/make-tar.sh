df13e549ae032f6cb6093b3c142d6497a21de8e2
Rom Walton <rwalton@ssl.berkeley.edu>
2009-02-04 18:12:00 +0000

    Fix sea tar script
    svn path=/branches/boinc_core_release_6_6a/; revision=17135

--- packages/generic/sea/make-tar.sh
+++ packages/generic/sea/make-tar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -fr BOINC/.CVS
+rm -fr BOINC/.svn
 mkdir BOINC
 mkdir BOINC/locale
-find ../locale/client -name 'BOINC-Manager.mo' | cut -d '/' -f 4 | awk '{print "BOINC/locale/"$0}' | xargs mkdir -p 
-find ../locale/client -name 'BOINC-Manager.mo' | cut -d '/' -f 4,5 | awk '{print "cp \"../locale/"$0"\" \"BOINC/locale/"$0"\""}' | sh
+find ../../../locale -name 'BOINC-Manager.mo' | cut -d '/' -f 5 | awk '{print "BOINC/locale/"$0}' | xargs mkdir -p 
+find ../../../locale -name 'BOINC-Manager.mo' | cut -d '/' -f 5,6 | awk '{print "cp \"../../../locale/"$0"\" \"BOINC/locale/"$0"\""}' | sh
 tar cvf sea.tar BOINC
 
