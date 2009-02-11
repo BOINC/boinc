017b61283bcf4cd27ec3c50210437f9d74c4a243
David Anderson <davea@ssl.berkeley.edu>
2009-02-11 23:01:21 +0000

    - client, API: populate APP_INIT_DATA::hostid.     Remove userid and teamid fields, since the client doesn't know these.
    svn path=/trunk/boinc/; revision=17216

--- client/app_start.cpp
+++ client/app_start.cpp
@@ -176,11 +176,12 @@ int ACTIVE_TASK::write_app_init_file() {
     safe_strcpy(aid.app_name, wup->app->name);
     safe_strcpy(aid.symstore, wup->project->symstore);
     safe_strcpy(aid.acct_mgr_url, gstate.acct_mgr_info.acct_mgr_url);
-    safe_strcpy(aid.user_name, wup->project->user_name);
-    safe_strcpy(aid.team_name, wup->project->team_name);
     if (wup->project->project_specific_prefs.length()) {
         aid.project_preferences = strdup(wup->project->project_specific_prefs.c_str());
     }
+    aid.hostid = wup->project->hostid;
+    safe_strcpy(aid.user_name, wup->project->user_name);
+    safe_strcpy(aid.team_name, wup->project->team_name);
     get_project_dir(wup->project, project_dir, sizeof(project_dir));
     relative_to_absolute(project_dir, project_path);
     strcpy(aid.project_dir, project_path);
