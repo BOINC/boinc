c0a93f5f76c2630a1a4eaf9b589fd1d3d45b5b3f
David Anderson <davea@ssl.berkeley.edu>
2009-02-12 21:51:42 +0000

    svn path=/trunk/boinc/; revision=17249

---
+++ locale/copy_files.php
@@ -0,0 +1,17 @@
+<?php
+
+// copy project-web-site translation files from new to old location.
+// This is a kludge, but the simplest thing to do at this point
+
+$d = opendir(".");
+while ($f = readdir($d)) {
+    if ($f == '.') continue;
+    if ($f == '..') continue;
+    $x = $f."/BOINC-Project-Generic.po";
+    if (file_exists($x)) {
+        $cmd = "cp $x ../html/languages/translations/$f.po";
+        system($cmd);
+    }
+}
+
+?>
