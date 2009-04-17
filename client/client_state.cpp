e46d8c6e63f363d9a5c8a38ac165bad3b4f4f329
David Anderson <davea@ssl.berkeley.edu>
2009-04-17 06:12:41 +0000

    - client: if detach a project, adjust debts and trigger     CPU sched and work fetch
    svn path=/trunk/boinc/; revision=17843

--- client/client_state.cpp
+++ client/client_state.cpp
@@ -1615,6 +1615,9 @@ int CLIENT_STATE::detach_project(PROJECT* project) {
     delete project;
     write_state_file();
 
+    adjust_debts();
+    request_schedule_cpus("Detach");
+    request_work_fetch("Detach");
     return 0;
 }
 
