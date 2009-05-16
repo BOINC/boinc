1579036d0efa609b400af3d7b47794361ee530c9
Charlie Fenton <charlief@example.com>
2009-05-16 07:30:58 +0000

    MGR: Fix bug which prevented skin change from being saved if exiting Manager while in Simple View
    svn path=/branches/boinc_core_release_6_6a/; revision=18136

--- clientgui/BOINCGUIApp.cpp
+++ clientgui/BOINCGUIApp.cpp
@@ -487,6 +487,7 @@ int CBOINCGUIApp::OnExit() {
         m_pDocument = NULL;
     }
 
+    m_pConfig->SetPath(wxT("/"));
     if (m_pSkinManager) {
         m_pConfig->Write(wxT("Skin"), m_pSkinManager->GetSelectedSkin());
         delete m_pSkinManager;
@@ -497,7 +498,6 @@ int CBOINCGUIApp::OnExit() {
     }
 
     // Save Application State
-    m_pConfig->SetPath(wxT("/"));
     m_pConfig->Write(wxT("AutomaticallyShutdownClient"), m_iShutdownCoreClient);
     m_pConfig->Write(wxT("DisplayShutdownClientDialog"), m_iDisplayExitDialog);
     m_pConfig->Write(wxT("DisableAutoStart"), m_iBOINCMGRDisableAutoStart);
