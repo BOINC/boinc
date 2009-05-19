177959f281260dba5c2697f056454f75b1dc7340
Rom Walton <rwalton@ssl.berkeley.edu>
2009-05-19 18:02:58 +0000

    Make Linux builds not show the pre-release message after a ./_autosetup
    svn path=/branches/boinc_core_release_6_6a/; revision=18156

--- packages/generic/sea/make-tar.sh
+++ packages/generic/sea/make-tar.sh
@@ -2,8 +2,5 @@
 
 rm -fr BOINC/.svn
 mkdir BOINC
-mkdir BOINC/locale
-find ../../../locale -name 'BOINC-Manager.mo' | cut -d '/' -f 5 | awk '{print "BOINC/locale/"$0}' | xargs mkdir -p 
-find ../../../locale -name 'BOINC-Manager.mo' | cut -d '/' -f 5,6 | awk '{print "cp \"../../../locale/"$0"\" \"BOINC/locale/"$0"\""}' | sh
 tar cvf sea.tar BOINC
 
