7580a01415c6e7728ecd032528f9e47fb3bbebf6
Rom Walton <rwalton@ssl.berkeley.edu>
2009-05-29 16:02:32 +0000

    - Change version number to 6.9.x
        /
            configure.ac
            version.h
    
    svn path=/trunk/boinc/; revision=18228

--- version.h
+++ version.h
@@ -7,13 +7,13 @@
 #define BOINC_MAJOR_VERSION 6
 
 /* Minor part of BOINC version number */
-#define BOINC_MINOR_VERSION 7
+#define BOINC_MINOR_VERSION 9
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 5
+#define BOINC_RELEASE 0
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "6.7.5"
+#define BOINC_VERSION_STRING "6.9.0"
 
 /* Package is a pre-release (Alpha/Beta) package */
 #define BOINC_PRERELEASE 1
@@ -29,13 +29,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 6.7.5"
+#define PACKAGE_STRING "BOINC 6.9.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "6.7.5"
+#define PACKAGE_VERSION "6.9.0"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
