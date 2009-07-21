e1634ea1687ea27fc182f96de179354d34504fab
Rom Walton <rwalton@ssl.berkeley.edu>
2009-07-21 20:24:25 +0000

    Quick Updates
    svn path=/branches/boinc_core_release_6_6a/; revision=18653

--- clientgui/DlgItemProperties.cpp
+++ clientgui/DlgItemProperties.cpp
@@ -117,7 +117,6 @@ void CDlgItemProperties::renderInfos(PROJECT* project_in) {
 		addProperty(_("File uploads deferred for"), FormatTime(project->upload_backoff));
     }
 	addProperty(_("Disk usage"),FormatDiskSpace(diskusage));
-    addProperty(_("Computer ID"), wxString::Format(wxT("%d"), project->hostid));
 	addProperty(_("Non CPU intensive"),project->non_cpu_intensive ? _("yes") : _("no"));
 	addProperty(_("Suspended via GUI"),project->suspended_via_gui ? _("yes") : _("no"));
 	addProperty(_("Don't request more work"),project->dont_request_more_work ? _("yes") : _("no"));
