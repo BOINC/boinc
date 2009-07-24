976240485c2d6a215d70aa78bbb3c3f29d33b479
Rom Walton <rwalton@ssl.berkeley.edu>
2009-07-24 19:17:25 +0000

    - client: in get_project_config_poll() GUI RPC,         return ERR_IN_PROGRESS is the reference site check is in progress.         This hopefully fixes a bug where:         - the user is connected via a proxy         - the manager is run for the first time, and an attach is tried         - the get_project_config.php fetch fails because no proxy config         - the manager gets the CC_STATUS before the ref site check has             finished, so it thinks there's a network connection,             and it doesn't ask the user for proxy info
        client/
            gui_rpc_server_ops.cpp
            net_stats.cpp,h
        clientgui/
            ProjectPropertiesPage.cpp
    
    svn path=/branches/boinc_core_release_6_8/; revision=18713

--- clientgui/ProjectPropertiesPage.cpp
+++ clientgui/ProjectPropertiesPage.cpp
@@ -420,15 +420,17 @@ void CProjectPropertiesPage::OnStateChange( CProjectPropertiesPageEvent& WXUNUSE
                 ::wxSafeYield(GetParent());
             }
  
-            // We either successfully retrieved the project's account creation 
-            //   policies or we were able to talk to the web server and found out
-            //   they do not support account creation through the wizard.  In either
-            //   case we should claim success and set the correct flags to show the
-            //   correct 'next' page.
             bSuccessfulCondition = 
                 (!iReturnValue) && (!pc->error_num) ||
                 (!iReturnValue) && (ERR_ACCT_CREATION_DISABLED == pc->error_num);
             if (bSuccessfulCondition) {
+                // We either successfully retrieved the project's
+                // account creation policies or we were able to talk
+                // to the web server and found out they do not support
+                // account creation through the wizard.
+                // In either case, claim success and set the correct flags
+                // to show the correct 'next' page.
+                //
                 SetProjectPropertiesSucceeded(true);
 
                 bSuccessfulCondition = pc->account_creation_disabled;
