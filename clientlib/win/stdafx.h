5c5e778d91c5bc94c0d5a5ac433757cb4c71ec81
Rom Walton <rwalton@ssl.berkeley.edu>
2009-07-25 16:47:04 +0000

    - MGR: Refine yesterdays checkin with support for the BOINC         Service Control utility, use ShellExecuteEx instead of         CreateProcess.  CreateProcess doesn't have the ability         to elevate processes.
        clientlib/win/
            Service.cpp
    
    svn path=/trunk/boinc/; revision=18730

--- clientlib/win/stdafx.h
+++ clientlib/win/stdafx.h
@@ -8,6 +8,17 @@
 #define STRICT
 #endif
 
+// Visual Studio 2005 has extended the C Run-Time Library by including "secure"
+// runtime functions and deprecating the previous function prototypes.  Since
+// we need to use the previous prototypes to maintain compatibility with other
+// platforms we are going to disable the deprecation warnings if we are compiling
+// on Visual Studio 2005
+#if _MSC_VER >= 1400
+#ifndef _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+#endif
+
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
 #ifndef WINVER				    // Allow use of features specific to Windows 2000 or later
@@ -25,6 +36,7 @@
 #include <windows.h>
 #include <initguid.h>
 #include <wininet.h>
+#include <shellapi.h>
 #include <crtdbg.h>
 #include <tchar.h>
 
