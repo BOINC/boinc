9f5c62a3756752e5da97c0e0debe94451c8cc60f
Rom Walton <rwalton@ssl.berkeley.edu>
2009-09-25 18:56:52 +0000

    - client: report the different Windows 7 and Windows 2008         "R2" correctly.
        client/
            hostinfo_win.cpp
    
    svn path=/trunk/boinc/; revision=19170

--- client/hostinfo_win.cpp
+++ client/hostinfo_win.cpp
@@ -66,79 +66,142 @@ HINSTANCE g_hClientLibraryDll;
 // Newer product types than what is currently defined in
 //   Visual Studio 2005
 #ifndef PRODUCT_ULTIMATE
-#define PRODUCT_ULTIMATE                        0x00000001
+#define PRODUCT_ULTIMATE                            0x00000001
 #endif
 #ifndef PRODUCT_HOME_BASIC
-#define PRODUCT_HOME_BASIC                      0x00000002
+#define PRODUCT_HOME_BASIC                          0x00000002
 #endif
 #ifndef PRODUCT_HOME_PREMIUM
-#define PRODUCT_HOME_PREMIUM                    0x00000003
+#define PRODUCT_HOME_PREMIUM                        0x00000003
 #endif
 #ifndef PRODUCT_ENTERPRISE
-#define PRODUCT_ENTERPRISE                      0x00000004
+#define PRODUCT_ENTERPRISE                          0x00000004
 #endif
 #ifndef PRODUCT_HOME_BASIC_N
-#define PRODUCT_HOME_BASIC_N                    0x00000005
+#define PRODUCT_HOME_BASIC_N                        0x00000005
 #endif
 #ifndef PRODUCT_BUSINESS
-#define PRODUCT_BUSINESS                        0x00000006
+#define PRODUCT_BUSINESS                            0x00000006
 #endif
 #ifndef PRODUCT_STANDARD_SERVER
-#define PRODUCT_STANDARD_SERVER                 0x00000007
+#define PRODUCT_STANDARD_SERVER                     0x00000007
 #endif
 #ifndef PRODUCT_DATACENTER_SERVER
-#define PRODUCT_DATACENTER_SERVER               0x00000008
+#define PRODUCT_DATACENTER_SERVER                   0x00000008
 #endif
 #ifndef PRODUCT_SMALLBUSINESS_SERVER
-#define PRODUCT_SMALLBUSINESS_SERVER            0x00000009
+#define PRODUCT_SMALLBUSINESS_SERVER                0x00000009
 #endif
 #ifndef PRODUCT_ENTERPRISE_SERVER
-#define PRODUCT_ENTERPRISE_SERVER               0x0000000A
+#define PRODUCT_ENTERPRISE_SERVER                   0x0000000A
 #endif
 #ifndef PRODUCT_STARTER
-#define PRODUCT_STARTER                         0x0000000B
+#define PRODUCT_STARTER                             0x0000000B
 #endif
 #ifndef PRODUCT_DATACENTER_SERVER_CORE
-#define PRODUCT_DATACENTER_SERVER_CORE          0x0000000C
+#define PRODUCT_DATACENTER_SERVER_CORE              0x0000000C
 #endif
 #ifndef PRODUCT_STANDARD_SERVER_CORE
-#define PRODUCT_STANDARD_SERVER_CORE            0x0000000D
+#define PRODUCT_STANDARD_SERVER_CORE                0x0000000D
 #endif
 #ifndef PRODUCT_ENTERPRISE_SERVER_CORE
-#define PRODUCT_ENTERPRISE_SERVER_CORE          0x0000000E
+#define PRODUCT_ENTERPRISE_SERVER_CORE              0x0000000E
 #endif
 #ifndef PRODUCT_ENTERPRISE_SERVER_IA64
-#define PRODUCT_ENTERPRISE_SERVER_IA64          0x0000000F
+#define PRODUCT_ENTERPRISE_SERVER_IA64              0x0000000F
 #endif
 #ifndef PRODUCT_BUSINESS_N
-#define PRODUCT_BUSINESS_N                      0x00000010
+#define PRODUCT_BUSINESS_N                          0x00000010
 #endif
 #ifndef PRODUCT_WEB_SERVER
-#define PRODUCT_WEB_SERVER                      0x00000011
+#define PRODUCT_WEB_SERVER                          0x00000011
 #endif
 #ifndef PRODUCT_CLUSTER_SERVER
-#define PRODUCT_CLUSTER_SERVER                  0x00000012
+#define PRODUCT_CLUSTER_SERVER                      0x00000012
 #endif
 #ifndef PRODUCT_HOME_SERVER
-#define PRODUCT_HOME_SERVER                     0x00000013
+#define PRODUCT_HOME_SERVER                         0x00000013
 #endif
 #ifndef PRODUCT_STORAGE_EXPRESS_SERVER
-#define PRODUCT_STORAGE_EXPRESS_SERVER          0x00000014
+#define PRODUCT_STORAGE_EXPRESS_SERVER              0x00000014
 #endif
 #ifndef PRODUCT_STORAGE_STANDARD_SERVER
-#define PRODUCT_STORAGE_STANDARD_SERVER         0x00000015
+#define PRODUCT_STORAGE_STANDARD_SERVER             0x00000015
 #endif
 #ifndef PRODUCT_STORAGE_WORKGROUP_SERVER
-#define PRODUCT_STORAGE_WORKGROUP_SERVER        0x00000016
+#define PRODUCT_STORAGE_WORKGROUP_SERVER            0x00000016
 #endif
 #ifndef PRODUCT_STORAGE_ENTERPRISE_SERVER
-#define PRODUCT_STORAGE_ENTERPRISE_SERVER       0x00000017
+#define PRODUCT_STORAGE_ENTERPRISE_SERVER           0x00000017
 #endif
 #ifndef PRODUCT_SERVER_FOR_SMALLBUSINESS
-#define PRODUCT_SERVER_FOR_SMALLBUSINESS        0x00000018
+#define PRODUCT_SERVER_FOR_SMALLBUSINESS            0x00000018
 #endif
 #ifndef PRODUCT_SMALLBUSINESS_SERVER_PREMIUM
-#define PRODUCT_SMALLBUSINESS_SERVER_PREMIUM    0x00000019
+#define PRODUCT_SMALLBUSINESS_SERVER_PREMIUM        0x00000019
+#endif
+#ifndef PRODUCT_HOME_PREMIUM_N
+#define PRODUCT_HOME_PREMIUM_N                      0x0000001A
+#endif
+#ifndef PRODUCT_ENTERPRISE_N
+#define PRODUCT_ENTERPRISE_N                        0x0000001B
+#endif
+#ifndef PRODUCT_ULTIMATE_N
+#define PRODUCT_ULTIMATE_N                          0x0000001C
+#endif
+#ifndef PRODUCT_WEB_SERVER_CORE
+#define PRODUCT_WEB_SERVER_CORE                     0x0000001D
+#endif
+#ifndef PRODUCT_MEDIUMBUSINESS_SERVER_MANAGEMENT
+#define PRODUCT_MEDIUMBUSINESS_SERVER_MANAGEMENT    0x0000001E
+#endif
+#ifndef PRODUCT_MEDIUMBUSINESS_SERVER_SECURITY
+#define PRODUCT_MEDIUMBUSINESS_SERVER_SECURITY      0x0000001F
+#endif
+#ifndef PRODUCT_MEDIUMBUSINESS_SERVER_MESSAGING
+#define PRODUCT_MEDIUMBUSINESS_SERVER_MESSAGING     0x00000020
+#endif
+#ifndef PRODUCT_SMALLBUSINESS_SERVER_PRIME
+#define PRODUCT_SMALLBUSINESS_SERVER_PRIME          0x00000021
+#endif
+#ifndef PRODUCT_HOME_PREMIUM_SERVER
+#define PRODUCT_HOME_PREMIUM_SERVER                 0x00000022
+#endif
+#ifndef PRODUCT_SERVER_FOR_SMALLBUSINESS_V
+#define PRODUCT_SERVER_FOR_SMALLBUSINESS_V          0x00000023
+#endif
+#ifndef PRODUCT_STANDARD_SERVER_V
+#define PRODUCT_STANDARD_SERVER_V                   0x00000024
+#endif
+#ifndef PRODUCT_DATACENTER_SERVER_V
+#define PRODUCT_DATACENTER_SERVER_V                 0x00000025
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_V
+#define PRODUCT_ENTERPRISE_SERVER_V                 0x00000026
+#endif
+#ifndef PRODUCT_DATACENTER_SERVER_CORE_V
+#define PRODUCT_DATACENTER_SERVER_CORE_V            0x00000027
+#endif
+#ifndef PRODUCT_STANDARD_SERVER_CORE_V
+#define PRODUCT_STANDARD_SERVER_CORE_V              0x00000028
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_CORE_V
+#define PRODUCT_ENTERPRISE_SERVER_CORE_V            0x00000029
+#endif
+#ifndef PRODUCT_HYPERV
+#define PRODUCT_HYPERV                              0x0000002A
+#endif
+#ifndef PRODUCT_STORAGE_EXPRESS_SERVER_CORE
+#define PRODUCT_STORAGE_EXPRESS_SERVER_CORE         0x0000002B
+#endif
+#ifndef PRODUCT_STORAGE_STANDARD_SERVER_CORE
+#define PRODUCT_STORAGE_STANDARD_SERVER_CORE        0x0000002C
+#endif
+#ifndef PRODUCT_STORAGE_WORKGROUP_SERVER_CORE
+#define PRODUCT_STORAGE_WORKGROUP_SERVER_CORE       0x0000002D
+#endif
+#ifndef PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE
+#define PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE      0x0000002E
 #endif
 
 
@@ -307,7 +370,7 @@ int get_os_information(
                 // Test for the workstation type.
                 if ( osvi.wProductType == VER_NT_WORKSTATION ) {
 
-                    if( (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 0) ) {
+                    if( (osvi.dwMajorVersion == 6) ) {
                         switch(dwType) {
                             case PRODUCT_ULTIMATE:
                                strcat(szSKU, "Ultimate ");
@@ -349,7 +412,7 @@ int get_os_information(
             
                 // Test for the server type.
                 else if ( (osvi.wProductType == VER_NT_SERVER) || (osvi.wProductType == VER_NT_DOMAIN_CONTROLLER) ) {
-                    if( (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 0) ) {
+                    if( (osvi.dwMajorVersion == 6) ) {
 
                         switch(dwType) {
                             case PRODUCT_CLUSTER_SERVER:
