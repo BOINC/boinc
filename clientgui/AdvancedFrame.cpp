c0c4d0f58f542fe6deda6cb2e30fab3d87bd87d7
Rom Walton <rwalton@ssl.berkeley.edu>
2009-10-23 16:49:33 +0000

    - MGR: Change the shortcut key for switching to the simple view.
        clientgui/
            AdvancedFrame.cpp
    
    svn path=/trunk/boinc/; revision=19369

--- clientgui/AdvancedFrame.cpp
+++ clientgui/AdvancedFrame.cpp
@@ -372,7 +372,7 @@ bool CAdvancedFrame::CreateMenu( bool bRPCsSafe ) {
 
     menuView->Append(
         ID_CHANGEGUI,
-        _("&Simple View...\tCTRL+SHIFT+S"),
+        _("Simple &View...\tCTRL+SHIFT+V"),
         _("Display the simple graphical interface.")
     );
 
