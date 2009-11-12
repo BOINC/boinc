e8e48f36c9ac3f7c407e3747187e2e1d260301c7
Rom Walton <rwalton@ssl.berkeley.edu>
2009-11-12 21:48:04 +0000

    Switching to vim
    svn path=/trunk/boinc/; revision=19560

--- locale/updatetrans.sh
+++ locale/updatetrans.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+# Automate the compilation of the various locale PO files by automatically
+# generating them at night.
+projname=boinctrunk
+projdir=$HOME/pootle/po/$projname
+
+echo $projname
+echo $projdir
+
+exit 0
+
 for file in `find -name 'BOINC-Manager.mo'` ; do
   dir=`dirname $file`
   locale=`basename $dir`
+  
   cp -f $file BOINC/locale/${locale}
+
+  # http://boinc.berkeley.edu/translate/ar/boinctrunk/BOINC-Manager.mo
+
 done  
 
 exit 0
