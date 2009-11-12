f4a2c9cc0d52c8c9a1a2d59ecf513cd8f363a354
BOINC Translator <boinctrans@ssl.berkeley.edu>
2009-11-12 22:42:54 +0000

    Update Translations
    svn path=/trunk/boinc/; revision=19563

--- locale/updatetrans.sh
+++ locale/updatetrans.sh
@@ -17,11 +17,11 @@ for file in `find -name 'BOINC-Manager.po'` ; do
   timestampPO=`date -r ${projdir}/${locale}/BOINC-Manager.po`
   timestampMO=`date -r ${projdir}/${locale}/BOINC-Manager.mo`
  
-  if [ "${timestampPO}" -ne "${timestampMO}" ]; then
+  if [ "${timestampPO}" != "${timestampMO}" ]; then
 
     # Remove old MO from previous compilation
     #
-    rm ${projdir}/BOINC-Manager.mo
+    rm ${projdir}/BOINC-Manager.mo > /dev/null 2> /dev/null
 
     # Use wget to cause the Pottle system to compile the PO file into an MO file.
     #
@@ -29,10 +29,10 @@ for file in `find -name 'BOINC-Manager.po'` ; do
     #
     # Example: http://boinc.berkeley.edu/translate/ar/boinctrunk/BOINC-Manager.mo
     #
-    wget http://boinc.berkeley.edu/translate/${locale}/${projname}/BOINC-Manager.mo
+    wget http://boinc.berkeley.edu/translate/${locale}/${projname}/BOINC-Manager.mo > /dev/null 2> /dev/null
     
     # Add any new MO files to SVN
-    svn add ${projdir}/${locale}/BOINC-Manager.mo
+    svn add ${projdir}/${locale}/BOINC-Manager.mo > /dev/null 2> /dev/null
 
     # Touch each file to adjust timestamps
     touch ${projdir}/${locale}/BOINC-Manager.po
@@ -41,6 +41,10 @@ for file in `find -name 'BOINC-Manager.po'` ; do
   fi  
 
 done
+
+# Remove old MO from previous compilation
+#
+rm ${projdir}/BOINC-Manager.mo > /dev/null 2> /dev/null
 
 # Commit any changes to SVN
 svn commit -m 'Update Translations'
