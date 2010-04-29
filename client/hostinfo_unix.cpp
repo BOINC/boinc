73a95378f54964b118f84352c6cdd72b6915b124
Charlie Fenton <charlief@example.com>
2010-04-29 10:31:57 +0000

    client: Add get_max_cpu_temperature to the Macintosh client to report the highest current CPU temperature as degrees Celsius
    svn path=/trunk/boinc/; revision=21325

--- client/hostinfo_unix.cpp
+++ client/hostinfo_unix.cpp
@@ -899,7 +899,198 @@ int get_network_usage_totals(unsigned int& total_received, unsigned int& total_s
         
     return 0;
 }
+
+
+#if defined(__i386__) || defined(__x86_64__)
+
+// Code to get maximum CPU temperature (Apple Intel only)
+// Adapted from Apple System Management Control (SMC) Tool under the GPL
+
+#define KERNEL_INDEX_SMC      2
+
+#define SMC_CMD_READ_BYTES    5
+#define SMC_CMD_READ_KEYINFO  9
+
+typedef struct {
+    char                  major;
+    char                  minor;
+    char                  build;
+    char                  reserved[1]; 
+    UInt16                release;
+} SMCKeyData_vers_t;
+
+typedef struct {
+    UInt16                version;
+    UInt16                length;
+    UInt32                cpuPLimit;
+    UInt32                gpuPLimit;
+    UInt32                memPLimit;
+} SMCKeyData_pLimitData_t;
+
+typedef struct {
+    UInt32                dataSize;
+    UInt32                dataType;
+    char                  dataAttributes;
+} SMCKeyData_keyInfo_t;
+
+typedef char              SMCBytes_t[32]; 
+
+typedef struct {
+  UInt32                  key; 
+  SMCKeyData_vers_t       vers; 
+  SMCKeyData_pLimitData_t pLimitData;
+  SMCKeyData_keyInfo_t    keyInfo;
+  char                    result;
+  char                    status;
+  char                    data8;
+  UInt32                  data32;
+  SMCBytes_t              bytes;
+} SMCKeyData_t;
+
+static io_connect_t conn;
+
+kern_return_t SMCOpen()
+{
+    kern_return_t       result;
+    mach_port_t         masterPort;
+    io_iterator_t       iterator;
+    io_object_t         device;
+
+    result = IOMasterPort(MACH_PORT_NULL, &masterPort);
+
+    CFMutableDictionaryRef matchingDictionary = IOServiceMatching("AppleSMC");
+    result = IOServiceGetMatchingServices(masterPort, matchingDictionary, &iterator);
+    if (result != kIOReturnSuccess)
+    {
+        return result;
+    }
+
+    device = IOIteratorNext(iterator);
+    IOObjectRelease(iterator);
+    if (device == 0)
+    {
+        return result;
+    }
+
+    result = IOServiceOpen(device, mach_task_self(), 0, &conn);
+    IOObjectRelease(device);
+    if (result != kIOReturnSuccess)
+    {
+        return result;
+    }
+
+    return kIOReturnSuccess;
+}
+
+kern_return_t SMCClose()
+{
+    if (conn) {
+        return IOServiceClose(conn);
+    }
+    return kIOReturnSuccess;
+}
+
+
+kern_return_t SMCReadKey(UInt32 key, SMCBytes_t val)
+{
+    kern_return_t       result;
+    SMCKeyData_t        inputStructure;
+    SMCKeyData_t        outputStructure;
+ 	size_t              structureInputSize;
+    size_t              structureOutputSize;
+
+    memset(&inputStructure, 0, sizeof(SMCKeyData_t));
+    memset(&outputStructure, 0, sizeof(SMCKeyData_t));
+    memset(val, 0, sizeof(val));
+
+    inputStructure.key = key;
+    inputStructure.data8 = SMC_CMD_READ_KEYINFO;    
+
+    structureInputSize = sizeof(inputStructure);
+    structureOutputSize = sizeof(outputStructure);
+	result = IOConnectMethodStructureIStructureO(
+                    conn, KERNEL_INDEX_SMC, structureInputSize, &structureOutputSize, 
+                    &inputStructure, &outputStructure
+            );
+    if (result != kIOReturnSuccess)
+        return result;
+
+    inputStructure.keyInfo.dataSize = outputStructure.keyInfo.dataSize;
+    inputStructure.data8 = SMC_CMD_READ_BYTES;
+
+	result = IOConnectMethodStructureIStructureO(
+                    conn, KERNEL_INDEX_SMC, structureInputSize, &structureOutputSize, 
+                    &inputStructure, &outputStructure
+            );
+    if (result != kIOReturnSuccess)
+        return result;
+
+    memcpy(val, outputStructure.bytes, sizeof(outputStructure.bytes));
+
+    return kIOReturnSuccess;
+}
+
+
+// Check up to 10 die temperatures (TC0D, TC1D, etc.) and 
+// 10 heatsink temperatures (TCAH, TCBH, etc.)
+// Returns the highest current CPU temperature as degrees Celsius.
+// Returns zero if it fails (or on a PowerPC Mac).
+int get_max_cpu_temperature() {
+    kern_return_t       result;
+    int                 maxTemp = 0, thisTemp, i;
+    union tempKey {
+        UInt32          word;
+        char            bytes[4];
+    };
+    tempKey             key;
+    SMCBytes_t          val;
+    static bool         skip[20];
+
+    // open connection to SMC kext if this is the first time
+    if (!conn) {
+        result = SMCOpen();
+        if (result != kIOReturnSuccess) {
+            return 0;
+        }
+    }
+
+    for (i=0; i<20; ++i) {
+        if (skip[i]) continue;
+        if (i < 10) {
+            key.word = 'TC0D';
+            key.bytes[1] += i;          // TC0D, TC1D, TC2D, etc.
+        } else {
+            key.word = 'TCAH';
+            key.bytes[1] += (i - 10);   // TCAH, TCBH, TCCH, etc.
+        }
+        result = SMCReadKey(key.word, val);
+        if (result != kIOReturnSuccess) {
+            skip[i] = true;
+            continue;
+        }
+        
+        if (val[0] < 1) {
+            skip[i] = true;
+            continue;
+        }
+        
+        thisTemp = val[0];
+        if (val[1] & 0x80) ++thisTemp;
+        if (thisTemp > maxTemp) {
+            maxTemp = thisTemp;
+        }
+    }
     
+    return maxTemp;
+}
+
+#else       // PowerPC
+
+int GetMaxCPUTemperature() {
+    return 0;
+}
+
+#endif
 
 #endif  // __APPLE__
 
