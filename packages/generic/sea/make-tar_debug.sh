d9d112f9fb66017208a1fb3100a4e8f21deac094
Rom Walton <rwalton@ssl.berkeley.edu>
2010-07-13 20:38:20 +0000

    Quick Updates
    svn path=/trunk/boinc/; revision=21917

--- packages/generic/sea/make-tar_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -fr BOINC_debug/.CVS
-mkdir BOINC_debug
-mkdir BOINC_debug/locale
-find ../locale/client -name 'BOINC-Manager.mo' | cut -d '/' -f 4 | awk '{print "BOINC_debug/locale/"$0}' | xargs mkdir -p 
-find ../locale/client -name 'BOINC-Manager.mo' | cut -d '/' -f 4,5 | awk '{print "cp \"../locale/"$0"\" \"BOINC_debug/locale/"$0"\""}' | sh
-tar cvf sea_debug.tar BOINC_debug
-
