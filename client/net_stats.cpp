4de59ea48638c7d59244dfb79233eabe641c192b
Rom Walton <rwalton@ssl.berkeley.edu>
2010-08-02 19:34:24 +0000

    - client: wait until after the proxy check completes before attempting to         check on the reference site.
        client/
            net_status.cpp
    
    svn path=/trunk/boinc/; revision=22097

--- client/net_stats.cpp
+++ client/net_stats.cpp
@@ -269,6 +269,11 @@ void NET_STATUS::poll() {
     // otherwise might show spurious "need connection" message
     //
     if (gstate.now < gstate.last_wakeup_time + 30) return;
+    // wait until after a round of automatic proxy detection 
+    // before attempting to contact the reference site
+    //
+    if (need_autodetect_proxy_settings && !have_autodetect_proxy_settings) return;
+
 	if (net_status.need_to_contact_reference_site && !gstate.gui_http.is_busy()) {
 		net_status.contact_reference_site();
 	}
