c58558cba49c0139186d398810d9d731165e462f
David Anderson <davea@ssl.berkeley.edu>
2010-09-30 05:05:05 +0000

    - lib: make str_util.h C-compatible (from Matt Arsenault)
    svn path=/trunk/boinc/; revision=22426

--- lib/str_util.h
+++ lib/str_util.h
@@ -18,27 +18,73 @@
 #ifndef STR_UTIL_H
 #define STR_UTIL_H
 
-#include <cstdlib>
-#include <ctime>
-#include <cctype>
-#include <cstring>
-#include <string>
-#include <vector>
+#ifdef __cplusplus
+  #include <cstdlib>
+  #include <ctime>
+  #include <cctype>
+  #include <cstring>
+  #include <string>
+  #include <vector>
+#else
+  #include <stdlib.h>
+  #include <time.h>
+  #include <ctype.h>
+  #include <string.h>
+#endif /* __cplusplus */
 
 #define KILO (1024.0)
 #define MEGA (1048576.0)
 #define GIGA (1024.*1048576.0)
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern int ndays_to_string(double x, int smallest_timescale, char *buf);
 extern void nbytes_to_string(double nbytes, double total_bytes, char* str, int len);
 extern int parse_command_line(char*, char**);
 extern void c2x(char *what);
 extern void strip_whitespace(char *str);
-extern void strip_whitespace(std::string&);
+
 #define safe_strcpy(x, y) strlcpy(x, y, sizeof(x))
 #define safe_strcat(x, y) if (strlen(x)+strlen(y)<sizeof(x)) strcat(x, y)
 extern char* time_to_string(double);
 extern char* precision_time_to_string(double);
+
+inline void downcase_string(char* p) {
+    while (*p) {
+        *p = (char)tolower((int)*p);
+        p++;
+    }
+}
+
+extern int string_substitute(
+    const char* haystack, char* out, int out_len,
+    const char* needle, const char* target
+);
+
+// returns short text description of error corresponding to
+// int errornumber from error_numbers.h
+//
+extern const char* boincerror(int which_error);
+extern const char* network_status_string(int);
+extern const char* rpc_reason_string(int);
+extern const char* suspend_reason_string(int reason);
+
+#ifdef __cplusplus
+}
+#endif
+
+// C++ Specific pieces
+#ifdef __cplusplus
+
+// convert UNIX time to MySQL timestamp (yyyymmddhhmmss)
+//
+extern void mysql_timestamp(double, char*);
+
+extern void strip_whitespace(std::string&);
+
 extern std::string timediff_format(double);
 
 inline bool ends_with(std::string const& s, std::string const& suffix) {
@@ -57,29 +103,7 @@ inline void downcase_string(std::string& w) {
     }
 }
 
-inline void downcase_string(char* p) {
-    while (*p) {
-        *p = (char)tolower((int)*p);
-        p++;
-    }
-}
+#endif /* __cplusplus */
 
-extern int string_substitute(
-    const char* haystack, char* out, int out_len,
-    const char* needle, const char* target
-);
+#endif /* STR_UTIL_H */
 
-// convert UNIX time to MySQL timestamp (yyyymmddhhmmss)
-//
-extern void mysql_timestamp(double, char*);
-
-// returns short text description of error corresponding to
-// int errornumber from error_numbers.h
-//
-extern const char* boincerror(int which_error);
-extern const char* network_status_string(int);
-extern const char* rpc_reason_string(int);
-extern const char* suspend_reason_string(int reason);
-
-
-#endif
