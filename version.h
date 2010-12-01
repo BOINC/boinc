ff6f8b96787ab030a659eb7aec2e0d48e379f067
Rom Walton <rwalton@ssl.berkeley.edu>
2010-12-01 18:14:55 +0000

    - Tag for 6.12.7 release, all platforms       boinc_core_release_6_12_7
        /
            configure.ac
            version.h
    
    svn path=/branches/boinc_core_release_6_12/; revision=22788

--- version.h
+++ version.h
@@ -10,10 +10,10 @@
 #define BOINC_MINOR_VERSION 12
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 6
+#define BOINC_RELEASE 7
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "6.12.6"
+#define BOINC_VERSION_STRING "6.12.7"
 
 /* Package is a pre-release (Alpha/Beta) package */
 /* #define BOINC_PRERELEASE 1 */
@@ -29,13 +29,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 6.12.6"
+#define PACKAGE_STRING "BOINC 6.12.7"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "6.12.6"
+#define PACKAGE_VERSION "6.12.7"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
