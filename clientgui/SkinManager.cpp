06844f530a5853b1beb8754ddd8c59fcd92a403d
Rom Walton <rwalton@ssl.berkeley.edu>
2011-02-08 17:48:36 +0000

    - MGR: Fix a bug that is several years old, appears to have been         introduced during the Unicode conversion phase for Windows. Passing         a wide character string into an narrow character function causes         a NULL reference exception.
            TODO: There is still an issue if the manager attempts to write to
              stderr when the BOINC Manager log files are redirected to a
              directory in which the user doesn't have access too.
              
        clientgui/
            SkinManager.cpp
    
    svn path=/branches/boinc_core_release_6_12/; revision=22999

--- clientgui/SkinManager.cpp
+++ clientgui/SkinManager.cpp
@@ -1301,8 +1301,8 @@ bool CSkinManager::ReloadSkin(wxLocale* pLocale, wxString strSkin) {
         }
     }
 
-    if (retval) {
-        fprintf(stderr, "Skin Manager: Failed to load skin '%s'.\n", (char *)ConstructSkinFileName().c_str());
+    if (retval && show_error_msgs) {
+        fprintf(stderr, "Skin Manager: Failed to load skin '%s'.\n", (const char *)ConstructSkinFileName().mb_str(wxConvUTF8));
     }
 
     InitializeDelayedValidation();
