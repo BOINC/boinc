fa9681b2d357e14cb64f1ed3d4e92541ae30abc7
Bernd Machenschalk <bernd.machenschalk@aei.mpg.de>
2011-03-10 14:47:01 +0000

    - unix build system: generate_svn_version   extended support for git repos:   generate numeric version from last commit date,   record host, remote and branch names
    svn path=/trunk/boinc/; revision=23213

--- generate_svn_version.sh
+++ generate_svn_version.sh
@@ -12,6 +12,10 @@ if [ -d .git/svn ]; then
 elif [ -d .git ]; then
     GIT_LOG=`git log -n1 --pretty="format:%H"`
     HOST=`hostname`
+    BRANCH=`git branch | sed -n 's/^\* *//p'`
+    remote=`git config --get branch.$BRANCH.remote`
+    URL=`git config --get remote.$remote.url`
+    DATE=`git log -n1 --pretty="format:%ct"`
 elif [ -d .svn ]; then
     CMD="svn info"
 else
@@ -19,15 +23,26 @@ else
 fi
 
 if [ "x$GIT_LOG" != "x" ]; then
-    echo "#define SVN_VERSION \"$GIT_LOG ($HOST:$PWD)\"" >> $TMPFILE
+    echo "#define SVN_VERSION \"$GIT_LOG [$URL] ($HOST:$PWD [$BRANCH])\"" >> $TMPFILE
+    echo "$GIT_LOG" | sed 's/^\(........\).*/#define GIT_REVISION 0x\1/' >> $TMPFILE
+    echo "#define GIT_DATE $DATE" >> $TMPFILE
+    test "x$URL" = "x" ||
+        echo "$URL" |
+            sed 's/.git$//;s%.*://%%;s/[^/]*@//;s/[^a-zA-Z0-9]/_/g;s/__*/_/g;
+                 y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/;
+                 s/^/#define REPOSITORY_/;s/$/ 1/' >> $TMPFILE
 elif [ "x$CMD" != "x" ]; then
-    LANG=C $CMD | awk '/^URL/ { url = $2; }; \
-                /^Rev/ { rev = $2; }; \
+    LANG=C $CMD |
+           awk '/^URL/ { url = $2; };
+                /^Rev/ { rev = $2; };
                 END { print "#define SVN_VERSION \"Repository: " url \
-                            " Revision: " rev "\"";
-                      print "#define SVN_REPOSITORY \"" url "\""; \
-                      print "#define SVN_REVISION " rev; };' \
-               >> $TMPFILE
+                            " Revision: " rev "\"" >> "'"$TMPFILE"'";
+                      print "#define SVN_REPOSITORY \"" url "\"" >> "'"$TMPFILE"'";
+                      print "#define SVN_REVISION " rev >> "'"$TMPFILE"'";
+                      print url };' |
+            sed 's%.*://%%;s/[^/]*@//;s/[^a-zA-Z0-9]/_/g;s/__*/_/g;
+                 y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/;
+                 s/^/#define REPOSITORY_/;s/$/ 1/' >> $TMPFILE
 else
     echo "#include \"version.h\"" >> $TMPFILE
     echo "#define SVN_VERSION BOINC_VERSION_STRING" >> $TMPFILE
