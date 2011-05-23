d0ec10a8fb9b6d7e5fb13c59305d8ce341da5d88
Rom Walton <rwalton@ssl.berkeley.edu>
2011-05-23 16:35:28 +0000

    - client: implement "report immediately" at the level of         individual jobs rather than globally.         To use this, projects must add <report_immediately/>         to the <result> elements in job templates
        client/
            client_types.cpp,h
            cs_scheduler.cpp
    
    svn path=/branches/boinc_core_release_6_12/; revision=23580

--- client/cs_scheduler.cpp
+++ client/cs_scheduler.cpp
@@ -1171,6 +1171,10 @@ PROJECT* CLIENT_STATE::find_project_with_overdue_results() {
         if (p->waiting_until_min_rpc_time()) continue;
         if (p->suspended_via_gui) continue;
 
+        if (r->report_immediately) {
+            return p;
+        }
+
         if (config.report_results_immediately) {
             return p;
         }
