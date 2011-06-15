1cd1c3d5284601e72e2ba89d3f5ea8be916a8c6e
Rom Walton <rwalton@ssl.berkeley.edu>
2011-06-15 03:05:17 +0000

    Quick Updates
    svn path=/trunk/boinc/; revision=23725

--- doc/manager_links.php
+++ doc/manager_links.php
@@ -23,7 +23,27 @@ if (isset($_GET['target'])) $target = $_GET['target'];
 if (isset($_GET['version'])) $version = $_GET['version'];
 if (isset($_GET['controlid'])) $controlid = $_GET['controlid'];
 
-if (($target == "advanced") && version_compare($version, "5.10.0", ">=") && version_compare($version, "6.12.0", "<=")) {
+if ($target == "notice") {
+	if ($controlid == 'download') {
+		header('Location: http://boinc.berkeley.edu/download.php');
+	} else if ($controlid == 'statefile') {
+		header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+	} else if ($controlid == 'proxy_env') {
+		header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Environment_variables');
+	} else if ($controlid == 'app_info') {
+		header('Location: http://boinc.berkeley.edu/wiki/Anonymous_platform');
+	} else if ($controlid == 'remote_hosts') {
+		header('Location: http://boinc.berkeley.edu/wiki/Controlling_BOINC_remotely');
+	} else if ($controlid == 'log_flags') {
+		header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Logging_flags');
+	} else if ($controlid == 'config') {
+		header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Configuration_file');
+	} else {
+		header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+	}
+} else if  ($target == "advanced_preferences") {
+    header('Location: http://boinc.berkeley.edu/wiki/Preferences');
+} else if (($target == "advanced") && version_compare($version, "5.10.0", ">=") && version_compare($version, "6.12.0", "<=")) {
 	if ($controlid == "6024") {
 		header('Location: http://boinc.berkeley.edu');
 	} else if ($controlid == "6025") {
@@ -33,49 +53,47 @@ if (($target == "advanced") && version_compare($version, "5.10.0", ">=") && vers
 	} else {
 		header('Location: http://boinc.berkeley.edu/wiki/Advanced_view');
 	}
-} else if (($target == "simple") && version_compare($version, "6.2.0", ">=")) {
-    if ($controlid == "6024") {
-        // "Show info about BOINC" item on Mac simple-view menu
-        //
-        header('Location: http://boinc.berkeley.edu');
-    } else if ($controlid == "6025") {
-        // "Show info about BOINC manager" item on Mac simple-view menu
-        //
-        header('Location: http://boinc.berkeley.edu/wiki/Simple_view');
-    } else if ($controlid == "6035") {
-        // "Show info about BOINC and BOINC Manager"
-        // item on Mac simple-view menu ?? do we need this item?
-        //
-        header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
-    } else {
-        // the question-mark button
-        //
-        header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
-    }
-} else if ($target == "advanced_preferences") {
-    header('Location: http://boinc.berkeley.edu/wiki/Preferences');
-} else if ($target == "notice") {
-    if ($controlid == 'download') {
-        header('Location: http://boinc.berkeley.edu/download.php');
-    } else if ($controlid == 'statefile') {
-        header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
-    } else if ($controlid == 'proxy_env') {
-        header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Environment_variables');
-    } else if ($controlid == 'app_info') {
-        header('Location: http://boinc.berkeley.edu/wiki/Anonymous_platform');
-    } else if ($controlid == 'remote_hosts') {
-        header('Location: http://boinc.berkeley.edu/wiki/Controlling_BOINC_remotely');
-    } else if ($controlid == 'log_flags') {
-        header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Logging_flags');
-    } else if ($controlid == 'config') {
-        header('Location: http://boinc.berkeley.edu/wiki/Client_configuration#Configuration_file');
-    } else {
-        header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
-    }
+} else if (($target == "simple") && version_compare($version, "5.10.0", ">=") && version_compare($version, "6.12.0", "<=")) {
+	if ($controlid == "6024") {
+		header('Location: http://boinc.berkeley.edu');
+	} else if ($controlid == "6025") {
+		header('Location: http://boinc.berkeley.edu/wiki/Simple_view');
+	} else if ($controlid == "6035") {
+		header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+	} else {
+		header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+	}
 } else {
     if ($target == "advanced") {
-        header('Location: http://boinc.berkeley.edu/wiki/Advanced_View_6_12');
-    } else {
+		if ($controlid == "6024") {
+			header('Location: http://boinc.berkeley.edu');
+		} else if ($controlid == "6025") {
+			header('Location: http://boinc.berkeley.edu/wiki/Advanced_View_6_12');
+		} else if ($controlid == "6035") {
+			header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+		} else {
+			header('Location: http://boinc.berkeley.edu/wiki/Advanced_View_6_12');
+		}
+	} else if ($target == "simple") {
+		if ($controlid == "6024") {
+			// "Show info about BOINC" item on Mac simple-view menu
+			//
+			header('Location: http://boinc.berkeley.edu');
+		} else if ($controlid == "6025") {
+			// "Show info about BOINC manager" item on Mac simple-view menu
+			//
+			header('Location: http://boinc.berkeley.edu/wiki/Simple_view');
+		} else if ($controlid == "6035") {
+			// "Show info about BOINC and BOINC Manager"
+			// item on Mac simple-view menu ?? do we need this item?
+			//
+			header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+		} else {
+			// the question-mark button
+			//
+			header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
+		}
+	} else {
         header('Location: http://boinc.berkeley.edu/wiki/BOINC_Help');
     }
 }
