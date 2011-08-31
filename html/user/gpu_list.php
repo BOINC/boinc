0972aa28d4ec061f3e7205100140a53f676edb5a
David Anderson <davea@ssl.berkeley.edu>
2011-08-31 20:09:58 +0000

    svn path=/trunk/boinc/; revision=24099

--- html/user/gpu_list.php
+++ html/user/gpu_list.php
@@ -1,5 +1,29 @@
 <?php
 
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2011 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+// generate a page of the best-performing GPU models.
+//
+// "best-performing" is defined as minimizing the average of
+//
+// elapsed_time(J)/rsc_fpops_est(J)
+// over completed jobs J currently in the DB
+
 require_once("../inc/util.inc");
 
 function get_gpu_model($x, $vendor) {
