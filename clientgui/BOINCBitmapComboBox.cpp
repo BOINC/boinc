6d095d371e58f52905ee69740dfa538f091ed61f
Rom Walton <rwalton@ssl.berkeley.edu>
2011-11-21 19:56:44 +0000

    - MGR: Fix bad asserts.
        clientgui/
            BOINCBitmapComboBox.cpp
    
    svn path=/trunk/boinc/; revision=24641

--- clientgui/BOINCBitmapComboBox.cpp
+++ clientgui/BOINCBitmapComboBox.cpp
@@ -100,7 +100,7 @@ int CBOINCBitmapComboBox::Insert(const wxString& item, const wxBitmap& bitmap, u
 void CBOINCBitmapComboBox::Delete(unsigned int n) {
     if (n < GetCount()) {
         // Caller must have already deleted the data and set the pointer to NULL
-        wxASSERT(!m_pClientData[n]);    
+        wxASSERT(m_pClientData[n]);    
         std::vector<void*>::iterator deletionPoint = m_pClientData.begin();
         m_pClientData.erase(deletionPoint + n);
     }
@@ -114,7 +114,7 @@ void CBOINCBitmapComboBox::Clear() {
     int count = GetCount();
 	for(int j = count-1; j >=0; --j) {
         // Caller must have already deleted the data and set the pointer to NULL
-        wxASSERT(!m_pClientData[j]);    
+        wxASSERT(m_pClientData[j]);    
         m_pClientData[j] = NULL;
         }
     m_pClientData.clear();
