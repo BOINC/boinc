5fd660bd2291bfc49e8dea01f03f03555a79edc6
Rom Walton <rwalton@ssl.berkeley.edu>
2012-02-03 19:00:28 +0000

    - client: add async file copy stuff to Windows project file.
        client/
            async_file.cpp
        win_build/
            boinc_cli.vcproj
    
    svn path=/trunk/boinc/; revision=25193

--- client/async_file.cpp
+++ client/async_file.cpp
@@ -15,7 +15,11 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+#ifdef _WIN32
+#include "boinc_win.h"
+#else
 #include <string.h>
+#endif
 
 #include "error_numbers.h"
 #include "filesys.h"
