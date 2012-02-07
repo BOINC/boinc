4599eab858a9467380f378b57c4875dfbada0a3d
Charlie Fenton <charlief@example.com>
2012-02-07 04:27:09 +0000

    client: remove now-redundant "GPU n is OpenCL-capable" messages
    svn path=/trunk/boinc/; revision=25212

--- client/client_state.cpp
+++ client/client_state.cpp
@@ -406,12 +406,6 @@ int CLIENT_STATE::init() {
     if (coprocs.none() ) {
         msg_printf(NULL, MSG_INFO, "No usable GPUs found");
     }
-	for (int j=1; j<coprocs.n_rsc; j++) {
-		COPROC& cp = coprocs.coprocs[j];
-		if (cp.have_opencl) {
-			msg_printf(NULL, MSG_INFO, "%s GPU %d is OpenCL-capable", cp.type, cp.device_num);
-		}
-	}
 
     set_no_rsc_config();
 
