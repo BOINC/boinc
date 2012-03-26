6d8cf13abec82228aba75e4bea85f491c58c65f1
David Anderson <davea@ssl.berkeley.edu>
2012-03-26 22:46:02 +0000

    svn path=/trunk/boinc/; revision=25495

--- html/ops/fix_blank_user_names.php
+++ html/ops/fix_blank_user_names.php
@@ -1,4 +1,20 @@
 <?php
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2012 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
 // replace user names that are all whitespace with the user ID
 
