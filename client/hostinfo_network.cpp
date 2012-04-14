f4d985c6590d79fe439555551a1007c20819dd08
David Anderson <davea@ssl.berkeley.edu>
2012-04-14 04:48:20 +0000

    - client: add PID to random stuff used to make host CPID,     in case running multiple clients on same host
    svn path=/trunk/boinc/; revision=25559

--- client/hostinfo_network.cpp
+++ client/hostinfo_network.cpp
@@ -92,7 +92,9 @@ int HOST_INFO::get_local_network_info() {
 void HOST_INFO::make_random_string(const char* salt, char* out) {
     char buf[1024];
 
-    sprintf(buf, "%.15e%s%s%f%s", dtime(), domain_name, ip_addr, d_free, salt);
+    sprintf(buf, "%d%.15e%s%s%f%s",
+        getpid(), dtime(), domain_name, ip_addr, d_free, salt
+    );
     md5_block((const unsigned char*) buf, (int)strlen(buf), out);
 }
 
