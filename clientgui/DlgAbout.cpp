e3571aab9c93971e90e29bb7016c77b254a23cf3
Rom Walton <rwalton@ssl.berkeley.edu>
2012-04-19 20:43:35 +0000

    - Update copyrights.
        client/win/
            boinc_cli.rc
            boinc_cmd.rc
            boinc_log.rc
        clientctrl/
            boincsvcctrl.rc
        clientgui/
            BOINCGUIApp.rc
            DlgAbout.cpp
        clientscr/
            boinc_ss.rc
            boinc_ss_opengl.rc
        clienttray/
            boinc_tray.rc
    
    svn path=/trunk/boinc/; revision=25583

--- clientgui/DlgAbout.cpp
+++ clientgui/DlgAbout.cpp
@@ -178,7 +178,7 @@ void CDlgAbout::CreateControls() {
     itemFlexGridSizer7->Add(itemStaticText12, 0, wxALIGN_RIGHT|wxALIGN_TOP|wxLEFT|wxRIGHT, 5);
 
     wxStaticText* itemStaticText13 = new wxStaticText;
-    itemStaticText13->Create( itemDialog1, wxID_STATIC, _("(C) 2003-2011 University of California, Berkeley.\nAll Rights Reserved."), wxDefaultPosition, wxDefaultSize, 0 );
+    itemStaticText13->Create( itemDialog1, wxID_STATIC, _("(C) 2003-2012 University of California, Berkeley.\nAll Rights Reserved."), wxDefaultPosition, wxDefaultSize, 0 );
     itemFlexGridSizer7->Add(itemStaticText13, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT, 5);
 
     m_AboutBOINCSloganCtrl = new wxStaticText;
