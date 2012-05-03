23e69d95b0e4f1d98415e06be3fd1f6e79cbdb40
Tristan Olive <tolive@information-dynamics.com>
2012-05-03 01:22:54 -0400

    Updated username theme function to use boincuser_name rather than the silly unique IDs that drupal uses as names

--- drupal/sites/default/boinc/themes/boinc/template.php
+++ drupal/sites/default/boinc/themes/boinc/template.php
@@ -174,4 +174,51 @@ function boinc_preprocess_comment(&$vars, $hook) {
 function boinc_preprocess_block(&$vars, $hook) {
   $vars['sample_variable'] = t('Lorem ipsum.');
 }
-// */
+// */ 
+
+/**
+ * Override the username theme function so that it returns a display name
+ * rather than the unique Drupal auth name
+ */
+function phptemplate_username($object) {
+  
+  if ($object->uid && $object->name) {
+    
+    // Set the object name to use the profile name
+    $object->name = user_load($object->uid)->boincuser_name;
+    
+    // Shorten the name when it is too long or it will break many tables.
+    if (drupal_strlen($object->name) > 20) {
+      $name = drupal_substr($object->name, 0, 15) . '...';
+    }
+    else {
+      $name = $object->name;
+    }
+
+    if (user_access('access user profiles')) {
+      $output = l($name, 'user/' . $object->uid, array('attributes' => array('title' => t('View user profile.'))));
+    }
+    else {
+      $output = check_plain($name);
+    }
+  }
+  else if ($object->name) {
+    // Sometimes modules display content composed by people who are
+    // not registered members of the site (e.g. mailing list or news
+    // aggregator modules). This clause enables modules to display
+    // the true author of the content.
+    if (!empty($object->homepage)) {
+      $output = l($object->name, $object->homepage, array('attributes' => array('rel' => 'nofollow')));
+    }
+    else {
+      $output = check_plain($object->name);
+    }
+
+    $output .= ' (' . t('not verified') . ')';
+  }
+  else {
+    $output = check_plain(variable_get('anonymous', t('Anonymous')));
+  }
+
+  return $output;
+}
