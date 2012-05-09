7e16deb79096081c85924f89ca7b631618af3d6f
David Anderson <davea@ssl.berkeley.edu>
2012-05-09 18:24:54 +0000

    - compile fix
    svn path=/trunk/boinc/; revision=25663

--- lib/str_util.cpp
+++ lib/str_util.cpp
@@ -212,14 +212,15 @@ int ndays_to_string (double x, int smallest_timescale, char *buf) {
 }
 
 // convert seconds into a string "0h00m00s00"
-void secs_to_hmsf(double secs, char*buf) {
-  uint s = secs;
-  uint f = (secs - s) * 100.0;
-  uint h = s / 3600;
-  s -= h * 3600;
-  uint m = s / 60;
-  s -= m * 60;
-  sprintf(buf, "%uh%02um%02us%02u", h, m, s, f);
+//
+void secs_to_hmsf(double secs, char* buf) {
+    int s = secs;
+    int f = (secs - s) * 100.0;
+    int h = s / 3600;
+    s -= h * 3600;
+    int m = s / 60;
+    s -= m * 60;
+    sprintf(buf, "%uh%02um%02us%02u", h, m, s, f);
 }
 
 // Convert nbytes into a string.  If total_bytes is non-zero,
