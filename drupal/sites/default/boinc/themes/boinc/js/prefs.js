590f942c6b25c91f7ac8459b7ae13853b6c54a52
Tristan Olive <tolive@information-dynamics.com>
2012-06-08 01:30:35 -0400

    Added function to automatically set a preference set as "custom" when a user starts fiddling with advanced settings

---
+++ drupal/sites/default/boinc/themes/boinc/js/prefs.js
@@ -0,0 +1,10 @@
+
+  function customPrefsListener() {
+    $('.advanced-settings input, .advanced-settings select, .advanced-settings textarea').change(function(){
+      $('#edit-prefs-preset-custom').attr('checked', true);
+    });
+  };
+  
+  $(document).ready(function(){
+    customPrefsListener();
+  });
