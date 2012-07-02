1b37c5c15b7c3ce69e9f08770f51b53168f04978
Rom Walton <rwalton@ssl.berkeley.edu>
2012-07-02 17:50:31 +0000

    - client: add missing end tag for <pci_info>.  Doh!
        lib/
            coproc.cpp
    
    svn path=/branches/boinc_core_release_7_0a/; revision=25832

--- lib/coproc.cpp
+++ lib/coproc.cpp
@@ -91,7 +91,8 @@ void PCI_INFO::write(MIOFILE& f) {
         "<pci_info>\n"
         "   <bus_id>%d</bus_id>\n"
         "   <device_id>%d</device_id>\n"
-        "   <domain_id>%d</domain_id>\n",
+        "   <domain_id>%d</domain_id>\n"
+        "</pci_info>\n",
         bus_id,
         device_id,
         domain_id
