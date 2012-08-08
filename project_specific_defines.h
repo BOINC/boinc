04c485b8da3e77d70f89144c0d1f2a6bb7be6c4d
Eric J. Korpela <korpela@ssl.berkeley.edu>
2012-08-08 20:20:52 +0000

    svn path=/trunk/boinc/; revision=25994

---
+++ project_specific_defines.h
@@ -0,0 +1,18 @@
+// This is a place for project specific defines that might be necessary for your
+// builds.  Put them here so configure doesn't clobber them
+#ifndef _PROJECT_SPECIFIC_DEFINES_H_
+#define _PROJECT_SPECIFIC_DEFINES_H_ 1
+
+//#ifndef SETIATHOME
+//#define SETIATHOME 1
+//#endif
+
+//#ifndef EINSTEIN_AT_HOME
+//#define EINSTEIN_AT_HOME 1
+//#endif
+
+//#ifndef _WCG
+//#define _WCG 1
+//#endif
+
+#endif
