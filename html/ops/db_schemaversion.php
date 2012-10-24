9df3e3df95cc7f58a7f97a5a727a76b47746f1fa
David Anderson <davea@ssl.berkeley.edu>
2012-10-24 15:06:50 -0700

    - fix db revision

--- html/ops/db_schemaversion.php
+++ html/ops/db_schemaversion.php
@@ -17,10 +17,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-// Perform DB updates.
-// Used by "upgrade".
-// Can also be run manually in project/html/ops.
-
 require_once("../inc/util_ops.inc");
 
 $db_revision = 0;
@@ -30,12 +26,12 @@ if (file_exists("../../db_revision")) {
 require_once("db_update.php");
 
 $updates = array();
-foreach($db_updates as $db_update) {
+foreach ($db_updates as $db_update) {
     if ($db_update[0] > $db_revision) {
 		$db_revision = $db_update[0];
     }
 }
 
-echo "\n$db_version\n";
+echo "$db_revision\n";
 
 ?>
