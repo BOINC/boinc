959aee51d0d78b141ee640dff7a2ff0b1d30cdf8
David Anderson <davea@ssl.berkeley.edu>
2012-11-22 10:12:51 -0800

    - client: if a project is anonymous platform,     don't send list of alternate platforms in scheduler RPC.

--- client/cs_platforms.cpp
+++ client/cs_platforms.cpp
@@ -316,20 +316,21 @@ void CLIENT_STATE::detect_platforms() {
 // write XML list of supported platforms
 //
 void CLIENT_STATE::write_platforms(PROJECT* p, MIOFILE& mf) {
-
-    mf.printf(
-        "    <platform_name>%s</platform_name>\n",
-        p->anonymous_platform ? "anonymous" : get_primary_platform()
-    );
-
-    for (unsigned int i=1; i<platforms.size(); i++) {
-        PLATFORM& platform = platforms[i];
+    if (p->anonymous_platform) {
+        mf.printf("    <platform_name>anonymous</platform_name>\n");
+    } else {
         mf.printf(
-            "    <alt_platform>\n"
-            "        <name>%s</name>\n"
-            "    </alt_platform>\n",
-            platform.name.c_str()
+            "    <platform_name>%s</platform_name>\n", get_primary_platform()
         );
+        for (unsigned int i=1; i<platforms.size(); i++) {
+            PLATFORM& platform = platforms[i];
+            mf.printf(
+                "    <alt_platform>\n"
+                "        <name>%s</name>\n"
+                "    </alt_platform>\n",
+                platform.name.c_str()
+            );
+        }
     }
 }
 
