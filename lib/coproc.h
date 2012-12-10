60f750e395540996f3f0f29bb13e835e386a6981
Charlie Fenton <charlief@example.com>
2012-12-10 07:29:33 -0800

    client: COPROC_NVIDIA, COPROC_ATI and COPROC_INTEL constructors must initialize the entire struct, not just the GPU type field

--- lib/coproc.h
+++ lib/coproc.h
@@ -321,7 +321,7 @@ struct COPROC_NVIDIA : public COPROC {
     void write_xml(MIOFILE&, bool scheduler_rpc);
 #endif
     COPROC_NVIDIA(): COPROC() {
-        strcpy(type, proc_type_name_xml(PROC_TYPE_NVIDIA_GPU));
+        clear();
     }
     void get(
         bool use_all,
@@ -360,7 +360,7 @@ struct COPROC_ATI : public COPROC {
     void write_xml(MIOFILE&, bool scheduler_rpc);
 #endif
     COPROC_ATI(): COPROC() {
-        strcpy(type, proc_type_name_xml(PROC_TYPE_AMD_GPU));
+        clear();
     }
     void get(
         bool use_all,
@@ -385,7 +385,7 @@ struct COPROC_INTEL : public COPROC {
     void write_xml(MIOFILE&, bool scheduler_rpc);
 #endif
     COPROC_INTEL(): COPROC() {
-        strcpy(type, proc_type_name_xml(PROC_TYPE_INTEL_GPU));
+        clear();
     }
     void get(
         bool use_all,
