d8a78b5e4aa1083d419cf57f5c81443380c541e2
Rom Walton <romw@romwnet.org>
2013-01-18 13:28:07 -0500

    android: ugh, one more time

--- samples/wrapper/build_android.sh
+++ samples/wrapper/build_android.sh
@@ -7,15 +7,15 @@ export TCINCLUDES="$ANDROIDTC/arm-linux-androideabi"
 export TCSYSROOT="$ANDROIDTC/sysroot"
 export STDCPPTC="$TCINCLUDES/lib/libstdc++.a"
 export BOINC_DIR="../.."
-export BOINC_API_DIR="$(BOINC_DIR)/api"
-export BOINC_LIB_DIR="$(BOINC_DIR)/lib"
-export BOINC_ZIP_DIR="$(BOINC_DIR)/zip"
+export BOINC_API_DIR="$BOINC_DIR/api"
+export BOINC_LIB_DIR="$BOINC_DIR/lib"
+export BOINC_ZIP_DIR="$BOINC_DIR/zip"
 
 export PATH="$PATH:$TCBINARIES:$TCINCLUDES/bin"
 export CXX=arm-linux-androideabi-g++
 export LD=arm-linux-androideabi-ld
-export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -Wall  -funroll-loops -fexceptions -O3 -fomit-frame-pointer -I$TCINCLUDES/include -I$(BOINC_DIR) -I$(BOINC_LIB_DIR) -I$(BOINC_API_DIR) -I$(BOINC_ZIP_DIR)"
-export LDFLAGS="-L$TCSYSROOT/usr/lib -L$TCINCLUDES/lib -L$(BOINC_DIR) -L$(BOINC_LIB_DIR) -L$(BOINC_API_DIR) -L$(BOINC_ZIP_DIR) -llog"
+export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -Wall  -funroll-loops -fexceptions -O3 -fomit-frame-pointer -I$TCINCLUDES/include -I$BOINC_DIR -I$BOINC_LIB_DIR -I$BOINC_API_DIR -I$BOINC_ZIP_DIR"
+export LDFLAGS="-L$TCSYSROOT/usr/lib -L$TCINCLUDES/lib -L$BOINC_DIR -L$BOINC_LIB_DIR -L$BOINC_API_DIR -L$BOINC_ZIP_DIR -llog"
 export GDB_CFLAGS="--sysroot=$TCSYSROOT -Wall -g -I$TCINCLUDES/include"
 
 # Prepare android toolchain and environment
