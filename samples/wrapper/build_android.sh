e95265a1aa7a388be3caed5b8026cc552c474ce9
Rom Walton <romw@romwnet.org>
2013-01-18 12:28:32 -0500

    android: commit shell scripts to build uppercase and wrapper for use on Android.

---
+++ samples/wrapper/build_android.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+#script to compile Wrapper for Android
+
+export ANDROIDTC="$HOME/android-tc"
+export TCBINARIES="$ANDROIDTC/bin"
+export TCINCLUDES="$ANDROIDTC/arm-linux-androideabi"
+export TCSYSROOT="$ANDROIDTC/sysroot"
+export STDCPPTC="$TCINCLUDES/lib/libstdc++.a"
+
+export PATH="$PATH:$TCBINARIES:$TCINCLUDES/bin"
+export CC=arm-linux-androideabi-gcc
+export CXX=arm-linux-androideabi-g++
+export LD=arm-linux-androideabi-ld
+export CFLAGS="--sysroot=$TCSYSROOT -DANDROID -DDECLARE_TIMEZONE -Wall -I$TCINCLUDES/include -O3 -fomit-frame-pointer"
+export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -Wall  -I$TCINCLUDES/include -funroll-loops -fexceptions -O3 -fomit-frame-pointer"
+export LDFLAGS="-L$TCSYSROOT/usr/lib -L$TCINCLUDES/lib -llog"
+export GDB_CFLAGS="--sysroot=$TCSYSROOT -Wall -g -I$TCINCLUDES/include"
+
+# Prepare android toolchain and environment
+./build_androidtc.sh
+
+make
