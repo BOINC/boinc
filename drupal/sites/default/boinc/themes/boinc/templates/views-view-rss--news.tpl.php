b90b86770963500b09f5ab7e49a380dde1874f98
Tristan Olive <tristan.olive@studiodelta.us>
2013-06-27 12:52:28 -0400

    RSS feeds
    Removed the feed title from the template, which doesn't set the auto discovery link title, and instead set it in the view by using a null argument. Description must remain in the template.

--- drupal/sites/default/boinc/themes/boinc/templates/views-view-rss--news.tpl.php
+++ drupal/sites/default/boinc/themes/boinc/templates/views-view-rss--news.tpl.php
@@ -6,10 +6,14 @@
  * @ingroup views_templates
  */
  
+ // Set the feed description. Note that the title is set in the News view...
+ // that works there, this works here. Setting title here does not set the
+ // title in the RSS auto discovery link on the front page, and setting the
+ // description in the view does not allow the site_name variable.
  $site_name = variable_get('site_name', 'BOINC');
- $title = t('@site_name | News', array('@site_name' => $site_name));
  $description = t('The latest news from the @site_name project',
     array('@site_name' => $site_name));
+  
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
 <rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
