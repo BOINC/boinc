1dd8350c95ced85d14675c433eb23b3c9b7b12bc
David Anderson <davea@ssl.berkeley.edu>
2013-09-06 15:41:43 -0700

    client: allow <app_version> elements in app_config.xml, allowing users to override the parameters of particular app versions

--- client/app_config.cpp
+++ client/app_config.cpp
@@ -56,6 +56,26 @@ int APP_CONFIG::parse(XML_PARSER& xp, PROJECT* p) {
     return ERR_XML_PARSE;
 }
 
+int APP_VERSION_CONFIG::parse(XML_PARSER& xp, PROJECT* p) {
+    memset(this, 0, sizeof(APP_VERSION_CONFIG));
+
+    while (!xp.get_tag()) {
+        if (xp.match_tag("/app_version")) return 0;
+        if (xp.parse_str("app_name", app_name, 256)) continue;
+        if (xp.parse_str("cmdline", cmdline, 256)) continue;
+        if (xp.parse_double("avg_ncpus", avg_ncpus)) continue;
+        if (xp.parse_double("ngpus", ngpus)) continue;
+        if (log_flags.unparsed_xml) {
+            msg_printf(p, MSG_INFO,
+                "Unparsed line in app_info.xml: %s",
+                xp.parsed_tag
+            );
+        }
+        xp.skip_unexpected(log_flags.unparsed_xml, "APP_VERSION_CONFIG::parse");
+    }
+    return ERR_XML_PARSE;
+}
+
 int APP_CONFIGS::parse(XML_PARSER& xp, PROJECT* p) {
     app_configs.clear();
     if (!xp.parse_start("app_config")) return ERR_XML_PARSE;
@@ -66,6 +86,14 @@ int APP_CONFIGS::parse(XML_PARSER& xp, PROJECT* p) {
             int retval = ac.parse(xp, p);
             if (!retval) {
                 app_configs.push_back(ac);
+            }
+            continue;
+        }
+        if (xp.match_tag("app_version")) {
+            APP_VERSION_CONFIG avc;
+            int retval = avc.parse(xp, p);
+            if (!retval) {
+                app_version_configs.push_back(avc);
             }
             continue;
         }
@@ -88,17 +116,20 @@ int APP_CONFIGS::parse_file(FILE* f, PROJECT* p) {
     return retval;
 }
 
+static void show_warning(PROJECT* p, char* name) {
+    msg_printf(p, MSG_USER_ALERT,
+        "Your app_config.xml file refers to an unknown application '%s'.  Known applications: %s",
+        name, app_list_string(p).c_str()
+    );
+}
+
 void APP_CONFIGS::config_app_versions(PROJECT* p, bool show_warnings) {
-    for (unsigned int i=0; i<app_configs.size(); i++) {
+    unsigned int i;
+    for (i=0; i<app_configs.size(); i++) {
         APP_CONFIG& ac = app_configs[i];
         APP* app = gstate.lookup_app(p, ac.name);
         if (!app) {
-            if (show_warnings) {
-                msg_printf(p, MSG_USER_ALERT,
-                    "Your app_config.xml file refers to an unknown application '%s'.  Known applications: %s",
-                    ac.name, app_list_string(p).c_str()
-                );
-            }
+            if (show_warnings) show_warning(p, ac.name);
             continue;
         }
         app->max_concurrent = ac.max_concurrent;
@@ -109,6 +140,28 @@ void APP_CONFIGS::config_app_versions(PROJECT* p, bool show_warnings) {
             if (!avp->gpu_usage.rsc_type) continue;
             avp->gpu_usage.usage = ac.gpu_gpu_usage;
             avp->avg_ncpus = ac.gpu_cpu_usage;
+        }
+    }
+    for (i=0; i<app_version_configs.size(); i++) {
+        APP_VERSION_CONFIG& avc = app_version_configs[i];
+        APP* app = gstate.lookup_app(p, avc.app_name);
+        if (!app) {
+            if (show_warnings) show_warning(p, avc.app_name);
+            continue;
+        }
+        for (unsigned int j=0; j<gstate.app_versions.size(); j++) {
+            APP_VERSION* avp = gstate.app_versions[j];
+            if (avp->app != app) continue;
+            if (strcmp(avp->plan_class, avc.plan_class)) continue;
+            if (strlen(avc.cmdline)) {
+                strcpy(avp->cmdline, avc.cmdline);
+            }
+            if (avc.avg_ncpus) {
+                avp->avg_ncpus = avc.avg_ncpus;
+            }
+            if (avc.ngpus) {
+                avp->gpu_usage.usage = avc.ngpus;
+            }
         }
     }
 }
