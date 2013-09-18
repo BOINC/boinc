34933c8cd635d7b2dc22fef4c2d6eb18737aec7f
David Anderson <davea@ssl.berkeley.edu>
2013-09-17 23:13:49 -0700

    make_project: revert change that doesn't work with Apache 2.2

---
+++ db/schema_condor.sql
@@ -0,0 +1,7 @@
+create table batch_file_assoc (
+    job_file_id             integer         not null,
+    batch_id                integer         not null
+) engine=InnoDB;
+
+alter table batch_file_assoc
+    add unique(job_file_id, batch_id);
