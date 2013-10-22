efc25bbd78be2222faf8b0b0a33ffbb16c726d99
Rom Walton <romw@romwnet.org>
2013-10-22 11:19:42 -0400

    android: Add cross-compile scripts for the x86 and MIPS CPU architectures

--- android/build_openssl_x86.sh
+++ android/build_openssl_x86.sh
@@ -14,21 +14,21 @@ OPENSSL="/home/boincadm/src/openssl-1.0.1c" #openSSL sources, requiered by BOINC
 
 export ANDROIDTC="$HOME/android-tc"
 export TCBINARIES="$ANDROIDTC/bin"
-export TCINCLUDES="$ANDROIDTC/arm-linux-androideabi"
+export TCINCLUDES="$ANDROIDTC/i686-android-linux"
 export TCSYSROOT="$ANDROIDTC/sysroot"
 export STDCPPTC="$TCINCLUDES/lib/libstdc++.a"
 
 export PATH="$PATH:$TCBINARIES:$TCINCLUDES/bin"
-export CC=arm-linux-androideabi-gcc
-export CXX=arm-linux-androideabi-g++
-export LD=arm-linux-androideabi-ld
+export CC=i686-android-linux-gcc
+export CXX=i686-android-linux-g++
+export LD=i686-android-linux-ld
 export CFLAGS="--sysroot=$TCSYSROOT -DANDROID -Wall -I$TCINCLUDES/include -O3 -fomit-frame-pointer"
 export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -Wall -funroll-loops -fexceptions -O3 -fomit-frame-pointer"
 export LDFLAGS="-L$TCSYSROOT/usr/lib -L$TCINCLUDES/lib -llog"
 export GDB_CFLAGS="--sysroot=$TCSYSROOT -Wall -g -I$TCINCLUDES/include"
 
 # Prepare android toolchain and environment
-./build_androidtc.sh
+./build_androidtc_x86.sh
 
 if [ -n "$COMPILEOPENSSL" ]; then
 echo "================building openssl from $OPENSSL============================="
