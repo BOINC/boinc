0341a5d0a01f2837398c919d66e3a9cd2cf08c33
Charlie Fenton <charliefenton@CharlieFs-MacPro.local>
2013-11-18 00:56:44 -0800

    Mac installer: Make checks for OS X version consistent

--- mac_installer/uninstall.cpp
+++ mac_installer/uninstall.cpp
@@ -1603,7 +1603,7 @@ static int compareOSVersionTo(int toMajor, int toMinor) {
     if (err != noErr) {
         fprintf(stderr, "Gestalt(gestaltSystemVersionMajor) returned error %ld\n", err);
         fflush(stderr);
-        return 0;
+        return -1;  // gestaltSystemVersionMajor selector was not available before OS 10.4
     }
     if (major < toMajor) return -1;
     if (major > toMajor) return 1;
@@ -1611,7 +1611,7 @@ static int compareOSVersionTo(int toMajor, int toMinor) {
     if (err != noErr) {
         fprintf(stderr, "Gestalt(gestaltSystemVersionMinor) returned error %ld\n", err);
         fflush(stderr);
-        return 0;
+        return -1;  // gestaltSystemVersionMajor selector was not available before OS 10.4
     }
     if (minor < toMinor) return -1;
     if (minor > toMinor) return 1;
