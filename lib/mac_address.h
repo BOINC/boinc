5346ff3b7a35005541186ceafc837304dd6c3c03
David Anderson <davea@ssl.berkeley.edu>
2013-11-18 10:20:46 -0800

    BOINC lib: change get_mac_address to return just one address; prefer Ethernet

--- lib/mac_address.h
+++ lib/mac_address.h
@@ -1,9 +1,11 @@
 #ifndef MAC_ADDRESS_H
 #define MAC_ADDRESS_H
 
+// Get the MAC address of a network interface.
+// If there's more than one, prefer eth0
 // Note: the code on Mac OS X requires the following linkage flags
 //			-framework CoreFoundation -lIOKit
-bool
-get_mac_addresses(char* addresses);
+//
+int get_mac_address(char* address);
 
 #endif // MAC_ADDRESS_H
