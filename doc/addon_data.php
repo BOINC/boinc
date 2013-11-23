37237033e138259f9c0665ac4b9d1db5227c508b
David Anderson <davea@ssl.berkeley.edu>
2013-11-23 11:39:45 -0800

    web: string tweak

--- doc/addon_data.php
+++ doc/addon_data.php
@@ -91,7 +91,7 @@ array(
 array(
     'http://www.bc-team.org/downloads.php',
     'BOINCcalculator',
-    '0.4.8',
+    '0.4.9',
     'Show details of CPDN jobs',
     'http://wiki.bc-team.org/index.php?title=BOINCcalculator',
     'Windows (all)',
