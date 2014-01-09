749666727293fb95c4ff36fe9d6f18554fa92c70
Charlie Fenton <charliefenton@CharlieFs-MacPro.local>
2014-01-09 02:50:40 -0800

    MGR: Simple View fixes: * Avoid unnecessary horizontal scroll bar in Project Description text for enlarged fonts in Windows 7. * Align Task Selection control with its label on Macintosh.

--- clientgui/sg_TaskPanel.cpp
+++ clientgui/sg_TaskPanel.cpp
@@ -31,10 +31,10 @@
 #define SLIDESHOWWIDTH 290
 #define SLIDESHOWHEIGHT 126
 #define SLIDESHOWBORDER 1
-#define DESCRIPTIONSPACER 4
 #define HIDEDEFAULTSLIDE 1
 #define TESTALLDESCRIPTIONS 0
-#define SCROLLBARWIDTH 18
+#define SCROLLBARSPACER 8
+
 
 enum { suspendedIcon, waitingIcon, runningIcon };
 
@@ -55,7 +55,7 @@ CScrolledTextBox::CScrolledTextBox( wxWindow* parent) :
     SetForegroundColour(*wxBLACK);
 
     m_TextSizer = new wxBoxSizer( wxVERTICAL );
-    m_hLine = 0;
+    m_hLine = GetCharHeight();
 
     this->SetSizerAndFit( m_TextSizer );
     this->Layout();
@@ -70,7 +70,7 @@ CScrolledTextBox::~CScrolledTextBox() {
 
 
 void CScrolledTextBox::SetValue(const wxString& s) {
-    int lineHeight, totalLines, availableWidth;
+    int lineHeight, totalLines, totalWidth, usableWidth;
     wxString t = s;
 
     // Delete sizer & its children (CTransparentStaticText objects)
@@ -81,11 +81,18 @@ void CScrolledTextBox::SetValue(const wxString& s) {
     t.Replace(wxT("</sup>"), wxT(""), true);
     t.Replace(wxT("&lt;"), wxT("<"), true);
 
-    wxSize taskPanelSize = GetGrandParent()->GetSize();
-    availableWidth = taskPanelSize.GetWidth() - (2*SIDEMARGINS);
-    totalLines = Wrap(t, availableWidth - SCROLLBARWIDTH, &lineHeight);
-    
+    // First see if it fits without vertical scroll bar
+    totalWidth = GetSize().GetWidth();
+    totalLines = Wrap(t, totalWidth, &lineHeight);
     m_TextSizer->FitInside(this);
+    usableWidth = GetClientSize().GetWidth();
+    if (usableWidth < totalWidth) {
+        // It has a vertical scroll bar, so wrap again for reduced width
+        m_TextSizer->Clear(true);
+        totalLines = Wrap(t, usableWidth - SCROLLBARSPACER, &lineHeight);
+        m_TextSizer->FitInside(this);
+    }
+
     SetScrollRate(1, lineHeight);
 }
 
@@ -112,9 +119,6 @@ void CScrolledTextBox::OnOutputLine(const wxString& line) {
     if ( !line.empty() ) {
         m_TextSizer->Add(new CTransparentStaticText(this, wxID_ANY, line));
     } else { // empty line, no need to create a control for it
-        if ( !m_hLine ) {
-            m_hLine = GetCharHeight();
-        }
         m_TextSizer->Add(5, m_hLine/3);
     }
 }
@@ -428,6 +432,7 @@ END_EVENT_TABLE()
 CSimpleTaskPanel::CSimpleTaskPanel() {
 }
 
+
 CSimpleTaskPanel::CSimpleTaskPanel( wxWindow* parent ) :
     CSimplePanelBase( parent )
 {
@@ -451,7 +456,7 @@ CSimpleTaskPanel::CSimpleTaskPanel( wxWindow* parent ) :
 
     wxBoxSizer* bSizer2;
     bSizer2 = new wxBoxSizer( wxHORIZONTAL );
-    
+
     m_myTasksLabel = new CTransparentStaticText( this, wxID_ANY, _("Tasks:"), wxDefaultPosition, wxDefaultSize, 0 );
     m_myTasksLabel->Wrap( -1 );
     bSizer2->Add( m_myTasksLabel, 0, wxRIGHT, 5 );
