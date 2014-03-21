BOINC/boinc

commit fdbf20fe359290fd6f477393522e12283ad594a0

client & manager: Update icons for all client-side components.

clientgui/BOINCTaskBar.cpp
@@ -82,16 +82,21 @@ BEGIN_EVENT_TABLE(CTaskBarIcon, wxTaskBarIconEx)
 END_EVENT_TABLE()
 
 
-CTaskBarIcon::CTaskBarIcon(wxString title, wxIcon* icon, wxIcon* iconDisconnected, wxIcon* iconSnooze) : 
+CTaskBarIcon::CTaskBarIcon(wxString title, wxIconBundle* icon, wxIconBundle* iconDisconnected, wxIconBundle* iconSnooze
+#ifdef __WXMAC__
+, wxTaskBarIconType iconType
+#endif
+) :
 #ifdef __WXMAC__
     wxTaskBarIcon(DOCK)
 #else 
     wxTaskBarIconEx(wxT("BOINCManagerSystray"), 1)
 #endif
 {
-    m_iconTaskBarNormal = *icon;
-    m_iconTaskBarDisconnected = *iconDisconnected;
-    m_iconTaskBarSnooze = *iconSnooze;
+    wxSize size = wxSize(wxSystemSettings::GetMetric(wxSYS_SMALLICON_X), wxSystemSettings::GetMetric(wxSYS_SMALLICON_Y));
+    m_iconTaskBarNormal = icon->GetIcon(size);
+    m_iconTaskBarDisconnected = iconDisconnected->GetIcon(size);
+    m_iconTaskBarSnooze = iconSnooze->GetIcon(size);
     m_SnoozeGPUMenuItem = NULL;
 
     m_bTaskbarInitiatedShutdown = false;
@@ -337,9 +342,10 @@ void CTaskBarIcon::OnReloadSkin(CTaskbarEvent& WXUNUSED(event)) {
     wxASSERT(pSkinAdvanced);
     wxASSERT(wxDynamicCast(pSkinAdvanced, CSkinAdvanced));
 
-    m_iconTaskBarNormal = *pSkinAdvanced->GetApplicationIcon();
-    m_iconTaskBarDisconnected = *pSkinAdvanced->GetApplicationDisconnectedIcon();
-    m_iconTaskBarSnooze = *pSkinAdvanced->GetApplicationSnoozeIcon();
+    wxSize size = wxSize(wxSystemSettings::GetMetric(wxSYS_SMALLICON_X), wxSystemSettings::GetMetric(wxSYS_SMALLICON_Y));
+    m_iconTaskBarNormal = pSkinAdvanced->GetApplicationIcon()->GetIcon(size);
+    m_iconTaskBarDisconnected = pSkinAdvanced->GetApplicationDisconnectedIcon()->GetIcon(size);
+    m_iconTaskBarSnooze = pSkinAdvanced->GetApplicationSnoozeIcon()->GetIcon(size);
 
 #ifdef __WXMAC__
     // For unknown reasons, menus won't work if we call BuildMenu() here 

client/win/boinc_cli.rc
@@ -38,7 +38,7 @@ IDI_MAIN_ICON           ICON                    "res\\ce.ico"
 #elif defined(_WCG)
 IDI_MAIN_ICON           ICON                    "res\\wcg.ico"
 #else
-IDI_MAIN_ICON           ICON                    "res\\icon.ico"
+IDI_MAIN_ICON           ICON                    "res\\boinc.ico"
 #endif
 
 /////////////////////////////////////////////////////////////////////////////

client/win/boinc_cmd.rc
@@ -38,7 +38,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 #elif defined(_WCG)
 IDI_MAIN_ICON           ICON                    "res\\wcg.ico"
 #else
-IDI_MAIN_ICON           ICON                    "res\\icon.ico"
+IDI_MAIN_ICON           ICON                    "res\\boinc.ico"
 #endif
 
 /////////////////////////////////////////////////////////////////////////////

client/win/boinc_log.rc
@@ -36,7 +36,7 @@ LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
 #elif defined(_WCG)
 IDI_MAIN_ICON           ICON                    "res\\wcg.ico"
 #else
-IDI_MAIN_ICON           ICON                    "res\\icon.ico"
+IDI_MAIN_ICON           ICON                    "res\\boinc.ico"
 #endif
 
 /////////////////////////////////////////////////////////////////////////////

clientgui/AdvancedFrame.cpp
@@ -203,7 +203,7 @@ CAdvancedFrame::CAdvancedFrame() {
 }
 
 
-CAdvancedFrame::CAdvancedFrame(wxString title, wxIcon* icon, wxIcon* icon32, wxPoint position, wxSize size) : 
+CAdvancedFrame::CAdvancedFrame(wxString title, wxIconBundle* icons, wxPoint position, wxSize size) : 
     CBOINCBaseFrame((wxFrame *)NULL, ID_ADVANCEDFRAME, title, position, size, wxDEFAULT_FRAME_STYLE)
 {
     wxLogTrace(wxT("Function Start/End"), wxT("CAdvancedFrame::CAdvancedFrame - Function Begin"));
@@ -216,10 +216,7 @@ CAdvancedFrame::CAdvancedFrame(wxString title, wxIcon* icon, wxIcon* icon32, wxP
     m_strBaseTitle = title;
 
     // Initialize Application
-    wxIconBundle icons;
-    icons.AddIcon(*icon);
-    icons.AddIcon(*icon32);
-    SetIcons(icons);
+    SetIcons(*icons);
 
     // Create UI elements
     wxCHECK_RET(CreateMenu(), _T("Failed to create menu bar."));

clientgui/AdvancedFrame.h
@@ -54,7 +54,7 @@ class CAdvancedFrame : public CBOINCBaseFrame
 
 public:
     CAdvancedFrame();
-    CAdvancedFrame( wxString title, wxIcon* icon, wxIcon* icon32, wxPoint position, wxSize size );
+    CAdvancedFrame( wxString title, wxIconBundle* icons, wxPoint position, wxSize size );
 
     ~CAdvancedFrame(void);
 

commit 329a23b6f73dc0478988c843c77e1853c2f4670c

MGR: Fix Mac build breaks introduced by commits fdbf20f, 8d3632e, 504e9d7, 2b235d8.

clientgui/BOINCTaskBar.cpp
@@ -83,9 +83,6 @@ END_EVENT_TABLE()
 
 
 CTaskBarIcon::CTaskBarIcon(wxString title, wxIconBundle* icon, wxIconBundle* iconDisconnected, wxIconBundle* iconSnooze
-#ifdef __WXMAC__
-, wxTaskBarIconType iconType
-#endif
 ) :
 #ifdef __WXMAC__
     wxTaskBarIcon(DOCK)

clientgui/mac/MacSysMenu.cpp
@@ -116,7 +116,7 @@ public:
 #endif
 
 
-CMacSystemMenu::CMacSystemMenu(wxString title, wxIcon* icon, wxIcon* iconDisconnected, wxIcon* iconSnooze)
+CMacSystemMenu::CMacSystemMenu(wxString title, wxIconBundle* icon, wxIconBundle* iconDisconnected, wxIconBundle* iconSnooze)
                                 : CTaskBarIcon(title, icon, iconDisconnected, iconSnooze) {
      CFBundleRef	SysMenuBundle	= NULL;
 
@@ -199,9 +199,9 @@ void CMacSystemMenu::BuildMenu() {
     wxASSERT(pSkinAdvanced);
     wxASSERT(wxDynamicCast(pSkinAdvanced, CSkinAdvanced));
 
-    m_iconTaskBarNormal = *pSkinAdvanced->GetApplicationIcon();
-    m_iconTaskBarDisconnected = *pSkinAdvanced->GetApplicationDisconnectedIcon();
-    m_iconTaskBarSnooze = *pSkinAdvanced->GetApplicationSnoozeIcon();
+    m_iconTaskBarNormal = pSkinAdvanced->GetApplicationIcon()->GetIcon(GetBestIconSize());
+    m_iconTaskBarDisconnected = pSkinAdvanced->GetApplicationDisconnectedIcon()->GetIcon(GetBestIconSize());
+    m_iconTaskBarSnooze = pSkinAdvanced->GetApplicationSnoozeIcon()->GetIcon(GetBestIconSize());
 
     theBits.CopyFromIcon(m_iconTaskBarNormal);
     CGImageRef imageRef = (CGImageRef)theBits.CGImageCreate();                

clientgui/mac/MacSysMenu.h
@@ -29,7 +29,7 @@
 class CMacSystemMenu : public CTaskBarIcon
 {
 public:
-    CMacSystemMenu(wxString title, wxIcon* icon, wxIcon* iconDisconnected, wxIcon* iconSnooze);
+    CMacSystemMenu(wxString title, wxIconBundle* icon, wxIconBundle* iconDisconnected, wxIconBundle* iconSnooze);
     ~CMacSystemMenu();
 
     bool SetMacMenuIcon(const wxIcon& icon);