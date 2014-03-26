92747bcd32e3a7ae816ddceb9a1d66aae741289b
Rom Walton <rwalton@ssl.berkeley.edu>
2014-03-26 18:11:24 -0400

    - Tag for 7.2.47 release   client_release/7.2/7.2.47

--- version.h
+++ version.h
@@ -10,10 +10,10 @@
 #define BOINC_MINOR_VERSION 2
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 44
+#define BOINC_RELEASE 47
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "7.2.44"
+#define BOINC_VERSION_STRING "7.2.47"
 
 /* Package is a pre-release (Alpha/Beta) package */
 /* #define BOINC_PRERELEASE 1 */
@@ -29,13 +29,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 7.2.44"
+#define PACKAGE_STRING "BOINC 7.2.47"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.2.44"
+#define PACKAGE_VERSION "7.2.47"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
