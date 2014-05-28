52a2f1b9217f57586404b1cafae6b3cf76c7b7b6
Charlie Fenton <charlief@example.com>
2014-05-28 06:00:47 -0700

    MGR: Fix error in my commit a862fa5

--- clientgui/BOINCGUIApp.h
+++ clientgui/BOINCGUIApp.h
@@ -207,7 +207,7 @@ public:
     //
     void                HideThisApp(void);
 
-#if !wxCHECK_VERSION(3,0,0)
+#if !wxCHECK_VERSION(3,0,1)
 // This should be fixed after wxCocoa 3.0.0:
 // http://trac.wxwidgets.org/ticket/16156
 
