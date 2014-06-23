93fc60c5a7381cd368c3c7e8454d79b88f6c6cab
Rom Walton <rwalton@ssl.berkeley.edu>
2014-06-23 12:14:56 -0400

    BUILD: Update vboxwrapper's version to 26090.

--- version.h
+++ version.h
@@ -16,7 +16,7 @@
 #define WRAPPER_RELEASE 26010
 
 /* Release part of vboxwrapper version number */
-#define VBOXWRAPPER_RELEASE 26089
+#define VBOXWRAPPER_RELEASE 26090
 
 /* String representation of BOINC version number */
 #define BOINC_VERSION_STRING "7.5.0"
