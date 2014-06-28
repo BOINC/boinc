BOINC/boinc

commit 45aaaeed9955845bc1ad461c21329bd31aa11741

Fixed bad #defines MinGW and Cygwin

lib/diagnostics.h
@@ -252,7 +252,7 @@ extern format_backtrace_line_t format_backtrace_line;
 
 #if defined(__MINGW32__) || defined(__CYGWIN32__)
 #define BOINCASSERT(expr)   assert(expr)
-#define BOINCTRACE(...)     boinc_trace
+#define BOINCTRACE          boinc_trace
 #else  // __MINGW32__
 #define BOINCASSERT(expr)   __noop
 #define BOINCTRACE          boinc_trace
@@ -274,7 +274,7 @@ extern format_backtrace_line_t format_backtrace_line;
 
 #define BOINCASSERT(expr)         
 #ifndef IRIX
-#define BOINCTRACE(...)          
+#define BOINCTRACE          
 #endif
 
 #endif // _DEBUG

commit 538e1ab5d1190d9b0b23bd11d61be7c81c2478a8

MGR: Fix compiler warnings, including many caused by commit 45aaaee

clientgui/DlgAdvPreferences.cpp
@@ -779,7 +779,10 @@ bool CDlgAdvPreferences::EnsureTabPageVisible(wxTextCtrl* txtCtrl) {
 
 /* show an error message and set the focus to the control that caused the error */
 void CDlgAdvPreferences::ShowErrorMessage(wxString& message,wxTextCtrl* errorCtrl) {
-    bool visibleOK = this->EnsureTabPageVisible(errorCtrl);
+#if wxDEBUG_LEVEL   // Prevent compiler warning (unused variable)
+    bool visibleOK =
+#endif
+    this->EnsureTabPageVisible(errorCtrl);
     wxASSERT(visibleOK);
     //
     if(message.IsEmpty()){

lib/diagnostics.h
@@ -274,7 +274,11 @@ extern format_backtrace_line_t format_backtrace_line;
 
 #define BOINCASSERT(expr)         
 #ifndef IRIX
-#define BOINCTRACE          
+#if defined(__MINGW32__) || defined(__CYGWIN32__)
+#define BOINCTRACE
+#else
+#define BOINCTRACE(...)
+#endif
 #endif
 
 #endif // _DEBUG