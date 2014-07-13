c84e3f2607c8c5c4688053ab7ae1f75cc56335d7
David Anderson <davea@ssl.berkeley.edu>
2014-07-13 00:42:34 -0700

    client: fix build break

--- client/work_fetch.cpp
+++ client/work_fetch.cpp
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <cmath>
+
 #include "cpp.h"
 
 #ifdef _WIN32
