3b60fa1679b45c54c2df3b2bc93c9bc9e749a237
David Anderson <davea@ssl.berkeley.edu>
2014-08-25 06:01:47 -0700

    Manager: brighten colors in disk/stats views

--- clientgui/MainDocument.cpp
+++ clientgui/MainDocument.cpp
@@ -2626,7 +2626,7 @@ static void hsv2rgb(
 void color_cycle(int i, int n, wxColour& color) {
     double h = (double)i/(double)n;
     double r, g, b;
-    double v = .6;
+    double v = .7;
     if (n > 6) v = .5 + (i % 3)*.125;
         // cycle through 3 different brightnesses
     hsv2rgb(h*6, .5, v, r, g, b);
