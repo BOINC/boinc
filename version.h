220fe5a2148ece1618c99a4607ae6b297d6db220
Rom Walton <rwalton@ssl.berkeley.edu>
2014-09-16 18:03:01 -0400

    BUILD: Increase the build number for vboxwrapper.

--- version.h
+++ version.h
@@ -16,7 +16,7 @@
 #define WRAPPER_RELEASE 26011
 
 /* Release part of vboxwrapper version number */
-#define VBOXWRAPPER_RELEASE 26107
+#define VBOXWRAPPER_RELEASE 26108
 
 /* String representation of BOINC version number */
 #define BOINC_VERSION_STRING "7.5.0"
