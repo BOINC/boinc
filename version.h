b04287879b3f2707fe75454d1590b65ef10f4cc2
Rom Walton <romw@romwnet.org>
2015-05-11 11:45:21 -0400

    BUILD: Post-form stable release version change.

--- version.h
+++ version.h
@@ -7,10 +7,10 @@
 #define BOINC_MAJOR_VERSION 7
 
 /* Minor part of BOINC version number */
-#define BOINC_MINOR_VERSION 5
+#define BOINC_MINOR_VERSION 6
 
 /* Release part of BOINC version number */
-#define BOINC_RELEASE 1
+#define BOINC_RELEASE 0
 
 /* Release part of wrapper version number */
 #define WRAPPER_RELEASE 26014
@@ -19,10 +19,10 @@
 #define VBOXWRAPPER_RELEASE 26167
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "7.5.1"
+#define BOINC_VERSION_STRING "7.6.0"
 
 /* Package is a pre-release (Alpha/Beta) package */
-#define BOINC_PRERELEASE 1
+/* #define BOINC_PRERELEASE 1 */
 
 #if (defined(_WIN32) || defined(__APPLE__))
 /* Name of package */
@@ -35,13 +35,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 7.5.1"
+#define PACKAGE_STRING "BOINC 7.6.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.5.1"
+#define PACKAGE_VERSION "7.6.0"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
