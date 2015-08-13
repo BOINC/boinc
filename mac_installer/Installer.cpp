BOINC/boinc

commit 3e1428c8ce4c39cbafe9a0a78c40eafa4519b9d1

Mac installer: improved changes for compatibility with OS 10.11 El Capitan.

mac_installer/Installer.cpp
@@ -68,7 +68,6 @@ CFStringRef     valueNoRestart = CFSTR("NoRestart");
 int main(int argc, char *argv[])
 {
     char                    pkgPath[MAXPATHLEN];
-    char                    postInstallAppPath[MAXPATHLEN];
     char                    temp[MAXPATHLEN];
     char                    brand[64], s[256];
     char                    *p;
@@ -91,9 +90,6 @@ int main(int argc, char *argv[])
     strlcpy(temp, pkgPath, sizeof(temp));
 
     strlcat(pkgPath, "/Contents/Resources/", sizeof(pkgPath));
-    
-    strlcpy(postInstallAppPath, pkgPath, sizeof(postInstallAppPath));
-    strlcat(postInstallAppPath, "PostInstall.app", sizeof(postInstallAppPath));
 
    // To allow for branding, assume name of installer package inside bundle corresponds to name of this application
     p = strrchr(temp, '/');         // Point to name of this application (e.g., "BOINC Installer.app")
@@ -113,16 +109,12 @@ int main(int argc, char *argv[])
     // Expand the installer package
     system("rm -dfR /tmp/BOINC.pkg");
     system("rm -dfR /tmp/expanded_BOINC.pkg");
-    system("rm -dfR /tmp/PostInstall.app");
     system("rm -f /tmp/BOINC_preferred_languages");
     system("rm -f /tmp/BOINC_restart_flag");
 
-    sprintf(temp, "cp -fpR \"%s\" /tmp/PostInstall.app", postInstallAppPath);
-    err = system(temp);
-    
     sprintf(temp, "pkgutil --expand \"%s\" /tmp/expanded_BOINC.pkg", pkgPath);
     err = system(temp);
-
+    
     if (err == noErr) {
         GetPreferredLanguages();
     }

mac_installer/postinstall
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ##
-# post-Install Script for Macintosh BOINC Manager for OS X revised 3/12/14
+# post-Install Script for Macintosh BOINC Manager for OS X revised 7/2/15
 ##
 
 echo "Starting BOINC postinstall script"
@@ -14,18 +14,22 @@ if [ -f "Contents/Resources/acct_mgr_url.xml" ]; then
     cp -fp Contents/Resources/acct_mgr_url.xml "/Library/Application Support/BOINC Data/acct_mgr_url.xml"
 fi
 
+rm -f /tmp/BOINCInstallLog.txt
+
+pipath=`echo $1 | sed -e "s/BOINC.pkg/PostInstall.app/"`
+## echo "pi path = $pipath" >> /tmp/BOINCInstallLog.txt
+
 # Run the Postinstall Application
 if [ "${COMMAND_LINE_INSTALL}" = "1" ]; then
-rm -f /tmp/BOINCInstallLog.txt
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part1 >> /tmp/BOINCInstallLog.txt
+"$pipath/Contents/MacOS/PostInstall" -part1 >> /tmp/BOINCInstallLog.txt
 sleep 2
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
+"$pipath/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
 else
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part1
+"$pipath/Contents/MacOS/PostInstall" -part1
 sleep 2
 # part2 continues to run after Installer finishes, 
 # and fails unless we redirect stdout to a file
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
+"$pipath/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
 fi
 
 exit 0

mac_installer/postupgrade
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ##
-# post-Upgrade Script for Macintosh BOINC Manager for OS X revised 3/12/14
+# post-Upgrade Script for Macintosh BOINC Manager for OS X revised 7/2/15
 ##
 
 echo "Starting BOINC postupgrade script"
@@ -14,18 +14,22 @@ if [ -f "Contents/Resources/acct_mgr_url.xml" ]; then
     cp -fp Contents/Resources/acct_mgr_url.xml "/Library/Application Support/BOINC Data/acct_mgr_url.xml"
 fi
 
+rm -f /tmp/BOINCInstallLog.txt
+
+pipath=`echo $1 | sed -e "s/BOINC.pkg/PostInstall.app/"`
+## echo "pi path = $pipath" >> /tmp/BOINCInstallLog.txt
+
 # Run the Postinstall Application
 if [ "${COMMAND_LINE_INSTALL}" = "1" ]; then
-rm -f /tmp/BOINCInstallLog.txt
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part1 >> /tmp/BOINCInstallLog.txt
+"$pipath/Contents/MacOS/PostInstall" -part1 >> /tmp/BOINCInstallLog.txt
 sleep 2
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
+"$pipath/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
 else
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part1
+"$pipath/Contents/MacOS/PostInstall" -part1
 sleep 2
 # part2 continues to run after Installer finishes, 
 # and fails unless we redirect stdout to a file
-"/tmp/PostInstall.app/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
+"$pipath/Contents/MacOS/PostInstall" -part2 >> /tmp/BOINCInstallLog.txt &
 fi
 
 exit 0

commit d6a5ab2241a828dd0662c7d036d04fcef8b1d3b6

Mac installer: Fix bug introduced in my commit 3e1428c which prevents installing on OS X older than OS 10.9 unless BOINC users and groups already exist (i.e., which causes a clean  install to fail.)

mac_installer/Installer.cpp
@@ -68,6 +68,7 @@ CFStringRef     valueNoRestart = CFSTR("NoRestart");
 int main(int argc, char *argv[])
 {
     char                    pkgPath[MAXPATHLEN];
+    char                    postInstallAppPath[MAXPATHLEN];
     char                    temp[MAXPATHLEN];
     char                    brand[64], s[256];
     char                    *p;
@@ -91,6 +92,9 @@ int main(int argc, char *argv[])
 
     strlcat(pkgPath, "/Contents/Resources/", sizeof(pkgPath));
 
+    strlcpy(postInstallAppPath, pkgPath, sizeof(postInstallAppPath));
+    strlcat(postInstallAppPath, "PostInstall.app", sizeof(postInstallAppPath));
+
    // To allow for branding, assume name of installer package inside bundle corresponds to name of this application
     p = strrchr(temp, '/');         // Point to name of this application (e.g., "BOINC Installer.app")
     if (p == NULL)
@@ -109,9 +113,13 @@ int main(int argc, char *argv[])
     // Expand the installer package
     system("rm -dfR /tmp/BOINC.pkg");
     system("rm -dfR /tmp/expanded_BOINC.pkg");
+    system("rm -dfR /tmp/PostInstall.app");
     system("rm -f /tmp/BOINC_preferred_languages");
     system("rm -f /tmp/BOINC_restart_flag");
 
+    sprintf(temp, "cp -fpR \"%s\" /tmp/PostInstall.app", postInstallAppPath);
+    err = system(temp);
+    
     sprintf(temp, "pkgutil --expand \"%s\" /tmp/expanded_BOINC.pkg", pkgPath);
     err = system(temp);
     