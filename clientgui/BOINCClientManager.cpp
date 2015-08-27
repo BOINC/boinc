efe495981283f402cef0b8dfd448b76fcec1af92
Rom Walton <romw@romwnet.org>
2015-08-27 14:54:55 -0400

    MGR: Update comment to match my previous change. (reverted from commit f64d95673aa5279c7bc8fb9e671518239aa8fff9)

--- clientgui/BOINCClientManager.cpp
+++ clientgui/BOINCClientManager.cpp
@@ -447,7 +447,7 @@ void CBOINCClientManager::ShutdownBOINCCore(bool ShuttingDownManager) {
     wxASSERT(pDoc);
     wxASSERT(wxDynamicCast(pDoc, CMainDocument));
 
-    // Manager shuts down client only if Manager started client
+    // Mac Manager shuts down client only if Manager started client
     if (!m_bBOINCStartedByManager) return;
 
 #ifdef __WXMSW__
