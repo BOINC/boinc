BOINC/boinc

commit ce00365f866e97ae48b1f41ba9608dff4c459ffe

file upload handler: make uploaded file read-only to prevent repeat upload
... and if we try to upload a file and it exists and is read-only,
return success so the client won't keep trying.

sched/file_upload_handler.cpp
@@ -159,6 +159,13 @@ int copy_socket_to_file(FILE* in, char* path, double offset, double nbytes) {
                 S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH
             );
             if (fd<0) {
+                if (errno == EACCES) {
+                    // this is this case when the file was already uploaded
+                    // and made read-only;
+                    // return success to the client won't keep trying
+                    //
+                    return return_success(0);
+                }
                 return return_error(ERR_TRANSIENT,
                     "can't open file %s: %s\n", path, strerror(errno)
                 );
@@ -253,6 +260,9 @@ int copy_socket_to_file(FILE* in, char* path, double offset, double nbytes) {
 
         bytes_left -= n;
     }
+    // upload complete; make the file read-only so we won't try to upload it again.
+    //
+    fchmod(fd, S_IRUSR|S_IRGRP|S_IROTH);
     close(fd);
     return return_success(0);
 }

commit bff1cafe5022b0594a98f10e51216bf72b5aee5d

file upload handler: add logging and error checking
Add logging to the code introduced in ce00365 to eventually get to know the frequency and cause of reuploads. Also check if the file upload handler can write into the upload_dir at startup. This should more clearly detect permission issues.

sched/file_upload_handler.cpp
@@ -164,6 +164,10 @@ int copy_socket_to_file(FILE* in, char* path, double offset, double nbytes) {
                     // and made read-only;
                     // return success to the client won't keep trying
                     //
+                    log_messages.printf(MSG_CRITICAL,
+                      "client tried to reupload the read-only file %s\n",
+                      path
+                    );
                     return return_success(0);
                 }
                 return return_error(ERR_TRANSIENT,
@@ -262,7 +266,9 @@ int copy_socket_to_file(FILE* in, char* path, double offset, double nbytes) {
     }
     // upload complete; make the file read-only so we won't try to upload it again.
     //
-    fchmod(fd, S_IRUSR|S_IRGRP|S_IROTH);
+    if (fchmod(fd, S_IRUSR|S_IRGRP|S_IROTH)) {
+        log_messages.printf(MSG_CRITICAL, "can't make file %s read only: %s\n", path, strerror(errno));
+    }
     close(fd);
     return return_success(0);
 }
@@ -726,6 +732,12 @@ int main(int argc, char *argv[]) {
             return_error(ERR_TRANSIENT, "can't read key file");
             exit(1);
         }
+    }
+
+    if (access(config.upload_dir, W_OK)) {
+        log_messages.printf(MSG_CRITICAL, "can't write to upload_dir\n");
+        return_error(ERR_TRANSIENT, "can't write to upload_dir");
+        exit(1);
     }
 
 #ifdef _USING_FCGI_