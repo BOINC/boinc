BOINC/boinc

commit f0df42efbdd0938fbcc3d0e2dcf22586fe9348af

client (Unix): dlclose() dynamic libraries in GPU code

client/gpu_nvidia.cpp
@@ -299,6 +299,7 @@ void COPROC_NVIDIA::get(
     __cuMemAlloc = (int(*)(unsigned int*, size_t)) dlsym( cudalib, "cuMemAlloc" );
     __cuMemFree = (int(*)(unsigned int)) dlsym( cudalib, "cuMemFree" );
     __cuMemGetInfo = (int(*)(size_t*, size_t*)) dlsym( cudalib, "cuMemGetInfo" );
+    dlclose(cudalib);
 #endif
 
     if (!__cuDriverGetVersion) {

client/gpu_amd.cpp
@@ -115,6 +115,8 @@ void COPROC_ATI::get(
     CALuint numDevices, cal_major, cal_minor, cal_imp;
     char buf[256];
     int retval;
+    COPROC_ATI cc, cc2;
+    string s, gpu_name;
 
     attribs.struct_size = sizeof(CALdeviceattribs);
     numDevices =0;
@@ -156,7 +158,7 @@ void COPROC_ATI::get(
 
 #else
 
-    void* callib;
+    void* callib = NULL;
 
     callib = dlopen("libaticalrt.so", RTLD_NOW);
     if (!callib) {
@@ -180,63 +182,61 @@ void COPROC_ATI::get(
 
     if (!__calInit) {
         warnings.push_back("calInit() missing from CAL library");
-        return;
+        goto leave;
     }
     if (!__calGetVersion) {
         warnings.push_back("calGetVersion() missing from CAL library");
-        return;
+        goto leave;
     }
     if (!__calDeviceGetCount) {
         warnings.push_back("calDeviceGetCount() missing from CAL library");
-        return;
+        goto leave;
     }
     if (!__calDeviceGetAttribs) {
         warnings.push_back("calDeviceGetAttribs() missing from CAL library");
-        return;
+        goto leave;
     }
     if (!__calDeviceGetInfo) {
         warnings.push_back("calDeviceGetInfo() missing from CAL library");
-        return;
+        goto leave;
     }
 
     retval = (*__calInit)();
     if (retval != CAL_RESULT_OK) {
         sprintf(buf, "calInit() returned %d", retval);
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
     retval = (*__calDeviceGetCount)(&numDevices);
     if (retval != CAL_RESULT_OK) {
         sprintf(buf, "calDeviceGetCount() returned %d", retval);
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
     retval = (*__calGetVersion)(&cal_major, &cal_minor, &cal_imp);
     if (retval != CAL_RESULT_OK) {
         sprintf(buf, "calGetVersion() returned %d", retval);
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
 
     if (!numDevices) {
         warnings.push_back("No usable CAL devices found");
-        return;
+        goto leave;
     }
 
-    COPROC_ATI cc, cc2;
-    string s, gpu_name;
     for (CALuint i=0; i<numDevices; i++) {
         retval = (*__calDeviceGetInfo)(&info, i);
         if (retval != CAL_RESULT_OK) {
             sprintf(buf, "calDeviceGetInfo() returned %d", retval);
             warnings.push_back(buf);
-            return;
+            goto leave;
         }
         retval = (*__calDeviceGetAttribs)(&attribs, i);
         if (retval != CAL_RESULT_OK) {
             sprintf(buf, "calDeviceGetAttribs() returned %d", retval);
             warnings.push_back(buf);
-            return;
+            goto leave;
         }
         switch ((int)attribs.target) {
         case CAL_TARGET_600:
@@ -389,8 +389,9 @@ void COPROC_ATI::get(
     if (!ati_gpus.size()) {
         warnings.push_back("No ATI GPUs found");
     }
+leave:
+    if (callib) dlclose(callib);
 }
-
 
 void COPROC_ATI::correlate(
     bool use_all,

commit 4a536c7cba04283565312f0be19dbc1d078eb296

client: Fix crash bug introduced  in commits f0df42e and 1618ce3: do not dlclose(cudalib) until after we are finished using the CUDA library.  Also added corresponding FreeLibrary(cudalib) for MS Windows builds.

client/gpu_nvidia.cpp
@@ -209,7 +209,6 @@ CUDA_MA __cuMemAlloc = NULL;
 CUDA_MF __cuMemFree = NULL;
 CUDA_MGI __cuMemGetInfo = NULL;
 #else
-void* cudalib;
 int (*__cuInit)(unsigned int);
 int (*__cuDeviceGetCount)(int*);
 int (*__cuDriverGetVersion)(int*);
@@ -233,6 +232,9 @@ void COPROC_NVIDIA::get(
 ) {
     int cuda_ndevs, retval;
     char buf[256];
+    int j, itemp;
+    size_t global_mem = 0;
+    COPROC_NVIDIA cc;
 
 #ifdef _WIN32
     HMODULE cudalib = LoadLibrary("nvcuda.dll");
@@ -277,6 +279,8 @@ void COPROC_NVIDIA::get(
 #endif
 #else
 
+void* cudalib = NULL;
+
 #ifdef __APPLE__
     cudalib = dlopen("/usr/local/cuda/lib/libcuda.dylib", RTLD_NOW);
 #else
@@ -299,44 +303,43 @@ void COPROC_NVIDIA::get(
     __cuMemAlloc = (int(*)(unsigned int*, size_t)) dlsym( cudalib, "cuMemAlloc" );
     __cuMemFree = (int(*)(unsigned int)) dlsym( cudalib, "cuMemFree" );
     __cuMemGetInfo = (int(*)(size_t*, size_t*)) dlsym( cudalib, "cuMemGetInfo" );
-    dlclose(cudalib);
 #endif
 
     if (!__cuDriverGetVersion) {
         warnings.push_back("cuDriverGetVersion() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuInit) {
         warnings.push_back("cuInit() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuDeviceGetCount) {
         warnings.push_back("cuDeviceGetCount() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuDeviceGet) {
         warnings.push_back("cuDeviceGet() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuDeviceGetAttribute) {
         warnings.push_back("cuDeviceGetAttribute() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuDeviceTotalMem) {
         warnings.push_back("cuDeviceTotalMem() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuDeviceComputeCapability) {
         warnings.push_back("cuDeviceComputeCapability() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuMemAlloc) {
         warnings.push_back("cuMemAlloc() missing from NVIDIA library");
-        return;
+        goto leave;
     }
     if (!__cuMemFree) {
         warnings.push_back("cuMemFree() missing from NVIDIA library");
-        return;
+        goto leave;
     }
 
 #ifdef __APPLE__
@@ -355,14 +358,14 @@ void COPROC_NVIDIA::get(
     if (retval) {
         sprintf(buf, "NVIDIA drivers present but no GPUs found");
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
 
     retval = (*__cuDriverGetVersion)(&cuda_version);
     if (retval) {
         sprintf(buf, "cuDriverGetVersion() returned %d", retval);
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
 
     have_cuda = true;
@@ -371,15 +374,11 @@ void COPROC_NVIDIA::get(
     if (retval) {
         sprintf(buf, "cuDeviceGetCount() returned %d", retval);
         warnings.push_back(buf);
-        return;
+        goto leave;
     }
     sprintf(buf, "NVIDIA library reports %d GPU%s", cuda_ndevs, (cuda_ndevs==1)?"":"s");
     warnings.push_back(buf);
 
-    int j, itemp;
-    size_t global_mem = 0;
-    COPROC_NVIDIA cc;
-    string s;
     for (j=0; j<cuda_ndevs; j++) {
         memset(&cc.prop, 0, sizeof(cc.prop));
         CUdevice device;
@@ -387,13 +386,13 @@ void COPROC_NVIDIA::get(
         if (retval) {
             sprintf(buf, "cuDeviceGet(%d) returned %d", j, retval);
             warnings.push_back(buf);
-            return;
+            goto leave;
         }
         (*__cuDeviceGetName)(cc.prop.name, 256, device);
         if (retval) {
             sprintf(buf, "cuDeviceGetName(%d) returned %d", j, retval);
             warnings.push_back(buf);
-            return;
+            goto leave;
         }
         (*__cuDeviceComputeCapability)(&cc.prop.major, &cc.prop.minor, device);
         (*__cuDeviceTotalMem)(&global_mem, device);
@@ -440,6 +439,13 @@ void COPROC_NVIDIA::get(
     if (!nvidia_gpus.size()) {
         warnings.push_back("No CUDA-capable NVIDIA GPUs found");
     }
+    
+leave:
+#ifdef _WIN32
+    if (cudalib) FreeLibrary(cudalib);
+#else
+    if (cudalib) dlclose(cudalib);
+#endif
 }
 
 