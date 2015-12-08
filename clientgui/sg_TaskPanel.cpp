BOINC/boinc

commit b9fd2db943ea8efe840dfce9b6ce18b71ef2fad3

Manager: fix simple view display bug when use <dont_suspend_nci>

clientgui/sg_TaskPanel.cpp
@@ -1113,9 +1113,7 @@ void CSimpleTaskPanel::UpdateTaskSelectionList(bool reskin) {
     for(j = 0; j < count; ++j) {
         selData = (TaskSelectionData*)m_TaskSelectionCtrl->GetClientData(j);
         ctrlResult = selData->result;
-        if (Suspended() || ctrlResult->suspended_via_gui || ctrlResult->project_suspended_via_gui) {
-            newIcon = suspendedIcon;
-        } else if (isRunning(ctrlResult)) {
+        if (isRunning(ctrlResult)) {
             newIcon = runningIcon;
         } else if (ctrlResult->scheduler_state == CPU_SCHED_PREEMPTED) {
             newIcon = waitingIcon;
@@ -1150,25 +1148,7 @@ void CSimpleTaskPanel::UpdateTaskSelectionList(bool reskin) {
 
 
 bool CSimpleTaskPanel::isRunning(RESULT* result) {
-    bool outcome = false;
-
-    // It must be scheduled to be running
-    if ( result->scheduler_state == CPU_SCHED_SCHEDULED ) {
-        // If either the project or task have been suspended, then it cannot be running
-        if ( !result->suspended_via_gui && !result->project_suspended_via_gui ) {
-            CC_STATUS status;
-            CMainDocument*      pDoc = wxGetApp().GetDocument();
-            wxASSERT(pDoc);
-            
-            pDoc->GetCoreClientStatus(status);
-            // Make sure that the core client isn't global suspended for some reason
-            if ( status.task_suspend_reason == 0 || status.task_suspend_reason == SUSPEND_REASON_CPU_THROTTLE ) {
-                outcome = true;
-            }
-        }
-    }
-
-    return outcome;
+    return (result->scheduler_state == CPU_SCHED_SCHEDULED);
 }
 
 

commit 18e6ec56efc3101ec14cb1fd9dfa8eed37c9cc0f

MGR: Fix bug introduced by commit b9fd2db which incorrectly showed suspended tasks as waiting to run in Simple View

clientgui/sg_TaskPanel.cpp
@@ -1094,10 +1094,10 @@ void CSimpleTaskPanel::UpdateTaskSelectionList(bool reskin) {
         ctrlResult = selData->result;
         if (isRunning(ctrlResult)) {
             newIcon = runningIcon;
-        } else if (ctrlResult->scheduler_state == CPU_SCHED_PREEMPTED) {
-            newIcon = waitingIcon;
-        } else {
+        } else if (Suspended() || ctrlResult->suspended_via_gui || ctrlResult->project_suspended_via_gui) {
             newIcon = suspendedIcon;
+        } else {
+            newIcon = waitingIcon;
         }
 
         if (reskin || (newIcon != selData->dotColor)) {