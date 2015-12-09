f1e8e69b445b80be6de74698bce13fc2f65d1448
Eric J Korpela <korpela@ssl.berkeley.edu>
2015-12-09 13:21:31 -0800

    SETI@home specific server mods

--- project_specific_defines.h
+++ project_specific_defines.h
@@ -3,14 +3,17 @@
 #ifndef _PROJECT_SPECIFIC_DEFINES_H_
 #define _PROJECT_SPECIFIC_DEFINES_H_ 1
 
-//#ifndef SETIATHOME
-//#define SETIATHOME 1
-//#define MEGS (1048576.0)
-//#define ATI_MIN_RAM 222*MEGS
-//#define OPENCL_ATI_MIN_RAM ATI_MIN_RAM
-//#define OPENCL_NVIDIA_MIN_RAM ATI_MIN_RAM
-//#undef MEGS
-//#endif
+#ifndef SETIATHOME
+#define SETIATHOME 1
+// scheduler defines
+//    Shared Memory parameters
+#define MAX_APP_VERSIONS 100
+#define MAX_WU_RESULTS 250
+//    Plan Class defines
+#define ATI_MIN_RAM 222*MEGA
+#define OPENCL_ATI_MIN_RAM ATI_MIN_RAM
+#define OPENCL_NVIDIA_MIN_RAM ATI_MIN_RAM
+#endif
 
 //#ifndef EINSTEIN_AT_HOME
 //#define EINSTEIN_AT_HOME 1
