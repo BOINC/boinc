efcbc3c38db3d76e98f217a48cce64ecd9c6f60e
Rom Walton <romw@romwnet.org>
2016-02-07 17:20:00 -0500

    BUILD: Update vboxwrappers version to 26183.

--- version.h
+++ version.h
@@ -16,7 +16,7 @@
 #define WRAPPER_RELEASE 26016
 
 /* Release part of vboxwrapper version number */
-#define VBOXWRAPPER_RELEASE 26182
+#define VBOXWRAPPER_RELEASE 26183
 
 /* String representation of BOINC version number */
 #define BOINC_VERSION_STRING "7.7.0"
