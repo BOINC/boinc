BOINC/boinc

commit 2cc9a0b6c4da8b4f841902b07152069b17fd9a19

client: Cleanup low hanging fruit with regards to cleaning up sprintf.
Use snprintf instead.

client/app_start.cpp
@@ -351,7 +351,7 @@ static int create_dirs_for_logical_name(
 
 static void prepend_prefix(APP_VERSION* avp, char* in, char* out, int len) {
     if (strlen(avp->file_prefix)) {
-        sprintf(out, "%s/%s", avp->file_prefix, in);
+        snprintf(out, len, "%s/%s", avp->file_prefix, in);
     } else {
         strlcpy(out, in, len);
     }
@@ -395,12 +395,12 @@ int ACTIVE_TASK::setup_file(
         }
         retval = create_dirs_for_logical_name(open_name, slot_dir);
         if (retval) return retval;
-        sprintf(link_path, "%s/%s", slot_dir, open_name);
+        snprintf(link_path, sizeof(link_path), "%s/%s", slot_dir, open_name);
     } else {
-        sprintf(link_path, "%s/%s", slot_dir, fip->name);
+        snprintf(link_path, sizeof(link_path), "%s/%s", slot_dir, fip->name);
     }
 
-    sprintf(rel_file_path, "../../%s", file_path );
+    snprintf(rel_file_path, sizeof(rel_file_path), "../../%s", file_path );
 
     if (boinc_file_exists(link_path)) {
         return 0;
@@ -476,7 +476,7 @@ int ACTIVE_TASK::copy_output_files() {
         prepend_prefix(
             app_version, fref.open_name, open_name, sizeof(open_name)
         );
-        sprintf(slotfile, "%s/%s", slot_dir, open_name);
+        snprintf(slotfile, sizeof(slotfile), "%s/%s", slot_dir, open_name);
         get_pathname(fip, projfile, sizeof(projfile));
         int retval = boinc_rename(slotfile, projfile);
         // the rename fails if the output file isn't there.
@@ -606,7 +606,7 @@ int ACTIVE_TASK::start(bool test) {
     if (!app_client_shm.shm) {
         retval = get_shmem_seg_name();
         if (retval) {
-            sprintf(buf,
+            snprintf(buf, sizeof(buf),
                 "Can't get shared memory segment name: %s",
                 boincerror(retval)
             );
@@ -620,7 +620,7 @@ int ACTIVE_TASK::start(bool test) {
     init_app_init_data(aid);
     retval = write_app_init_file(aid);
     if (retval) {
-        sprintf(buf, "Can't write init file: %s", boincerror(retval));
+        snprintf(buf, sizeof(buf), "Can't write init file: %s", boincerror(retval));
         goto error;
     }
 
@@ -638,12 +638,12 @@ int ACTIVE_TASK::start(bool test) {
         get_pathname(fip, file_path, sizeof(file_path));
         if (fref.main_program) {
             if (is_image_file(fip->name)) {
-                sprintf(buf, "Main program %s is an image file", fip->name);
+                snprintf(buf, sizeof(buf), "Main program %s is an image file", fip->name);
                 retval = ERR_NO_SIGNATURE;
                 goto error;
             }
             if (!fip->executable && !wup->project->anonymous_platform) {
-                sprintf(buf, "Main program %s is not executable", fip->name);
+                snprintf(buf, sizeof(buf), "Main program %s is not executable", fip->name);
                 retval = ERR_NO_SIGNATURE;
                 goto error;
             }
@@ -697,7 +697,7 @@ int ACTIVE_TASK::start(bool test) {
 
     // remove temporary exit file from last run
     //
-    sprintf(file_path, "%s/%s", slot_dir, TEMPORARY_EXIT_FILE);
+    snprintf(file_path, sizeof(file_path), "%s/%s", slot_dir, TEMPORARY_EXIT_FILE);
     delete_project_owned_file(file_path, true);
 
     if (cc_config.exit_before_start) {
@@ -732,7 +732,8 @@ int ACTIVE_TASK::start(bool test) {
         return 0;
     }
 
-    sprintf(cmdline, "%s %s %s",
+    snprintf(cmdline, sizeof(cmdline),
+        "%s %s %s",
         exec_path, wup->command_line.c_str(), app_version->cmdline
     );
     if (!app_version->api_version_at_least(7, 5)) {
@@ -825,7 +826,7 @@ int ACTIVE_TASK::start(bool test) {
     }
 
     if (!success) {
-        sprintf(buf, "CreateProcess() failed - %s", error_msg);
+        snprintf(buf, sizeof(buf), "CreateProcess() failed - %s", error_msg);
 
         if (last_error == ERROR_NOT_ENOUGH_MEMORY) {
             // if CreateProcess() failed because system is low on memory,
@@ -884,10 +885,10 @@ int ACTIVE_TASK::start(bool test) {
     if (log_flags.task_debug) {
         debug_print_argv(argv);
     }
-    sprintf(buf, "../../%s", exec_path );
+    snprintf(buf, sizeof(buf), "../../%s", exec_path );
     pid = spawnv(P_NOWAIT, buf, argv);
     if (pid == -1) {
-        sprintf(buf, "Process creation failed: %s\n", boincerror(retval));
+        snprintf(buf, sizeof(buf), "Process creation failed: %s\n", boincerror(retval));
         chdir(current_dir);
         retval = ERR_EXEC;
         goto error;
@@ -916,11 +917,12 @@ int ACTIVE_TASK::start(bool test) {
     char current_dir[1024];
 
     if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
-        sprintf(buf, "Can't get cwd");
+        snprintf(buf, sizeof(buf), "Can't get cwd");
         goto error;
     }
 
-    sprintf(cmdline, "%s %s",
+    snprintf(cmdline, sizeof(cmdline),
+        "%s %s",
         wup->command_line.c_str(), app_version->cmdline
     );
 
@@ -940,7 +942,7 @@ int ACTIVE_TASK::start(bool test) {
         if (app_version->api_version_at_least(6, 0)) {
 #endif
             // Use mmap() shared memory
-            sprintf(buf, "%s/%s", slot_dir, MMAPPED_FILE_NAME);
+            snprintf(buf, sizeof(buf), "%s/%s", slot_dir, MMAPPED_FILE_NAME);
             if (g_use_sandbox) {
                 if (!boinc_file_exists(buf)) {
                     int fd = open(buf, O_RDWR | O_CREAT, 0660);
@@ -988,7 +990,7 @@ int ACTIVE_TASK::start(bool test) {
     }
     pid = fork();
     if (pid == -1) {
-        sprintf(buf, "fork() failed: %s", strerror(errno));
+        snprintf(buf, sizeof(buf), "fork() failed: %s", strerror(errno));
         retval = ERR_FORK;
         goto error;
     }
@@ -1013,9 +1015,9 @@ int ACTIVE_TASK::start(bool test) {
         //
         char libpath[8192];
         char newlibs[256];
-        sprintf(newlibs, "../../%s:.:../..", wup->project->project_dir());
+        snprintf(newlibs, sizeof(newlibs), "../../%s:.:../..", wup->project->project_dir());
 #ifdef __APPLE__
-        strcat(newlibs, ":/usr/local/cuda/lib/");
+        strlcat(newlibs, sizeof(newlibs), ":/usr/local/cuda/lib/");
 #endif
         char* p = getenv("LD_LIBRARY_PATH");
         if (p) {
@@ -1106,11 +1108,12 @@ int ACTIVE_TASK::start(bool test) {
         if (test) {
             strcpy(buf, exec_path);
         } else {
-            sprintf(buf, "../../%s", exec_path);
+            snprintf(buf, sizeof(buf), "../../%s", exec_path);
         }
         if (g_use_sandbox) {
             char switcher_path[MAXPATHLEN];
-            sprintf(switcher_path, "../../%s/%s",
+            snprintf(switcher_path, sizeof(switcher_path), 
+                "../../%s/%s",
                 SWITCHER_DIR, SWITCHER_FILE_NAME
             );
             argv[0] = const_cast<char*>(SWITCHER_FILE_NAME);
@@ -1218,7 +1221,7 @@ int ACTIVE_TASK::resume_or_start(bool first_time) {
         char buf[256];
         safe_strcpy(buf, "");
         if (strlen(app_version->plan_class)) {
-            sprintf(buf, " (%s)", app_version->plan_class);
+            snprintf(buf, sizeof(buf), " (%s)", app_version->plan_class);
         }
         msg_printf(result->project, MSG_INFO,
             "[cpu_sched] %s task %s using %s version %d%s in slot %d",

client/acct_mgr.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
+
 #include "crypt.h"
 #include "error_numbers.h"
 #include "parse.h"
@@ -201,7 +205,7 @@ int ACCT_MGR_OP::do_rpc(
     gstate.net_stats.write(mf);
     fprintf(f, "</acct_mgr_request>\n");
     fclose(f);
-    sprintf(buf, "%srpc.php", url);
+    snprintf(buf, sizeof(buf), "%srpc.php", url);
     retval = gui_http->do_rpc_post(
         this, buf, ACCT_MGR_REQUEST_FILENAME, ACCT_MGR_REPLY_FILENAME, true
     );

client/app.cpp
@@ -52,6 +52,9 @@
 #include <cstdlib>
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
 
 #include "error_numbers.h"
 #include "filesys.h"
@@ -517,8 +520,11 @@ int ACTIVE_TASK::move_trickle_file() {
     char new_path[MAXPATHLEN], old_path[MAXPATHLEN];
     int retval;
 
-    sprintf(old_path, "%s/trickle_up.xml", slot_dir);
-    sprintf(new_path,
+    snprintf(old_path, sizeof(old_path),
+        "%s/trickle_up.xml",
+        slot_dir
+    );
+    snprintf(new_path, sizeof(new_path),
         "%s/trickle_up_%s_%d.xml",
         result->project->project_dir(), result->name, (int)time(0)
     );
@@ -1060,7 +1066,7 @@ int ACTIVE_TASK::handle_upload_files() {
                     "Can't find uploadable file %s", p
                 );
             }
-            sprintf(path, "%s/%s", slot_dir, buf);
+            snprintf(path, sizeof(path), "%s/%s", slot_dir, buf);
             delete_project_owned_file(path, true);  // delete the link file
         }
     }
@@ -1095,7 +1101,10 @@ void ACTIVE_TASK_SET::network_available() {
 
 void ACTIVE_TASK::upload_notify_app(const FILE_INFO* fip, const FILE_REF* frp) {
     char path[MAXPATHLEN];
-    sprintf(path, "%s/%s%s", slot_dir, UPLOAD_FILE_STATUS_PREFIX, frp->open_name);
+    snprintf(path, sizeof(path), 
+        "%s/%s%s",
+        slot_dir, UPLOAD_FILE_STATUS_PREFIX, frp->open_name
+    );
     FILE* f = boinc_fopen(path, "w");
     if (!f) return;
     fprintf(f, "<status>%d</status>\n", fip->status);

client/async_file.cpp
@@ -21,6 +21,10 @@
 #include <string.h>
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
+
 #include "crypt.h"
 #include "error_numbers.h"
 #include "filesys.h"

client/client_state.cpp
@@ -36,6 +36,10 @@
 #endif
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
+
 #ifdef __EMX__
 #define INCL_DOS
 #include <os2.h>
@@ -412,7 +416,7 @@ static void set_client_priority() {
 #endif
 #ifdef __linux__
     char buf[1024];
-    sprintf(buf, "ionice -c 3 -p %d", getpid());
+    snprintf(buf, sizeof(buf), "ionice -c 3 -p %d", getpid());
     system(buf);
 #endif
 }
@@ -1003,14 +1007,14 @@ bool CLIENT_STATE::poll_slow_events() {
         if (!old_network_suspend_reason) {
             char buf[256];
             if (network_suspended) {
-                sprintf(buf,
+                snprintf(buf, sizeof(buf),
                     "Suspending network activity - %s",
                     suspend_reason_string(network_suspend_reason)
                 );
                 request_schedule_cpus("network suspended");
                     // in case any "needs_network" jobs are running
             } else {
-                sprintf(buf,
+                snprintf(buf, sizeof(buf),
                     "Suspending file transfers - %s",
                     suspend_reason_string(network_suspend_reason)
                 );
@@ -2080,7 +2084,7 @@ int CLIENT_STATE::detach_project(PROJECT* project) {
 
     // delete statistics file
     //
-    get_statistics_filename(project->master_url, path);
+    get_statistics_filename(project->master_url, path, sizeof(path));
     retval = boinc_delete_file(path);
     if (retval) {
         msg_printf(project, MSG_INTERNAL_ERROR,
@@ -2090,7 +2094,7 @@ int CLIENT_STATE::detach_project(PROJECT* project) {
 
     // delete account file
     //
-    get_account_filename(project->master_url, path);
+    get_account_filename(project->master_url, path, sizeof(path));
     retval = boinc_delete_file(path);
     if (retval) {
         msg_printf(project, MSG_INTERNAL_ERROR,

client/client_types.cpp
@@ -37,6 +37,10 @@
 #include <cstring>
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
+
 #include "error_numbers.h"
 #include "filesys.h"
 #include "log_flags.h"
@@ -598,7 +602,7 @@ int FILE_INFO::merge_info(FILE_INFO& new_info) {
 
     if (max_nbytes <= 0 && new_info.max_nbytes) {
         max_nbytes = new_info.max_nbytes;
-        sprintf(buf, "    <max_nbytes>%.0f</max_nbytes>\n", new_info.max_nbytes);
+        snprintf(buf, sizeof(buf), "    <max_nbytes>%.0f</max_nbytes>\n", new_info.max_nbytes);
     }
 
     // replace existing URLs with new ones
@@ -668,7 +672,7 @@ bool FILE_INFO::had_failure(int& failnum) {
 
 void FILE_INFO::failure_message(string& s) {
     char buf[1024];
-    sprintf(buf,
+    snprintf(buf, sizeof(buf), 
         "<file_xfer_error>\n"
         "  <file_name>%s</file_name>\n"
         "  <error_code>%d (%s)</error_code>\n",
@@ -677,7 +681,7 @@ void FILE_INFO::failure_message(string& s) {
     );
     s = buf;
     if (error_msg.size()) {
-        sprintf(buf,
+        snprintf(buf, sizeof(buf),
             "  <error_message>%s</error_message>\n",
             error_msg.c_str()
             );

client/cs_account.cpp
@@ -59,7 +59,7 @@ int PROJECT::write_account_file() {
     FILE* f;
     int retval;
 
-    get_account_filename(master_url, path);
+    get_account_filename(master_url, path, sizeof(path));
     f = boinc_fopen(TEMP_ACCT_FILE_NAME, "w");
     if (!f) return ERR_FOPEN;
 
@@ -202,7 +202,7 @@ int PROJECT::parse_account_file_venue() {
     bool in_right_venue = false, btemp;
     double dtemp;
 
-    get_account_filename(master_url, path);
+    get_account_filename(master_url, path, sizeof(path));
     FILE* in = boinc_fopen(path, "r");
     if (!in) return ERR_FOPEN;
 
@@ -291,7 +291,7 @@ int PROJECT::parse_account_file() {
     int retval;
     FILE* f;
 
-    get_account_filename(master_url, path);
+    get_account_filename(master_url, path, sizeof(path));
     f = boinc_fopen(path, "r");
     if (!f) return ERR_FOPEN;
     retval = parse_account(f);
@@ -464,7 +464,7 @@ int PROJECT::write_statistics_file() {
     FILE* f;
     int retval;
 
-    get_statistics_filename(master_url, path);
+    get_statistics_filename(master_url, path, sizeof(path));
     f = boinc_fopen(TEMP_STATS_FILE_NAME, "w");
     if (!f) return ERR_FOPEN;
     fprintf(f, 
@@ -551,7 +551,7 @@ int CLIENT_STATE::add_project(
         return retval;
     }
 
-    get_account_filename(canonical_master_url, path);
+    get_account_filename(canonical_master_url, path, sizeof(path));
     f = boinc_fopen(path, "r");
     if (!f) {
         delete project;

client/cs_benchmark.cpp
@@ -54,6 +54,10 @@
 #include <ctime>
 #endif
 
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
+
 #include "error_numbers.h"
 #include "file_names.h"
 #include "filesys.h"
@@ -190,7 +194,7 @@ int cpu_benchmarks(BENCHMARK_DESC* bdp) {
 #endif
     if (retval) {
         bdp->error = true;
-        sprintf(bdp->error_str, "FP benchmark ran only %f sec; ignoring", fp_time);
+        snprintf(bdp->error_str, sizeof(bdp->error_str), "FP benchmark ran only %f sec; ignoring", fp_time);
         return 0;
     }
 #ifdef _WIN32
@@ -204,7 +208,7 @@ int cpu_benchmarks(BENCHMARK_DESC* bdp) {
     retval = dhrystone(vax_mips, int_loops, int_time, MIN_CPU_TIME);
     if (retval) {
         bdp->error = true;
-        sprintf(bdp->error_str, "Integer benchmark ran only %f sec; ignoring", int_time);
+        snprintf(bdp->error_str, sizeof(bdp->error_str), "Integer benchmark ran only %f sec; ignoring", int_time);
         return 0;
     }
     host_info.p_iops = vax_mips*1e6;

commit 55efe2444d78b13075bb1b8bb18d30420140e8b3

client: fix build break on Mac introduced by commit 2cc9a0b,

client/app_start.cpp
@@ -1017,7 +1017,7 @@ int ACTIVE_TASK::start(bool test) {
         char newlibs[256];
         snprintf(newlibs, sizeof(newlibs), "../../%s:.:../..", wup->project->project_dir());
 #ifdef __APPLE__
-        strlcat(newlibs, sizeof(newlibs), ":/usr/local/cuda/lib/");
+        safe_strcat(newlibs, ":/usr/local/cuda/lib/");
 #endif
         char* p = getenv("LD_LIBRARY_PATH");
         if (p) {