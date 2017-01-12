7cb58b5268e49751e0b16adf99319b01832b33ee
David Anderson <davea@ssl.berkeley.edu>
2017-01-12 13:15:42 -0800

    client: include # GPUs in <host_info> XML, including acct mgr requests

--- client/acct_mgr.cpp
+++ client/acct_mgr.cpp
@@ -189,7 +189,6 @@ int ACCT_MGR_OP::do_rpc(
             "      <gpu_time>%f</gpu_time>\n"
             "      <njobs_success>%d</njobs_success>\n"
             "      <njobs_fail>%d</njobs_fail>\n"
-            "      <usable_gpu>%d</usable_gpu>\n"
             "   </project>\n",
             p->master_url,
             p->project_name,
@@ -208,8 +207,7 @@ int ACCT_MGR_OP::do_rpc(
             p->gpu_ec,
             p->gpu_time,
             p->njobs_success,
-            p->njobs_fail,
-            coprocs.n_rsc>1?1:0
+            p->njobs_fail
         );
     }
     MIOFILE mf;
