8e84ff30b7d29e83c1677a040179b533436b4860
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-01-24 02:13:06 +0200

    client: Version check. Fix message about new version.

--- client/current_version.cpp
+++ client/current_version.cpp
@@ -87,15 +87,15 @@ static void show_newer_version_msg(const char* new_vers) {
     if (cc_config.client_new_version_text.empty()) {
         msg_printf_notice(0, true,
             "http://boinc.berkeley.edu/manager_links.php?target=notice&controlid=download",
-            "%s (%s) <a href=%s>%s</a>",
-            _("A new version of BOINC is available."),
+            "%s (%s). <a href=%s>%s</a>",
+            _("A new version of BOINC is available"),
             new_vers,
             cc_config.client_download_url.c_str(),
             _("Download")
         );
     } else {
         msg_printf_notice(0, true, NULL,
-            "%s (%s) <a href=%s>%s</a>",
+            "%s (%s). <a href=%s>%s</a>",
             cc_config.client_new_version_text.c_str(),
             new_vers,
             cc_config.client_download_url.c_str(),
