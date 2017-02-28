BOINC/boinc

commit 91784e996cccb8cf459e70d5bca1d5f46889f319

MGR: Mac: replace all deprecated Mac APIs as of OS 10.8

clientgui/MainDocument.cpp
@@ -1817,8 +1817,9 @@ int CMainDocument::WorkShowGraphics(RESULT* rp) {
 
 int CMainDocument::WorkShowVMConsole(RESULT* res) {
     int iRetVal = 0;
+    char pathToCoRD[MAXPATHLEN];
     
-    if (strlen(res->remote_desktop_addr)) {
+//    if (strlen(res->remote_desktop_addr)) {
         wxString strConnection(res->remote_desktop_addr, wxConvUTF8);
         wxString strCommand;
 
@@ -1829,7 +1830,6 @@ int CMainDocument::WorkShowVMConsole(RESULT* res) {
         strCommand = wxT("rdesktop-vrdp ") + strConnection;
         wxExecute(strCommand);
 #elif defined(__WXMAC__)
-        CFURLRef appURL = NULL;
         OSStatus status = noErr;
 
         // I have found no reliable way to pass the IP address and port to Microsoft's 
@@ -1837,11 +1837,7 @@ int CMainDocument::WorkShowVMConsole(RESULT* res) {
         // Unfortunately, CoRD does not seem as reliable as I would like either.
         //
         // First try to find the CoRD application by Bundle ID and Creator Code
-        status = LSFindApplicationForInfo('RDC#', CFSTR("net.sf.cord"),   
-                                            NULL, NULL, &appURL);
-        if (appURL) {
-            CFRelease(appURL);
-        }
+        status = GetPathToAppFromID('RDC#', CFSTR("net.sf.cord"), pathToCoRD, MAXPATHLEN);
         if (status != noErr) {
             CBOINCBaseFrame* pFrame = wxGetApp().GetFrame();
             if (pFrame) {
@@ -1853,7 +1849,7 @@ int CMainDocument::WorkShowVMConsole(RESULT* res) {
                 );
         } 
         return ERR_FILE_MISSING;
-    }
+//    }
 
     strCommand = wxT("osascript -e 'tell application \"CoRD\"' -e 'activate' -e 'open location \"rdp://") + strConnection + wxT("\"' -e 'end tell'");
     strCommand.Replace(wxT("localhost"), wxT("127.0.0.1"));

clientgui/sg_BoincSimpleFrame.cpp
@@ -52,8 +52,7 @@
 
 #ifdef __WXMAC__
 #include "util.h"
-
-static int compareOSVersionTo(int toMajor, int toMinor);
+#include "mac_util.h"
 #endif
 
 // Workaround for Linux refresh problem
@@ -1351,29 +1350,3 @@ void CSimpleGUIPanel::OnEraseBackground(wxEraseEvent& event) {
 #endif
     dc->DrawBitmap(m_bmpBg, 0, 0);
 }
-
-
-#ifdef __WXMAC__
-static int compareOSVersionTo(int toMajor, int toMinor) {
-    SInt32 major, minor;
-    OSStatus err = noErr;
-    
-    err = Gestalt(gestaltSystemVersionMajor, &major);
-    if (err != noErr) {
-        fprintf(stderr, "Gestalt(gestaltSystemVersionMajor) returned error %ld\n", err);
-        fflush(stderr);
-        return -1;  // gestaltSystemVersionMajor selector was not available before OS 10.4
-    }
-    if (major < toMajor) return -1;
-    if (major > toMajor) return 1;
-    err = Gestalt(gestaltSystemVersionMinor, &minor);
-    if (err != noErr) {
-        fprintf(stderr, "Gestalt(gestaltSystemVersionMinor) returned error %ld\n", err);
-        fflush(stderr);
-        return -1;  // gestaltSystemVersionMajor selector was not available before OS 10.4
-    }
-    if (minor < toMinor) return -1;
-    if (minor > toMinor) return 1;
-    return 0;
-}
-#endif

commit 9edb755852f5d755f1671dcbf9f17fca94d44357

MGR: Fix bug I introduced in commit 91784e9 earlier today.

clientgui/MainDocument.cpp
@@ -1817,9 +1817,8 @@ int CMainDocument::WorkShowGraphics(RESULT* rp) {
 
 int CMainDocument::WorkShowVMConsole(RESULT* res) {
     int iRetVal = 0;
-    char pathToCoRD[MAXPATHLEN];
     
-//    if (strlen(res->remote_desktop_addr)) {
+    if (strlen(res->remote_desktop_addr)) {
         wxString strConnection(res->remote_desktop_addr, wxConvUTF8);
         wxString strCommand;
 
@@ -1831,6 +1830,7 @@ int CMainDocument::WorkShowVMConsole(RESULT* res) {
         wxExecute(strCommand);
 #elif defined(__WXMAC__)
         OSStatus status = noErr;
+        char pathToCoRD[MAXPATHLEN];
 
         // I have found no reliable way to pass the IP address and port to Microsoft's 
         // Remote Desktop Connection application for the Mac, so I'm using CoRD.  
@@ -1849,7 +1849,7 @@ int CMainDocument::WorkShowVMConsole(RESULT* res) {
                 );
         } 
         return ERR_FILE_MISSING;
-//    }
+    }
 
     strCommand = wxT("osascript -e 'tell application \"CoRD\"' -e 'activate' -e 'open location \"rdp://") + strConnection + wxT("\"' -e 'end tell'");
     strCommand.Replace(wxT("localhost"), wxT("127.0.0.1"));