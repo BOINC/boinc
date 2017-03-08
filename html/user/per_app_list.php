92d6110477b61cc1a1a49ae041cb62f33633a3a0
nikolasgianna <nikolagiannakis@gmail.com>
2017-03-08 17:33:23 +0100

    Added pagination and first level caching to per_app_list

--- html/user/per_app_list.php
+++ html/user/per_app_list.php
@@ -26,6 +26,10 @@
 require_once("../inc/util.inc");
 require_once("../inc/team.inc");
 
+check_get_args(array("is_team", "appid", "is_total", "offset"));
+
+define ('ITEM_LIMIT', 10000);
+
 // return a column title (Average or Total),
 // hyperlinked if this is not the current sort column
 //
@@ -63,13 +67,20 @@ function show_header($is_team, $apps, $appid, $is_total) {
 // show a user or team, with their credit for each app
 //
 function show_row($item, $apps, $is_team, $i) {
+    global $i;
     if ($is_team) {
         $team = BoincTeam::lookup_id($item->teamid);
-        if (!$team) return;
+        if (!$team) {
+            $i--;
+            return;
+        }
         $x = "<td>".team_links($team)."</td>\n";
     } else {
         $user = BoincUser::lookup_id($item->userid);
-        if (!$user) return;
+        if (!$user) {
+            $i--;
+            return;
+        }
         $x= "<td>".user_links($user, BADGE_HEIGHT_MEDIUM)."</td>\n";
     }
     echo "<tr>";
@@ -96,34 +107,72 @@ function show_row($item, $apps, $is_team, $i) {
     echo "</tr>\n";
 }
 
-function show_list($is_team, $appid, $is_total) {
-    $x = $is_team?"teams":"participants";
-    page_head("Top $x by application");
-    $apps = BoincApp::enum("deprecated=0");
-    if (!$appid) {
-        $appid = $apps[0]->id;
-    }
-    start_table('table-striped');
-    show_header($is_team, $apps, $appid, $is_total);
+function get_top_items($is_team, $appid, $is_total, $offset) {
+    global $items_per_page;
     $x = $is_total?"total":"expavg";
     if ($is_team) {
-        $items = BoincCreditTeam::enum("appid=$appid order by $x desc");
+        return  BoincCreditTeam::enum("appid=$appid order by $x desc limit $offset,$items_per_page");
     } else {
-        $items = BoincCreditUser::enum("appid=$appid order by $x desc");
+        return  BoincCreditUser::enum("appid=$appid order by $x desc limit $offset,$items_per_page");
     }
-    $i = 0;
-    foreach ($items as $item) {
-        show_row($item, $apps, $is_team, $i);
-        $i++;
-    }
-    end_table();
-    page_tail();
 }
 
 $is_team = get_int('is_team', true);
 $appid = get_int('appid', true);
 $is_total = get_int('is_total', true);
+$items_per_page = 20;
+$offset = get_int('offset', true);
+if (!$offset) $offset=0;
+if ($offset % $items_per_page) $offset = 0;
+$x = $is_team?"teams":"participants";
+page_head("Top $x by application");
+$apps = BoincApp::enum("deprecated=0");
+if (!$appid) {
+    $appid = $apps[0]->id;
+}
 
-show_list($is_team, $appid, $is_total);
+if ($offset < ITEM_LIMIT) {
+     $cache_args = "appid=$appid&is_team=$is_team&is_total=$is_total&offset=$offset";
+     $cacheddata = get_cached_data(TOP_PAGES_TTL,$cache_args);
+
+     // Do we have the data in cache?
+     //
+     if ($cacheddata){
+         $data = unserialize($cacheddata); // use the cached data
+     } else {
+         //if not do queries etc to generate new data
+         $data = get_top_items($is_team, $appid, $is_total, $offset);
+
+         //save data in cache
+         //
+         set_cached_data(TOP_PAGES_TTL, serialize($data),$cache_args);
+       }
+} else {
+    error_page(tra("Limit exceeded - Sorry, first %1 items only", ITEM_LIMIT));
+  }
+
+start_table('table_striped');
+show_header($is_team, $apps, $appid, $is_total);
+$i = 1 + $offset;
+$n = sizeof($data);
+
+foreach ($data as $item) {
+         show_row($item, $apps, $is_team, $i);
+         $i++;
+}
+
+end_table();
+
+if ($offset > 0) {
+    $new_offset = $offset - $items_per_page;
+    echo "<a href=per_app_list.php?appid=$appid&is_team=$is_team&is_total=$is_total&offset=$new_offset>".tra("Previous %1", $items_per_page)."</a> &middot; ";
+}
+
+if ($n==$items_per_page) {
+    $new_offset = $offset + $items_per_page;
+    echo "<a href=per_app_list.php?appid=$appid&is_team=$is_team&is_total=$is_total&offset=$new_offset>".tra("Next %1", $items_per_page)."</a>";
+}
+
+page_tail();
 
 ?>
