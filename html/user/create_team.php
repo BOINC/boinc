04c3e538c7e77e14bb07ee3e78a0641bb651fef9
David Anderson <davea@ssl.berkeley.edu>
2017-03-27 12:17:38 -0700

    disable team creation by web RPC
    This was being used by spammers.
    I can't think of a legit use of it off the top of my head.

--- html/user/create_team.php
+++ html/user/create_team.php
@@ -16,10 +16,15 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+// Disabled.  being used by spammers,
+// and I can't think of a legit use for this.
+
 require_once("../inc/boinc_db.inc");
 require_once("../inc/xml.inc");
 require_once("../inc/team.inc");
 require_once("../inc/team_types.inc");
+
+xml_error(-1, "Nope");
 
 if (DISABLE_TEAMS) xml_error(-1, "Teams are disabled");
 
