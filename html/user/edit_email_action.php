e76e339c641317f133c9618d7fa1e41086a18098
David Anderson <davea@ssl.berkeley.edu>
2017-06-26 13:48:38 -0700

    web: fix error when user updates email addr
    Also fix error that showed credit in non-computing projects

--- html/user/edit_email_action.php
+++ html/user/edit_email_action.php
@@ -19,7 +19,7 @@
 require_once("../inc/boinc_db.inc");
 require_once("../inc/util.inc");
 require_once("../inc/email.inc");
-require_once("../inc/user.inc");
+require_once("../inc/user_util.inc");
 
 check_get_args(array());
 
