BOINC/boinc

commit 671446ced132dbfffd2c7001df3c3b78535fd3db

code cleanup: in scheduler, factor project prefs into their own struct

sched/sched_array.cpp
@@ -381,7 +381,7 @@ void send_work_old() {
     // (projects should load beta work with care,
     // otherwise your users won't get production work done!
     //
-    if (g_wreq->allow_beta_work) {
+    if (g_wreq->project_prefs.allow_beta_work) {
         g_wreq->beta_only = true;
         if (config.debug_send_scan) {
             log_messages.printf(MSG_NORMAL,
@@ -429,12 +429,12 @@ void send_work_old() {
     // If user has selected apps but will accept any,
     // and we haven't found any jobs for selected apps, try others
     //
-    if (!g_wreq->njobs_sent && g_wreq->allow_non_preferred_apps ) {
+    if (!g_wreq->njobs_sent && g_wreq->project_prefs.allow_non_selected_apps ) {
         g_wreq->user_apps_only = false;
-        preferred_app_message_index = g_wreq->no_work_messages.size();
+        selected_app_message_index = g_wreq->no_work_messages.size();
         if (config.debug_send_scan) {
             log_messages.printf(MSG_NORMAL,
-                "[send_scan] scanning for jobs from non-preferred applications\n"
+                "[send_scan] scanning for jobs from non-selected applications\n"
             );
         }
         if (scan_work_array()) return;

sched/sched_locality.cpp
@@ -296,7 +296,7 @@ static int possibly_send_result(SCHED_DB_RESULT& result) {
     retval = wu.lookup_id(result.workunitid);
     if (retval) return ERR_DB_NOT_FOUND;
 
-    // This doesn't take into account g_wreq->allow_non_preferred_apps,
+    // This doesn't take into account g_wreq->allow_non_selected_apps,
     // however Einstein@Home, which is the only project that currently uses
     // this locality scheduler, doesn't support the respective project-specific
     // preference setting

sched/sched_nci.cpp
@@ -163,14 +163,14 @@ int send_nci() {
             }
             continue;
         }
-        if (app.beta  && !g_wreq->allow_beta_work) {
+        if (app.beta  && !g_wreq->project_prefs.allow_beta_work) {
             if (config.debug_send) {
                 log_messages.printf(MSG_NORMAL, "%s is beta\n", app.name);
             }
             continue;
         }
         if (app_not_selected(app.id)) {
-            if (!g_wreq->allow_non_preferred_apps) {
+            if (!g_wreq->project_prefs.allow_non_selected_apps) {
                 if (config.debug_send) {
                     log_messages.printf(MSG_NORMAL,
                         "%s is not selected\n", app.name

sched/sched_score.cpp
@@ -64,7 +64,7 @@ bool JOB::get_score(WU_RESULT& wu_result) {
     }
 
     if (app->beta) {
-        if (g_wreq->allow_beta_work) {
+        if (g_wreq->project_prefs.allow_beta_work) {
             score += 1;
         } else {
             if (config.debug_send_job) {
@@ -78,7 +78,7 @@ bool JOB::get_score(WU_RESULT& wu_result) {
     }
 
     if (app_not_selected(app->id)) {
-        if (g_wreq->allow_non_preferred_apps) {
+        if (g_wreq->project_prefs.allow_non_selected_apps) {
             score -= 1;
         } else {
             if (config.debug_send_job) {

sched/sched_types.cpp
@@ -169,6 +169,58 @@ int CLIENT_PLATFORM::parse(XML_PARSER& xp) {
     return ERR_XML_PARSE;
 }
 
+// Parse user's project preferences.
+// TODO: use XML_PARSER
+//
+void PROJECT_PREFS::parse() {
+    char buf[8096];
+    std::string str;
+    unsigned int pos = 0;
+    int temp_int=0;
+    bool flag;
+
+    extract_venue(g_reply->user.project_prefs, g_reply->host.venue, buf, sizeof(buf));
+    str = buf;
+
+    // scan user's project prefs for elements of the form <app_id>N</app_id>,
+    // indicating the apps they want to run.
+    //
+    selected_apps.clear();
+    while (parse_int(str.substr(pos,str.length()-pos).c_str(), "<app_id>", temp_int)) {
+        APP_INFO ai;
+        ai.appid = temp_int;
+        ai.work_available = false;
+        selected_apps.push_back(ai);
+
+        pos = str.find("<app_id>", pos) + 1;
+    }
+    if (parse_bool(buf,"allow_non_selected_apps", flag)) {
+        allow_non_selected_apps = flag;
+    }
+    if (parse_bool(buf,"allow_beta_work", flag)) {
+        allow_beta_work = flag;
+    }
+    if (parse_bool(buf,"no_gpus", flag)) {
+        // deprecated, but need to handle
+        if (flag) {
+            for (int i=1; i<NPROC_TYPES; i++) {
+                dont_use_proc_type[i] = true;
+            }
+        }
+    }
+    if (parse_bool(buf,"no_cpu", flag)) {
+        dont_use_proc_type[PROC_TYPE_CPU] = flag;
+    }
+    if (parse_bool(buf,"no_cuda", flag)) {
+        dont_use_proc_type[PROC_TYPE_NVIDIA_GPU] = flag;
+    }
+    if (parse_bool(buf,"no_ati", flag)) {
+        dont_use_proc_type[PROC_TYPE_AMD_GPU] = flag;
+    }
+    if (parse_bool(buf,"no_intel_gpu", flag)) {
+        dont_use_proc_type[PROC_TYPE_INTEL_GPU] = flag;
+    }
+}
 
 void WORK_REQ::add_no_work_message(const char* message) {
     for (unsigned int i=0; i<no_work_messages.size(); i++) {

sched/sched_types.h
@@ -362,8 +362,25 @@ struct DISK_LIMITS {
     double min_free;
 };
 
+// parsed version of project prefs that relate to scheduling
+//
+struct PROJECT_PREFS {
+    std::vector<APP_INFO> selected_apps;
+    bool dont_use_proc_type[NPROC_TYPES];
+    bool allow_non_selected_apps;
+    bool allow_beta_work;
+    void parse();
+
+    PROJECT_PREFS() {
+        memset(&dont_use_proc_type, 0, sizeof(dont_use_proc_type));
+        allow_non_selected_apps = false;
+        allow_beta_work = false;
+    }
+};
+
 // summary of a client's request for work, and our response to it
-// Note: this is zeroed out in SCHEDULER_REPLY constructor
+// Note: this is zeroed out in SCHEDULER_REPLY constructor,
+// so don't put any vectors here
 //
 struct WORK_REQ_BASE {
     bool anonymous_platform;
@@ -388,12 +405,6 @@ struct WORK_REQ_BASE {
         // This is evidence that the earlier reply was not received
         // by the client.  It may have contained results,
         // so check and resend just in case.
-
-    // user preferences
-    //
-    bool dont_use_proc_type[NPROC_TYPES];
-    bool allow_non_preferred_apps;
-    bool allow_beta_work;
 
     bool has_reliable_version;
         // whether the host has a reliable app version
@@ -489,7 +500,7 @@ struct WORK_REQ_BASE {
 };
 
 struct WORK_REQ : public WORK_REQ_BASE {
-    std::vector<APP_INFO> preferred_apps;
+    PROJECT_PREFS project_prefs;
     std::vector<USER_MESSAGE> no_work_messages;
     std::vector<BEST_APP_VERSION*> best_app_versions;
     std::vector<DB_HOST_APP_VERSION> host_app_versions;

lib/filesys.cpp
@@ -42,7 +42,6 @@
 #include <cerrno>
 #include <sys/stat.h>
 #include <sys/file.h>
-#include <ctime>
 #include <cstring>
 #include <cstdlib>
 #include <sys/time.h>

sched/sched_check.cpp
@@ -50,10 +50,10 @@ const char* infeasible_string(int code) {
 bool app_not_selected(int appid) {
     unsigned int i;
 
-    if (g_wreq->preferred_apps.size() == 0) return false;
-    for (i=0; i<g_wreq->preferred_apps.size(); i++) {
-        if (appid == g_wreq->preferred_apps[i].appid) {
-            g_wreq->preferred_apps[i].work_available = true;
+    if (g_wreq->project_prefs.selected_apps.size() == 0) return false;
+    for (i=0; i<g_wreq->project_prefs.selected_apps.size(); i++) {
+        if (appid == g_wreq->project_prefs.selected_apps[i].appid) {
+            g_wreq->project_prefs.selected_apps[i].work_available = true;
             return false;
         }
     }

commit 5c573ddae862b306c3a5661103dd4d6e429db132

scheduler: use "allow_non_preferred_apps" to match PHP code
Fix bug introduced in 671446c

sched/sched_array.cpp
@@ -429,7 +429,7 @@ void send_work_old() {
     // If user has selected apps but will accept any,
     // and we haven't found any jobs for selected apps, try others
     //
-    if (!g_wreq->njobs_sent && g_wreq->project_prefs.allow_non_selected_apps ) {
+    if (!g_wreq->njobs_sent && g_wreq->project_prefs.allow_non_preferred_apps ) {
         g_wreq->user_apps_only = false;
         selected_app_message_index = g_wreq->no_work_messages.size();
         if (config.debug_send_scan) {

sched/sched_locality.cpp
@@ -296,7 +296,7 @@ static int possibly_send_result(SCHED_DB_RESULT& result) {
     retval = wu.lookup_id(result.workunitid);
     if (retval) return ERR_DB_NOT_FOUND;
 
-    // This doesn't take into account g_wreq->allow_non_selected_apps,
+    // This doesn't take into account g_wreq->allow_non_preferred_apps,
     // however Einstein@Home, which is the only project that currently uses
     // this locality scheduler, doesn't support the respective project-specific
     // preference setting

sched/sched_nci.cpp
@@ -170,7 +170,7 @@ int send_nci() {
             continue;
         }
         if (app_not_selected(app.id)) {
-            if (!g_wreq->project_prefs.allow_non_selected_apps) {
+            if (!g_wreq->project_prefs.allow_non_preferred_apps) {
                 if (config.debug_send) {
                     log_messages.printf(MSG_NORMAL,
                         "%s is not selected\n", app.name

sched/sched_score.cpp
@@ -78,7 +78,7 @@ bool JOB::get_score(WU_RESULT& wu_result) {
     }
 
     if (app_not_selected(app->id)) {
-        if (g_wreq->project_prefs.allow_non_selected_apps) {
+        if (g_wreq->project_prefs.allow_non_preferred_apps) {
             score -= 1;
         } else {
             if (config.debug_send_job) {

sched/sched_types.cpp
@@ -194,8 +194,8 @@ void PROJECT_PREFS::parse() {
 
         pos = str.find("<app_id>", pos) + 1;
     }
-    if (parse_bool(buf,"allow_non_selected_apps", flag)) {
-        allow_non_selected_apps = flag;
+    if (parse_bool(buf,"allow_non_preferred_apps", flag)) {
+        allow_non_preferred_apps = flag;
     }
     if (parse_bool(buf,"allow_beta_work", flag)) {
         allow_beta_work = flag;

sched/sched_types.h
@@ -367,7 +367,7 @@ struct DISK_LIMITS {
 struct PROJECT_PREFS {
     std::vector<APP_INFO> selected_apps;
     bool dont_use_proc_type[NPROC_TYPES];
-    bool allow_non_selected_apps;
+    bool allow_non_preferred_apps;
     bool allow_beta_work;
     int max_jobs_in_progress;
     int max_cpus;
@@ -376,7 +376,7 @@ struct PROJECT_PREFS {
 
     PROJECT_PREFS() {
         memset(&dont_use_proc_type, 0, sizeof(dont_use_proc_type));
-        allow_non_selected_apps = false;
+        allow_non_preferred_apps = false;
         allow_beta_work = false;
         max_jobs_in_progress = 0;
         max_cpus = 0;