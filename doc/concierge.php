93dfc049ed04c8f3909d127a04139aa47dc7c2bd
David Anderson <davea@ssl.berkeley.edu>
2017-07-13 01:13:17 -0700

    Concierge: fixes - caller must supply installer URL - fix typos - send info in filename, not cookies

--- doc/concierge.php
+++ doc/concierge.php
@@ -16,37 +16,29 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+// concierge is a mechanism for downloading an installer
+// pre-configured for a particular project or AM account
+//
 // This script gets (via POST)
-//  - the URL of a project or AM
-//  - optional credentials
-// It
-// - looks up the URL in a list of vetted entities
-// - send cookies to the user's browser containing project/account info,
-// - optionally starts a download of the appropriate client installer.
+//  - info about a project or AM
+//  - optional account info
+//  - the name of an installer file
+// Action:
+// - look up the URL in a list of vetted entities
+// - start a download of the requested installer,
+//   with a filename that encodes the entity and account info
+//
+// The installer looks at its own filename and creates init files accordingly
 
-require_once("../inc/translation.inc");
+require_once("../inc/util.inc");
 require_once("versions.inc");
 require_once("projects.inc");
 require_once("concierge.inc");
 
-function get_download_url($pname, $need_vbox) {
-    global $platforms;
-    global $url_base;
-
-    $p = $platforms[$pname];
-    $v = latest_version($p);
-    $file = $v['file'];
-    if ($need_vbox && array_key_exists('vbox_file', $v)) {
-        $file = $v['vbox_file'];
-    }
-    $url = $url_base.$file;
-    return $url;
-}
+$master_url = urldecode(post_str("master_url"));
 
 // check if URL is in vetted list
 //
-$master_url = get_post("master_url");
-$master_url = urldecode($master_url);
 $vp = vps_lookup($master_url);
 if (!$vp) {
     echo "URL not found";
@@ -55,35 +47,42 @@ if (!$vp) {
 
 // could also check HTTP_REFERER here, but that can be forged
 
-$auth = get_post("auth", true);
+$auth = post_str("auth", true);
 if ($auth) $auth = urldecode($auth);
 
-$user_name = get_post("user_name", true);
+$user_name = post_str("user_name", true);
 if ($user_name) $user_name = urldecode($user_name);
 
-$download = post_str('download', true);
+$download_filename = post_str("download_filename");
 
-if ($download) {
-    $client_info = $_SERVER['HTTP_USER_AGENT'];
-    $platform = client_infO_to_platform($client_info);
-    $download_url = get_download_url($platform);
-    if (!$download_url) {
-        echo "no file to download";
-        exit;
-    }
-}
+$download_url = "https://boinc.berkeley.edu/dl/$filename";
 
-setrawcookie('attach_master_url', rawurlencode($master_url), $expire);
-setrawcookie('attach_project_name', rawurlencode($vp->name), $expire);
+// add info to filename.
+// We should keep the filename as short as possible;
+// long filenames are unexpected and might scare people off.
+// So add only the basics:
+// - entity URL
+// - whether it's an AM
+// - account user name and account token
+//
+// Would be nice to include other stuff (entity institution/description)
+// to show on the welcome screen.
+// Can get that by other means (e.g. get_config RPC)
+
+$info = sprintf("pu=%s&am=%d",
+    $master_url,
+    $vp->is_am?1:0
+);
+
 if ($auth && $user_name) {
-    setrawcookie('attach_auth', rawurlencode($auth), $expire);
-    setrawcookie('attach_user_name', rawurlencode($user_name), $expire);
+    $info .= sprintf("&ut=%s&un=%s",
+        $auth,
+        $user_name
+    );
 }
-setrawcookie('attach_project_desc', rawurlencode($vp->description), $expire);
-setrawcookie('attach_project_inst', rawurlencode($vp->institution), $expire);
 
-if ($download) {
-Header("Location: ".$download_url);
-}
+$filename .= '__'.base64_encode($info);
+header("Location: ".$download_url);
+header(sprintf('Content-Disposition: attachment; filename="%s"', $filename));
 
 ?>
