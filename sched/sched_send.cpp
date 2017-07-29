83db661f9849c0e13ce602631535b80b203eb815
David Anderson <davea@ssl.berkeley.edu>
2017-07-29 13:44:25 -0700

    scheduler: send workunit keywords in reply

--- sched/sched_send.cpp
+++ sched/sched_send.cpp
@@ -553,6 +553,14 @@ static int insert_wu_tags(WORKUNIT& wu, APP& app) {
         wu.name,
         app.name
     );
+    if (!empty(wu.keywords)) {
+        char buf2[1024];
+        sprintf(buf2,
+            "    <job_keywords>%s</job_keywords>\n",
+            wu.keywords
+        );
+        strcat(buf, buf2);
+    }
     return insert_after(wu.xml_doc, "<workunit>\n", buf);
 }
 
