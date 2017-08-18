61c3c64b3d4d5f04a5969cc5daae3677748b8f2a
Charlie Fenton <charlief@example.com>
2017-08-18 02:12:51 -0700

    Manager: Include android_debug in Event Log Options and in cc_config.xml file only if running on Android

--- lib/cc_config.cpp
+++ lib/cc_config.cpp
@@ -63,7 +63,9 @@ int LOG_FLAGS::parse(XML_PARSER& xp) {
         if (xp.parse_bool("sched_ops", sched_ops)) continue;
         if (xp.parse_bool("task", task)) continue;
 
+#ifdef ANDROID
         if (xp.parse_bool("android_debug", android_debug)) continue;
+#endif
         if (xp.parse_bool("app_msg_receive", app_msg_receive)) continue;
         if (xp.parse_bool("app_msg_send", app_msg_send)) continue;
         if (xp.parse_bool("async_file_debug", async_file_debug)) continue;
@@ -111,7 +113,9 @@ int LOG_FLAGS::write(MIOFILE& out) {
         "        <file_xfer>%d</file_xfer>\n"
         "        <sched_ops>%d</sched_ops>\n"
         "        <task>%d</task>\n"
+#ifdef ANDROID
         "        <android_debug>%d</android_debug>\n"
+#endif
         "        <app_msg_receive>%d</app_msg_receive>\n"
         "        <app_msg_send>%d</app_msg_send>\n"
         "        <async_file_debug>%d</async_file_debug>\n"
@@ -152,7 +156,9 @@ int LOG_FLAGS::write(MIOFILE& out) {
         file_xfer ? 1 : 0,
         sched_ops ? 1 : 0,
         task ? 1 : 0,
+#ifdef ANDROID
         android_debug ? 1 : 0,
+#endif
         app_msg_receive ? 1 : 0,
         app_msg_send ? 1 : 0,
         async_file_debug ? 1 : 0,
