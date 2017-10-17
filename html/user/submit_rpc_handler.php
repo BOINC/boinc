2010e4c419c45eef05eb45bd504e37e07781a169
Kevin Reed <knreed@us.ibm.com>
2017-10-17 08:36:23 -0500

    Declare variable $batch at start of function in order to resolve Scrutinizer issue

--- html/user/submit_rpc_handler.php
+++ html/user/submit_rpc_handler.php
@@ -595,6 +595,7 @@ function show_job_details($wu) {
 // return a batch specified by the command, using either ID or name
 //
 function get_batch($r) {
+    $batch = NULL;
     if (!empty($r->batch_id)) {
         $batch_id = (int)($r->batch_id);
         $batch = BoincBatch::lookup_id($batch_id);
