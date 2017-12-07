3f6c9beaecd16ebfb2ee7894d1a812cef783eb84
Shawn Kwang <kwangs@uwm.edu>
2017-12-07 14:38:17 -0600

    Drupal: Updated private message template and CSS to fix layout bugs.

--- drupal/sites/default/boinc/themes/boinc/templates/privatemsg-view.tpl.php
+++ drupal/sites/default/boinc/themes/boinc/templates/privatemsg-view.tpl.php
@@ -1,8 +1,3 @@
-<?php
-// Each file loads it's own styles because we cant predict which file will be
-// loaded.
-drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-view.css');
-?>
 <?php print $anchors; ?>
 
 <div id="privatemsg-mid-<?php print $mid; ?>" class="privatemsg-box-fb <?php print $zebra; ?> clearfix">
@@ -11,12 +6,12 @@ drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-vie
     <?php print $author_picture; ?>
   </div>
   <div class="message-body">
-    <div class="foobar">
-      <div class="name"><?php print $author_name_link; ?></div>
-      <div class="date">
+    <div class="headerinfo">
+      <div class="message-name"><?php print $author_name_link; ?></div>
+      <div class="message-date">
         <?php print $message_timestamp; ?>
       </div>
-      <div class="standard-links">
+      <div class="message-links">
         <?php if ( isset($message_actions)) : ?>
           <?php print $message_actions ?>
         <?php endif ?>
