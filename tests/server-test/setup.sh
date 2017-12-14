9417c4382901bad737afd4a7e1238619f66e0487
Kevin Reed <knreed@us.ibm.com>
2017-12-14 16:54:27 -0600

    Script should exit with 1 if it is missing access to a required repository

--- tests/server-test/setup.sh
+++ tests/server-test/setup.sh
@@ -10,6 +10,7 @@ then
     if [ $? -ne 0 ];
     then
         echo "Please set up the repository for docker-ce.  See https://docs.docker.com/engine/installation/linux/docker-ce/$(. /etc/os-release; echo "$ID")/"
+        exit 1
     fi
     sudo usermod -a -G docker $USER
 fi
@@ -21,6 +22,7 @@ then
     if [ $? -ne 0 ];
     then
         echo "Please enable the EPEL repository in order to install ansible.  See https://fedoraproject.org/wiki/EPEL"
+        exit 1
     fi
     sudo usermod -a -G docker $USER
 fi
