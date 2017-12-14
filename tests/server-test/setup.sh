e72499e0560b533ffd2398b4f80d6c5cdb2318b5
Kevin Reed <knreed@us.ibm.com>
2017-12-14 16:55:52 -0600

    Seperate docker and ansible on fedora/centos based installs in order to provide clearer messaging

--- tests/server-test/setup.sh
+++ tests/server-test/setup.sh
@@ -5,8 +5,8 @@ echo "Attempting to install ansible and docker"
 #Check if using apt-get
 if [ -n "$(command -v apt-get)"  ];
 then
-   sudo apt-get -y install ansible
-   sudo apt-get -y install docker-engine
+    sudo apt-get -y install ansible
+    sudo apt-get -y install docker-engine
     if [ $? -ne 0 ];
     then
         echo "Please set up the repository for docker-ce.  See https://docs.docker.com/engine/installation/linux/docker-ce/$(. /etc/os-release; echo "$ID")/"
@@ -18,11 +18,12 @@ fi
 #Check if using yum
 if [ -n "$(command -v yum)" ]; 
 then
-    sudo yum -y install ansible docker
+    sudo yum -y install ansible
     if [ $? -ne 0 ];
     then
         echo "Please enable the EPEL repository in order to install ansible.  See https://fedoraproject.org/wiki/EPEL"
         exit 1
     fi
+    sudo yum -y install docker
     sudo usermod -a -G docker $USER
 fi
