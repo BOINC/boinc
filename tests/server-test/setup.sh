19835f8e4e56a331c230dfa54f9cb2996c1990f4
Kevin Reed <knreed@us.ibm.com>
2017-12-14 16:59:13 -0600

    Fix package name for docker on debian

--- tests/server-test/setup.sh
+++ tests/server-test/setup.sh
@@ -6,7 +6,7 @@ echo "Attempting to install ansible and docker"
 if [ -n "$(command -v apt-get)"  ];
 then
     sudo apt-get -y install ansible
-    sudo apt-get -y install docker-engine
+    sudo apt-get -y install docker-ce
     if [ $? -ne 0 ];
     then
         echo "Please set up the repository for docker-ce.  See https://docs.docker.com/engine/installation/linux/docker-ce/$(. /etc/os-release; echo "$ID")/"
