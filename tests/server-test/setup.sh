6c6e5f0e038f186f413e43ac8831f3d83c877e19
Kevin Reed <knreed@us.ibm.com>
2017-12-15 12:52:06 -0600

    Finish setting up tests for docker-compose and instructions to install

--- tests/server-test/setup.sh
+++ tests/server-test/setup.sh
@@ -5,35 +5,38 @@ echo "Attempting to install ansible and docker"
 #Check if using apt-get
 if [ -n "$(command -v apt-get)"  ];
 then
-    sudo apt-get -y install ansible
-    sudo apt-get -y install docker-ce
+    sudo apt-get -y -q install ansible
+    sudo apt-get -y -q install docker-ce
     if [ $? -ne 0 ];
     then
         echo "Please set up the repository for docker-ce.  See https://docs.docker.com/engine/installation/linux/docker-ce/$(. /etc/os-release; echo "$ID")/"
         exit 1
-    fi
-    sudo usermod -a -G docker $USER
-    if [ -z "$(command -v docker-compose)" ]; 
-    then
-        echo "Please install docker-compose.  See https://docs.docker.com/compose/install/"
     fi
 fi
 
 #Check if using yum
 if [ -n "$(command -v yum)" ]; 
 then
-    sudo yum -y install ansible
+    sudo yum -y -q install ansible
     if [ $? -ne 0 ];
     then
         echo "Please enable the EPEL repository in order to install ansible.  See https://fedoraproject.org/wiki/EPEL"
         exit 1
     fi
-    sudo yum -y install docker
-    sudo usermod -a -G docker $USER
-    if [ -z "$(command -v docker-compose)" ]; 
-    then
-        echo "Please install docker-compose.  See https://docs.docker.com/compose/install/"
-    fi
+    sudo yum -y -q install docker
 fi
 
-echo "You need to restart your system in order to pick up permission changes"
+# Check if docker-compose is installed.  If it isn't, direct user to instructions
+if [ -z "$(command -v docker-compose)" ]; 
+then
+    echo "Please install docker-compose.  See https://docs.docker.com/compose/install/"
+    exit 1
+fi
+
+# Add user to docker group so that they can run docker commands
+if [ `groups $USER | grep docker | wc -l` -ne 1 ];
+then
+    sudo usermod -a -G docker $USER
+    echo "You need to restart your system in order to pick up permission changes"
+fi
+
