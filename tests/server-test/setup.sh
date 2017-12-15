3476ecf2ab32930c094fa466752de1bab3d3129a
Kevin Reed <knreed@us.ibm.com>
2017-12-15 14:40:28 -0600

    Add step to create group docker if it doesn't exist

--- tests/server-test/setup.sh
+++ tests/server-test/setup.sh
@@ -12,16 +12,9 @@ then
         echo "Please set up the repository for docker-ce.  See https://docs.docker.com/engine/installation/linux/docker-ce/$(. /etc/os-release; echo "$ID")/"
         exit 1
     fi
+
     sudo systemctl enable docker
     sudo systemctl start docker
-
-    # Add user to docker group so that they can run docker commands
-    if [ `groups $USER | grep docker | wc -l` -ne 1 ];
-    then
-        sudo usermod -a -G docker $USER
-        echo "You need to restart your system in order to pick up permission changes"
-        exit 1
-    fi
 fi
 
 #Check if using yum
@@ -29,16 +22,21 @@ if [ -n "$(command -v yum)" ];
 then
     sudo yum -y -q install epel-release
     sudo yum -y -q install ansible java-1.8.0-openjdk docker
+
     sudo systemctl enable docker
     sudo systemctl start docker
-    
-    # Add user to dockerroot group so that they can run docker commands
-    if [ `groups $USER | grep dockerroot | wc -l` -ne 1 ];
+fi
+
+# Add user to docker group so that they can run docker commands
+if [ `groups $USER | grep docker | wc -l` -ne 1 ];
+then
+    if ! grep -q docker /etc/group
     then
-        sudo usermod -a -G dockerroot $USER
-        echo "You need to restart your system in order to pick up permission changes"
-        exit 1
+         groupadd docker
     fi
+    sudo usermod -a -G docker $USER
+    echo "You have been added to the docker group.  In order to continue you may need to log out and back in again or restart your system in order to pick up the group change"
+    exit 1
 fi
 
 # Check if docker-compose is installed.  If it isn't, direct user to instructions
