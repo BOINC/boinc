07e09b0b049144c54ea00b97aecc1f807aeba0ca
David Anderson <davea@ssl.berkeley.edu>
2018-02-27 16:29:49 -0800

    web: fix bugs in change-password page.
    BTW, I'd like to have a "show password" checkbox here
    for people with no one looking over their shoulder.
    Maybe later.

--- html/user/edit_passwd_form.php
+++ html/user/edit_passwd_form.php
@@ -25,8 +25,8 @@ $user = get_logged_in_user();
 page_head(tra("Change password"));
 
 form_start(secure_url_base()."edit_passwd_action.php", "post");
-form_input_text(tra("New password"), "passwd", "password");
-form_input_text(tra("New password, again"), "passwd2", "password");
+form_input_text(tra("New password"), "passwd", "", "password");
+form_input_text(tra("New password, again"), "passwd2", "", "password");
 form_submit(tra("Change password"));
 form_end();
 page_tail();
