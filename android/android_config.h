b27803ab04e8204f67e42a0dbfb8e4a90babd799
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-03-02 01:03:50 +0200

    Fix STATVFS error
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- android/android_config.h
+++ android/android_config.h
@@ -25,4 +25,15 @@
 #undef _LARGEFILE_SOURCE
 #undef _LARGEFILE64_SOURCE
 
+#ifndef ANDROID_64
+#if HAVE_SYS_STATFS_H
+#if HAVE_SYS_STATVFS_H
+#undef HAVE_SYS_STATVFS_H
+#define HAVE_SYS_STATVFS_H 0
+#endif
+#else
+#error Need to specify a method to obtain free/total disk space
+#endif
+#endif
+
 #endif // double-inclusion protection
