4606938667fa2877941099ef72e230b614140319
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-03-02 04:18:05 +0200

    Fix typo
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- client/coproc_sched.cpp
+++ client/coproc_sched.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifdef(ANDROID)
+#ifdef ANDROID
 #include "../android/android_config.h"
 #endif
 
