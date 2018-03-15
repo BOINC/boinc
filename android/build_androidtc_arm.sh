1d3547b523be2422865b8a2e93cb7bf7c5d70038
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-03-15 03:58:17 +0200

    Fix build
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- android/build_androidtc_arm.sh
+++ android/build_androidtc_arm.sh
@@ -10,6 +10,39 @@ export NDK_ROOT="${NDK_ROOT:-$HOME/NVPACK/android-ndk-r10e}"
 export ANDROID_TC="${ANDROID_TC:-$HOME/android-tc}"
 export ANDROID_TC_ARM="${ANDROID_TC_ARM:-$ANDROID_TC/arm}"
 
+
+# checks if a given path is canonical (absolute and does not contain relative links)
+# from http://unix.stackexchange.com/a/256437
+isPathCanonical() {
+  case "x$1" in
+    (x*/..|x*/../*|x../*|x*/.|x*/./*|x./*)
+        rc=1
+        ;;
+    (x/*)
+        rc=0
+        ;;
+    (*)
+        rc=1
+        ;;
+  esac
+  return $rc
+}
+
+cache_dir=""
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --cache_dir)
+        cache_dir="$2"
+        shift
+        ;;
+        *)
+        echo "unrecognized option $key"
+        ;;
+    esac
+    shift # past argument or value
+done
+
 if [ ! -d "$ANDROID_TC_ARM/arm-linux-androideabi" ]; then
     "$NDK_ROOT/build/tools/make-standalone-toolchain.sh" --verbose --platform=android-16 --arch=arm --install-dir="$ANDROID_TC_ARM" --stl=libc++ "$@"
     if [ $? -ne 0 ]; then exit 1; fi
--- android/build_androidtc_mips64.sh
+++ android/build_androidtc_mips64.sh
@@ -10,6 +10,38 @@ export NDK_ROOT="${NDK_ROOT:-$HOME/NVPACK/android-ndk-r10e}"
 export ANDROID_TC="${ANDROID_TC:-$HOME/android-tc}"
 export ANDROID_TC_MIPS64="${ANDROID_TC_MIPS64:-$ANDROID_TC/mips64}"
 
+# checks if a given path is canonical (absolute and does not contain relative links)
+# from http://unix.stackexchange.com/a/256437
+isPathCanonical() {
+  case "x$1" in
+    (x*/..|x*/../*|x../*|x*/.|x*/./*|x./*)
+        rc=1
+        ;;
+    (x/*)
+        rc=0
+        ;;
+    (*)
+        rc=1
+        ;;
+  esac
+  return $rc
+}
+
+cache_dir=""
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --cache_dir)
+        cache_dir="$2"
+        shift
+        ;;
+        *)
+        echo "unrecognized option $key"
+        ;;
+    esac
+    shift # past argument or value
+done
+
 if [ ! -d "$ANDROID_TC_MIPS64/mips64el-linux-android" ]; then
     "$NDK_ROOT/build/tools/make-standalone-toolchain.sh" --verbose --platform=android-21 --arch=mips64 --install-dir="$ANDROID_TC_MIPS64" --stl=libc++ "$@"
     if [ $? -ne 0 ]; then exit 1; fi
