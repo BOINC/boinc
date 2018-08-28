3c3d8f6f89c227bfc828c918273782bbf6592f23
Charlie Fenton <charlief@example.com>
2018-08-28 03:04:41 -0700

    Mac Screensaver: fixes for dual GPU MacBook Pros running OS 10.13+

--- clientscr/Mac_Saver_ModuleView.h
+++ clientscr/Mac_Saver_ModuleView.h
@@ -1,6 +1,6 @@
 // This file is part of BOINC.
 // http://boinc.berkeley.edu
-// Copyright (C) 2017 University of California
+// Copyright (C) 2018 University of California
 //
 // BOINC is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License
@@ -49,7 +49,7 @@
 @interface SharedGraphicsController : NSObject <NSMachPortDelegate>
 
 @property (NS_NONATOMIC_IOSONLY, readonly) GLuint currentTextureName;
-- (instancetype)init:(NSView*)saverView;
+- (void)init:(NSView*)saverView;
 - (void)portDied:(NSNotification *)notification;
 - (void)testConnection;
 
