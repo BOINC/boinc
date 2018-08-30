a51f31f6e3a32b861207e2573d0a0a77ae6afe14
Oliver Behnke <oliver.behnke@aei.mpg.de>
2018-08-30 09:04:50 +0000

    Fix arm64 BOINC build
    * Specify minimum Android API version explicitly, based on platform (or existing version)
    * Enable verbose builds

--- android/build_boinc_arm64.sh
+++ android/build_boinc_arm64.sh
@@ -24,8 +24,8 @@ export PATH="$TCBINARIES:$TCINCLUDES/bin:$PATH"
 export CC=aarch64-linux-android-gcc
 export CXX=aarch64-linux-android-g++
 export LD=aarch64-linux-android-ld
-export CFLAGS="--sysroot=$TCSYSROOT -DANDROID -DANDROID_64 -DDECLARE_TIMEZONE -Wall -I$TCINCLUDES/include -O3 -fomit-frame-pointer -fPIE"
-export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -DANDROID_64 -Wall -I$TCINCLUDES/include -funroll-loops -fexceptions -O3 -fomit-frame-pointer -fPIE"
+export CFLAGS="--sysroot=$TCSYSROOT -DANDROID -DANDROID_64 -DDECLARE_TIMEZONE -Wall -I$TCINCLUDES/include -O3 -fomit-frame-pointer -fPIE -D__ANDROID_API__=21"
+export CXXFLAGS="--sysroot=$TCSYSROOT -DANDROID -DANDROID_64 -Wall -I$TCINCLUDES/include -funroll-loops -fexceptions -O3 -fomit-frame-pointer -fPIE -D__ANDROID_API__=21"
 export LDFLAGS="-L$TCSYSROOT/usr/lib -L$TCINCLUDES/lib -llog -fPIE -pie"
 export GDB_CFLAGS="--sysroot=$TCSYSROOT -Wall -g -I$TCINCLUDES/include"
 export PKG_CONFIG_SYSROOT_DIR="$TCSYSROOT"
