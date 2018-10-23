02eb3342eb636199895a4ac3c855ea4165b4ab74
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-10-23 03:40:02 +0300

    Add Release dependencies to cache.
    Looks like AppVeyor have only one cache for all.
    Removing Debug dependencies will break everything in master branch.
    
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- win_build/load_dependencies.bat
+++ win_build/load_dependencies.bat
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 set dependencies_git_path=%1
 set dependencies_path=%2
 set revision_lst_path=%dependencies_path%\revision.lst
@@ -8,12 +8,10 @@ set configuration=%4
 if exist %revision_lst_path% (
   set /p temp_saved_revision=<%dependencies_path%\revision.lst
   set saved_revision=%temp_saved_revision%
-  echo %saved_revision%
 
   for /f %%i in ('git ls-remote %dependencies_git_path% HEAD') do (
     set last_revision=%%i
-    set last_revision=%last_revision%_%platform%_%configuration%
-    echo %last_revision%
+
     if "%last_revision%" == "%saved_revision%" (
        echo Dependencies are up-to-date
        goto :EOF
@@ -49,25 +47,7 @@ if "%platform%" == "Win32" (
   rd /s /q %dependencies_path%\wxwidgets\mswin\Win32
   rd /s /q %dependencies_path%\zlib\mswin\Win32
 )
-  
-if "%configuration%" == "Debug" (
-  rd /s /q %dependencies_path%\curl\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\freetype\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\ftgl\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\openssl\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\sqlite3\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\wxwidgets\mswin\%platform%\Release
-  rd /s /q %dependencies_path%\zlib\mswin\%platform%\Release
-) else (
-  rd /s /q %dependencies_path%\curl\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\freetype\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\ftgl\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\openssl\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\sqlite3\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\wxwidgets\mswin\%platform%\Debug
-  rd /s /q %dependencies_path%\zlib\mswin\%platform%\Debug
-)
 
-echo | set /p dummy="%last_revision%_%platform%_%configuration%" > %revision_lst_path%
+echo | set /p dummy="%last_revision%" > %revision_lst_path%
 
 goto :EOF
