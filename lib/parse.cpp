BOINC/boinc

commit ff0b8d08985707f089f934816b0d28841b709c80

lib: escape CDATA-section-close delimiter in xml_escape()
Per XML standard CDATA-section-close delimiter ']]>' is not allowed to
appear in element content without being escaped. XML tools like libxml
that require well-formed XML refuse to parse text that doesn't follow
this rule.

In particular this bug broke processing database result archives
generated by db_purge.

lib/parse.cpp
@@ -371,6 +371,13 @@ void xml_escape(const char* in, char* out, int len) {
                 p += strlen(buf);
                 break;
             }
+        } else if (x == ']') {
+            // two stage check, strncmp() is slow
+            if (!strncmp(in, "]]>", 3)) {
+                strcpy(p, "]]&gt;");
+                p += 6;
+                in += 3;
+            }
         } else {
             *p++ = x;
         }

commit 5e23a9482c82303399ee8c2fb08d5072ba7c2df6

lib: fix xml_escape(), again
Commit ff0b8d08985707f089f934816b0d28841b709c80 (fix CDATA escaping) introduced two bugs to xml_escape():

- it eats ']' characters that are not part of ']]>'
- it eats the next character after ']]>'

Fixes #2809

lib/parse.cpp
@@ -376,7 +376,9 @@ void xml_escape(const char* in, char* out, int len) {
             if (!strncmp(in, "]]>", 3)) {
                 strcpy(p, "]]&gt;");
                 p += 6;
-                in += 3;
+                in += 2;  // +1 from for loop
+            } else {
+                *p++ = x;
             }
         } else {
             *p++ = x;