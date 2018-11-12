88f4c71b0c488c85f0b29781399324ee446cb058
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-11-12 21:31:55 +0200

    Try to fix call issue
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- win_build/load_dependencies.bat
+++ win_build/load_dependencies.bat
@@ -1,8 +1,8 @@
 REM @echo off
-set dependencies_path=%1
+set dependencies_path=%3
 set revision_lst_path=%dependencies_path%\revision.lst
-set platform=%2
-set configuration=%3
+set platform=%1
+set configuration=%2
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/revision.lst', '%TEMP%\revision.lst')"
 set /p last_revision=<%TEMP%\revision.lst
@@ -16,12 +16,12 @@ if exist %revision_lst_path% (
     goto :EOF
   )
 
-if exist %dependencies_path%\boinc_depends_win_vs2013 (
-  rd /s /q %dependencies_path%\boinc_depends_win_vs2013
+if exist %dependencies_path% (
+  rd /s /q %dependencies_path%
 )
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/boinc_depends_win_vs2013_%platform%_%configuration%.zip', '%TEMP%\boinc_dependencies.zip')"
-7z x %TEMP%\boinc_dependencies.zip -o%dependencies_path%
+7z x %TEMP%\boinc_dependencies.zip -o%dependencies_path%\..\
 
 echo | set /p dummy="%last_revision%" > %revision_lst_path%
 
