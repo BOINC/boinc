6da04e825e4842b544a0732487d17815607d89ef
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-11-12 22:40:00 +0200

    Try to find powershell
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- win_build/load_dependencies.bat
+++ win_build/load_dependencies.bat
@@ -7,7 +7,9 @@ set revision_lst_path=%dependencies_path%\revision.lst
 set platform=%2
 set configuration=%3
 
-curl -o %TEMP%\revision.lst https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/revision.lst
+where powershell
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/revision.lst', '%TEMP%\revision.lst')"
 set /p last_revision=<%TEMP%\revision.lst
 
 if exist %revision_lst_path% (
@@ -23,7 +25,7 @@ if exist %dependencies_path% (
   rd /s /q %dependencies_path%
 )
 
-curl -o %TEMP%\boinc_dependencies.zip https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/boinc_depends_win_vs2013_%platform%_%configuration%.zip
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://s3-us-west-2.amazonaws.com/boinc-win-dependencies/latest/boinc_depends_win_vs2013_%platform%_%configuration%.zip', '%TEMP%\boinc_dependencies.zip')"
 7z x %TEMP%\boinc_dependencies.zip -o%dependencies_path%\..\
 
 echo | set /p dummy="%last_revision%" > %revision_lst_path%
