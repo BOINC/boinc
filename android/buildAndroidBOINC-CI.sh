8cde576efef83dbb6e09bb7fadf3ddc188358d10
Adam Radocz <adam.radocz@gmail.com>
2018-12-07 20:03:39 +0000

    [Android] Update NDK to r18b.

--- android/buildAndroidBOINC-CI.sh
+++ android/buildAndroidBOINC-CI.sh
@@ -21,7 +21,7 @@ set -e
 
 export OPENSSL_VERSION=1.0.2q
 export CURL_VERSION=7.62.0
-export NDK_VERSION=17c
+export NDK_VERSION=18b
 
 export ANDROID_HOME=$HOME/Android/Sdk
 export NDK_ROOT=$HOME/Android/Ndk
