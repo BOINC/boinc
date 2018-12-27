51ec2d2abb38a9e9866fc5f84c8e43d7b7f18c2d
Adam Radocz <adam.radocz@gmail.com>
2018-12-27 16:06:48 +0000

    [Android] Add 'Bug report' menu, which redirect to GitHub/issues. 'Help menu' link changed to https.

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/BOINCActivity.java
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/BOINCActivity.java
@@ -271,8 +271,10 @@ public class BOINCActivity extends AppCompatActivity {
 				fragmentChanges = true;
 				break;
 	    	case R.string.menu_help:
-	    		Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://boinc.berkeley.edu/wiki/BOINC_Help"));
-	    		startActivity(i);
+	    		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://boinc.berkeley.edu/wiki/BOINC_Help")));
+	    		break;
+	    	case R.string.menu_bug_report:
+	    		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/BOINC/boinc/issues")));
 	    		break;
 	    	case R.string.menu_about:
 				final Dialog dialog = new Dialog(this);
