1bca16344fa590446fd225045e5193a6c7815c12
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2019-04-30 11:33:55 +0300

    [Android] Fix delayed Account Managers list fill
    Simplify procedure to fill Account Managers list when showing view to connect  to Account Manager.
    Fix issue found after merging with current master when selected Accound Manager URL field was filled with Account Manager name instead of its URL.
    Fix indents
    
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/attach/AcctMgrFragment.java
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/attach/AcctMgrFragment.java
@@ -77,64 +77,32 @@ public class AcctMgrFragment extends DialogFragment {
     private AttachProjectAsyncTask asyncTask;
 
 
-	private boolean mAdapterDataFilled = false;
-	private void fillAdapterData() {
-		ArrayList<AccountManager> accountManagers = null;
-		if (mIsBound && !mAdapterDataFilled){
-			try {
-				accountManagers = (ArrayList<AccountManager>) monitor.getAccountManagers();
-			} catch (Exception e) {
-				if (Logging.ERROR) Log.e(Logging.TAG, "AcctMgrFragment onCreateView() error: " + e);
-			}
-			List<AccountManagerSpinner> adapterData = new ArrayList<>();
-			for (AccountManager accountManager : accountManagers) {
-				adapterData.add(new AccountManagerSpinner(accountManager.name, accountManager.url));
-			}
-			ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, adapterData);
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			//accountManagerSpinner.setAdapter(adapter); //Delete this line if everything is working
-      urlSpinner.setAdapter(adapter);
-			mAdapterDataFilled = true;
-		}
-	}
+    private void fillAdapterData() {
+        ArrayList<AccountManager> accountManagers = null;
+        if (mIsBound){
+            try {
+                accountManagers = (ArrayList<AccountManager>) monitor.getAccountManagers();
+            } catch (Exception e) {
+                if (Logging.ERROR) Log.e(Logging.TAG, "AcctMgrFragment onCreateView() error: " + e);
+            }
+            List<AccountManagerSpinner> adapterData = new ArrayList<>();
+            for (AccountManager accountManager : accountManagers) {
+                adapterData.add(new AccountManagerSpinner(accountManager.name, accountManager.url));
+            }
+            ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, adapterData);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            urlSpinner.setAdapter(adapter);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (Logging.DEBUG) {
             Log.d(Logging.TAG, "AcctMgrFragment onCreateView");
         }
-        doBindService();
         View v = inflater.inflate(R.layout.attach_project_acctmgr_dialog, container, false);
 
-        // Get the account managers list.
-        ArrayList<AccountManager> accountManagers = null;
-        if (mIsBound && !mAdapterDataFilled) {
-            try {
-                accountManagers = (ArrayList<AccountManager>) monitor.getAccountManagers();
-            } catch (Exception e) {
-                if (Logging.ERROR) Log.e(Logging.TAG, "AcctMgrFragment onCreateView() error: " + e);
-            }
-            mAdapterDataFilled = true;
-        } else {
-            accountManagers = new ArrayList<>();
-            AccountManager am = new AccountManager();
-            am.name = "";
-            am.url = "";
-            accountManagers.add(am);
-        }
-
-        // Filter AccountManager data to AccountManagerSpinner data.
-        List<AccountManagerSpinner> adapterData = new ArrayList<>();
-        for (AccountManager accountManager : accountManagers) {
-            adapterData.add(new AccountManagerSpinner(accountManager.name, accountManager.url));
-        }
-
         urlSpinner = v.findViewById(R.id.url_spinner);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, adapterData);
-        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.acct_mgr_url_list, android.R.layout.simple_spinner_item); //Delete this line if everything is working
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        urlSpinner.setAdapter(adapter);
-
         urlInput = v.findViewById(R.id.url_input);
         nameInput = v.findViewById(R.id.name_input);
         pwdInput = v.findViewById(R.id.pwd_input);
@@ -145,9 +113,8 @@ public class AcctMgrFragment extends DialogFragment {
         // change url text field on url spinner change
         urlSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                urlInput.setText(urlSpinner.getSelectedItem().toString());
-                //AccountManagerSpinner accountManagerSpinner = (AccountManagerSpinner) AcctMgrFragment.this.accountManagerSpinner.getSelectedItem(); //Delete this line if everything is working
-                //urlInput.setText(accountManagerSpinner.url); //Delete this line if everything is working
+                AccountManagerSpinner accountManagerSpinner = (AccountManagerSpinner) urlSpinner.getSelectedItem();
+                urlInput.setText(accountManagerSpinner.url);
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -192,6 +159,8 @@ public class AcctMgrFragment extends DialogFragment {
             }
 
         });
+
+        doBindService();
 
         return v;
     }
