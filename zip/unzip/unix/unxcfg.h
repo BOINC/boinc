d47e58e47a93d5aa6d8ff705669aa370a4fab248
James Molson <james.molson@cern.ch>
2019-06-05 17:07:46 +0200

    OpenBSD: We do not have sys/timeb.h

--- zip/unzip/unix/unxcfg.h
+++ zip/unzip/unix/unxcfg.h
@@ -111,7 +111,9 @@ typedef struct stat z_stat;
 
 #ifdef BSD
 #  include <sys/time.h>
+#ifndef __OpenBSD__
 #  include <sys/timeb.h>
+#endif
 #  if (defined(_AIX) || defined(__GLIBC__) || defined(__GNU__))
 #    include <time.h>
 #  endif
