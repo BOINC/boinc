af787715e3292211a8dada2aa030ea80f92c5704
Isira Seneviratne <isirasen96@gmail.com>
2020-04-04 05:10:44 +0530

    Override the BaseParser methods in MessagesParser.

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/rpc/MessagesParser.kt
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/rpc/MessagesParser.kt
@@ -19,112 +19,61 @@
 package edu.berkeley.boinc.rpc
 
 import android.util.Log
-import edu.berkeley.boinc.utils.Logging
+import android.util.Xml
+import org.xml.sax.Attributes
+import org.xml.sax.SAXException
 
-class MessagesParser {
-    private val messages: MutableList<Message> = mutableListOf()
+class MessagesParser : BaseParser() {
+    val messages: MutableList<Message> = mutableListOf()
+    private lateinit var message: Message
 
-    @Throws(MessageParseException::class)
-    private fun parseMessages(xml: String) {
-        // Removes whitespace at the start of the string.
-        val xml = xml.trimStart()
-        var pos = 0
-        val end = xml.length
-        var inMsgs = false
-        var newPos: Int
-        var message: Message? = null
+    @Throws(SAXException::class)
+    override fun startElement(uri: String?, localName: String, qName: String?, attributes: Attributes?) {
+        super.startElement(uri, localName, qName, attributes)
+        if (localName.equals(MESSAGE, ignoreCase = true) && !this::message.isInitialized) {
+            message = Message()
+        } else {
+            mElementStarted = true
+            mCurrentElement.setLength(0)
+        }
+    }
+
+    @Throws(SAXException::class)
+    override fun endElement(uri: String?, localName: String, qName: String?) {
+        super.endElement(uri, localName, qName)
         try {
-            while (pos < end) {
-                if (!inMsgs) {
-                    newPos = xml.indexOf("<msgs>")
-                    if (newPos == -1) {
-                        throw MessageParseException()
+            if (localName.equals(MESSAGE, ignoreCase = true)) {
+                if (message.seqno != -1) {
+                    messages.add(message)
+                }
+                message = Message()
+                mElementStarted = false
+            } else {
+                when {
+                    localName.equals(Message.Fields.BODY, ignoreCase = true) -> {
+                        message.body = mCurrentElement.toString()
                     }
-                    pos = newPos + 6
-                    inMsgs = true
-                } else if (message == null && xml.startsWith("<msg>", pos)) {
-                    pos += 5
-                    message = Message()
-                } else if (message != null && xml.startsWith("</msg>", pos)) {
-                    this.messages.add(message)
-                    message = null
-                    pos += 6
-                } else if (xml.startsWith("</msgs>", pos)) {
-                    break // end
-                } else if (message != null) {
-                    when {
-                        xml.startsWith("<project>", pos) -> {
-                            pos += 9
-                            newPos = xml.indexOf("</project>", pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            message.project = xml.substring(pos, newPos).trim { it <= ' ' }
-                            pos = newPos + 10
-                        }
-                        xml.startsWith("<seqno>", pos) -> {
-                            pos += 7
-                            newPos = xml.indexOf("</seqno>", pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            message.seqno = xml.substring(pos, newPos).trim { it <= ' ' }.toInt()
-                            pos = newPos + 8
-                        }
-                        xml.startsWith("<pri>", pos) -> {
-                            pos += 5
-                            newPos = xml.indexOf("</pri>", pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            message.priority = xml.substring(pos, newPos).trim { it <= ' ' }.toInt()
-                            pos = newPos + 6
-                        }
-                        xml.startsWith("<time>", pos) -> {
-                            pos += 6
-                            newPos = xml.indexOf("</time>", pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            message.timestamp =
-                                    xml.substring(pos, newPos).trim { it <= ' ' }.toDouble().toLong()
-                            pos = newPos + 7
-                        }
-                        xml.startsWith("<body>", pos) -> {
-                            pos += 6
-                            newPos = xml.indexOf("</body>", pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            message.body = xml.substring(pos, newPos).trim { it <= ' ' }
-                            pos = newPos + 7
-                        }
-                        else -> { // skip unknown tag
-                            pos++
-                            val endTagIndex = xml.indexOf('>', pos)
-                            if (endTagIndex == -1) {
-                                throw MessageParseException()
-                            }
-                            val tag = xml.substring(pos, endTagIndex)
-                            pos = endTagIndex + 1
-                            val endString = "</$tag>"
-                            newPos = xml.indexOf(endString, pos)
-                            if (newPos == -1) {
-                                throw MessageParseException()
-                            }
-                            pos = newPos + endString.length
-                        }
+                    localName.equals(Message.Fields.PRIORITY, ignoreCase = true) -> {
+                        message.priority = mCurrentElement.toInt()
+                    }
+                    localName.equals(PROJECT, ignoreCase = true) -> {
+                        message.project = mCurrentElement.toString()
+                    }
+                    localName.equals(Message.Fields.TIMESTAMP, ignoreCase = true) -> {
+                        message.timestamp = mCurrentElement.toDouble().toLong()
+                    }
+                    localName.equals(SEQNO, ignoreCase = true) -> {
+                        message.seqno = mCurrentElement.toInt()
                     }
                 }
             }
         } catch (e: NumberFormatException) {
-            if (Logging.ERROR) {
-                Log.e(Logging.TAG, "MessagesParser.parseMessages error: ", e)
-            }
+            Log.d("MessagesParser", "NumberFormatException $localName $mCurrentElement")
         }
     }
 
     companion object {
+        const val MESSAGE = "msg"
         /**
          * Parse the RPC result (messages) and generate corresponding list.
          *
@@ -133,18 +82,13 @@ class MessagesParser {
          */
         @JvmStatic
         fun parse(rpcResult: String): List<Message> {
-            val parser = MessagesParser()
             return try {
-                parser.parseMessages(rpcResult)
+                val parser = MessagesParser()
+                Xml.parse(rpcResult, parser)
                 parser.messages
-            } catch (ex: MessageParseException) {
+            } catch (ex: SAXException) {
                 emptyList()
             }
         }
     }
 }
-
-/**
- * Exception to be thrown by MessagesParser if an issue occurs while parsing.
- */
-internal class MessageParseException : Exception("Can't parse messages")
