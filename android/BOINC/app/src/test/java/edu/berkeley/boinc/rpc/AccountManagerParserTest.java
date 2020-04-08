5f9402097db684cb2b985cc19046dbdc5ae079e5
Isira Seneviratne <isirasen96@gmail.com>
2020-04-08 09:23:25 +0530

    Add SAXException test for AccountManagerParser.

--- android/BOINC/app/src/test/java/edu/berkeley/boinc/rpc/AccountManagerParserTest.java
+++ android/BOINC/app/src/test/java/edu/berkeley/boinc/rpc/AccountManagerParserTest.java
@@ -25,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 import java.util.List;
@@ -34,6 +35,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
@@ -57,6 +61,18 @@ public class AccountManagerParserTest {
     @Test
     public void testParse_whenRpcStringIsNull_thenExpectEmptyList() {
         mockStatic(Xml.class);
+
+        List<AccountManager> accountManagers = AccountManagerParser.parse(null);
+
+        assertNotNull(accountManagers);
+        assertTrue(accountManagers.isEmpty());
+    }
+
+    @Test
+    public void testParse_whenSAXExceptionIsThrown_thenExpectEmptyList() throws Exception {
+        mockStatic(Xml.class);
+
+        doThrow(new SAXException()).when(Xml.class, "parse", anyString(), any(ContentHandler.class));
 
         List<AccountManager> accountManagers = AccountManagerParser.parse(null);
 
