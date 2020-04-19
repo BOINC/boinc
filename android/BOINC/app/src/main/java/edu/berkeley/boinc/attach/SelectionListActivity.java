df58a36d713b0fd913b0c56cb49a8dd9db60dd81
Isira Seneviratne <isirasen96@gmail.com>
2020-04-20 04:06:29 +0530

    Update copyright year for modified classes.

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/attach/SelectionListActivity.java
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/attach/SelectionListActivity.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of BOINC.
  * http://boinc.berkeley.edu
- * Copyright (C) 2019 University of California
+ * Copyright (C) 2020 University of California
  *
  * BOINC is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License
