e2a96c4aa022636d536da6c82bb3398f2d82033b
Isira Seneviratne <isirasen96@gmail.com>
2020-04-22 04:28:04 +0530

    Add statement to mock the Log class in the SAXException test.

--- android/BOINC/app/src/test/java/edu/berkeley/boinc/rpc/ProjectInfoParserTest.java
+++ android/BOINC/app/src/test/java/edu/berkeley/boinc/rpc/ProjectInfoParserTest.java
@@ -18,6 +18,7 @@
  */
 package edu.berkeley.boinc.rpc;
 
+import android.util.Log;
 import android.util.Xml;
 
 import org.junit.Before;
@@ -39,7 +40,7 @@ import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(Xml.class)
+@PrepareForTest({Log.class, Xml.class})
 public class ProjectInfoParserTest {
     private static final String PROJECT_INFO_NAME = "Project Info";
 
@@ -61,6 +62,7 @@ public class ProjectInfoParserTest {
 
     @Test
     public void testParse_whenSAXExceptionIsThrown_thenExpectEmptyList() throws Exception {
+        mockStatic(Log.class);
         mockStatic(Xml.class);
 
         doThrow(new SAXException()).when(Xml.class, "parse", anyString(), any(ContentHandler.class));
