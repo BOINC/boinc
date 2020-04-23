628132fb0b2c81acfadb033f6092fb8a367dfe2e
Isira Seneviratne <isirasen96@gmail.com>
2020-04-23 07:16:52 +0530

    Convert PowerConnectedReceiver to Kotlin.

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/receiver/PowerConnectedReceiver.kt
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/receiver/PowerConnectedReceiver.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of BOINC.
  * http://boinc.berkeley.edu
- * Copyright (C) 2012 University of California
+ * Copyright (C) 2020 University of California
  *
  * BOINC is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License
@@ -16,24 +16,18 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
  */
-package edu.berkeley.boinc.receiver;
+package edu.berkeley.boinc.receiver
 
-import edu.berkeley.boinc.client.Monitor;
-import edu.berkeley.boinc.utils.Logging;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import edu.berkeley.boinc.client.Monitor
+import edu.berkeley.boinc.utils.Logging
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-
-public class PowerConnectedReceiver extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-        Log.d(Logging.TAG, "power connected, start service...");
-        Intent startServiceIntent = new Intent(context, Monitor.class);
-        context.startService(startServiceIntent);
+class PowerConnectedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(Logging.TAG, "power connected, start service...")
+        context.startService(Intent(context, Monitor::class.java))
     }
 }
-
