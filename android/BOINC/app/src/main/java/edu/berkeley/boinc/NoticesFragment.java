070cbdb1664d1d3bdacd583a247907a39b493c1f
Isira Seneviratne <isirasen96@gmail.com>
2020-05-12 20:13:08 +0530

    Revert "Convert NoticesFragment to Kotlin and use KTX to automatically retrieve noticesList."
    This reverts commit ac8d2802

---
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/NoticesFragment.java
@@ -0,0 +1,119 @@
+/*
+ * This file is part of BOINC.
+ * http://boinc.berkeley.edu
+ * Copyright (C) 2020 University of California
+ *
+ * BOINC is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * BOINC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.berkeley.boinc;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import edu.berkeley.boinc.adapter.NoticesListAdapter;
+import edu.berkeley.boinc.rpc.Notice;
+import edu.berkeley.boinc.utils.Logging;
+
+public class NoticesFragment extends Fragment {
+    private NoticesListAdapter noticesListAdapter;
+    private List<Notice> data = new ArrayList<>();
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if(Logging.VERBOSE) {
+            Log.d(Logging.TAG, "NoticesFragment onCreateView");
+        }
+        View layout = inflater.inflate(R.layout.notices_layout, container, false);
+        ListView noticesList = layout.findViewById(R.id.noticesList);
+        updateNotices();
+        noticesListAdapter = new NoticesListAdapter(getActivity(), R.id.noticesList, data);
+        noticesList.setAdapter(noticesListAdapter);
+        return layout;
+    }
+
+    @Override
+    public void onResume() {
+        if(Logging.DEBUG) {
+            Log.d(Logging.TAG, "NoticesFragment onResume()");
+        }
+        getActivity().registerReceiver(mClientStatusChangeRec, ifcsc);
+
+        // clear notice notification
+        try {
+            BOINCActivity.monitor.cancelNoticeNotification();
+        }
+        catch(Exception e) {
+            if(Logging.ERROR) {
+                Log.e(Logging.TAG, "NoticesFragment.onResume error: ", e);
+            }
+        }
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        //unregister receiver, so there are not multiple intents flying in
+        if(Logging.DEBUG) {
+            Log.d(Logging.TAG, "NoticesFragment remove receiver");
+        }
+        getActivity().unregisterReceiver(mClientStatusChangeRec);
+        super.onPause();
+    }
+
+    private BroadcastReceiver mClientStatusChangeRec = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(Logging.VERBOSE) {
+                Log.d(Logging.TAG, "NoticesFragment ClientStatusChange - onReceive()");
+            }
+
+            // data retrieval
+            updateNotices();
+            noticesListAdapter.clear();
+            noticesListAdapter.addAll(data);
+            noticesListAdapter.notifyDataSetChanged();
+        }
+    };
+    private IntentFilter ifcsc = new IntentFilter("edu.berkeley.boinc.clientstatuschange");
+
+    private void updateNotices() {
+        try {
+            data = BOINCActivity.monitor.getRssNotices();
+            // sorting policy:
+            // latest arrival first.
+            Collections.sort(data, (lhs, rhs) ->
+                    Double.compare(rhs.getCreateTime(), lhs.getCreateTime()));
+        }
+        catch(Exception e) {
+            if(Logging.ERROR) {
+                Log.e(Logging.TAG, "NoticesFragment.updateNotices error: ", e);
+            }
+        }
+    }
+}
