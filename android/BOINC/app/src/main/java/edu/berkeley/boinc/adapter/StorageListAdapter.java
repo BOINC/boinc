7771a5051d7b0c7aed42e7763c770a07c6035210
Tal Regev <tal.regev@gmail.com>
2020-05-13 14:57:59 +0300

    Show project size.
    (cherry picked from commit 85f0b4f3cd6e91cc1d36c60a033196e9bed5eb6d)
    (cherry picked from commit 2c9c0b3f51b9ceb2bece647abb7979c6cf7d0971)
    (cherry picked from commit a24f3278efc52d528a26f9476b3d079d6aa7e32a)

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/adapter/StorageListAdapter.java
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/adapter/StorageListAdapter.java
@@ -21,7 +21,6 @@ package edu.berkeley.boinc.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,16 +35,11 @@ import androidx.annotation.NonNull;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.text.NumberFormat;
-import java.util.Calendar;
 import java.util.List;
 
 import edu.berkeley.boinc.BOINCActivity;
 import edu.berkeley.boinc.StorageFragment.StorageListData;
 import edu.berkeley.boinc.R;
-import edu.berkeley.boinc.StorageFragment;
-import edu.berkeley.boinc.rpc.Notice;
-import edu.berkeley.boinc.rpc.Transfer;
 import edu.berkeley.boinc.utils.Logging;
 
 public class StorageListAdapter extends ArrayAdapter<StorageListData> {
@@ -79,15 +73,9 @@ public class StorageListAdapter extends ArrayAdapter<StorageListData> {
         return entries.get(position).project.getProjectName();
     }
 
-    private String getUser(int position) {
-        String user = entries.get(position).project.getUserName();
-        String team = entries.get(position).project.getTeamName();
-
-        if(!team.isEmpty()) {
-            return (user + " (" + team + ")");
-        }
-
-        return user;
+    private String getSize(int position) {
+        double serverSize = entries.get(position).project.getDiskUsage();
+        return Double.valueOf(serverSize).toString();
     }
 
     private Bitmap getIcon(int position) {
@@ -107,7 +95,6 @@ public class StorageListAdapter extends ArrayAdapter<StorageListData> {
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         StorageListData data = entries.get(position);
-        boolean isAcctMgr = data.isMgr;
 
         View vi = convertView;
         // setup new view, if:
@@ -128,179 +115,43 @@ public class StorageListAdapter extends ArrayAdapter<StorageListData> {
             final LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             assert layoutInflater != null;
             // first time getView is called for this element
-            if(isAcctMgr) {
-                vi = layoutInflater.inflate(R.layout.projects_layout_listitem_acctmgr, null);
-            }
-            else {
-                vi = layoutInflater.inflate(R.layout.projects_layout_listitem, null);
-            }
+            vi = layoutInflater.inflate(R.layout.storage_layout_listitem, null);
             vi.setTag(data.id);
         }
 
-        if(isAcctMgr) {
-            // element is account manager
+        // element is project
+        // set data of standard elements
+        TextView tvName = vi.findViewById(R.id.project_name);
+        tvName.setText(getName(position));
 
-            // populate name
-            TextView tvName = vi.findViewById(R.id.name);
-            tvName.setText(data.acctMgrInfo.getAcctMgrName());
+        TextView tvSize = vi.findViewById(R.id.project_size);
+        String sizeText = getSize(position);
+        tvSize.setText(sizeText);
 
-            // populate url
-            TextView tvUrl = vi.findViewById(R.id.url);
-            tvUrl.setText(data.acctMgrInfo.getAcctMgrUrl());
+        ImageView ivIcon = vi.findViewById(R.id.project_icon);
+        String finalIconId = (String) ivIcon.getTag();
+        if(!StringUtils.equals(finalIconId, data.id)) {
+            Bitmap icon = getIcon(position);
+            // if available set icon, if not boinc logo
+            if(icon == null) {
+                // BOINC logo
+                ivIcon.setImageDrawable(getContext().getResources().getDrawable(R.drawable.boinc));
+            }
+            else {
+                // project icon
+                ivIcon.setImageBitmap(icon);
+                // mark as final
+                ivIcon.setTag(data.id);
+            }
+        }
+
+        // icon background
+        RelativeLayout iconBackground = vi.findViewById(R.id.icon_background);
+        if(data.project.getAttachedViaAcctMgr()) {
+            iconBackground.setBackground(activity.getApplicationContext().getResources().getDrawable(R.drawable.shape_light_blue_background_wo_stroke));
         }
         else {
-            // element is project
-            // set data of standard elements
-            TextView tvName = vi.findViewById(R.id.project_name);
-            tvName.setText(getName(position));
-
-            TextView tvUser = vi.findViewById(R.id.project_user);
-            String userText = getUser(position);
-            if(userText.isEmpty()) {
-                tvUser.setVisibility(View.GONE);
-            }
-            else {
-                tvUser.setVisibility(View.VISIBLE);
-                tvUser.setText(userText);
-            }
-
-            String statusText = "";
-            try {
-                statusText = BOINCActivity.monitor.getProjectStatus(data.project.getMasterURL());
-            }
-            catch(Exception e) {
-                if(Logging.ERROR) {
-                    Log.e(Logging.TAG, "ProjectsListAdapter.getView error: ", e);
-                }
-            }
-            TextView tvStatus = vi.findViewById(R.id.project_status);
-            if(statusText.isEmpty()) {
-                tvStatus.setVisibility(View.GONE);
-            }
-            else {
-                tvStatus.setVisibility(View.VISIBLE);
-                tvStatus.setText(statusText);
-            }
-
-            ImageView ivIcon = vi.findViewById(R.id.project_icon);
-            String finalIconId = (String) ivIcon.getTag();
-            if(!StringUtils.equals(finalIconId, data.id)) {
-                Bitmap icon = getIcon(position);
-                // if available set icon, if not boinc logo
-                if(icon == null) {
-                    // BOINC logo
-                    ivIcon.setImageDrawable(getContext().getResources().getDrawable(R.drawable.boinc));
-                }
-                else {
-                    // project icon
-                    ivIcon.setImageBitmap(icon);
-                    // mark as final
-                    ivIcon.setTag(data.id);
-                }
-            }
-
-            // transfers
-            int numberTransfers = data.projectTransfers.size();
-            TextView tvTransfers = vi.findViewById(R.id.project_transfers);
-            String transfersString = "";
-            if(numberTransfers > 0) { // ongoing transfers
-                // summarize information for compact representation
-                int numberTransfersUpload = 0;
-                boolean uploadsPresent = false;
-                int numberTransfersDownload = 0;
-                boolean downloadsPresent = false;
-                boolean transfersActive = false; // true if at least one transfer is active
-                long nextRetryS = 0;
-                for(Transfer trans : data.projectTransfers) {
-                    if(trans.isUpload()) {
-                        numberTransfersUpload++;
-                        uploadsPresent = true;
-                    }
-                    else {
-                        numberTransfersDownload++;
-                        downloadsPresent = true;
-                    }
-                    if(trans.isTransferActive()) {
-                        transfersActive = true;
-                    }
-                    else if(trans.getNextRequestTime() < nextRetryS || nextRetryS == 0) {
-                        nextRetryS = trans.getNextRequestTime();
-                    }
-                }
-
-                String numberTransfersString = "("; // will never be empty
-                if(downloadsPresent) {
-                    numberTransfersString +=
-                            numberTransfersDownload + " " + activity.getResources().getString(R.string.trans_download);
-                }
-                if(downloadsPresent && uploadsPresent) {
-                    numberTransfersString += " / ";
-                }
-                if(uploadsPresent) {
-                    numberTransfersString +=
-                            numberTransfersUpload + " " + activity.getResources().getString(R.string.trans_upload);
-                }
-                numberTransfersString += ")";
-
-                String activityStatus = ""; // will never be empty
-                String activityExplanation = "";
-                if(!transfersActive) { // no transfers active, give reason
-                    activityStatus += activity.getResources().getString(R.string.trans_pending);
-
-                    if(nextRetryS > 0) { // next try at defined time
-                        long retryAtMs = nextRetryS * 1000;
-                        long retryInMs = retryAtMs - Calendar.getInstance().getTimeInMillis();
-                        if(retryInMs < 0) {
-                        }// timestamp in the past, write nothing
-                        else {
-                            activityExplanation += activity.getResources().getString(R.string.trans_retryin) + " " +
-                                                   DateUtils.formatElapsedTime(retryInMs / 1000);
-                        }
-
-                    }
-                }
-                else { // transfers active
-                    activityStatus += activity.getResources().getString(R.string.trans_active);
-                }
-
-                transfersString +=
-                        activity.getResources().getString(R.string.tab_transfers) + " " + activityStatus + " " +
-                        numberTransfersString + " " + activityExplanation;
-                tvTransfers.setVisibility(View.VISIBLE);
-                tvTransfers.setText(transfersString);
-
-            }
-            else { // no ongoing transfers
-                tvTransfers.setVisibility(View.GONE);
-            }
-
-            // credits
-            final long userCredit = Math.round(data.project.getUserTotalCredit()),
-                    hostCredit = Math.round(data.project.getHostTotalCredit());
-            ((TextView) vi.findViewById(R.id.project_credits)).setText(hostCredit == userCredit ?
-                                                                       NumberFormat.getIntegerInstance().format(hostCredit) :
-                                                                       this.activity.getString(R.string.projects_credits_host_and_user, hostCredit, userCredit));
-
-            // server notice
-            Notice notice = data.getLastServerNotice();
-            TextView tvNotice = vi.findViewById(R.id.project_notice);
-            if(notice == null) {
-                tvNotice.setVisibility(View.GONE);
-            }
-            else {
-                tvNotice.setVisibility(View.VISIBLE);
-                String noticeText = notice.getDescription().trim();
-                tvNotice.setText(noticeText);
-            }
-
-            // icon background
-            RelativeLayout iconBackground = vi.findViewById(R.id.icon_background);
-            if(data.project.getAttachedViaAcctMgr()) {
-                iconBackground.setBackground(activity.getApplicationContext().getResources().getDrawable(R.drawable.shape_light_blue_background_wo_stroke));
-            }
-            else {
-                iconBackground.setBackgroundColor(activity.getApplicationContext().getResources().getColor(android.R.color.transparent));
-            }
+            iconBackground.setBackgroundColor(activity.getApplicationContext().getResources().getColor(android.R.color.transparent));
         }
 
         return vi;
