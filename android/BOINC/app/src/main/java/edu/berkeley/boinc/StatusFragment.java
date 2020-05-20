33b878adf35d06d4e77e4552049bcd22fdd45ee6
Tal Regev <tal.regev@gmail.com>
2020-05-20 20:16:49 +0300

    Change status fragment to be with black icons (The background is light color).

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/StatusFragment.java
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/StatusFragment.java
@@ -122,7 +122,7 @@ public class StatusFragment extends Fragment {
                             statusWrapper.setVisibility(View.VISIBLE);
                             statusHeader.setText(BOINCActivity.monitor.getCurrentStatusTitle());
                             statusHeader.setVisibility(View.VISIBLE);
-                            statusImage.setImageResource(R.drawable.ic_baseline_play_arrow_48);
+                            statusImage.setImageResource(R.drawable.ic_baseline_play_arrow_black_48);
                             statusImage.setContentDescription(BOINCActivity.monitor.getCurrentStatusTitle());
                             statusDescriptor.setText(BOINCActivity.monitor.getCurrentStatusDescription());
                             centerWrapper.setVisibility(View.VISIBLE);
@@ -132,7 +132,7 @@ public class StatusFragment extends Fragment {
                             statusWrapper.setVisibility(View.VISIBLE);
                             statusHeader.setText(BOINCActivity.monitor.getCurrentStatusTitle());
                             statusHeader.setVisibility(View.VISIBLE);
-                            statusImage.setImageResource(R.drawable.ic_baseline_pause_48);
+                            statusImage.setImageResource(R.drawable.ic_baseline_pause_black_48);
                             statusImage.setContentDescription(BOINCActivity.monitor.getCurrentStatusTitle());
                             statusImage.setClickable(false);
                             centerWrapper.setVisibility(View.VISIBLE);
