1d22f387497631a0ca6bc373ae085f1c7eccc0f7
talregev <tal.regev@gmail.com>
2020-06-23 16:13:39 +0300

    Fix travis compilation

---
+++ android/build_all_silent.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+#
+# See: https://boinc.berkeley.edu/trac/wiki/AndroidBuildClient
+#
+
+# Script to compile everything BOINC needs for Android
+export VERBOSE="no"
+
+cd ../
+BUILD_DIR="$PWD/3rdParty/buildCache"
+cd android/
+
+./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch arm     --silent
+./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch arm64   --silent
+./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch x86     --silent
+./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch x86_64  --silent
+
+echo '===== BOINC for all platforms build done ====='
