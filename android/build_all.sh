70f513dd76358e05656f71bfa92754966bae1a4b
talregev <tal.regev@gmail.com>
2020-07-05 03:41:53 +0300

    Fix neon optimizations scritps. Fix clear environment scriprt.

--- android/build_all.sh
+++ android/build_all.sh
@@ -17,8 +17,14 @@ cd android/
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch x86
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --arch x86_64
 
+if [ ! -d ../3rdParty/buildCache/android-tc/arm ]; then
+    echo You run this script from diffrent folder: $PWD
+    echo Please run it from boinc/android
+    exit 1
+fi
+
 for i in "libcrypto.a" "libssl.a" "libcurl.a"; do
-    if [[ $(readelf -A $(find $ANDROID_TC/arm  -name "$i") | grep -i neon | head -c1 | wc -c) -ne 0 ]]; then
+    if [ $(readelf -A $(find ../3rdParty/buildCache/android-tc/arm  -name "$i") | grep -i neon | head -c1 | wc -c) -ne 0 ]; then
         echo "$i" is with neon optimization
         exit 1
     fi
