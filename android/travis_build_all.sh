551c40cad3197c5c1bfb63fa38fd2bd0ed53ea7b
talregev <tal.regev@gmail.com>
2020-07-10 19:48:42 +0300

    Define default ANDROID_TC in build_all.sh and travis_build_all.sh and use ANDROID_TC for the neon test.

--- android/travis_build_all.sh
+++ android/travis_build_all.sh
@@ -12,7 +12,9 @@ set -e
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch x86
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch x86_64
 
-if [ ! -d ../3rdParty/buildCache/android-tc/arm ]; then
+ANDROID_TC="${ANDROID_TC:-../3rdParty/buildCache/android-tc/arm}"
+
+if [ ! -d $ANDROID_TC ]; then
     echo You run this script from diffrent folder: $PWD
     echo Please run it from boinc/android
     exit 1
@@ -21,7 +23,7 @@ fi
 list_libs="libcrypto.a libssl.a libcurl.a"
 
 for i in $list_libs; do
-    if [ $(readelf -A $(find ../3rdParty/buildCache/android-tc/arm  -name "$i") | grep -i neon | head -c1 | wc -c) -ne 0 ]; then
+    if [ $(readelf -A $(find $ANDROID_TC -name "$i") | grep -i neon | head -c1 | wc -c) -ne 0 ]; then
         echo "$i" is with neon optimization
         exit 1
     fi
