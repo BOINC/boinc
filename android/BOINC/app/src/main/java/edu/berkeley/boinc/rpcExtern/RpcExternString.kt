f639f0659299e6e65664ce9ffdd97479cd353d8f
Fred <github@efmer.com>
2020-10-24 16:02:16 +0200

    Update android/BOINC/app/src/main/java/edu/berkeley/boinc/rpcExtern/RpcExternString.kt
    Co-authored-by: Isira Seneviratne <31027858+Isira-Seneviratne@users.noreply.github.com>

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/rpcExtern/RpcExternString.kt
+++ android/BOINC/app/src/main/java/edu/berkeley/boinc/rpcExtern/RpcExternString.kt
@@ -1,7 +1,5 @@
 package edu.berkeley.boinc.rpcExtern
 
-class RpcExternString {
-    val mRpcReplyBegin = "<boinc_gui_rpc_reply>"
-    val mRpcReplyEnd = "</boinc_gui_rpc_reply>"
-    val mRpcRequestBegin = "<boinc_gui_rpc_request>"
-}
+const val mRpcReplyBegin = "<boinc_gui_rpc_reply>"
+const val mRpcReplyEnd = "</boinc_gui_rpc_reply>"
+const val mRpcRequestBegin = "<boinc_gui_rpc_request>"
