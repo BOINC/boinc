85305e4c1ca0555eafdb2519cbf275eec26edf57
Fred <github@efmer.com>
2020-10-31 16:08:32 +0100

    Delete RpcExternString.kt

--- android/BOINC/app/src/main/java/edu/berkeley/boinc/rpcExtern/RpcExternString.kt
+++
@@ -1,7 +0,0 @@
-package edu.berkeley.boinc.rpcExtern
-
-class RpcExternString {
-    val mRpcReplyBegin = "<boinc_gui_rpc_reply>"
-    val mRpcReplyEnd = "</boinc_gui_rpc_reply>"
-    val mRpcRequestBegin = "<boinc_gui_rpc_request>"
-}
