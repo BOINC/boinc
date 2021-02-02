c35ce745531c329d248d4288f78c856585903f73
Hector Espert <hectorespertpardo@gmail.com>
2021-02-02 19:03:32 +0100

    Add test_util tests file

---
+++ tests/unit-tests/lib/test_util.cpp
@@ -0,0 +1,61 @@
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2020 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "gtest/gtest.h"
+#include "util.h"
+
+using namespace std;
+
+namespace test_util {
+
+    // The fixture for testing class Foo.
+
+    class test_util : public ::testing::Test {
+    protected:
+        // You can remove any or all of the following functions if its body
+        // is empty.
+
+        test_util() {
+            // You can do set-up work for each test here.
+        }
+
+        virtual ~test_util() {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
+
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
+
+        virtual void SetUp() {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+        }
+
+        virtual void TearDown() {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
+
+        // Objects declared here can be used by all tests in the test case for Foo.
+    };
+
+    TEST_F(test_util, dtime) {
+        EXPECT_TRUE(dtime() >= 0);
+    }
+
+
+} // namespace
