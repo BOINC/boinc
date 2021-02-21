c7db645273aa01016872edcf260aacf7c40e61cd
Tal Regev <tal.regev@gmail.com>
2021-02-21 05:34:51 +0200

    Fix indent in the function.

--- clientgui/ViewStatistics.cpp
+++ clientgui/ViewStatistics.cpp
@@ -1930,119 +1930,119 @@ CViewStatistics::CViewStatistics(wxNotebook* pNotebook) :
 	CTaskItemGroup* pGroup = NULL;
 	CTaskItem*      pItem = NULL;
 
-    //
-    // Setup View
-    //
-    wxFlexGridSizer* itemFlexGridSizer = new wxFlexGridSizer(2, 0, 0);
-    wxASSERT(itemFlexGridSizer);
+	//
+	// Setup View
+	//
+	wxFlexGridSizer* itemFlexGridSizer = new wxFlexGridSizer(2, 0, 0);
+	wxASSERT(itemFlexGridSizer);
 
-    itemFlexGridSizer->AddGrowableRow(0);
-    itemFlexGridSizer->AddGrowableCol(1);
+	itemFlexGridSizer->AddGrowableRow(0);
+	itemFlexGridSizer->AddGrowableCol(1);
     
-    m_pTaskPane = new CBOINCTaskCtrl(this, ID_TASK_STATISTICSVIEW, DEFAULT_TASK_FLAGS);
-    wxASSERT(m_pTaskPane);
+	m_pTaskPane = new CBOINCTaskCtrl(this, ID_TASK_STATISTICSVIEW, DEFAULT_TASK_FLAGS);
+	wxASSERT(m_pTaskPane);
 
 	m_PaintStatistics = new CPaintStatistics(this, ID_LIST_STATISTICSVIEW, wxDefaultPosition, wxSize(-1, -1), 0);
 	wxASSERT(m_PaintStatistics);
 
 	m_pTaskPane->SetLayoutDirection(wxLayout_LeftToRight);
 	m_PaintStatistics->SetLayoutDirection(wxLayout_LeftToRight);
-    itemFlexGridSizer->Add(m_pTaskPane, 1, wxGROW|wxALL, 1);
-    itemFlexGridSizer->Add(m_PaintStatistics, 1, wxGROW|wxALL, 1);
+	itemFlexGridSizer->Add(m_pTaskPane, 1, wxGROW|wxALL, 1);
+	itemFlexGridSizer->Add(m_PaintStatistics, 1, wxGROW|wxALL, 1);
 
-    SetSizer(itemFlexGridSizer);
+	SetSizer(itemFlexGridSizer);
 
-    Layout();
+	Layout();
 
 	pGroup = new CTaskItemGroup( _("Commands") );
 	m_TaskGroups.push_back( pGroup );
 
 	pItem = new CTaskItem(
-        _("Show user total"),
-        _("Show total credit for user"),
-        ID_TASK_STATISTICS_USERTOTAL 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("Show user total"),
+		_("Show total credit for user"),
+		ID_TASK_STATISTICS_USERTOTAL 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pItem = new CTaskItem(
-        _("Show user average"),
-        _("Show average credit for user"),
-        ID_TASK_STATISTICS_USERAVERAGE 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("Show user average"),
+		_("Show average credit for user"),
+		ID_TASK_STATISTICS_USERAVERAGE 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pItem = new CTaskItem(
-        _("Show host total"),
-        _("Show total credit for host"),
-        ID_TASK_STATISTICS_HOSTTOTAL 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("Show host total"),
+		_("Show total credit for host"),
+		ID_TASK_STATISTICS_HOSTTOTAL 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pItem = new CTaskItem(
-        _("Show host average"),
-        _("Show average credit for host"),
-        ID_TASK_STATISTICS_HOSTAVERAGE 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("Show host average"),
+		_("Show average credit for host"),
+		ID_TASK_STATISTICS_HOSTAVERAGE 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pGroup = new CTaskItemGroup( _("Project") );
 	m_TaskGroups.push_back( pGroup );
 
 	pItem = new CTaskItem(
-        _("< &Previous project"),
-        _("Show chart for previous project"),
-        ID_TASK_STATISTICS_PREVPROJECT 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("< &Previous project"),
+		_("Show chart for previous project"),
+		ID_TASK_STATISTICS_PREVPROJECT 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 	pItem = new CTaskItem(
-        _("&Next project >"),
-        _("Show chart for next project"),
-        ID_TASK_STATISTICS_NEXTPROJECT 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("&Next project >"),
+		_("Show chart for next project"),
+		ID_TASK_STATISTICS_NEXTPROJECT 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pItem = new CTaskItem(
-        _("Hide project list"),
-        _("Use entire area for graphs"),
-        ID_TASK_STATISTICS_HIDEPROJLIST 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("Hide project list"),
+		_("Use entire area for graphs"),
+		ID_TASK_STATISTICS_HIDEPROJLIST 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pGroup = new CTaskItemGroup( _("Mode view") );
 	m_TaskGroups.push_back( pGroup );
 
 	pItem = new CTaskItem(
-        _("One project"),
-        _("Show one chart with selected project"),
-        ID_TASK_STATISTICS_MODEVIEWONEPROJECT 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("One project"),
+		_("Show one chart with selected project"),
+		ID_TASK_STATISTICS_MODEVIEWONEPROJECT 
+	);
+	pGroup->m_Tasks.push_back( pItem );
         
 	pItem = new CTaskItem(
-        _("All projects (separate)"),
-        _("Show all projects, one chart per project"),
-        ID_TASK_STATISTICS_MODEVIEWALLSEPARATE 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("All projects (separate)"),
+		_("Show all projects, one chart per project"),
+		ID_TASK_STATISTICS_MODEVIEWALLSEPARATE 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
 	pItem = new CTaskItem(
-        _("All projects (together)"),
-        _("Show one chart with all projects"),
-        ID_TASK_STATISTICS_MODEVIEWALLTOGETHER 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("All projects (together)"),
+		_("Show one chart with all projects"),
+		ID_TASK_STATISTICS_MODEVIEWALLTOGETHER 
+	);
+	pGroup->m_Tasks.push_back( pItem );
  
 	pItem = new CTaskItem(
-        _("All projects (sum)"),
-        _("Show one chart with sum of projects"),
-        ID_TASK_STATISTICS_MODEVIEWSUM 
-    );
-    pGroup->m_Tasks.push_back( pItem );
+		_("All projects (sum)"),
+		_("Show one chart with sum of projects"),
+		ID_TASK_STATISTICS_MODEVIEWSUM 
+	);
+	pGroup->m_Tasks.push_back( pItem );
 
-    // Create Task Pane Items
-    m_pTaskPane->UpdateControls();
+	// Create Task Pane Items
+	m_pTaskPane->UpdateControls();
 
-    UpdateSelection();
+	UpdateSelection();
 }
 
 CViewStatistics::~CViewStatistics() {
