333d897764b336275c459221c31d079382f7f329
Charlie Fenton <charlief@example.com>
2021-06-01 21:18:24 -0700

    Merge pull request #4130 from BOINC/cb_migrate_to_github_actions
    Cherry picked from master. From original merge:
    
    [CI] replace Travis and AppVeyor with Github Actions
    Commit: 180ea883ed3c257eab850a0ea717ee24bfc34a56 [180ea883ed]
    Parents: 89fa2cf080, c9e922f924
    Author: Christian Beer <christian.beer@posteo.de>
    Date: December 31, 2020 at 3:42:37 AM PST
    Committer: GitHub <noreply@github.com>

--- android/ci_build_all.sh
+++ android/ci_build_all.sh
@@ -7,9 +7,27 @@ set -e
 
 # Script to compile everything BOINC needs for Android
 
+# check working directory because the script needs to be called like: ./android/ci_build_all.sh
+if [ ! -d "android" ]; then
+    echo "start this script in the source root directory"
+    exit 1
+fi
+
+cd android
+
+echo '===== BOINC Client for all platforms build start ====='
+
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch arm
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch arm64
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch x86
 ./buildAndroidBOINC-CI.sh --cache_dir "$ANDROID_TC" --build_dir "$BUILD_DIR" --silent --ci --arch x86_64
 
-echo '===== BOINC for all platforms build done ====='
+echo '===== BOINC Client for all platforms build done ====='
+
+cd BOINC 
+
+echo '===== BOINC Manager build start ====='
+
+./gradlew clean assemble jacocoTestReportDebug
+
+echo '===== BOINC Manager build done ====='
