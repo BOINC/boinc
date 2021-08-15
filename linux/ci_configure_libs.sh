a67e4e6531ac17f00dbb9c6a209fcfa592ba52c0
Tal Regev <tal.regev@gmail.com>
2021-08-15 19:40:57 +0000

    Compile gui apps for linux.

--- linux/ci_configure_libs.sh
+++ linux/ci_configure_libs.sh
@@ -11,7 +11,7 @@ BUILD_DIR="$PWD/3rdParty/linux"
 VCPKG_ROOT="$BUILD_DIR/vcpkg"
 export VCPKG_DIR="$VCPKG_ROOT/installed/x64-linux"
 
-linux/update_vcpkg.sh
+linux/update_vcpkg_libs.sh
 
 export _libcurl_pc="$VCPKG_DIR/lib/pkgconfig/libcurl.pc"
 ./configure --with-libcurl=$VCPKG_DIR --with-ssl=$VCPKG_DIR --disable-server --disable-client --disable-manager
