199170ed2d05a87e5f267871d57d657c60987a4b
Tal Regev <tal.regev@gmail.com>
2021-08-26 15:41:19 +0300

    refactor to c++

--- samples/collect_data/collect_data.cpp
+++ samples/collect_data/collect_data.cpp
@@ -1,6 +1,6 @@
 // This file is part of BOINC.
 // http://boinc.berkeley.edu
-// Copyright (C) 2008 University of California
+// Copyright (C) 2021 University of California
 //
 // BOINC is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License
@@ -19,6 +19,8 @@
 // TODO: make it exit after a certain amount of time
 
 #include <sstream>
+#include <fstream>
+#include <iostream>
 #include <sockpp/socket.h>
 #include <sockpp/tcp_connector.h>
 #ifdef _WIN32
@@ -26,7 +28,6 @@
 #else
     #include <sockpp/unix_connector.h>
 #endif
-
 
 void mySleep(int seconds) {
 #ifdef _WIN32
@@ -36,36 +37,25 @@ void mySleep(int seconds) {
 #endif // _WIN32
 }
 
-#if _WIN32
-sockpp::connector* getConnected(sockpp::tcp_connector& tcp_conn) {
-    if (tcp_conn.is_connected()) {
-        return &tcp_conn;
+sockpp::connector* getConnected(sockpp::connector* array_connection[]) {
+    for (int i = 0; i < 2; ++i) {
+        sockpp::connector* conn = array_connection[i];
+        if (conn != NULL && conn->is_connected()) {
+            return conn;
+        }
     }
     return NULL;
 }
-#else
-sockpp::connector* getConnected(sockpp::tcp_connector& tcp_conn, sockpp::unix_connector& unix_conn) {
-    if (tcp_conn.is_connected()) {
-        return &tcp_conn;
+
+bool isConnected(sockpp::connector* array_connection[]) {
+    for (int i = 0; i < 2; ++i) {
+        sockpp::connector* conn = array_connection[i];
+        if (conn != NULL && conn->is_connected()) {
+            return true;
+        }
     }
-
-    if (unix_conn.is_connected()) {
-        return &unix_conn;
-    }
-
-    return NULL;
+    return false;
 }
-#endif
-
-#if _WIN32
-bool isConnected(sockpp::tcp_connector& conn) {
-    return conn.is_connected();
-}
-#else
-bool isConnected(sockpp::tcp_connector& conn, sockpp::unix_connector& unix_conn) {
-    return conn.is_connected() || unix_conn.is_connected();
-}
-#endif
 
 int main(int, char**) {
     //boinc_init();
@@ -73,15 +63,16 @@ int main(int, char**) {
     bool isDebug = false;
     sockpp::socket_initializer sockInit;
     sockpp::tcp_connector tcp_conn;
+    sockpp::connector* array_connection[] = { &tcp_conn, NULL };
 #if !_WIN32
     sockpp::unix_connector unix_conn;
+    array_connection[1] = &unix_conn;
 #endif
     sockpp::connector* conn = NULL;
     int16_t port = 31416;
     std::string android_address = "edu_berkeley_boinc_client_socket";
     std::string unix_address = "boinc_socket";
     std::string tcp_address = "localhost";
-    bool isConn = false;
     std::stringstream stream;
     const int sizeBuffer = 1024;
     char buf[sizeBuffer];
@@ -89,48 +80,37 @@ int main(int, char**) {
     while (true)
     {
         int attemped_connections = 0;
-#if _WIN32
-        isConn = isConnected(tcp_conn);
-#else
-        isConn = isConnected(tcp_conn, unix_conn);
-#endif
-        while (!isConn) {
+        while (!isConnected(array_connection)) {
+            attemped_connections++;
+            std::cout << "attemped connections: " << attemped_connections << "\n";
             if (!tcp_conn.connect(sockpp::inet_address(tcp_address, port))) {
-                printf("tcp connection fail:\n");
-                printf("%s\n",tcp_conn.last_error_str().c_str());
+                std::cout << "tcp connection fail:\n";
+                std::cout << tcp_conn.last_error_str() << "\n";
                 tcp_conn.clear();
             }
 #if! _WIN32
-            if (!isConnected(tcp_conn, unix_conn) && !unix_conn.connect(sockpp::unix_address(unix_address))) {
-                printf("unix connection fail:\n");
-                printf("%s\n",unix_conn.last_error_str().c_str());
+            if (!isConnected(array_connection) && !unix_conn.connect(sockpp::unix_address(unix_address))) {
+                std::cout << "unix connection fail:\n";
+                std::cout << unix_conn.last_error_str() << "\n";
                 unix_conn.clear();                
             }
 
-            if (!isConnected(tcp_conn, unix_conn) && !unix_conn.connect(sockpp::unix_address(android_address))) {
-                printf("android unix connection fail:\n");
-                printf("%s\n",unix_conn.last_error_str().c_str());
+            if (!isConnected(array_connection) && !unix_conn.connect(sockpp::unix_address(android_address))) {
+                std::cout << "android unix connection fail:\n";
+                std::cout << unix_conn.last_error_str() << "\n";
                 unix_conn.clear();                
-            }
-            isConn = isConnected(tcp_conn, unix_conn);
-            if (isConn) {
-                conn = getConnected(tcp_conn, unix_conn);
-            }
-#else
-            isConn = isConnected(tcp_conn);
-            if (isConn) {
-                conn = getConnected(tcp_conn);
             }
 #endif
-
-            attemped_connections++;
-            printf("attemped connections: %d\n\n", attemped_connections);
+            if (isConnected(array_connection)) {
+                conn = getConnected(array_connection);
+            }
+            std::cout << "\n";
             mySleep(1);
         }
 
         std::string request = "<boinc_gui_rpc_request><get_state/></boinc_gui_rpc_request>\3";
         if (isDebug) {
-            printf("before NULL == conn: %ld\n", conn);
+            std::cout << "before NULL == conn: " << conn << "\n";
         }
         if(NULL == conn) {
 #if !_WIN32
@@ -140,12 +120,12 @@ int main(int, char**) {
             continue;
         }
         if (isDebug) {
-            printf("before write: \n");
+            std::cout << "before write: \n";
         }
         int res = conn->write(request);
 
         if (isDebug) {
-            printf("After write: %d\n", res);
+            std::cout << "after write: " << res << "\n";            
         }
         if (res == -1) {
             conn->close();
@@ -156,11 +136,11 @@ int main(int, char**) {
             continue;
         }
         
-        printf("request bytes: %ld\n", request.size());
-        printf("write bytes: %d\n", res);
+        std::cout << "request bytes: " << request.size() << "\n";
+        std::cout << "write bytes: " << res << "\n";
 
         if (isDebug) {
-            printf("After write, before read: \n");
+            std::cout << "after write, before read: \n";
         }
 
         ssize_t n = sizeBuffer;
@@ -168,17 +148,21 @@ int main(int, char**) {
         while (n == sizeBuffer) {
             n = conn->read(buf, sizeof(buf));
             if (isDebug) {
-                printf("result:\n");
-                printf("%s\n", buf);
+                std::cout << "result size: "<< n << "\n";
+                std::cout << "buf: " << buf << "\n";
             }
             stream << buf;
-;
-            if (n < sizeBuffer) {
-                printf("buf: %s\n", buf);
-            }
         }
 
-        printf("read bytes: %d\n", stream.str().size());
+        std::cout << "read bytes: " << stream.str().size() << "\n";
+        std::ofstream outFile;
+        std::stringstream file_name;
+        file_name.str("");
+        file_name << "tal" << suffix_file << ".txt";
+        outFile.open(file_name.str());
+        outFile << stream.rdbuf();
+        std::cout << "write to file: tal" << suffix_file << ".txt\n\n";
+        suffix_file++;
         mySleep(1);
     }
 }
