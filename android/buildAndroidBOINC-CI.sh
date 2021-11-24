e77ae7d89ffede33c2bec7cc1b7266911ff36f3e
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2021-11-24 10:48:25 +0100

    Test
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- android/buildAndroidBOINC-CI.sh
+++ android/buildAndroidBOINC-CI.sh
@@ -268,6 +268,7 @@ if [ $build_with_vcpkg = "yes" ]; then
     if [ ! -e /tmp/vcpkg_updated ]; then
         git -C $VCPKG_ROOT reset --hard
         git -C $VCPKG_ROOT pull
+        git -C $BUILD_DIR checkout 683665efb8e3c1a249cb7935956849d1b9f2e31d
         $VCPKG_ROOT/bootstrap-vcpkg.sh
         touch /tmp/vcpkg_updated
     fi
@@ -306,7 +307,7 @@ vcpkgDir()
     arch=$1
     shift
     vcpkg_dir="$VCPKG_ROOT/installed"
-    
+
     if [ $arch = "armv6" ]; then
         vcpkg_dir="$vcpkg_dir/armv6-android"
     elif [ $arch = "arm" ]; then
