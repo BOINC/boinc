e1f81f75262763daf786cd8be381ea8aba8ba016
Christian Beer <christian.beer@posteo.de>
2021-12-27 00:31:50 +0100

    [unit tests] Add test for project_init.cpp
    This also checks basic file reading and writing.

---
+++ tests/unit-tests/lib/test_project_init.cpp
@@ -0,0 +1,128 @@
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2020 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include <fstream>
+#include "gtest/gtest.h"
+#include "project_init.h"
+
+using namespace std;
+
+namespace test_project_init {
+
+    // The fixture for testing class Foo.
+
+    class test_project_init : public ::testing::Test {
+    protected:
+        PROJECT_INIT pi;
+        std::string emptyFile = R"xxx(<project_init>
+  <url></url>
+  <name></name>
+  <account_key></account_key>
+  <embedded>0</embedded>
+</project_init>
+)xxx";
+        std::string exampleFile1 = R"xxx(<project_init>
+  <url>http://www.example.com</url>
+  <name>Example Project</name>
+  <account_key>1234567890abcdefghijklmnopqrstuvwxyz</account_key>
+  <embedded>0</embedded>
+</project_init>
+)xxx";
+        std::string exampleFile2 = R"xxx(<project_init>
+  <url>https://secure.example.com</url>
+  <name>Secure Example Project</name>
+  <account_key>zyxwvutsrqponmlkjihgfedcba0987654321</account_key>
+  <embedded>1</embedded>
+</project_init>
+)xxx";
+        // You can remove any or all of the following functions if its body
+        // is empty.
+
+        test_project_init() {
+            // You can do set-up work for each test here.
+
+        }
+
+        virtual ~test_project_init() {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
+
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
+
+        virtual void SetUp() {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+        }
+
+        virtual void TearDown() {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
+
+        // Objects declared here can be used by all tests in the test case for Foo.
+    };
+
+    TEST_F(test_project_init, init) {
+        int result = pi.init();
+        EXPECT_EQ(result, 0);
+
+        std::ofstream ofs ("project_init.xml", std::ofstream::out);
+        ofs << exampleFile2;
+        ofs.close();
+        result = pi.init();
+        EXPECT_EQ(result, 0);
+        EXPECT_STREQ(pi.url, "https://secure.example.com/");
+        ASSERT_STREQ(pi.name, "Secure Example Project");
+        ASSERT_STREQ(pi.account_key, "zyxwvutsrqponmlkjihgfedcba0987654321");
+        ASSERT_EQ(pi.embedded, true);
+
+        pi.remove();
+        pi.init();
+    }
+
+    TEST_F(test_project_init, write) {
+        int result = pi.write();
+        EXPECT_EQ(result, 0);
+        std::ifstream t("project_init.xml");
+        std::string genfile((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+        ASSERT_EQ(emptyFile, genfile);
+
+        strncpy(pi.url, "http://www.example.com", sizeof (pi.url));
+        strncpy(pi.name, "Example Project", sizeof (pi.name));
+        strncpy(pi.account_key, "1234567890abcdefghijklmnopqrstuvwxyz", sizeof (pi.account_key));
+        result = pi.write();
+        EXPECT_EQ(result, 0);
+        std::ifstream t2("project_init.xml");
+        std::string genfile2((std::istreambuf_iterator<char>(t2)), std::istreambuf_iterator<char>());
+        ASSERT_EQ(exampleFile1, genfile2);
+    }
+
+    TEST_F(test_project_init, remove) {
+        int result = pi.remove();
+        EXPECT_EQ(result, 0);
+        std::ifstream t("project_init.xml");
+        ASSERT_EQ(t.is_open(), false);
+        result = pi.remove();
+        EXPECT_EQ(result, 0);
+    }
+
+
+
+
+} // namespace
