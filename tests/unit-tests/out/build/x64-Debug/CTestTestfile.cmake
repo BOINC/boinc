ce6f1614aed3cd2384f8aea79bfb680583eb1f27
Vulpine05 <wbauerinil@yahoo.com>
2021-12-29 13:16:37 -0600

    Remove files accidentally added to previous commit.

--- tests/unit-tests/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests
-# Build directory: C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("sched")
--- tests/unit-tests/out/build/x64-Debug/sched/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/sched
-# Build directory: C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/out/build/x64-Debug/sched
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_sched "C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/out/build/x64-Debug/sched/test_sched.exe")
-set_tests_properties(test_sched PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/sched/CMakeLists.txt;9;add_test;C:/Users/wbaue/source/repos/Vulpine05-BOINC/tests/unit-tests/sched/CMakeLists.txt;0;")
