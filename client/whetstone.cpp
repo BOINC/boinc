884310d089737daff57d3269ec44f340a7357c21
computezrmle <57127745+computezrmle@users.noreply.github.com>
2022-01-20 09:22:35 +0100

    Fix compiler warnings "-Wmaybe-uninitialized"
      whetstone.cpp:274:12: warning: 'finisec' may be used uninitialized in this function [-Wmaybe-uninitialized]
      274 |     double diff = finisec - startsec;
          |            ^
    whetstone.cpp:101:22: note: 'finisec' was declared here
      101 |     double startsec, finisec;
          |                      ^
    whetstone.cpp:274:12: warning: 'startsec' may be used uninitialized in this function [-Wmaybe-uninitialized]
      274 |     double diff = finisec - startsec;
          |            ^
    whetstone.cpp:101:12: note: 'startsec' was declared here
      101 |     double startsec, finisec;
          |            ^

--- client/whetstone.cpp
+++ client/whetstone.cpp
@@ -98,7 +98,7 @@ int whetstone(double& flops, double& cpu_time, double min_cpu_time) {
     SPDP x,y,z;
     long j,k,l, jjj;
     SPDP e1[4];
-    double startsec, finisec;
+    double startsec = 0.0, finisec = 0.0;
     double KIPS;
     int xtra, ii;
     int x100 = 1000;   // chosen to make each pass take about 0.1 sec
