ec1472c25b2c2bfd27e993ebbb8a233cea206596
Charlie Fenton <charlief@example.com>
2022-02-04 01:03:07 -0800

    try to generate more info on MacOS CI build failures

--- mac_build/buildMacBOINC-CI.sh
+++ mac_build/buildMacBOINC-CI.sh
@@ -110,7 +110,10 @@ if [ "${style}" == "Development" ]; then
 fi
 
 target="BOINC_Client"
-source BuildMacBOINC.sh ${config} -noclean -target ${target} -setting HEADER_SEARCH_PATHS "../clientgui ${cache_dir}/include" -setting LIBRARY_SEARCH_PATHS "${libSearchPathDbg} ${cache_dir}/lib" -setting OTHER_LDFLAGS "-L. -lboinc -lcurl -lcares -lcrypto -lssl -lldap -lz -v" | tee xcodebuild_${target}.log | $beautifier; retval=${PIPESTATUS[0]}
+source BuildMacBOINC.sh ${config} -noclean -target ${target} -setting HEADER_SEARCH_PATHS "../clientgui ${cache_dir}/include" -setting LIBRARY_SEARCH_PATHS "${libSearchPathDbg} ${cache_dir}/lib" -setting OTHER_LDFLAGS "-L. -lboinc -lcurl -lcares -lcrypto -lssl -lldap -lz -v" | tee xcodebuild_${target}.log; retval=$?
+if [ ${retval} -eq 0 ]; then
+    retval=${PIPESTATUS[0]}
+fi
 if [ ${retval} -ne 0 ]; then
     curl --upload-file ./xcodebuild_${target}.log https://transfer.sh/xcodebuild_${target}.log
     cd ..; exit 1;
