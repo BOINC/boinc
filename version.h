7be1ea068ce5902a4c4fe084b80a0a1e529e4737
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2022-06-07 14:25:29 +0200

    Update version to 7.20.0
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #define BOINC_MAJOR_VERSION 7
 
 /* Minor part of BOINC version number */
-#define BOINC_MINOR_VERSION 19
+#define BOINC_MINOR_VERSION 20
 
 /* Release part of BOINC version number */
 #define BOINC_RELEASE 0
@@ -21,10 +21,10 @@
 #define VBOXWRAPPER_RELEASE 26204
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "7.19.0"
+#define BOINC_VERSION_STRING "7.20.0"
 
 /* Package is a pre-release (Alpha/Beta) package */
-#define BOINC_PRERELEASE 1
+//#define BOINC_PRERELEASE 1
 
 #if (defined(_WIN32) || defined(__APPLE__))
 /* Name of package */
@@ -37,13 +37,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 7.19.0"
+#define PACKAGE_STRING "BOINC 7.20.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.19.0"
+#define PACKAGE_VERSION "7.20.0"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
