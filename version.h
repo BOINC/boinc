ad87e8e75ea699ff96275b0c4cea39b914250bcb
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2022-06-07 18:17:33 +0200

    Update version
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #define BOINC_MAJOR_VERSION 7
 
 /* Minor part of BOINC version number */
-#define BOINC_MINOR_VERSION 19
+#define BOINC_MINOR_VERSION 21
 
 /* Release part of BOINC version number */
 #define BOINC_RELEASE 0
@@ -18,10 +18,10 @@
 
 // vboxwrapper version number
 // when you change this, change .exe file names in VS project files
-#define VBOXWRAPPER_RELEASE 26204
+#define VBOXWRAPPER_RELEASE 26205
 
 /* String representation of BOINC version number */
-#define BOINC_VERSION_STRING "7.19.0"
+#define BOINC_VERSION_STRING "7.21.0"
 
 /* Package is a pre-release (Alpha/Beta) package */
 #define BOINC_PRERELEASE 1
@@ -37,13 +37,13 @@
 #define PACKAGE_NAME "BOINC"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BOINC 7.19.0"
+#define PACKAGE_STRING "BOINC 7.21.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "boinc"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.19.0"
+#define PACKAGE_VERSION "7.21.0"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
