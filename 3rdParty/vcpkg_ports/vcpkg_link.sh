00bae1fd47c0a1351e85628f1b50b106be83c9b5
Tal Regev <tal.regev@gmail.com>
2022-07-23 16:14:56 +0300

    Test vcpkg wxwidgets 3.2.0

--- 3rdParty/vcpkg_ports/vcpkg_link.sh
+++ 3rdParty/vcpkg_ports/vcpkg_link.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export VCPKG_LINK=https://github.com/microsoft/vcpkg
+export VCPKG_LINK="https://github.com/dg0yt/vcpkg -b wxwidgets-3.2"
--- linux/update_vcpkg_manager.sh
+++ linux/update_vcpkg_manager.sh
@@ -20,4 +20,4 @@ fi
 git -C $VCPKG_ROOT pull
 $VCPKG_ROOT/bootstrap-vcpkg.sh
 $VCPKG_ROOT/vcpkg install curl[core,openssl] wxwidgets[core] --clean-after-build --overlay-triplets=$VCPKG_PORTS/triplets/ci
-$VCPKG_ROOT/vcpkg install freeglut ftgl --clean-after-build --overlay-triplets=$VCPKG_PORTS/triplets/ci
+# $VCPKG_ROOT/vcpkg install freeglut ftgl --clean-after-build --overlay-triplets=$VCPKG_PORTS/triplets/ci
