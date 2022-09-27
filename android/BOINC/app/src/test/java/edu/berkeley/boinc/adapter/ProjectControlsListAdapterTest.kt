c6f05fb5c61a636a44e2a8e7588d121782946f0e
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2022-09-27 20:40:11 +0530

    Fix Tests for Recycler View Adapter

--- android/BOINC/app/src/test/java/edu/berkeley/boinc/adapter/ProjectControlsListAdapterTest.kt
+++ android/BOINC/app/src/test/java/edu/berkeley/boinc/adapter/ProjectControlsListAdapterTest.kt
@@ -68,32 +68,24 @@ class ProjectControlsListAdapterTest {
 
     @Test
     fun `Check that entry count equal passed count`() {
-        Assert.assertEquals(3, projectControlsListAdapter.count)
+        Assert.assertEquals(3, projectControlsListAdapter.itemCount)
     }
 
     @Test
-    fun `Check content and order of entries`() {
+    fun `Check content of entries`() {
         Assert.assertEquals("Project 1",
-            projectControlsListAdapter.getItem(0).data.project?.projectName
+            projectControlsListAdapter.entries[0].data.project?.projectName
         )
-        Assert.assertEquals(0, projectControlsListAdapter.getItemId(0))
         Assert.assertEquals("Project 2",
-            projectControlsListAdapter.getItem(1).data.project?.projectName
+            projectControlsListAdapter.entries[1].data.project?.projectName
         )
-        Assert.assertEquals(1, projectControlsListAdapter.getItemId(1))
         Assert.assertEquals("Project 3",
-            projectControlsListAdapter.getItem(2).data.project?.projectName
+            projectControlsListAdapter.entries[2].data.project?.projectName
         )
-        Assert.assertEquals(2, projectControlsListAdapter.getItemId(2))
     }
 
     @Test(expected = IndexOutOfBoundsException::class)
-    fun `Check that getItem() fails with IndexOutOfBoundsException when incorrect position provided`() {
-        projectControlsListAdapter.run { getItem(42) }
-    }
-
-    @Test(expected = IndexOutOfBoundsException::class)
-    fun `Check that getItemId() fails with IndexOutOfBoundsException when incorrect position provided`() {
-        projectControlsListAdapter.run { getItemId(42) }
+    fun `Check that getItemCount() fails with IndexOutOfBoundsException when incorrect position provided`() {
+        projectControlsListAdapter.run { entries[42] }
     }
 }
