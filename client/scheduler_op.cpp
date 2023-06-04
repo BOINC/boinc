8e6517ece1aecf6f6af926481163f9bed8b130c3
davidpanderson <davea@ssl.berkeley.edu>
2023-06-04 15:47:47 -0700

    client: if sched request fails, show the scheduler URL (that might be the problem)

--- client/scheduler_op.cpp
+++ client/scheduler_op.cpp
@@ -69,7 +69,9 @@ bool SCHEDULER_OP::check_master_fetch_start() {
     retval = init_master_fetch(p);
     if (retval) {
         msg_printf(p, MSG_INTERNAL_ERROR,
-            "Couldn't start download of scheduler list: %s", boincerror(retval)
+            "Couldn't start download of scheduler list from %s: %s",
+            http_op.m_url,
+            boincerror(retval)
         );
         p->master_fetch_failures++;
         project_rpc_backoff(p, "scheduler list fetch failed\n");
@@ -456,13 +458,15 @@ bool SCHEDULER_OP::poll() {
                         cur_proj->nrpc_failures = 0;
                     }
                 }
-            } else {
+            }
+            else {
                 // master file fetch failed.
                 //
-                char buf[256];
-                snprintf(buf, sizeof(buf), "Scheduler list fetch failed: %s",
-                    boincerror(http_op.http_op_retval)
-                );
+                if (log_flags.sched_ops) {
+                    msg_printf(cur_proj, MSG_INFO, "Scheduler list fetch from %s failed: %s",
+                        http_op.m_url, boincerror(http_op.http_op_retval)
+                    );
+                }
                 cur_proj->master_fetch_failures++;
                 rpc_failed("Master file request failed");
             }
@@ -482,7 +486,7 @@ bool SCHEDULER_OP::poll() {
             if (http_op.http_op_retval) {
                 if (log_flags.sched_ops) {
                     msg_printf(cur_proj, MSG_INFO,
-                        "Scheduler request failed: %s", http_op.error_msg
+                        "Scheduler request to %s failed: %s", http_op.m_url, http_op.error_msg
                     );
                 }
 
