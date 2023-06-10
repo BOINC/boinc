6f7f63c836970405e59ed4669e709e3c667efaaf
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-06-10 22:52:09 +0200

    DEB repository workflow

---
+++ .github/workflows/debrepo/package_depends.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# See https://www.debian.org/doc/debian-policy/ch-relationships.html#s-binarydeps for the values
+# accepted by the "Depends" field
+
+# support functions
+function exit_usage() {
+	printf "Usage: deb_depends.sh <os-version> <package-name>\n"
+	exit 1
+}
+
+case "$1_$2" in
+# ubuntu distros
+"jammy_linux_client-vcpkg")
+    echo "libc6,libxss1 (>= 1.2.3)"
+    ;;
+"bionic_linux_client-vcpkg")
+    echo "libc6,libxss1 (>= 1.2.3)"
+    ;;
+
+# debian distros
+"bullseye_linux_client-vcpkg")
+    echo "libc6,libxss1 (>= 1.2.3)"
+    ;;
+"buster_linux_client-vcpkg")
+    echo "libc6,libxss1 (>= 1.2.3)"
+    ;;
+
+*)  echo "libc6"
+	;;
+
+esac
+
+exit 0
--- deploy/prepare_deployment.py
+++ deploy/prepare_deployment.py
@@ -21,7 +21,11 @@ import sys
 
 linux_client_list = [
     './client/boinc',
-    './client/boinccmd'
+    './client/boinccmd',
+    './client/scripts/boinc-client.service',
+    './client/scripts/boinc-client',
+    './client/scripts/boinc.bash',
+    './client/scripts/boinc-client.conf'
 ]
 
 linux_apps_list = [
@@ -44,6 +48,7 @@ linux_apps_list = [
 linux_manager_list = [
     './clientgui/boincmgr',
     './clientgui/skins',
+    './clientgui/res/boinc.desktop',
     'locale/*/*.mo',
 ]
 
