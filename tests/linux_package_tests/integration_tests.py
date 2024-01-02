beb30541778ce670c57042ecd2ec46d5d23425a6
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2024-01-02 16:05:38 +0100

    Added test to verify 'boinc' user and 'boinc' groups exist
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- tests/linux_package_tests/integration_tests.py
+++ tests/linux_package_tests/integration_tests.py
@@ -28,6 +28,15 @@ def get_version_from_string(string):
 def get_file_version(filename):
     return get_version_from_string(os.popen(("{app} --version").format(app=get_test_executable_file_path(filename))).read().strip())
 
+def get_user_exists(username):
+    return os.popen("id -u {username}".format(username=username)).read().strip() != ""
+
+def get_group_exists(groupname):
+    return os.popen("id -g {groupname}".format(groupname=groupname)).read().strip() != ""
+
+def get_user_in_group(username, groupname):
+    return os.popen("id -Gn {username}".format(username=username)).read().strip().find(groupname) != -1
+
 def test_files_exist():
     ts = testset.TestSet("Test files exist")
     ts.expect_equal("/usr/local/bin/boinc", get_test_executable_file_path("boinc"), "Test 'boinc' file location")
@@ -43,11 +52,19 @@ def test_version():
     ts.expect_equal(current_version, get_file_version("boinccmd"), "Test 'boinccmd' version is correctly set")
     return ts.result()
 
+def test_user():
+    ts = testset.TestSet("Test 'boinc' user and 'boinc' group exist")
+    ts.expect_true(get_user_exists("boinc"), "Test 'boinc' user exists")
+    ts.expect_true(get_group_exists("boinc"), "Test 'boinc' group exists")
+    ts.expect_true(get_user_in_group("boinc", "boinc"), "Test 'boinc' user is in 'boinc' group")
+    return ts.result()
+
 def main():
     result = True
 
     result &= test_files_exist()
     result &= test_version()
+    result &= test_user()
 
     return result
 
