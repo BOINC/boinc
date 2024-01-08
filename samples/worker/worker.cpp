d61f31c27b78453bfce0b0bf8916d8330efc1454
David Anderson <davea@ssl.berkeley.edu>
2024-01-08 14:24:07 -0800

    worker test app: fix small bug (--nsecs didn't work)

--- samples/worker/worker.cpp
+++ samples/worker/worker.cpp
@@ -64,6 +64,7 @@ int main(int argc, char** argv) {
     for (i=1; i<argc; i++) {
         if (!strcmp(argv[i], "--nsecs")) {
             nsecs = atoi(argv[++i]);
+            continue;
         }
         if (!in) {
             if (!strcmp(argv[i], "stdin")) {
