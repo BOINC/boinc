4117dd4cda3a9410c76585f7f814c52e3f150658
David Anderson <davea@ssl.berkeley.edu>
2024-01-07 23:02:01 -0800

    client: fix non-CPU-intensive logic
    Problem: If a project is non-CPU-intensive and has no jobs
    we ask it for work repeatedly, without backoff.
    A project is marked as non-CPU-intensive if
    it has no CPU-intensive apps.
    This means that a project with no apps
    (e.g. because it doesn't have any jobs)
    is marked as non-CPU-intensive and pinged without backoff.
    
    Solution: mark a project as non-CPU-intensive only if its
    scheduler reply says it is
    (i.e. there's <non_cpu_intensive/> in its config.xml).

--- client/scheduler_op.cpp
+++ client/scheduler_op.cpp
@@ -1,6 +1,6 @@
 // This file is part of BOINC.
-// http://boinc.berkeley.edu
-// Copyright (C) 2008 University of California
+// https://boinc.berkeley.edu
+// Copyright (C) 2024 University of California
 //
 // BOINC is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License
@@ -14,6 +14,10 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+// Functions for doing scheduler RPCs,
+// including creating requests and parsing replies.
+// The reply handler is in cs_scheduler.cpp
 
 #include "cpp.h"
 
@@ -883,6 +887,8 @@ int SCHEDULER_REPLY::parse(FILE* in, PROJECT* project) {
         } else if (xp.parse_bool("send_full_workload", send_full_workload)) {
             continue;
         } else if (xp.parse_bool("dont_use_dcf", dont_use_dcf)) {
+            continue;
+        } else if (xp.parse_bool("non_cpu_intensive", project->non_cpu_intensive)) {
             continue;
         } else if (xp.parse_int("send_time_stats_log", send_time_stats_log)){
             continue;
