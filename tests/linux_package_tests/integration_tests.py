e8d059779109cf6b5134b8b62f434f829b0f280d
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2024-02-07 02:44:38 +0100

    Refactor integration tests
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- tests/linux_package_tests/integration_tests.py
+++ tests/linux_package_tests/integration_tests.py
@@ -3,72 +3,73 @@ import re
 import sys
 import testset
 
-def get_test_executable_file_path(filename):
-    path = os.popen("echo $PATH").read().strip()
-    for p in path.split(":"):
-        if os.path.exists(os.path.join(p, filename)):
-            return os.path.join(p, filename)
-    return ""
+class IntegrationTests:
+    def __init__(self):
+        self.result = True
+        self.result &= self.test_files_exist()
+        self.result &= self.test_version()
+        self.result &= self.test_user()
 
-def get_current_version_number():
-    with open("version.h", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            res = re.search("#define BOINC_VERSION_STRING \"([\d]+.[\d]+.[\d]+)\"", line)
-            if res is not None:
-                return res[1]
-    return ""
+    def result(self):
+        return self.result
 
-def get_version_from_string(string):
-    res = re.search("([\d]+.[\d]+.[\d]+)", string)
-    if res is not None:
-        return res[1]
-    return ""
+    def _get_test_executable_file_path(self, filename):
+        path = os.popen("echo $PATH").read().strip()
+        for p in path.split(":"):
+            if os.path.exists(os.path.join(p, filename)):
+                return os.path.join(p, filename)
+        return ""
 
-def get_file_version(filename):
-    return get_version_from_string(os.popen(("{app} --version").format(app=get_test_executable_file_path(filename))).read().strip())
+    def _get_current_version_number(self):
+        with open("version.h", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                res = re.search("#define BOINC_VERSION_STRING \"([\d]+.[\d]+.[\d]+)\"", line)
+                if res is not None:
+                    return res[1]
+        return ""
 
-def get_user_exists(username):
-    return os.popen("id -un {username}".format(username=username)).read().strip() == username
+    def _get_version_from_string(self, string):
+        res = re.search("([\d]+.[\d]+.[\d]+)", string)
+        if res is not None:
+            return res[1]
+        return ""
 
-def get_group_exists(groupname):
-    return os.popen("getent group {groupname}".format(groupname=groupname)).read().strip() != ""
+    def _get_file_version(self, filename):
+        return self._get_version_from_string(os.popen(("{app} --version").format(app=self._get_test_executable_file_path(filename))).read().strip())
 
-def get_user_in_group(username, groupname):
-    return os.popen("id -Gn {username}".format(username=username)).read().strip().find(groupname) != -1
+    def _get_user_exists(self, username):
+        return os.popen("id -un {username}".format(username=username)).read().strip() == username
 
-def test_files_exist():
-    ts = testset.TestSet("Test files exist")
-    ts.expect_equal("/usr/local/bin/boinc", get_test_executable_file_path("boinc"), "Test 'boinc' file location")
-    ts.expect_equal("/usr/local/bin/boinccmd", get_test_executable_file_path("boinccmd"), "Test 'boinccmd' file location")
-    ts.expect_equal("/usr/local/bin/boincmgr", get_test_executable_file_path("boincmgr"), "Test 'boincmgr' file location")
-    return ts.result()
+    def _get_group_exists(self, groupname):
+        return os.popen("getent group {groupname}".format(groupname=groupname)).read().strip() != ""
 
-def test_version():
-    ts = testset.TestSet("Test version is correct")
-    current_version = get_current_version_number()
-    ts.expect_not_equal("", current_version, "Test current version could be read from the 'version.h' file")
-    ts.expect_equal(current_version, get_file_version("boinc"), "Test 'boinc' version is correctly set")
-    ts.expect_equal(current_version, get_file_version("boinccmd"), "Test 'boinccmd' version is correctly set")
-    return ts.result()
+    def _get_user_in_group(self, username, groupname):
+        return os.popen("id -Gn {username}".format(username=username)).read().strip().find(groupname) != -1
 
-def test_user():
-    ts = testset.TestSet("Test 'boinc' user and 'boinc' group exist")
-    ts.expect_true(get_user_exists("boinc"), "Test 'boinc' user exists")
-    ts.expect_true(get_group_exists("boinc"), "Test 'boinc' group exists")
-    ts.expect_true(get_user_in_group("boinc", "boinc"), "Test 'boinc' user is in 'boinc' group")
-    return ts.result()
+    def test_files_exist(self):
+        ts = testset.TestSet("Test files exist")
+        ts.expect_equal("/usr/local/bin/boinc", self._get_test_executable_file_path("boinc"), "Test 'boinc' file location")
+        ts.expect_equal("/usr/local/bin/boinccmd", self._get_test_executable_file_path("boinccmd"), "Test 'boinccmd' file location")
+        ts.expect_equal("/usr/local/bin/boincmgr", self._get_test_executable_file_path("boincmgr"), "Test 'boincmgr' file location")
+        return ts.result()
 
-def main():
-    result = True
+    def test_version(self):
+        ts = testset.TestSet("Test version is correct")
+        current_version = self._get_current_version_number()
+        ts.expect_not_equal("", current_version, "Test current version could be read from the 'version.h' file")
+        ts.expect_equal(current_version, self._get_file_version("boinc"), "Test 'boinc' version is correctly set")
+        ts.expect_equal(current_version, self._get_file_version("boinccmd"), "Test 'boinccmd' version is correctly set")
+        return ts.result()
 
-    result &= test_files_exist()
-    result &= test_version()
-    result &= test_user()
-
-    return result
+    def test_user(self):
+        ts = testset.TestSet("Test 'boinc' user and 'boinc' group exist")
+        ts.expect_true(self._get_user_exists("boinc"), "Test 'boinc' user exists")
+        ts.expect_true(self._get_group_exists("boinc"), "Test 'boinc' group exists")
+        ts.expect_true(self._get_user_in_group("boinc", "boinc"), "Test 'boinc' user is in 'boinc' group")
+        return ts.result()
 
 if __name__ == "__main__":
-    if not main():
+    if not IntegrationTests().result():
         sys.exit(1)
     sys.exit(0)
