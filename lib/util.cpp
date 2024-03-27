f2908927a6e652c98e3c4b2f443874584ab80f71
computezrmle <57127745+computezrmle@users.noreply.github.com>
2024-03-27 12:06:20 +0100

    Fixes applied

--- lib/util.cpp
+++ lib/util.cpp
@@ -549,11 +549,10 @@ unsigned int random_int() {
 #if defined(__CYGWIN32__)
     HMODULE hLib=LoadLibrary((const char *)"ADVAPI32.DLL");
 #else
-    HMODULE hLib=LoadLibrary("ADVAPI32.DLL");
+    HMODULE hLib=LoadLibraryA("ADVAPI32.DLL");
 #endif
     if (!hLib) {
         fprintf(stderr, "Error: can't load ADVAPI32.DLL\n");
-        exit(2);
     }
     BOOLEAN (APIENTRY *pfn)(void*, ULONG) =
         (BOOLEAN (APIENTRY *)(void*,ULONG))GetProcAddress(hLib,"SystemFunction036");
@@ -570,11 +569,9 @@ unsigned int random_int() {
     FILE* f = fopen("/dev/random", "r");
     if (!f) {
         fprintf(stderr, "Error: can't open /dev/random\n");
-        exit(2);
     }
     if (1 != fread(&n, sizeof(n), 1, f)) {
         fprintf(stderr, "Error: can't read from /dev/random\n");
-        exit(2);
     }
     fclose(f);
 #endif
