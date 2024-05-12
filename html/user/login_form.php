132c45a12aefd168fccb5889709f61e8c1e0d17a
David Anderson <davea@ssl.berkeley.edu>
2024-05-12 00:36:07 -0700

    make 'create account' into a button

--- html/user/login_form.php
+++ html/user/login_form.php
@@ -58,9 +58,9 @@ $config = get_config();
 if (!parse_bool($config, "disable_account_creation")
     && !parse_bool($config, "no_web_account_creation")
 ) {
-    echo tra("or %1 create an account %2.",
-        "<a href=\"create_account_form.php?next_url=$next_url\">",
-        "</a>"
+    show_button(
+        "create_account_form.php?next_url=$next_url",
+        tra("Create account")
     );
 }
 
