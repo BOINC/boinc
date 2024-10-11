47cf87ff860fc6219fce6c9eebe72f6923934fa7
lfield <Laurence.Field@cern.ch>
2024-10-11 10:51:18 +0200

    Improved input handling

--- html/user/edit_user_info_action.php
+++ html/user/edit_user_info_action.php
@@ -26,7 +26,7 @@ check_get_args(array("tnow", "ttok"));
 $user = get_logged_in_user();
 check_tokens($user->authenticator);
 
-$name = trim(post_str("user_name"));
+$name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_SPECIAL_CHARS);
 if ($name != sanitize_tags($name)) {
     error_page(tra("HTML tags are not allowed in your name."));
 }
