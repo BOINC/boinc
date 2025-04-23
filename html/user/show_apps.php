b8ae951779772aad5973d4d40f18d420cd17bff6
David Anderson <davea@ssl.berkeley.edu>
2025-04-23 13:52:31 -0700

    web (job submission) additions
    show_apps.php:
    - show researchers (job submitters); get the info from a JSON file.
    - show which apps need Docker

--- html/user/show_apps.php
+++ html/user/show_apps.php
@@ -4,6 +4,30 @@ require_once('../inc/util.inc');
 require_once('../inc/buda.inc');
 require_once('../inc/keywords.inc');
 require_once('../project/remote_apps.inc');
+require_once('../project/submitters.inc');
+
+function show_submitters() {
+    global $submitters;
+    if (!$submitters) return;
+    echo '<h2>Researchers</h2>';
+    foreach ($submitters as $s) {
+        if (empty($s->picture)) {
+            $s->picture = 'images/user.png';
+        }
+        echo sprintf(
+            '<img height=100px src=%s>
+            <p>Name: %s
+            <br>Research interests: %s
+            <br>Location: %s
+            <p>
+            ',
+            $s->picture,
+            $s->name,
+            $s->interests,
+            kw_array_to_str($s->loc_kw, '; ')
+        );
+    }
+}
 
 function show_buda_apps() {
     $apps = get_buda_apps();
@@ -12,8 +36,9 @@ function show_buda_apps() {
         table_row(
             $desc->long_name,
             $desc->description,
+            kw_array_to_str($desc->sci_kw),
             empty($desc->url)?'':"<a href=$desc->url>View</a>",
-            kw_array_to_str($desc->sci_kw)
+            'Yes'
         );
     }
 }
@@ -26,8 +51,9 @@ function main() {
     table_header(
         'Name',
         'Description',
+        'Science keywords',
         'Web page',
-        'Science keywords'
+        'Docker required?<br><small><a href=https://github.com/BOINC/boinc/wiki/Installing-Docker>Details</a>'
     );
     foreach ($remote_apps as $category => $apps) {
         if (strstr($category, 'Docker')) {
@@ -37,14 +63,16 @@ function main() {
                 table_row(
                     $app->long_name,
                     $app->description,
+                    kw_array_to_str($app->sci_kw),
                     "<a href=$app->url>View</a>",
-                    kw_array_to_str($app->sci_kw)
+                    ''
                 );
             }
             echo '</ul>';
         }
     }
     end_table();
+    show_submitters();
     page_tail();
 }
 
