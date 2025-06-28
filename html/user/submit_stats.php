db37703bd77a47fd2c0caf6638ed8102bd93c375
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-06-28 11:23:05 +0200

    remove trailing whitespace, fix year in copyright header
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- html/user/submit_stats.php
+++ html/user/submit_stats.php
@@ -1,7 +1,7 @@
 <?php
 // This file is part of BOINC.
 // https://boinc.berkeley.edu
-// Copyright (C) 2024 University of California
+// Copyright (C) 2025 University of California
 //
 // BOINC is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License
@@ -168,7 +168,7 @@ function graph($data, $xlabel, $ylabel) {
 
         var options = {
           title: 'Job runtime (hours)',
-          legend:  'none' 
+          legend:  'none'
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
