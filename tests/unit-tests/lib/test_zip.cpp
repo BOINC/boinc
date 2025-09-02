3694fb746e99356cf601a8505c3b7b86be18b2cf
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-09-02 13:59:53 +0200

    Add sample zip archive to test backward compatibility
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- tests/unit-tests/lib/test_zip.cpp
+++ tests/unit-tests/lib/test_zip.cpp
@@ -15,24 +15,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-#define DIRREF long
-
 #include <fstream>
 #include <filesystem>
-
-#include <zlib.h>  // test that we "co-exist" with the "stock" zlib library
 
 #include "gtest/gtest.h"
 #include "boinc_zip.h"
 #include "util.h"
-
-#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
-#  include <fcntl.h>
-#  include <io.h>
-#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
-#else
-#  define SET_BINARY_MODE(file)
-#endif
 
 using namespace std;
 
@@ -67,159 +55,6 @@ namespace test_zip {
         ofstream(subdir / "file5.dat") << "This is a dat file 5.";
         return test_dir;
     }
-
-    // *** ROUTINES TO COMPRESS AND UNCOMPRESS USING ZLIB ***
-
-    #define CHUNK 16384
-
-    /* Compress from file source to file dest until EOF on source.
-       def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
-       allocated for processing, Z_STREAM_ERROR if an invalid compression
-       level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
-       version of the library linked do not match, or Z_ERRNO if there is
-       an error reading or writing the files. */
-    int def(FILE *source, FILE *dest, int level)
-    {
-        int ret, flush;
-        unsigned have;
-        z_stream strm;
-        unsigned char in[CHUNK];
-        unsigned char out[CHUNK];
-
-        /* allocate deflate state */
-        strm.zalloc = Z_NULL;
-        strm.zfree = Z_NULL;
-        strm.opaque = Z_NULL;
-        ret = deflateInit(&strm, level);
-        if (ret != Z_OK)
-            return ret;
-
-        /* compress until end of file */
-        do {
-            strm.avail_in = fread(in, 1, CHUNK, source);
-            if (ferror(source)) {
-                (void)deflateEnd(&strm);
-                return Z_ERRNO;
-            }
-            flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
-            strm.next_in = in;
-
-            /* run deflate() on input until output buffer not full, finish
-               compression if all of source has been read in */
-            do {
-                strm.avail_out = CHUNK;
-                strm.next_out = out;
-                ret = deflate(&strm, flush);    /* no bad return value */
-                assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-                have = CHUNK - strm.avail_out;
-                if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                    (void)deflateEnd(&strm);
-                    return Z_ERRNO;
-                }
-            } while (strm.avail_out == 0);
-            assert(strm.avail_in == 0);     /* all input will be used */
-
-            /* done when last data in file processed */
-        } while (flush != Z_FINISH);
-        assert(ret == Z_STREAM_END);        /* stream will be complete */
-
-        /* clean up and return */
-        (void)deflateEnd(&strm);
-        return Z_OK;
-    }
-
-    /* Decompress from file source to file dest until stream ends or EOF.
-       inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be
-       allocated for processing, Z_DATA_ERROR if the deflate data is
-       invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
-       the version of the library linked do not match, or Z_ERRNO if there
-       is an error reading or writing the files. */
-    int inf(FILE *source, FILE *dest)
-    {
-        int ret;
-        unsigned have;
-        z_stream strm;
-        unsigned char in[CHUNK];
-        unsigned char out[CHUNK];
-
-        /* allocate inflate state */
-        strm.zalloc = Z_NULL;
-        strm.zfree = Z_NULL;
-        strm.opaque = Z_NULL;
-        strm.avail_in = 0;
-        strm.next_in = Z_NULL;
-        ret = inflateInit(&strm);
-        if (ret != Z_OK)
-            return ret;
-
-        /* decompress until deflate stream ends or end of file */
-        do {
-            strm.avail_in = fread(in, 1, CHUNK, source);
-            if (ferror(source)) {
-                (void)inflateEnd(&strm);
-                return Z_ERRNO;
-            }
-            if (strm.avail_in == 0)
-                break;
-            strm.next_in = in;
-
-            /* run inflate() on input until output buffer not full */
-            do {
-                strm.avail_out = CHUNK;
-                strm.next_out = out;
-                ret = inflate(&strm, Z_NO_FLUSH);
-                assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-                switch (ret) {
-                case Z_NEED_DICT:
-                    ret = Z_DATA_ERROR;     /* and fall through */
-                    [[fallthrough]];
-                case Z_DATA_ERROR:
-                case Z_MEM_ERROR:
-                    (void)inflateEnd(&strm);
-                    return ret;
-                }
-                have = CHUNK - strm.avail_out;
-                if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                    (void)inflateEnd(&strm);
-                    return Z_ERRNO;
-                }
-            } while (strm.avail_out == 0);
-
-            /* done when inflate() says it's done */
-        } while (ret != Z_STREAM_END);
-
-        /* clean up and return */
-        (void)inflateEnd(&strm);
-        return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-    }
-
-    /* report a zlib or i/o error */
-    void zerr(int ret)
-    {
-        fputs("zpipe: ", stderr);
-        switch (ret) {
-        case Z_ERRNO:
-            if (ferror(stdin))
-                fputs("error reading stdin\n", stderr);
-            if (ferror(stdout))
-                fputs("error writing stdout\n", stderr);
-            break;
-        case Z_STREAM_ERROR:
-            fputs("invalid compression level\n", stderr);
-            break;
-        case Z_DATA_ERROR:
-            fputs("invalid or incomplete deflate data\n", stderr);
-            break;
-        case Z_MEM_ERROR:
-            fputs("out of memory\n", stderr);
-            break;
-        case Z_VERSION_ERROR:
-            fputs("zlib version mismatch!\n", stderr);
-        }
-    }
-
-    // *** END OF ROUTINES TO COMPRESS AND UNCOMPRESS USING ZLIB ***
-
 
 #ifndef __APPLE__
     TEST_F(test_zip, Test_boinc_filelist_sorting) {
@@ -265,7 +100,7 @@ namespace test_zip {
         EXPECT_EQ(file_list[0], filesystem::absolute(test_dir / "file3.txt").string());
         EXPECT_EQ(file_list[1], filesystem::absolute(test_dir / "file2.txt").string());
         EXPECT_EQ(file_list[2], filesystem::absolute(test_dir / "file1.txt").string());
- 
+
         filesystem::remove_all(test_dir);
     }
 
@@ -607,6 +442,47 @@ namespace test_zip {
         filesystem::remove_all(test_dir);
         filesystem::remove(zip_path);
     }
+
+#ifndef _WIN32
+    TEST_F(test_zip, Test_boinc_unzip_sample_zip_and_check_permissions) {
+        // This test assumes the existence of a sample zip file "sample.zip"
+        // with known contents and permissions. Adjust the path as necessary.
+        filesystem::path sample_zip = filesystem::current_path().parent_path() / "unit-tests"/ "testdata" / "test_unix_permissions.zip";
+        ASSERT_TRUE(filesystem::exists(sample_zip)) << "Sample zip file does not exist.";
+
+        filesystem::path unzip_dir = "sample_unzipped";
+        filesystem::create_directory(unzip_dir);
+        int zip_result = boinc_zip(UNZIP_IT, sample_zip.string(), unzip_dir.string());
+        EXPECT_EQ(zip_result, 0);
+
+        // Check for expected files and their permissions
+        filesystem::path expected_file = unzip_dir / "or.txt";
+        EXPECT_TRUE(filesystem::exists(expected_file));
+        EXPECT_EQ(filesystem::status(expected_file).permissions(), filesystem::perms::owner_read);
+        // verify content
+        ifstream original(expected_file);
+        string original_content((istreambuf_iterator<char>(original)), istreambuf_iterator<char>());
+        EXPECT_EQ(original_content, "owner read\n");
+
+        expected_file = unzip_dir / "orw.txt";
+        EXPECT_TRUE(filesystem::exists(expected_file));
+        EXPECT_EQ(filesystem::status(expected_file).permissions(), filesystem::perms::owner_read | filesystem::perms::owner_write);
+        // verify content
+        original = ifstream(expected_file);
+        original_content = string((istreambuf_iterator<char>(original)), istreambuf_iterator<char>());
+        EXPECT_EQ(original_content, "owner read & owner write\n");
+
+        expected_file = unzip_dir / "orwx.txt";
+        EXPECT_TRUE(filesystem::exists(expected_file));
+        EXPECT_EQ(filesystem::status(expected_file).permissions(), filesystem::perms::owner_read | filesystem::perms::owner_write | filesystem::perms::owner_exec);
+        // verify content
+        original = ifstream(expected_file);
+        original_content = string((istreambuf_iterator<char>(original)), istreambuf_iterator<char>());
+        EXPECT_EQ(original_content, "owner read & owner write & owner execute\n");
+
+        filesystem::remove_all(unzip_dir);
+    }
+#endif
 
 #endif
 }
