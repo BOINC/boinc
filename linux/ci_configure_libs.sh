797fd050391bf5e9e7c991cc512cf7e14dc70996
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-09-05 01:56:56 +0200

    Try to fix linux vcpkg build
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- linux/ci_configure_libs.sh
+++ linux/ci_configure_libs.sh
@@ -13,4 +13,5 @@ export VCPKG_DIR="$VCPKG_ROOT/installed/x64-linux"
 linux/update_vcpkg_libs.sh
 
 export _libcurl_pc="$VCPKG_DIR/lib/pkgconfig/libcurl.pc"
+export PKG_CONFIG_PATH=$VCPKG_DIR/lib/pkgconfig/
 ./configure --with-libcurl=$VCPKG_DIR --with-ssl=$VCPKG_DIR --disable-server --disable-client --disable-manager
