b88fda42983218e4c92dd443394d7c44283b4ebe
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-09-06 01:15:49 +0200

    Build linux for arm64 in a simpler way
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- mingw/ci_configure_apps.sh
+++ mingw/ci_configure_apps.sh
@@ -16,5 +16,6 @@ mingw/update_vcpkg.sh
 export CXXFLAGS="-I$VCPKG_DIR/include -L$VCPKG_DIR/lib"
 export CFLAGS="$CXXFLAGS"
 
+export PKG_CONFIG_PATH=$VCPKG_DIR/lib/pkgconfig/
 export _libcurl_pc="$VCPKG_DIR/lib/pkgconfig/libcurl.pc"
-./configure --host=x86_64-w64-mingw32 --with-libcurl=$VCPKG_DIR --with-ssl=$VCPKG_DIR --with-libzip=$VCPKG_DIR --enable-apps --enable-apps-mingw --enable-apps-vcpkg --enable-apps-gui --disable-server --disable-client --disable-manager
+./configure --host=x86_64-w64-mingw32 --with-libcurl=$VCPKG_DIR --with-ssl=$VCPKG_DIR --enable-apps --enable-apps-mingw --enable-apps-vcpkg --enable-apps-gui --disable-server --disable-client --disable-manager
