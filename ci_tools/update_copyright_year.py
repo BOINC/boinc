7930ced1998bd50dad4ef5243515d8d7e742dd7a
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-10-02 21:42:28 +0200

    [ci] add missed shebangs to the python scripts, make them executable
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- ci_tools/update_copyright_year.py
+++ ci_tools/update_copyright_year.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 # This file is part of BOINC.
-# http://boinc.berkeley.edu
-# Copyright (C) 2023 University of California
+# https://boinc.berkeley.edu
+# Copyright (C) 2025 University of California
 #
 # BOINC is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License
