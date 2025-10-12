891d10e3a96f07a21f552f5777b15e7da19093cd
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-10-12 18:19:34 +0200

    [ci] build snap package for arm64
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- 3rdParty/vcpkg_ports/triplets/ci/snap-linux-arm64.cmake
+++ 3rdParty/vcpkg_ports/triplets/ci/snap-linux-arm64.cmake
@@ -16,6 +16,19 @@
 # along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
 include(${CMAKE_CURRENT_LIST_DIR}/../../vcpkg_root_find.cmake)
-include(${VCPKG_ROOT}/triplets/community/x64-linux-release.cmake)
+include(${VCPKG_ROOT}/triplets/community/arm64-linux-release.cmake)
 
 set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON)
+
+if(PORT STREQUAL "dbus")
+    if(NOT $ENV{TMPDIR} STREQUAL "")
+        set(DBUS_SESSION_SOCKET_DIR $ENV{TMPDIR})
+    elseif(NOT $ENV{TEMP} STREQUAL "")
+        set(DBUS_SESSION_SOCKET_DIR $ENV{TEMP})
+    elseif(NOT $ENV{TMP} STREQUAL "")
+        set(DBUS_SESSION_SOCKET_DIR $ENV{TMP})
+    else()
+        set(DBUS_SESSION_SOCKET_DIR /tmp)
+    endif()
+    LIST(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DDBUS_SESSION_SOCKET_DIR=${DBUS_SESSION_SOCKET_DIR}")
+endif()
