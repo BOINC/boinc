BOINC/boinc

commit 8cf9badade747712f872b2defd42e16c57d3dbbf

Mac: Fix bugs in Mac installer

clientgui/BOINCGUIApp.cpp
@@ -52,6 +52,11 @@
 #include "sg_BoincSimpleFrame.h"
 #include "DlgGenericMessage.h"
 
+#ifdef __WXMAC__
+#define CREATE_LOG 0    /* for debugging */
+
+void print_to_log_file(const char *format, ...);
+#endif
 
 bool s_bSkipExitConfirmation = false;
 
@@ -102,7 +107,6 @@ bool CBOINCGUIApp::OnInit() {
 #else
     g_use_sandbox = false;
 #endif
-
     m_isDarkMode = false;
 #if SUPPORTDARKMODE
     wxSystemAppearance appearance = wxSystemSettings::GetAppearance();
@@ -318,7 +322,6 @@ bool CBOINCGUIApp::OnInit() {
         BOINC_DIAG_TRACETOSTDOUT;
 
     diagnostics_init(dwDiagnosticsFlags, "stdoutgui", "stderrgui");
-
     // Enable Logging and Trace Masks
     m_pLog = new wxLogBOINC();
     wxLog::SetActiveTarget(m_pLog);
@@ -408,7 +411,6 @@ bool CBOINCGUIApp::OnInit() {
             g_use_sandbox, true, path_to_error, sizeof(path_to_error)
         );
     }
-
     if (iErrorCode) {
 
 #if (defined(__WXMAC__) && (!defined (_DEBUG)))
@@ -459,6 +461,7 @@ bool CBOINCGUIApp::OnInit() {
                                     );
 
         pDlg->ShowModal();
+printf("Returned from alert\n");
         if (pDlg)
             pDlg->Destroy();
 
@@ -504,6 +507,7 @@ bool CBOINCGUIApp::OnInit() {
     // foreground and then exit.
     if (DetectDuplicateInstance()) {
 #ifdef __WXMAC__
+SetActivationPolicyAccessory(true);
         // Hack to work around an issue with the Mac Installer
         if (m_bBOINCMGRAutoStarted) return false;
 #endif
@@ -522,7 +526,16 @@ bool CBOINCGUIApp::OnInit() {
         }
         return false;
     }
-
+#ifdef __WXMAC__
+    // Each time a second instance of BOINC Managr is launched, it normally
+    // nadds an additional BOINC icon to the "recently run apps" section
+    // of the Dock, cluttering it up. To avoid this, we set the LSUIElement
+    // key in its info.plist, which prevents the app from appearing in the
+    // Dock. But if this is not a duplicate instance, we call this routine
+    // to tell the system to show the icon in the Dock.
+    // https://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
+    SetActivationPolicyAccessory(false);    // Show our Dock tile
+#endif
     // Initialize the main document
     m_pDocument = new CMainDocument();
     wxASSERT(m_pDocument);
@@ -1128,24 +1141,36 @@ void CBOINCGUIApp::OnActivateApp(wxActivateEvent& event) {
 #endif
     {
 #ifdef __WXMAC__
-        ShowInterface();
-#elif defined(__WXGTK__)
-        // Linux allows the Event Log to be brought forward and made active
-        // even if we have a modal dialog displayed (associated with our
-        // main frame.) This test is needed to allow bringing the modal
-        // dialog forward again by clicking on its title bar.
-        if (!IsModalDialogDisplayed())
-        {
-            bool keepEventLogInFront = m_bEventLogWasActive;
+        // When our LaunchAgent / login item launches us at login, it activates
+        // this app, but we want it hidden. So we immediatly hide it upon the
+        // first activation when run as a login item.
+        static bool first = true;
 
-            if (m_pEventLog && !m_pEventLog->IsIconized() && !keepEventLogInFront) {
-                m_pEventLog->Raise();
-            }
-            if (m_pFrame) {
-                m_pFrame->Raise();
-            }
-            if (m_pEventLog && !m_pEventLog->IsIconized() && keepEventLogInFront) {
-                m_pEventLog->Raise();
+        CMainDocument*      pDoc = wxGetApp().GetDocument();
+        if (m_bBOINCMGRAutoStarted && (!pDoc->IsConnected()) && first) {
+            first = false;
+            ShowApplication(false);
+        } else
+#endif
+#if (defined (__WXMAC__) || defined(__WXGTK__))
+        {
+           // Linux allows the Event Log to be brought forward and made active
+            // even if we have a modal dialog displayed (associated with our
+            // main frame.) This test is needed to allow bringing the modal
+            // dialog forward again by clicking on its title bar.
+            if (!IsModalDialogDisplayed())
+            {
+                bool keepEventLogInFront = m_bEventLogWasActive;
+
+                if (m_pEventLog && !m_pEventLog->IsIconized() && !keepEventLogInFront) {
+                    m_pEventLog->Raise();
+                }
+                if (m_pFrame) {
+                    m_pFrame->Raise();
+                }
+                if (m_pEventLog && !m_pEventLog->IsIconized() && keepEventLogInFront) {
+                    m_pEventLog->Raise();
+                }
             }
         }
 #endif
@@ -1553,6 +1578,7 @@ bool CBOINCGUIApp::IsModalDialogDisplayed() {
     return false;
 }
 
+
 // Prevent recursive entry of CMainDocument::RequestRPC()
 int CBOINCGUIApp::FilterEvent(wxEvent &event) {
     int theEventType;
@@ -1606,3 +1632,52 @@ int CBOINCGUIApp::FilterEvent(wxEvent &event) {
     return -1;
 }
 
+
+#ifdef __WXMAC__
+
+#if CREATE_LOG
+void strip_cr(char *buf)
+{
+    char *theCR;
+
+    theCR = strrchr(buf, '\n');
+    if (theCR)
+        *theCR = '\0';
+    theCR = strrchr(buf, '\r');
+    if (theCR)
+        *theCR = '\0';
+}
+#endif    // CREATE_LOG
+
+void print_to_log_file(const char *format, ...) {
+#if CREATE_LOG
+    va_list args;
+    char buf[256];
+    time_t t;
+    safe_strcpy(buf, getenv("HOME"));
+    safe_strcat(buf, "/Documents/test_log_manager.txt");
+    FILE *f;
+    f = fopen(buf, "a");
+    if (!f) return;
+
+    // File may be owned by various users, so make it world readable & writable
+    chmod(buf, 0666);
+
+    time(&t);
+    strlcpy(buf, asctime(localtime(&t)), sizeof(buf));
+
+    strip_cr(buf);
+
+    fputs(buf, f);
+    fputs("   ", f);
+
+    va_start(args, format);
+    vfprintf(f, format, args);
+    va_end(args);
+
+    fputs("\n", f);
+    fflush(f);
+    fclose(f);
+#endif
+}
+#endif

clientgui/mac/BOINCGUIApp.mm
@@ -22,10 +22,6 @@
 #include "BOINCBaseFrame.h"
 #import <Cocoa/Cocoa.h>
 
-#if !wxCHECK_VERSION(3,0,1)
-// This should be fixed after wxCocoa 3.0.0:
-// http://trac.wxwidgets.org/ticket/16156
-
 #ifndef NSEventTypeApplicationDefined
 #define NSEventTypeApplicationDefined NSApplicationDefined
 #endif
@@ -45,12 +41,11 @@ bool CBOINCGUIApp::CallOnInit() {
                                          subtype:0 data1:0 data2:0];
         [NSApp postEvent:event atStart:FALSE];
 
-    bool retVal = wxApp::CallOnInit();
+   bool retVal = wxApp::CallOnInit();
 
     [mypool release];
     return retVal;
 }
-#endif
 
 
 // Our application can get into a strange state
@@ -189,6 +184,21 @@ Boolean IsWindowOnScreen(int iLeft, int iTop, int iWidth, int iHeight) {
     }
 
     return false;
+}
+
+// Each time a second instance of BOINC Managr is launched, it normally
+// nadds an additional BOINC icon to the "recently run apps" section
+// of the Dock, cluttering it up. To avoid this, we set the LSUIElement
+// key in its info.plist, which prevents the app from appearing in the
+// Dock. But if this is not a duplicate instance, we call this routine
+// to tell the system to show the icon in the Dock.
+// https://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
+void CBOINCGUIApp::SetActivationPolicyAccessory(bool hideDock) {
+    if (hideDock) {
+        [NSApp setActivationPolicy: NSApplicationActivationPolicyAccessory];
+    } else {
+        [NSApp setActivationPolicy: NSApplicationActivationPolicyRegular];
+    }
 }
 
 extern bool s_bSkipExitConfirmation;

commit d773b31566b09b8a1c70d374db75a5ef45430b38

Mac: Fix bugs introduced by commit 8cf9badade in PR #6131

clientgui/BOINCGUIApp.cpp
@@ -509,16 +509,6 @@ bool CBOINCGUIApp::OnInit() {
     // Detect if BOINC Manager is already running, if so, bring it into the
     // foreground and then exit.
     if (DetectDuplicateInstance()) {
-#ifdef __WXMAC__
-        SetActivationPolicyAccessory(true);
-        // Hack to work around an issue with the Mac Installer
-        if (m_bBOINCMGRAutoStarted) return false;
-        // TODO: Should we also ignore duplicate instances that occur within
-        // TODO: a short time after login (detemined by getlastlogxbyname(),
-        // TODO: in case MacOS's "Reopen windows when logging in" functionality
-        // TODO: relaunched us after our LaunchAgent autostarted us?
-
-#endif
         if (GetBOINCMGRDisplayAnotherInstanceRunningMessage()) {
             wxString appName = m_pSkinManager->GetAdvanced()->GetApplicationName();
             wxString message;
@@ -528,21 +518,37 @@ bool CBOINCGUIApp::OnInit() {
             params.message = message;
             params.button2 = CDlgGenericMessageButton(false);
             CDlgGenericMessage dlg(NULL, &params);
+            ShowApplication(true);
             dlg.ShowModal();
             SetBOINCMGRDisplayAnotherInstanceRunningMessage(!dlg.GetDisableMessageValue());
         }
+#ifdef __WXMAC__
+        // A second instance of BOINC Manager s osmetimes launched at login
+        // when the Manager is launched by the login startup item and also
+        // by the "restore open windows" or "restore open applications"
+        // feature of MacOS.
+        // Each time a second instance of BOINC Manager is launched, it
+        // normally adds an additional BOINC icon to the "recently run apps"
+        // section of the Dock, cluttering it up. To avoid this, we call
+        // this routine on the second instance to tell the system to hide
+        // the icon in the Dock. We do this after displaying the dialog to
+        // allow the dialog to be brought to the foreground.
+        // NOTE: This tehnique works only if BOINCManager.app is NOT in the
+        // /Applications directory or any of its subdirectories. Putting it
+        // in "/Library/Application Support" with a soft link to it from the
+        // /Applications directory does work.
+        // https://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
+        SetActivationPolicyAccessory(true);    // Hide our Dock tile
+        // Hack to work around an issue with the Mac Installer
+        if (m_bBOINCMGRAutoStarted) return false;
+        // TODO: Should we also ignore duplicate instances that occur within
+        // TODO: a short time after login (detemined by getlastlogxbyname(),
+        // TODO: in case MacOS's "Reopen windows when logging in" functionality
+        // TODO: relaunched us AFTER our LaunchAgent autostarted us?
+#endif
         return false;
     }
-#ifdef __WXMAC__
-    // Each time a second instance of BOINC Managr is launched, it normally
-    // nadds an additional BOINC icon to the "recently run apps" section
-    // of the Dock, cluttering it up. To avoid this, we set the LSUIElement
-    // key in its info.plist, which prevents the app from appearing in the
-    // Dock. But if this is not a duplicate instance, we call this routine
-    // to tell the system to show the icon in the Dock.
-    // https://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
-    SetActivationPolicyAccessory(false);    // Show our Dock tile
-#endif
+
     // Initialize the main document
     m_pDocument = new CMainDocument();
     wxASSERT(m_pDocument);

clientgui/mac/BOINCGUIApp.mm
@@ -125,7 +125,7 @@ bool CBOINCGUIApp::IsApplicationVisible() {
 ///
 void CBOINCGUIApp::ShowApplication(bool bShow) {
     if (bShow) {
-        [ NSApp activateIgnoringOtherApps:YES ];
+        [ NSApp activate ];
     } else {
         [ NSApp hide:NSApp ];
     }

clientgui/mac/templates/Info.plist
@@ -20,7 +20,7 @@
 	<string>%VERSION%</string>
 	<key>CFBundleShortVersionString</key>
 	<string>%VERSION%</string>
-	<key>LSUIElement</key>
-	<true/>
+	<key>NSPrincipalClass</key>
+	<string>NSApplication</string>
 </dict>
 </plist>

mac_installer/PostInstall.cpp
@@ -203,6 +203,7 @@ int main(int argc, char *argv[])
     OSStatus                err;
     FILE                    *f;
     char                    s[2048];
+    char                    realPath[MAXPATHLEN];
     struct stat             sbuf;
 
 #ifndef SANDBOX
@@ -315,7 +316,21 @@ int main(int argc, char *argv[])
         // "\pSorry, this version of GridRepublic requires system 10.6 or higher."
         ShowMessage(false, (char *)_("Sorry, this version of %s requires system %s or higher."), brandName[brandID], Deployment_target);
 
+        // We install the BOINC Manager in "/Library/Application Support" with a
+        // soft link to it from the /Applications directory. For an explanation
+        // why we do it this way see the comment in CBOINCGUIApp::OnInit()
+        // under "if (DetectDuplicateInstance())"
+        //
         // "rm -rf \"/Applications/GridRepublic Desktop.app\""
+        if (stat(appPath[brandID], &sbuf) != 0) {
+            if (S_ISLNK(sbuf.st_mode)) {
+                realpath(appPath[brandID], realPath);   // Get path to app from symbolic link
+                sprintf(s, "rm -rf \"%s\"", realPath);
+                err = callPosixSpawn (s);
+                REPORT_ERROR(err);
+            }
+        }
+        // Delete either the symbolic link or the actual app if not a symbolic link
         sprintf(s, "rm -rf \"%s\"", appPath[brandID]);
         err = callPosixSpawn (s);
         REPORT_ERROR(err);
@@ -343,6 +358,11 @@ int main(int argc, char *argv[])
     }
 
     sleep (2);
+
+    // Create a symbolic link in /Applications/ directory
+    sprintf(s, "ln -s \"/Library/Application Support/%s.app\" \"%s\"", appName[brandID], appPath[brandID]);
+    err = callPosixSpawn (s);
+    REPORT_ERROR(err);
 
     // Install all_projects_list.xml file, but only if one doesn't
     // already exist, since a pre-existing one is probably newer.
@@ -478,7 +498,21 @@ int main(int argc, char *argv[])
     for (i=0; i< NUMBRANDS; i++) {
         if (i == brandID) continue;
 
+        // We install the BOINC Manager in "/Library/Application Support" with a
+        // soft link to it from the /Applications directory. For an explanation
+        // why we do it this way see the comment in CBOINCGUIApp::OnInit()
+        // under "if (DetectDuplicateInstance())"
+        //
         // "rm -rf \"/Applications/GridRepublic Desktop.app\""
+        if (stat(appPath[brandID], &sbuf) != 0) {
+            if (S_ISLNK(sbuf.st_mode)) {
+                realpath(appPath[i], realPath);   // Get path to app from symbolic link
+                sprintf(s, "rm -rf \"%s\"", realPath);
+                err = callPosixSpawn (s);
+                REPORT_ERROR(err);
+            }
+        }
+        // Delete either the symbolic link or the actual app if not a symbolic link
         sprintf(s, "rm -rf \"%s\"", appPath[i]);
         err = callPosixSpawn (s);
         REPORT_ERROR(err);

mac_installer/complist.plist
@@ -12,7 +12,7 @@
 		<key>BundleOverwriteAction</key>
 		<string>upgrade</string>
 		<key>RootRelativeBundlePath</key>
-		<string>Applications/BOINCManager.app</string>
+		<string>/Library/Application Support/BOINCManager.app</string>
 	</dict>
 	<dict>
 		<key>BundleIsVersionChecked</key>

mac_installer/preinstall
@@ -12,6 +12,13 @@
 ##sudo rm -fR /Library/Application\ Support/BOINC\ Data/skins/World\ Community\ Grid/
 
 # Remove any old BOINC Manager and BOINC Screen Saver bundles to guarantee a clean install
+if [[-L "/Applications/BOINCManager.app"]]; then
+    pathToRealManager=$(readlink -f "/Applications/BOINCManager.app")
+    rm -fR "${pathToRealManager}"
+fi
+rm -fR "/Library/Application Support/BOINCManager.app"  # Usually redundant
+
+ ## If this is a symbolic link, deletes it. Otherise deletes the actual app
 rm -fR "/Applications/BOINCManager.app"
 rm -fR "/Library/Screen Savers/BOINCSaver.saver"
 

mac_installer/release_boinc.sh
@@ -66,6 +66,7 @@
 ## Updated 7/29/25 to add "BOINC podman" directory
 ## Updated 7/31/25 to add "Run_Podman" utility
 ## Updated 8/7/25 to add "gfx_ss_bridge" utility
+## Updated 10/23/25 to put BOINCManager.app in "/Library/Application Support/"
 ##
 ## NOTE: This script requires Mac OS 10.7 or later, and uses XCode developer
 ##   tools.  So you must have installed XCode Developer Tools on the Mac
@@ -291,11 +292,11 @@ cp -fpRL "${BUILDPATH}/detect_rosetta_cpu" ../BOINC_Installer/Pkg_Root/Library/A
 cp -fpRL "${BUILDPATH}/Fix_BOINC_Users" ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/
 cp -fpRL "${BUILDPATH}/Run_Podman" ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/
 
-cp -fpRL "${BUILDPATH}/BOINCManager.app" ../BOINC_Installer/Pkg_Root/Applications/
+cp -fpRL "${BUILDPATH}/BOINCManager.app" ../BOINC_Installer/Pkg_Root/Library/Application\ Support/
 
 cp -fpRL "${BUILDPATH}/BOINCSaver.saver" ../BOINC_Installer/Pkg_Root/Library/Screen\ Savers/
 
-echo "BrandId=0" > "../BOINC_Installer/Pkg_Root/Applications/BOINCManager.app/Contents/Resources/Branding"
+echo "BrandId=0" > "../BOINC_Installer/Pkg_Root/Library/Application Support/BOINCManager.app/Contents/Resources/Branding"
 echo "BrandId=0" > ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/Branding
 
 ## Copy the localization files into the installer tree
@@ -452,10 +453,10 @@ if [ -e "${HOME}/BOINCCodeSignIdentities.txt" ]; then
     sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Screen Savers/BOINCSaver.saver"
 
     # Code Sign the BOINC client embedded in the Manager if we have a signing identity
-    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Applications/BOINCManager.app/Contents/Resources/boinc"
+    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Application Support/BOINCManager.app/Contents/Resources/boinc"
 
     # Code Sign the BOINC Manager if we have a signing identity
-    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Applications/BOINCManager.app"
+    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Application Support/BOINCManager.app"
 
     # Code Sign the PostInstall app embedded in the BOINC installer app if we have a signing identity
     sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/New_Release_$1_$2_$3/boinc_$1.$2.$3_macOSX_$arch/BOINC Installer.app/Contents/Resources/PostInstall.app"

mac_installer/release_brand.sh
@@ -295,15 +295,15 @@ cp -fpRL "${BUILDPATH}/detect_rosetta_cpu" ../BOINC_Installer/Pkg_Root/Library/A
 cp -fpRL "${BUILDPATH}/Fix_BOINC_Users" ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/
 cp -fpRL "${BUILDPATH}/Run_Podman" ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/
 
-cp -fpRL "${BUILDPATH}/BOINCManager.app/." "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/"
-sed -i "" s/BOINCManager/"${MANAGERAPPNAME}"/g "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Info.plist"
-sed -i "" s/BOINCMgr/"${MANAGERICON}"/g "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Info.plist"
-mv "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/MacOS/BOINCManager" "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/MacOS/${MANAGERAPPNAME}"
-cp -fpRL clientgui/res/${MANAGERICON}.icns "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Resources/"
-rm -rf "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Resources/BOINCMgr.icns"
-sed -i "" s/"BOINC Manager"/"${MANAGERAPPNAME}"/g "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Resources/English.lproj/InfoPlist.strings"
+cp -fpRL "${BUILDPATH}/BOINCManager.app/." "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/"
+sed -i "" s/BOINCManager/"${MANAGERAPPNAME}"/g "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Info.plist"
+sed -i "" s/BOINCMgr/"${MANAGERICON}"/g "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Info.plist"
+mv "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/MacOS/BOINCManager" "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/MacOS/${MANAGERAPPNAME}"
+cp -fpRL clientgui/res/${MANAGERICON}.icns "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Resources/"
+rm -rf "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Resources/BOINCMgr.icns"
+sed -i "" s/"BOINC Manager"/"${MANAGERAPPNAME}"/g "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Resources/English.lproj/InfoPlist.strings"
 
-echo ${BRANDING_INFO} > "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Resources/Branding"
+echo ${BRANDING_INFO} > "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Resources/Branding"
 echo ${BRANDING_INFO} > ../BOINC_Installer/Pkg_Root/Library/Application\ Support/BOINC\ Data/Branding
 
 cp -fpRL "${BUILDPATH}/BOINCSaver.saver/." "../BOINC_Installer/Pkg_Root/Library/Screen Savers/${SSAVERAPPNAME}.saver/"
@@ -499,10 +499,10 @@ if [ -e "${HOME}/BOINCCodeSignIdentities.txt" ]; then
     sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Screen Savers/${SSAVERAPPNAME}.saver/"
 
     # Code Sign the BOINC client embedded in the Manager if we have a signing identity
-    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app/Contents/Resources/boinc"
+    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app/Contents/Resources/boinc"
 
     # Code Sign the BOINC Manager if we have a signing identity
-    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Applications/${MANAGERAPPNAME}.app"
+    sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/Pkg_Root/Library/Application Support/${MANAGERAPPNAME}.app"
 
     # Code Sign the PostInstall app embedded in the BOINC installer app if we have a signing identity
     sudo codesign -f -o runtime -s "${APPSIGNINGIDENTITY}" "../BOINC_Installer/New_Release_${SHORTBRANDNAME}_$1_$2_$3/${SHORTBRANDNAME}_$1.$2.$3_macOSX_$arch/${INSTALLERAPPNAME}.app/Contents/Resources/PostInstall.app"

mac_installer/uninstall.cpp
@@ -59,7 +59,7 @@ using std::string;
 
 static OSStatus DoUninstall(void);
 static OSStatus CleanupAllVisibleUsers(void);
-static OSStatus DeleteOurBundlesFromDirectory(CFStringRef bundleID, char *extension, char *dirPath);
+static OSStatus DeleteOurBundlesFromDirectory(CFStringRef bundleID, char *extension, char *dirPath, Boolean deleteSymLink);
 static void DeleteLoginItemOSAScript(char *userName);
 static Boolean DeleteLoginItemLaunchAgent(long brandID, passwd *pw);
 static void DeleteScreenSaverLaunchAgent(passwd *pw);
@@ -507,10 +507,16 @@ fprintf(stdout, "Starting privileged tool\n");
 #endif  // SEARCHFORALLBOINCMANAGERS
 
     // Phase 2: step through default Applications directory searching for our applications
-    err = DeleteOurBundlesFromDirectory(CFSTR("edu.berkeley.boinc"), "app", "/Applications");
+    // We installed the BOINC Manager in "/Library/Application Support" with a
+    // soft link to it from the /Applications directory. For an explanation why
+    // we do it this way see the comment in CBOINCGUIApp::OnInit() under
+    // "if (DetectDuplicateInstance())"
+    err = DeleteOurBundlesFromDirectory(CFSTR("edu.berkeley.boinc"), "app", "/Library/Application Support", true);
+    // Older installations put BOINC Manager in /Applications
+    err = DeleteOurBundlesFromDirectory(CFSTR("edu.berkeley.boinc"), "app", "/Applications", false);
 
     // Phase 3: step through default Screen Savers directory searching for our screen savers
-    err = DeleteOurBundlesFromDirectory(CFSTR("edu.berkeley.boincsaver"), "saver", "/Library/Screen Savers");
+    err = DeleteOurBundlesFromDirectory(CFSTR("edu.berkeley.boincsaver"), "saver", "/Library/Screen Savers", false);
 
     // Phase 4: Delete our files and directories at our installer's default locations
     // Remove everything we may have installed, though the above 2 calls already deleted some
@@ -559,7 +565,7 @@ fprintf(stdout, "Starting privileged tool\n");
 }
 
 
-static OSStatus DeleteOurBundlesFromDirectory(CFStringRef bundleID, char *extension, char *dirPath) {
+static OSStatus DeleteOurBundlesFromDirectory(CFStringRef bundleID, char *extension, char *dirPath, Boolean deleteSymLink) {
     DIR                     *dirp;
     dirent                  *dp;
     CFStringRef             urlStringRef = NULL;
@@ -612,7 +618,13 @@ static OSStatus DeleteOurBundlesFromDirectory(CFStringRef bundleID, char *extens
                                 showDebugMsg("Bundles: %s", myRmCommand);
 #endif
 
-                               callPosixSpawn(myRmCommand);
+                                callPosixSpawn(myRmCommand);
+                                if (deleteSymLink) {
+                                    strlcpy(myRmCommand, "rm -rf \"/Applications/", 10);
+                                    strlcat(myRmCommand, dp->d_name, sizeof(myRmCommand));
+                                    strlcat(myRmCommand, "\"", sizeof(myRmCommand));
+                                    callPosixSpawn(myRmCommand);
+                                }
                             } else {
 #if TESTING
 //                                showDebugMsg("Bundles: Not deleting %s", myRmCommand+pathOffset);