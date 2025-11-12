BOINC/boinc

commit a40c3e596962230b198c2aa8603f9bbcc92e28a9

[manager] fix simple gui view on linux
Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

clientgui/sg_CustomControls.cpp
@@ -74,9 +74,10 @@ CTransparentStaticText::CTransparentStaticText(wxWindow* parent, wxWindowID id, 
 bool CTransparentStaticText::Create(wxWindow* parent, wxWindowID id, const wxString& label, const wxPoint& pos, const wxSize& size, long style, const wxString& name ) {
     bool bRetVal = wxStaticText::Create(parent, id, label, pos, size, style|wxTRANSPARENT_WINDOW, name);
 
+#ifndef __WXGTK__
     SetBackgroundColour(parent->GetBackgroundColour());
     SetForegroundColour(parent->GetForegroundColour());
-
+#endif
     return bRetVal;
 }
 
@@ -102,11 +103,13 @@ bool CTransparentButton::Create(wxWindow* parent, wxWindowID id, const wxString&
 #endif
     bool bRetVal = wxButton::Create(parent, id, label, pos, size, style|wxTRANSPARENT_WINDOW, validator, name);
 
-#ifndef __WXMAC__
+#ifdef __WXMAC__
     SetBackgroundStyle(wxBG_STYLE_CUSTOM);
 #endif
+#ifndef __WXGTK__
     SetBackgroundColour(parent->GetBackgroundColour());
     SetForegroundColour(parent->GetForegroundColour());
+#endif
 
     return bRetVal;
 }
@@ -160,7 +163,7 @@ bool CTransparentHyperlinkCtrl::Create(wxWindow *parent,
 
     m_pParentsBgBmp = parentsBgBmp;
 
-#ifndef __WXMAC__
+#if !defined(__WXMAC__) && !defined(__WXGTK__)
     SetBackgroundColour(parent->GetBackgroundColour());
     SetForegroundColour(wxColour(0, 100, 225));
     wxFont myFont = GetFont();
@@ -357,7 +360,7 @@ bool CTransparentCheckBox::Create(wxWindow *parent, wxWindowID id, const wxStrin
 
     m_pParentsBgBmp = parentsBgBmp;
 
-#ifndef __WXMAC__
+#if !defined(__WXMAC__) && !defined(__WXGTK__)
     SetBackgroundColour(parent->GetBackgroundColour());
     SetForegroundColour(parent->GetForegroundColour());
 #endif

commit dea07f3cc77aeaf17423593fa6438457bd11afc1

Mac: Fix wxWidgets assert caused by commit a40c3e5969 in PR #6660

clientgui/sg_CustomControls.cpp
@@ -103,9 +103,6 @@ bool CTransparentButton::Create(wxWindow* parent, wxWindowID id, const wxString&
 #endif
     bool bRetVal = wxButton::Create(parent, id, label, pos, size, style|wxTRANSPARENT_WINDOW, validator, name);
 
-#ifdef __WXMAC__
-    SetBackgroundStyle(wxBG_STYLE_CUSTOM);
-#endif
 #ifndef __WXGTK__
     SetBackgroundColour(parent->GetBackgroundColour());
     SetForegroundColour(parent->GetForegroundColour());