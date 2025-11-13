ea26d089b826ef779aab1a64d42e46c0f02026b6
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-11-13 04:15:01 +0100

    Client/Manager Release 8.2.7
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- version.h
+++ version.h
@@ -18,7 +18,7 @@
 #define BOINC_MINOR_VERSION 2
 
 // Release part of client version number
-#define BOINC_RELEASE 6
+#define BOINC_RELEASE 7
 
 // wrapper version number
 #define WRAPPER_RELEASE 26019
@@ -36,7 +36,7 @@
 #define MULTITHREAD_RELEASE 2
 
 // client version number as string
-#define BOINC_VERSION_STRING "8.2.6"
+#define BOINC_VERSION_STRING "8.2.7"
 
 // Package is a pre-release (Alpha/Beta) package
 //#define BOINC_PRERELEASE 1
@@ -52,13 +52,13 @@
 #define PACKAGE_NAME "BOINC"
 
 // full name and version of this package.
-#define PACKAGE_STRING "BOINC 8.2.6"
+#define PACKAGE_STRING "BOINC 8.2.7"
 
 // short name of this package.
 #define PACKAGE_TARNAME "boinc"
 
 // version of this package.
-#define PACKAGE_VERSION "8.2.6"
+#define PACKAGE_VERSION "8.2.7"
 
 #endif /* #if (defined(_WIN32) || defined(__APPLE__)) */
 
