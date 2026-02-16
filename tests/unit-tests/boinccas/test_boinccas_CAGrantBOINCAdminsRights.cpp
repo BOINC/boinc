eaa83da84d1ed58ff55e963afd7719e53ae91806
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2026-02-16 05:58:55 +0100

    Possible fix
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- tests/unit-tests/boinccas/test_boinccas_CAGrantBOINCAdminsRights.cpp
+++ tests/unit-tests/boinccas/test_boinccas_CAGrantBOINCAdminsRights.cpp
@@ -111,8 +111,20 @@ namespace test_boinccas_CAGrantBOINCAdminsRights {
         ASSERT_EQ(0u, result);
         ASSERT_TRUE(createLocalGroup("boinc_admins"));
         ASSERT_TRUE(localGroupExists("boinc_admins"));
-        const auto [opResult, _] = setAccountRights("boinc_admins", expectedRemovedRights);
-        ASSERT_TRUE(opResult);
+        auto rightSet = false;
+        for (auto i = 0; i < 10; ++i) {
+            const auto [opResult, failed] = setAccountRights("boinc_admins", expectedRemovedRights);
+            if (opResult) {
+                rightSet = true;
+                break;
+            }
+            if (!failed.empty()) {
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        ASSERT_TRUE(rightSet);
         EXPECT_EQ(0u, executeAction());
         const auto rights = getAccountRights("boinc_admins");
         for (const auto& expectedRight : expectedSetRights) {
