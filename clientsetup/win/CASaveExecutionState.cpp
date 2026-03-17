1058c530ac220e2f172edcb102620b57405173e0
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2026-03-17 03:52:27 +0100

    Add test_boinccas_CASaveExecutionState
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- clientsetup/win/CASaveExecutionState.cpp
+++ clientsetup/win/CASaveExecutionState.cpp
@@ -1,112 +1,71 @@
-// Berkeley Open Infrastructure for Network Computing
-// http://boinc.berkeley.edu
-// Copyright (C) 2005 University of California
+// This file is part of BOINC.
+// https://boinc.berkeley.edu
+// Copyright (C) 2026 University of California
 //
-// This is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation;
-// either version 2.1 of the License, or (at your option) any later version.
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
 //
-// This software is distributed in the hope that it will be useful,
+// BOINC is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
 //
-// To view the GNU Lesser General Public License visit
-// http://www.gnu.org/copyleft/lesser.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "stdafx.h"
 #include "boinccas.h"
-#include "CASaveExecutionState.h"
 
-#define CUSTOMACTION_NAME               _T("CASaveExecutionState")
-#define CUSTOMACTION_PROGRESSTITLE      _T("")
-
-
-/////////////////////////////////////////////////////////////////////
-//
-// Function:
-//
-// Description:
-//
-/////////////////////////////////////////////////////////////////////
-CASaveExecutionState::CASaveExecutionState(MSIHANDLE hMSIHandle) :
-    BOINCCABase(hMSIHandle, CUSTOMACTION_NAME, CUSTOMACTION_PROGRESSTITLE)
-{}
-
-
-/////////////////////////////////////////////////////////////////////
-//
-// Function:
-//
-// Description:
-//
-/////////////////////////////////////////////////////////////////////
-CASaveExecutionState::~CASaveExecutionState()
-{
-    BOINCCABase::~BOINCCABase();
-}
-
-
-/////////////////////////////////////////////////////////////////////
-//
-// Function:
-//
-// Description:
-//
-/////////////////////////////////////////////////////////////////////
-UINT CASaveExecutionState::OnExecution()
-{
-    tstring     strLaunchProgram;
-    tstring     strReturnRebootRequested;
-    tstring     strReturnValidateInstall;
-    tstring     strRebootPrompt;
-    tstring     strBOINCMasterAccountUsername;
-    tstring     strBOINCProjectAccountUsername;
-
-    GetProperty( _T("LAUNCHPROGRAM"), strLaunchProgram );
-    GetProperty( _T("REBOOTPROMPT"), strRebootPrompt );
-    GetProperty( _T("RETURN_REBOOTREQUESTED"), strReturnRebootRequested );
-    GetProperty( _T("RETURN_VALIDATEINSTALL"), strReturnValidateInstall );
-    GetProperty( _T("BOINC_MASTER_USERNAME"), strBOINCMasterAccountUsername );
-    GetProperty( _T("BOINC_PROJECT_USERNAME"), strBOINCProjectAccountUsername );
-
-    SetRegistryValue( _T("LAUNCHPROGRAM"), strLaunchProgram );
-    SetRegistryValue( _T("REBOOTPROMPT"), strRebootPrompt );
-    SetRegistryValue( _T("RETURN_REBOOTREQUESTED"), strReturnRebootRequested );
-    SetRegistryValue( _T("RETURN_VALIDATEINSTALL"), strReturnValidateInstall );
-    SetRegistryValue( _T("RETURN_BOINC_MASTER_USERNAME"), strBOINCMasterAccountUsername );
-    SetRegistryValue( _T("RETURN_BOINC_PROJECT_USERNAME"), strBOINCProjectAccountUsername );
-
-    // Disable the ability to launch BOINC Manager if either a reboot is
-    //   requested or validation of the installation executables fails.
-    if ((strReturnRebootRequested == _T("1")) || (strReturnValidateInstall == _T("0")))
-    {
-        SetRegistryValue( _T("LAUNCHPROGRAM"), _T("") );
+class CASaveExecutionState : public BOINCCABase {
+public:
+    virtual ~CASaveExecutionState() = default;
+    explicit CASaveExecutionState(MSIHANDLE hMSIHandle) :
+        BOINCCABase(hMSIHandle, _T("CASaveExecutionState"),
+            _T("Save current execution parameters")) {
     }
 
-    return ERROR_SUCCESS;
+    UINT OnExecution() override final {
+        tstring strLaunchProgram;
+        GetProperty(_T("LAUNCHPROGRAM"), strLaunchProgram);
+        SetRegistryValue(_T("LAUNCHPROGRAM"), strLaunchProgram);
+
+        tstring strReturnRebootRequested;
+        GetProperty(_T("RETURN_REBOOTREQUESTED"),
+            strReturnRebootRequested);
+        SetRegistryValue(_T("RETURN_REBOOTREQUESTED"),
+            strReturnRebootRequested);
+
+        tstring strReturnValidateInstall;
+        GetProperty(_T("RETURN_VALIDATEINSTALL"),
+            strReturnValidateInstall);
+        SetRegistryValue(_T("RETURN_VALIDATEINSTALL"),
+            strReturnValidateInstall);
+
+        tstring strBOINCMasterAccountUsername;
+        GetProperty(_T("BOINC_MASTER_USERNAME"),
+            strBOINCMasterAccountUsername);
+        SetRegistryValue(_T("RETURN_BOINC_MASTER_USERNAME"),
+            strBOINCMasterAccountUsername);
+
+        tstring strBOINCProjectAccountUsername;
+        GetProperty(_T("BOINC_PROJECT_USERNAME"),
+            strBOINCProjectAccountUsername);
+        SetRegistryValue(_T("RETURN_BOINC_PROJECT_USERNAME"),
+            strBOINCProjectAccountUsername);
+
+        // Disable the ability to launch BOINC Manager if either a reboot is
+        //   requested or validation of the installation executables fails.
+        if ((strReturnRebootRequested == _T("1")) ||
+            (strReturnValidateInstall == _T("0"))) {
+            SetRegistryValue(_T("LAUNCHPROGRAM"), _T(""));
+        }
+
+        return ERROR_SUCCESS;
+    }
+};
+
+UINT __stdcall SaveExecutionState(MSIHANDLE hInstall) {
+    return CASaveExecutionState(hInstall).Execute();
 }
-
-
-/////////////////////////////////////////////////////////////////////
-//
-// Function:    SaveExecutionState
-//
-// Description:
-//
-/////////////////////////////////////////////////////////////////////
-UINT __stdcall SaveExecutionState(MSIHANDLE hInstall)
-{
-    UINT uiReturnValue = 0;
-
-    CASaveExecutionState* pCA = new CASaveExecutionState(hInstall);
-    uiReturnValue = pCA->Execute();
-    delete pCA;
-
-    return uiReturnValue;
-}
-
---
+++ tests/unit-tests/boinccas/test_boinccas_CASaveExecutionState.cpp
@@ -0,0 +1,174 @@
+// This file is part of BOINC.
+// https://boinc.berkeley.edu
+// Copyright (C) 2026 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "boinccas_helper.h"
+#include "registry_helper.h"
+
+namespace test_boinccas_CASaveExecutionState {
+    class test_boinccas_CASaveExecutionState : public test_boinccas_TestBase {
+    protected:
+        test_boinccas_CASaveExecutionState() :
+            test_boinccas_TestBase("SaveExecutionState") {
+        }
+
+        void TearDown() override {
+            cleanRegistryKey();
+        }
+    };
+
+#ifdef BOINCCAS_TEST
+    TEST_F(test_boinccas_CASaveExecutionState,
+        NoPropertiesData_Expect_Empty_Values) {
+        const auto result = openMsi();
+        ASSERT_EQ(0u, result);
+
+        EXPECT_EQ(0u, executeAction());
+
+        EXPECT_TRUE(getRegistryValue("LAUNCHPROGRAM").empty());
+        EXPECT_TRUE(getRegistryValue("RETURN_REBOOTREQUESTED").empty());
+        EXPECT_TRUE(getRegistryValue("RETURN_VALIDATEINSTALL").empty());
+        EXPECT_TRUE(getRegistryValue("RETURN_BOINC_MASTER_USERNAME").empty());
+        EXPECT_TRUE(getRegistryValue("RETURN_BOINC_PROJECT_USERNAME").empty());
+    }
+
+    TEST_F(test_boinccas_CASaveExecutionState,
+        PropertiesData_Set_Expect_Valid_Values) {
+
+        const auto result = openMsi();
+        ASSERT_EQ(0u, result);
+
+        setMsiProperty("LAUNCHPROGRAM", "1");
+        auto [errorcode, value] = getMsiProperty("LAUNCHPROGRAM");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("RETURN_REBOOTREQUESTED", "0");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_REBOOTREQUESTED");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("0", value);
+
+        setMsiProperty("RETURN_VALIDATEINSTALL", "1");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_VALIDATEINSTALL");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("BOINC_MASTER_USERNAME", "master_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_MASTER_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("master_test", value);
+
+        setMsiProperty("BOINC_PROJECT_USERNAME", "project_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_PROJECT_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("project_test", value);
+
+        EXPECT_EQ(0u, executeAction());
+
+        EXPECT_EQ("1", getRegistryValue("LAUNCHPROGRAM"));
+        EXPECT_EQ("0", getRegistryValue("RETURN_REBOOTREQUESTED"));
+        EXPECT_EQ("1", getRegistryValue("RETURN_VALIDATEINSTALL"));
+        EXPECT_EQ("master_test",
+            getRegistryValue("RETURN_BOINC_MASTER_USERNAME"));
+        EXPECT_EQ("project_test",
+            getRegistryValue("RETURN_BOINC_PROJECT_USERNAME"));
+    }
+
+    TEST_F(test_boinccas_CASaveExecutionState,
+        REBOOTREQUESTED_Expect_LAUNCHPROGRAM_Empty) {
+
+        const auto result = openMsi();
+        ASSERT_EQ(0u, result);
+
+        setMsiProperty("LAUNCHPROGRAM", "1");
+        auto [errorcode, value] = getMsiProperty("LAUNCHPROGRAM");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("RETURN_REBOOTREQUESTED", "1");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_REBOOTREQUESTED");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("RETURN_VALIDATEINSTALL", "1");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_VALIDATEINSTALL");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("BOINC_MASTER_USERNAME", "master_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_MASTER_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("master_test", value);
+
+        setMsiProperty("BOINC_PROJECT_USERNAME", "project_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_PROJECT_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("project_test", value);
+
+        EXPECT_EQ(0u, executeAction());
+
+        EXPECT_TRUE(getRegistryValue("LAUNCHPROGRAM").empty());
+        EXPECT_EQ("1", getRegistryValue("RETURN_REBOOTREQUESTED"));
+        EXPECT_EQ("1", getRegistryValue("RETURN_VALIDATEINSTALL"));
+        EXPECT_EQ("master_test",
+            getRegistryValue("RETURN_BOINC_MASTER_USERNAME"));
+        EXPECT_EQ("project_test",
+            getRegistryValue("RETURN_BOINC_PROJECT_USERNAME"));
+    }
+
+    TEST_F(test_boinccas_CASaveExecutionState,
+        VALIDATEINSTALL_Equal_0_Expect_LAUNCHPROGRAM_Empty) {
+
+        const auto result = openMsi();
+        ASSERT_EQ(0u, result);
+
+        setMsiProperty("LAUNCHPROGRAM", "1");
+        auto [errorcode, value] = getMsiProperty("LAUNCHPROGRAM");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("1", value);
+
+        setMsiProperty("RETURN_REBOOTREQUESTED", "0");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_REBOOTREQUESTED");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("0", value);
+
+        setMsiProperty("RETURN_VALIDATEINSTALL", "0");
+        std::tie(errorcode, value) = getMsiProperty("RETURN_VALIDATEINSTALL");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("0", value);
+
+        setMsiProperty("BOINC_MASTER_USERNAME", "master_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_MASTER_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("master_test", value);
+
+        setMsiProperty("BOINC_PROJECT_USERNAME", "project_test");
+        std::tie(errorcode, value) = getMsiProperty("BOINC_PROJECT_USERNAME");
+        EXPECT_EQ(static_cast<unsigned int>(ERROR_SUCCESS), errorcode);
+        ASSERT_EQ("project_test", value);
+
+        EXPECT_EQ(0u, executeAction());
+
+        EXPECT_TRUE(getRegistryValue("LAUNCHPROGRAM").empty());
+        EXPECT_EQ("0", getRegistryValue("RETURN_REBOOTREQUESTED"));
+        EXPECT_EQ("0", getRegistryValue("RETURN_VALIDATEINSTALL"));
+        EXPECT_EQ("master_test",
+            getRegistryValue("RETURN_BOINC_MASTER_USERNAME"));
+        EXPECT_EQ("project_test",
+            getRegistryValue("RETURN_BOINC_PROJECT_USERNAME"));
+    }
+#endif
+}
