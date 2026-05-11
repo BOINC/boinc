7ca4bc31a818ef4e61137e006c7298d61c0ec56b
Oliver Behnke <oliver.behnke@aei.mpg.de>
2026-05-11 12:18:48 +0200

    Remove Drupal (legacy)

--- drupal/sites/default/boinc/modules/contrib/context/theme/context-block-editable-block.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php if ($empty): ?>
-<div id='<?php print $block->bid ?>' class='block context-block-hidden'><?php print $block->content ?></div>
-<?php else: ?>
-<?php print theme('block', $block) ?>
-<?php endif; ?>
