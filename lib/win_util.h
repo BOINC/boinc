3cd892a370a15dc25f5960c55322f636f8b7d730
David Anderson <davea@ssl.berkeley.edu>
2026-06-27 23:03:34 -0700

    white space

--- lib/win_util.h
+++ lib/win_util.h
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-// Windows-specific utility functions 
+// Windows-specific utility functions
 
 #ifndef BOINC_WIN_UTIL_H
 #define BOINC_WIN_UTIL_H
