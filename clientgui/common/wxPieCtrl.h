89462577f6d5bbaae1c15559c302012e5e90944d
Charlie Fenton <charlief@example.com>
2026-07-12 23:40:16 -0700

    Dark Mode: Update copyright dates

--- clientgui/common/wxPieCtrl.h
+++ clientgui/common/wxPieCtrl.h
@@ -8,6 +8,24 @@
 // Copyright:   (c) Volodymir (T-Rex) Tryapichko
 // Licence:     wxWidgets license
 /////////////////////////////////////////////////////////////////////////////
+
+// This file is part of BOINC and has been modified for BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2026 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef _WX_PIE_CTRL
 #define _WX_PIE_CTRL
 
