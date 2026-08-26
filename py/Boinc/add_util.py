8f6dddc9f1776fc5604b3e6e60e899f429bedbb6
Gianfranco Costamagna <locutusofborg@debian.org>
2026-08-26 11:09:36 +0200

    Update some code to use Python3

--- py/Boinc/add_util.py
+++ py/Boinc/add_util.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # add_util.py - used in make_project
 
--- py/Boinc/configxml.py
+++ py/Boinc/configxml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # configxml.py - module to read and parse config.xml, run_state.xml
 
